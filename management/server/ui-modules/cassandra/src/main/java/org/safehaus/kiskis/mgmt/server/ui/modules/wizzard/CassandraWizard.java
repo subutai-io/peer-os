b25e2c0059a980f382aec883aea05ee2e6b6b20f
daralbaev <daralbaev@gmail.com>
2013-11-26 16:26:55 +0600

    -m KSKSMGMT-76 Progress Bar indicator and inner Terminal added
    Former-commit-id: a614a8eb685bba578d22cebb996434f90fe31327

--- management/server/ui-modules/cassandra/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/wizzard/CassandraWizard.java
+++ management/server/ui-modules/cassandra/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/wizzard/CassandraWizard.java
@@ -47,14 +47,14 @@ public class CassandraWizard extends Window {
         progressBar.setIndeterminate(false);
         progressBar.setEnabled(true);
         progressBar.setValue(0f);
-        progressBar.setWidth(100, Sizeable.UNITS_PERCENTAGE);
+        progressBar.setWidth(90, Sizeable.UNITS_PERCENTAGE);
         progressBar.setHeight(20, Sizeable.UNITS_PIXELS);
         gridLayout.addComponent(progressBar, 0, 0);
         gridLayout.setComponentAlignment(progressBar, Alignment.TOP_CENTER);
 
         verticalLayout = new VerticalLayout();
         verticalLayout.setSpacing(true);
-        verticalLayout.setWidth(100, Sizeable.UNITS_PERCENTAGE);
+        verticalLayout.setWidth(90, Sizeable.UNITS_PERCENTAGE);
         verticalLayout.setHeight(100, Sizeable.UNITS_PERCENTAGE);
         gridLayout.addComponent(verticalLayout, 0, 1, 0, 8);
         gridLayout.setComponentAlignment(progressBar, Alignment.MIDDLE_CENTER);
@@ -141,7 +141,7 @@ public class CassandraWizard extends Window {
     }
 
     public void setOutput(Response response) {
-        if (response.getTaskUuid().equals(task.getUid())) {
+        if (response.getTaskUuid().equals(task.getUid().toString())) {
             StringBuilder output = new StringBuilder();
             output.append(textAreaTerminal.getValue());
 
