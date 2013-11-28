f29e9dbf6bcb64d460c180362171d7e2735141f6
bahadyr <bahadyr@acer>
2013-11-28 15:55:56 +0600

    Cassandra bundle code refactoring.
    Former-commit-id: 3848893ab80a1ffac60b8a4222806e8df4e44b41

---
+++ management/server/ui-modules/cassandra/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/cassandra/wizzard/Step43.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.kiskis.mgmt.server.ui.modules.cassandra.wizzard;
+
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.ui.*;
+
+/**
+ *
+ * @author bahadyr
+ */
+public class Step43 extends FormLayout {
+
+    public Step43(final CassandraWizard cassandraWizard) {
+        setCaption("Configuration");
+        setSizeFull();
+
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSizeFull();
+        verticalLayout.setHeight(600, Sizeable.UNITS_PIXELS);
+        verticalLayout.setMargin(true);
+
+        GridLayout grid = new GridLayout(6, 10);
+        grid.setSpacing(true);
+        grid.setSizeFull();
+
+        Panel panel = new Panel();
+        Label menu = new Label("Cluster Install Wizard<br>"
+                + " 1) Welcome<br>"
+                + " 2) List nodes<br>"
+                + " 3) Installation<br>"
+                + " 4) <font color=\"#f14c1a\"><strong>Configuration</strong></font>");
+        menu.setContentMode(Label.CONTENT_XHTML);
+        panel.addComponent(menu);
+
+        grid.addComponent(menu, 0, 0, 1, 5);
+        grid.setComponentAlignment(panel, Alignment.TOP_CENTER);
+
+        Label label = new Label("<strong>Choose directories</strong>");
+        label.setContentMode(Label.CONTENT_XHTML);
+
+        grid.addComponent(label, 2, 0, 5, 0);
+        grid.setComponentAlignment(label, Alignment.TOP_CENTER);
+
+        final TextField textFieldDataDir = new TextField("Data Directory:");
+        grid.addComponent(textFieldDataDir, 2, 1, 5, 1);
+        grid.setComponentAlignment(textFieldDataDir, Alignment.TOP_LEFT);
+
+        final TextField textFieldCommitLogDir = new TextField("Commit Log Directory:");
+        grid.addComponent(textFieldCommitLogDir, 2, 2, 5, 2);
+        grid.setComponentAlignment(textFieldCommitLogDir, Alignment.TOP_LEFT);
+
+        final TextField textFieldSavedCachesDir = new TextField("Saved Caches Directory:");
+        grid.addComponent(textFieldSavedCachesDir, 2, 3, 5, 3);
+        grid.setComponentAlignment(textFieldSavedCachesDir, Alignment.TOP_LEFT);
+
+        Button next = new Button("Start");
+        next.addListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                cassandraWizard.getCluster().setDataDir(textFieldDataDir.getValue().toString());
+                cassandraWizard.getCluster().setCommitLogDir(textFieldCommitLogDir.getValue().toString());
+                cassandraWizard.getCluster().setSavedCacheDir(textFieldSavedCachesDir.getValue().toString());
+                cassandraWizard.showNext();
+            }
+        });
+        Button back = new Button("Back");
+        back.addListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                cassandraWizard.showBack();
+            }
+        });
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.addComponent(back);
+        horizontalLayout.addComponent(next);
+
+        verticalLayout.addComponent(grid);
+        verticalLayout.addComponent(horizontalLayout);
+
+        addComponent(verticalLayout);
+    }
+
+}
---
+++ management/server/ui-modules/hadoop/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/hadoop/wizard/Step2.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.kiskis.mgmt.server.ui.modules.hadoop.wizard;
+
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.ui.*;
+import org.safehaus.kiskis.mgmt.shared.protocol.Command;
+import org.safehaus.kiskis.mgmt.shared.protocol.OutputRedirection;
+import org.safehaus.kiskis.mgmt.shared.protocol.Request;
+import org.safehaus.kiskis.mgmt.shared.protocol.enums.RequestType;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author bahadyr
+ */
+public class Step2 extends Panel {
+
+    private static final List<String> hosts = Arrays.asList(new String[]{
+            "hadoop-node1", "hadoop-node2", "hadoop-node3", "hadoop-node4", "hadoop-node5"});
+    //List<String> hosts;
+    HadoopWizard parent;
+    String installationCommand = "apt-get --force-yes --assume-yes install ksks-zookeeper";
+    String purgeCommand = "apt-get --force-yes --assume-yes purge ksks-zookeeper";
+
+    public Step2(final HadoopWizard hadoopWizard) {
+        parent = hadoopWizard;
+
+        setCaption("Welcome to Hadoop Cluster Installation");
+        setSizeFull();
+
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSizeFull();
+        verticalLayout.setHeight(100, Sizeable.UNITS_PERCENTAGE);
+        verticalLayout.setMargin(true);
+
+        GridLayout grid = new GridLayout(6, 10);
+        grid.setSpacing(true);
+        grid.setSizeFull();
+
+        Panel panel = new Panel();
+        Label menu = new Label("Cluster Install Wizard<br>"
+                + " 1) Master Configurations<br>"
+                + " 2) <font color=\"#f14c1a\"><strong>Slave Configurations</strong></font><br>");
+        menu.setContentMode(Label.CONTENT_XHTML);
+        panel.addComponent(menu);
+        grid.addComponent(menu, 0, 0, 0, 5);
+        grid.setComponentAlignment(panel, Alignment.TOP_CENTER);
+
+        VerticalLayout verticalLayoutForm = new VerticalLayout();
+        verticalLayoutForm.setSizeFull();
+        verticalLayoutForm.setSpacing(true);
+
+        Label label = new Label("<strong>Enter a list of hosts that will run as Data nodes.<br>" +
+                "(Provide at least 1)</strong>");
+        label.setContentMode(Label.CONTENT_XHTML);
+        verticalLayoutForm.addComponent(label);
+
+        TwinColSelect twinColSelectDataNodes = new TwinColSelect();
+        for (String host : hosts) {
+            twinColSelectDataNodes.addItem(host);
+        }
+        twinColSelectDataNodes.setRows(10);
+        twinColSelectDataNodes.setNullSelectionAllowed(true);
+        twinColSelectDataNodes.setMultiSelect(true);
+        twinColSelectDataNodes.setImmediate(true);
+        twinColSelectDataNodes.setLeftColumnCaption("Available Nodes");
+        twinColSelectDataNodes.setRightColumnCaption("Data Nodes");
+        twinColSelectDataNodes.setWidth("350px");
+        verticalLayoutForm.addComponent(twinColSelectDataNodes);
+
+        Label labelTaskTrackerCaption = new Label("<strong>Enter a list of hosts that will run as Task tracker.<br>" +
+                "(Provide at least 1)</strong>");
+        labelTaskTrackerCaption.setContentMode(Label.CONTENT_XHTML);
+        verticalLayoutForm.addComponent(labelTaskTrackerCaption);
+
+        TwinColSelect twinColSelectTaskTrackers = new TwinColSelect();
+        for (String host : hosts) {
+            twinColSelectTaskTrackers.addItem(host);
+        }
+        twinColSelectTaskTrackers.setRows(10);
+        twinColSelectTaskTrackers.setNullSelectionAllowed(true);
+        twinColSelectTaskTrackers.setMultiSelect(true);
+        twinColSelectTaskTrackers.setImmediate(true);
+        twinColSelectTaskTrackers.setLeftColumnCaption("Available Nodes");
+        twinColSelectTaskTrackers.setRightColumnCaption("Task Trackers");
+        twinColSelectTaskTrackers.setWidth("350px");
+        verticalLayoutForm.addComponent(twinColSelectTaskTrackers);
+
+        grid.addComponent(verticalLayoutForm, 1, 0, 5, 9);
+        grid.setComponentAlignment(verticalLayoutForm, Alignment.MIDDLE_CENTER);
+
+        Button next = new Button("Next");
+        next.addListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                parent.showNext();
+            }
+        });
+        Button back = new Button("Finish");
+        back.addListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                parent.showBack();
+            }
+        });
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.addComponent(back);
+        horizontalLayout.addComponent(next);
+
+        verticalLayout.addComponent(grid);
+        verticalLayout.addComponent(horizontalLayout);
+
+        addComponent(verticalLayout);
+    }
+
+    private Command buildCommand(String uuid, String program, long reqSeqNumber, String taskUuid) {
+
+        Request request = new Request();
+        request.setSource("HadoopModule Wizard");
+        request.setProgram(program);
+        request.setUuid(uuid);
+        request.setType(RequestType.EXECUTE_REQUEST);
+        request.setTaskUuid(taskUuid);
+        request.setWorkingDirectory("/");
+        request.setStdOut(OutputRedirection.RETURN);
+        request.setStdErr(OutputRedirection.RETURN);
+        request.setRunAs("root");
+        request.setTimeout(0l);
+        request.setRequestSequenceNumber(reqSeqNumber);
+        Command command = new Command(request);
+
+        return command;
+    }
+
+}
