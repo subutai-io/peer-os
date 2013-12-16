ddc6162ad642101932e16fde6877cf439e569933
dilshat <dilshat@sol-samsung>
2013-12-16 20:57:28 +0600

    activemq tuning
    Former-commit-id: 26e91d9ab39bd0872a6aef3f661c71a0b8f8d570

--- management/server/ui/vaadin-ui/src/main/java/org/safehaus/kiskis/mgmt/server/ui/MgmtApplication.java
+++ management/server/ui/vaadin-ui/src/main/java/org/safehaus/kiskis/mgmt/server/ui/MgmtApplication.java
@@ -1,6 +1,7 @@
 package org.safehaus.kiskis.mgmt.server.ui;
 
 import com.vaadin.Application;
+import com.vaadin.terminal.Sizeable;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.Runo;
 import org.safehaus.kiskis.mgmt.server.ui.services.Module;
@@ -15,8 +16,8 @@ import java.util.Iterator;
 public class MgmtApplication extends Application implements ModuleServiceListener {
 
     private ModuleServiceListener app;
-    private ModuleService moduleService;
-    private AgentManagerInterface agentManagerService;
+    private final ModuleService moduleService;
+    private final AgentManagerInterface agentManagerService;
     private Window window;
 
     public MgmtApplication(String title, ModuleService moduleService, AgentManagerInterface agentManagerService) {
@@ -24,7 +25,7 @@ public class MgmtApplication extends Application implements ModuleServiceListene
         this.agentManagerService = agentManagerService;
         this.title = title;
     }
-    private String title;
+    private final String title;
     private TabSheet tabs;
 
     @Override
@@ -47,9 +48,12 @@ public class MgmtApplication extends Application implements ModuleServiceListene
             layout.addComponent(horizontalSplit);
 
             layout.setExpandRatio(horizontalSplit, 1);
-            horizontalSplit.setSplitPosition(200, SplitPanel.UNITS_PIXELS);
+            horizontalSplit.setSplitPosition(200, Sizeable.UNITS_PIXELS);
 
-            horizontalSplit.setFirstComponent(new MgmtAgentManager(agentManagerService));
+            Panel panel = new Panel();
+            panel.addComponent(new MgmtAgentManager(agentManagerService));
+            panel.setSizeFull();
+            horizontalSplit.setFirstComponent(panel);
 
             tabs = new TabSheet();
             tabs.setSizeFull();
@@ -77,8 +81,8 @@ public class MgmtApplication extends Application implements ModuleServiceListene
                 }
             });
             //
-            final ProgressIndicator indicator =
-                    new ProgressIndicator(new Float(0.0));
+            final ProgressIndicator indicator
+                    = new ProgressIndicator(new Float(0.0));
             indicator.setPollingInterval(3000);
             indicator.setWidth("1px");
             indicator.setHeight("1px");
