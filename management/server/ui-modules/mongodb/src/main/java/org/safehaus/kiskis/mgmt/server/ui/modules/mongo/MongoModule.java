e0d040f6b93b8e076f576171d81361a23046d732
dilshat <dilshat.aliev@safehaus.org>
2013-12-20 16:58:24 +0600

    mongo in progress
    Former-commit-id: d038c71cf657ca50cd5050f88c9ede9ac1647ad6

--- management/server/ui-modules/mongodb/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/mongo/MongoModule.java
+++ management/server/ui-modules/mongodb/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/mongo/MongoModule.java
@@ -1,5 +1,6 @@
 package org.safehaus.kiskis.mgmt.server.ui.modules.mongo;
 
+import com.vaadin.terminal.Sizeable;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.*;
 import org.osgi.framework.BundleContext;
@@ -19,26 +20,56 @@ public class MongoModule implements Module {
 
     private BundleContext context;
     private static final String MODULE_NAME = "MongoDB";
+    private static final int MAX_STEPS = 2;
 
     public static class ModuleComponent extends CustomComponent implements
             CommandListener {
 
         private BundleContext context;
+        private final ProgressIndicator progressBar;
+        private final VerticalLayout verticalLayout;
+
+        int step = 1;
 
         public ModuleComponent(BundleContext context) {
             this.context = context;
-            VerticalLayout verticalLayout = new VerticalLayout();
+
+//            ThemeResource resource = new ThemeResource("icons/32/document.png");
+//            Embedded image = new Embedded("", resource);
+//            verticalLayout.addComponent(image);
+            VerticalLayout verticalLayoutRoot = new VerticalLayout();
+            verticalLayoutRoot.setSpacing(true);
+            verticalLayoutRoot.setWidth(90, Sizeable.UNITS_PERCENTAGE);
+            verticalLayoutRoot.setHeight(100, Sizeable.UNITS_PERCENTAGE);
+
+            GridLayout gridLayout = new GridLayout(1, 15);
+            gridLayout.setSpacing(true);
+//            gridLayout.setMargin(false, true, true, true);
+            gridLayout.setHeight(100, Sizeable.UNITS_PERCENTAGE);
+            gridLayout.setWidth(900, Sizeable.UNITS_PIXELS);
+
+            progressBar = new ProgressIndicator();
+            progressBar.setIndeterminate(false);
+            progressBar.setEnabled(false);
+            progressBar.setValue(0f);
+            progressBar.setWidth(100, Sizeable.UNITS_PERCENTAGE);
+            gridLayout.addComponent(progressBar, 0, 0);
+            gridLayout.setComponentAlignment(progressBar, Alignment.MIDDLE_CENTER);
+
+            verticalLayout = new VerticalLayout();
             verticalLayout.setSpacing(true);
-            verticalLayout.setSizeFull();
+            verticalLayout.setWidth(100, Sizeable.UNITS_PERCENTAGE);
+            verticalLayout.setHeight(100, Sizeable.UNITS_PERCENTAGE);
+            gridLayout.addComponent(verticalLayout, 0, 1, 0, 14);
+            gridLayout.setComponentAlignment(verticalLayout, Alignment.MIDDLE_CENTER);
 
-            ThemeResource resource = new ThemeResource("icons/32/document.png");
-            Embedded image = new Embedded("", resource);
-            verticalLayout.addComponent(image);
+            verticalLayoutRoot.addComponent(gridLayout);
+            verticalLayoutRoot.setMargin(true);
+            verticalLayoutRoot.setComponentAlignment(gridLayout, Alignment.MIDDLE_CENTER);
 
-            Button button = new Button("OK");
-            verticalLayout.addComponent(button);
+            putForm();
 
-            setCompositionRoot(verticalLayout);
+            setCompositionRoot(verticalLayoutRoot);
 
             try {
                 getCommandManager().addListener(this);
@@ -61,6 +92,36 @@ public class MongoModule implements Module {
                     .getServiceReference(CommandManagerInterface.class.getName());
             return (CommandManagerInterface) context.getService(reference);
         }
+
+        public void showNext() {
+            step++;
+            putForm();
+        }
+
+        public void showBack() {
+            step--;
+            putForm();
+        }
+
+        private void putForm() {
+            verticalLayout.removeAllComponents();
+            switch (step) {
+                case 1: {
+                    progressBar.setValue(0f);
+                    verticalLayout.addComponent(new Step1(this));
+                    break;
+                }
+                case 2: {
+                    progressBar.setValue((float) (step - 1) / MAX_STEPS);
+                    verticalLayout.addComponent(new Step2(this));
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+
     }
 
     @Override
