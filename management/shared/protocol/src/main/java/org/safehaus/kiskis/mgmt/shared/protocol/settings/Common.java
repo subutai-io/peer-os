30631054c022ffc2182a00aebc6d9cc7f5440d4d
dilshat <dilshat.aliev@safehaus.org>
2013-12-27 16:27:54 +0600

    Changed PARENT_CHILD_LXC_SEPARATOR to contain hyphens instead of underscores because of the bug in dns
    Former-commit-id: 234fad1edd3c864f78e874a2e565736b6c7637da

--- management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/settings/Common.java
+++ management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/settings/Common.java
@@ -3,7 +3,7 @@ package org.safehaus.kiskis.mgmt.shared.protocol.settings;
 public class Common {
 
     public static final String UNKNOWN_LXC_PARENT_NAME = "UNKNOWN";
-    public static final String PARENT_CHILD_LXC_SEPARATOR = "_lxc_";
+    public static final String PARENT_CHILD_LXC_SEPARATOR = "-lxc-";
     public static final int MAX_PENDING_MESSAGE_QUEUE_LENGTH = 10000;
     public static final int WEB_SERVER_PORT = 8888;
     public static final String WEB_SERVER_RES_FOLDER = "res";
