c0fc212b6472382228339da912dcab296646e0ee
dilshat <dilshat.aliev@safehaus.org>
2014-01-06 19:44:46 +0600

    Mongo in progress
    Former-commit-id: 89ec64735d950967bd65f78461d6e1ff9f7e5f36

--- management/server/core/manager-command/src/main/java/org/safehaus/kiskis/mgmt/server/command/CommandManager.java
+++ management/server/core/manager-command/src/main/java/org/safehaus/kiskis/mgmt/server/command/CommandManager.java
@@ -106,15 +106,12 @@ public class CommandManager implements CommandManagerInterface, ResponseListener
         try {
             for (Iterator<Entry<CommandListener, ReentrantLock>> it = listeners.entrySet().iterator(); it.hasNext();) {
                 Entry<CommandListener, ReentrantLock> listenerEntry = it.next();
-                System.out.println("Iterating listener " + listenerEntry.getKey().getName());
                 if (listenerEntry.getKey() != null && listenerEntry.getValue() != null
                         && listenerEntry.getKey().getName() != null) {
                     CommandListener listener = listenerEntry.getKey();
                     ReentrantLock lock = listenerEntry.getValue();
-                    System.out.println("Trying to notify listener " + listener.getName());
                     if (response != null && response.getSource() != null
                             && listener.getName().equalsIgnoreCase(response.getSource())) {
-                        System.out.println("Notifying listener " + listener.getName() + " with " + response);
                         notifierExecService.execute(new ResponseNotifier(lock, listener, response));
                     }
                 } else {
