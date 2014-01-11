4d68b8d48875a3d02bfab2e9deb1682bb4d6c242
bahadyr <bahadyr@acer>
2014-01-11 18:15:51 +0600

    Cassandra code refactoring.
    Former-commit-id: 91d25566e1ad377c234a10a61df21a3063c6251b

--- management/server/ui-modules/cassandra/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/cassandra/management/NodesWindow.java
+++ management/server/ui-modules/cassandra/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/cassandra/management/NodesWindow.java
@@ -131,7 +131,7 @@ public class NodesWindow extends Window {
 
             @Override
             public void buttonClick(Button.ClickEvent event) {
-                getWindow().showNotification("Checking the status of cassandra instance: " + agent.getHostname());
+                getWindow().showNotification("Checking the status: " + agent.getHostname());
                 cce = CassandraCommandEnum.STATUS;
                 selectedItem = item;
                 table.setEnabled(false);
@@ -153,7 +153,7 @@ public class NodesWindow extends Window {
                 if (event.getButton().getCaption().equals("Set as seed")) {
                     cce = CassandraCommandEnum.SET_SEED;
                     getWindow().showNotification("Adding instance to seeds list: " + agent.getHostname());
-//                    seeds.remove(agent.getUuid());
+//                    .remove(agent.getUuid());
                     cci.getSeeds().remove(agent.getUuid());
                 } else {
                     cce = CassandraCommandEnum.REMOVE_SEED;
@@ -171,31 +171,6 @@ public class NodesWindow extends Window {
 
             }
         });
-//        } else {
-//            setSeedsButton.addListener(new Button.ClickListener() {
-//
-//                @Override
-//                public void buttonClick(Button.ClickEvent event) {
-//                    getWindow().showNotification("Adding instance to a seeds list: " + agent.getHostname());
-//                    table.setEnabled(false);
-//                    selectedItem = item;
-//                    cce = CassandraCommandEnum.SET_SEED;
-//                    List<UUID> seeds = new ArrayList<UUID>(cci.getSeeds());
-//                    seeds.add(agent.getUuid());
-//                    cci.setSeeds(seeds);
-//                    StringBuilder seedsSB = new StringBuilder();
-//                    for (UUID seed : cci.getSeeds()) {
-//                        Agent agent = getAgentManager().getAgent(seed);
-//                        seedsSB.append(agent.getHostname()).append(".").append(cci.getDomainName()).append(",");
-//                    }
-//
-//                    serviceManager.updateSeeds(cci.getNodes(), seedsSB.substring(0, seedsSB.length() - 1));
-//                    if (ServiceLocator.getService(CommandManagerInterface.class).saveCassandraClusterData(cci)) {
-//                        System.out.println("updated");
-//                    }
-//                }
-//            });
-//        }
 
         item.getItemProperty("Start").setValue(startButton);
         item.getItemProperty("Stop").setValue(stopButton);
