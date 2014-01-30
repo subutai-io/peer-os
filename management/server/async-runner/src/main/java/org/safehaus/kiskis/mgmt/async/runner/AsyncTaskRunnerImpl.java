d0afa04cf79273779c52a098d890cb2455a010da
dilshat <dilshat.aliev@safehaus.org>
2014-01-30 18:05:19 +0600

    TaskRunner in progress
    Former-commit-id: ded98fa3f1422aa461cc2a477eb16c2d4f721a9c

--- management/server/async-runner/src/main/java/org/safehaus/kiskis/mgmt/async/runner/AsyncTaskRunnerImpl.java
+++ management/server/async-runner/src/main/java/org/safehaus/kiskis/mgmt/async/runner/AsyncTaskRunnerImpl.java
@@ -55,6 +55,7 @@ public class AsyncTaskRunnerImpl implements CommandListener, AsyncTaskRunner {
     public void destroy() {
         try {
             executors.clear();
+            taskRunner.removeAllTaskCallbacks();
             if (commandManager != null) {
                 commandManager.removeListener(this);
             }
