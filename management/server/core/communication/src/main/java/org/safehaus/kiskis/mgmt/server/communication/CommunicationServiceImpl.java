12abf7da2886ad1ef4bf66d27ab09b1c4f8c8ee6
dilshat <dilshat.aliev@safehaus.org>
2014-02-04 17:23:36 +0600

    TaskRunner in progress
    Former-commit-id: b3ea06f76b3d1c41d4d6b6ff6d26c3aea07f655e

--- management/server/core/communication/src/main/java/org/safehaus/kiskis/mgmt/server/communication/CommunicationServiceImpl.java
+++ management/server/core/communication/src/main/java/org/safehaus/kiskis/mgmt/server/communication/CommunicationServiceImpl.java
@@ -211,7 +211,7 @@ public class CommunicationServiceImpl implements CommunicationService {
             //executor service setup
             exec = Executors.newFixedThreadPool(amqMaxSenderPoolSize);
             //pooled connection factory setup
-            ActiveMQConnectionFactory amqFactory = new ActiveMQConnectionFactory("vm://localhost:7001?create=false");
+            ActiveMQConnectionFactory amqFactory = new ActiveMQConnectionFactory("vm://sol?create=false");
             amqFactory.setCheckForDuplicates(true);
             pooledConnectionFactory = new PooledConnectionFactory(amqFactory);
             pooledConnectionFactory.setMaxConnections(amqMaxPooledConnections);
