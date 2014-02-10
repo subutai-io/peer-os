ef5bc45b8d82f96654f180c17fc424418e64912c
Askat Asanaliev <aasanaliev@exadel.com>
2014-02-10 21:27:46 +0600

    Monitoring. Injected jquery.
    Former-commit-id: 49af9a8b49e294741fb5707327a0e2527a108486

--- management/server/ui-modules/monitor/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/monitor/view/ModuleComponent.java
+++ management/server/ui-modules/monitor/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/monitor/view/ModuleComponent.java
@@ -1,20 +1,20 @@
 package org.safehaus.kiskis.mgmt.server.ui.modules.monitor.view;
 
 import com.vaadin.terminal.Sizeable;
-import com.vaadin.ui.AbsoluteLayout;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.Layout;
+import com.vaadin.ui.*;
 import org.safehaus.kiskis.mgmt.server.ui.modules.monitor.util.FileUtil;
 import org.safehaus.kiskis.mgmt.shared.protocol.Response;
 import org.safehaus.kiskis.mgmt.shared.protocol.api.ui.CommandListener;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 public class ModuleComponent extends CustomComponent implements CommandListener {
 
     private final Logger log = Logger.getLogger(ModuleComponent.class.getName());
     private final String moduleName;
+
+    private static boolean loaded;
 
     public ModuleComponent(String moduleName) {
 
@@ -27,8 +27,8 @@ public class ModuleComponent extends CustomComponent implements CommandListener 
     public Layout getLayout() {
 
         AbsoluteLayout layout = new AbsoluteLayout();
-        layout.setWidth(400, Sizeable.UNITS_PIXELS);
-        layout.setHeight(400, Sizeable.UNITS_PIXELS);
+        layout.setWidth(1000, Sizeable.UNITS_PIXELS);
+        layout.setHeight(1000, Sizeable.UNITS_PIXELS);
 
         Button button = new Button("Test");
         button.setWidth(120, Sizeable.UNITS_PIXELS);
@@ -36,25 +36,34 @@ public class ModuleComponent extends CustomComponent implements CommandListener 
         button.addListener(new Button.ClickListener() {
             @Override
             public void buttonClick(Button.ClickEvent event) {
-
-                String s = FileUtil.getContent("js/snippet.js");
-                String code = "function hello(a) { console.log(a); }; hello('hello');";
-                s = s.replace("${code}", code);
-
-                getWindow().executeJavaScript(s);
+                loadScripts();
+                //getWindow().executeJavaScript("console.log( $('#subdiv') );");
+                getWindow().executeJavaScript("$('#subdiv').html('" + new Date() + "');");
             }
         });
 
         layout.addComponent(button, "left: 30px; top: 50px;");
 
         AbsoluteLayout layout2 = new AbsoluteLayout();
-        layout2.setWidth(100, Sizeable.UNITS_PIXELS);
-        layout2.setHeight(200, Sizeable.UNITS_PIXELS);
+        layout2.setWidth(300, Sizeable.UNITS_PIXELS);
+        layout2.setHeight(300, Sizeable.UNITS_PIXELS);
         layout2.setDebugId("subdiv");
 
-        layout.addComponent(layout2, "left: 100px; top: 10px;");
+        layout.addComponent(layout2, "left: 200px; top: 10px;");
 
         return layout;
+    }
+
+    private void loadScripts() {
+
+        if (loaded) {
+            return;
+        }
+
+        final String jquery = FileUtil.getContent("js/jquery.min.js");
+        getWindow().executeJavaScript(jquery);
+
+        loaded = true;
     }
 
     @Override
