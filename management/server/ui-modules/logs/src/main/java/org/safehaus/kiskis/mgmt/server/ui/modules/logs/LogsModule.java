1a871a95091236e67752238d0b8f63f06fad322b
bahadyr <bahadyr@acer>
2014-02-25 22:39:09 +0600

    Added Logs UI module to view system logs.
    Former-commit-id: 872cc3349492ad9f49a20922ddec5719c604fe24

---
+++ management/server/ui-modules/logs/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/logs/LogsModule.java
@@ -0,0 +1,84 @@
+package org.safehaus.kiskis.mgmt.server.ui.modules.logs;
+
+import com.vaadin.ui.*;
+import org.safehaus.kiskis.mgmt.server.ui.MgmtApplication;
+import org.safehaus.kiskis.mgmt.server.ui.services.Module;
+import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
+import org.safehaus.kiskis.mgmt.shared.protocol.Response;
+import org.safehaus.kiskis.mgmt.shared.protocol.api.ui.CommandListener;
+import java.util.logging.Logger;
+import org.safehaus.kiskis.mgmt.api.SomeApi;
+
+public class LogsModule implements Module {
+
+    public static final String MODULE_NAME = "Logs";
+    private static final Logger LOG = Logger.getLogger(LogsModule.class.getName());
+    private SomeApi someApi;
+
+    public SomeApi getSomeApi() {
+        return someApi;
+    }
+
+    public void setSomeApi(SomeApi someApi) {
+        this.someApi = someApi;
+    }
+
+    public static class ModuleComponent extends CustomComponent implements
+            CommandListener {
+
+        public ModuleComponent(final SomeApi someApi) {
+
+            setSizeFull();
+            VerticalLayout verticalLayout = new VerticalLayout();
+            verticalLayout.setSpacing(true);
+            verticalLayout.setSizeFull();
+
+//            TabSheet sheet = new TabSheet();
+//            sheet.setStyleName(Runo.TABSHEET_SMALL);
+//            sheet.setSizeFull();
+            final TextArea t = new TextArea();
+            t.setSizeFull();
+            Button b = new Button("Get System Logs");
+            b.addListener(new Button.ClickListener() {
+
+                @Override
+                public void buttonClick(Button.ClickEvent event) {
+
+                    String hello = someApi.sayHello("logs");
+                    t.setValue(hello);
+
+                }
+            });
+
+            verticalLayout.addComponent(b);
+            verticalLayout.addComponent(t);
+
+            setCompositionRoot(verticalLayout);
+        }
+
+        @Override
+        public void onCommand(Response response) {
+        }
+
+        @Override
+        public String getName() {
+            return MODULE_NAME;
+        }
+
+        public Iterable<Agent> getLxcList() {
+            return MgmtApplication.getSelectedAgents();
+        }
+
+    }
+
+    @Override
+    public String getName() {
+        return LogsModule.MODULE_NAME;
+    }
+
+    @Override
+    public Component createComponent() {
+        return new ModuleComponent(someApi);
+    }
+
+}
