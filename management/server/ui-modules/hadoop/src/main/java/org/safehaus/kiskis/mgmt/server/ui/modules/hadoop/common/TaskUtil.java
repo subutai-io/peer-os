97f065948ea3a6a277897b9c4d3c4fd6c726e2b4
dilshat <dilshat.aliev@safehaus.org>
2014-03-06 14:35:26 +0600

    Code cleanup
    Former-commit-id: f7a030f284660b6d14e49b266b58b0bc9955589d

--- management/server/ui-modules/hadoop/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/hadoop/common/TaskUtil.java
+++ management/server/ui-modules/hadoop/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/hadoop/common/TaskUtil.java
@@ -6,8 +6,6 @@
 package org.safehaus.kiskis.mgmt.server.ui.modules.hadoop.common;
 
 import java.util.HashMap;
-import org.safehaus.kiskis.mgmt.api.taskrunner.TaskCallback;
-import org.safehaus.kiskis.mgmt.server.ui.modules.hadoop.HadoopModule;
 import org.safehaus.kiskis.mgmt.shared.protocol.CommandJson;
 import org.safehaus.kiskis.mgmt.shared.protocol.Request;
 import org.safehaus.kiskis.mgmt.shared.protocol.Task;
@@ -17,29 +15,6 @@ import org.safehaus.kiskis.mgmt.shared.protocol.Task;
  * @author dilshat
  */
 public class TaskUtil {
-
-    public static Request createRequest(String command, String taskName, HashMap<String, String> map, TaskCallback callback, TaskType taskType) {
-        Task task = new Task(taskName);
-        if (taskType != null) {
-            task.setData(taskType);
-        }
-        String json = command;
-
-        json = json.replaceAll(":taskUuid", task.getUuid().toString());
-        json = json.replaceAll(":requestSequenceNumber", task.getIncrementedReqSeqNumber().toString());
-
-        for (String key : map.keySet()) {
-            json = json.replaceAll(key, map.get(key));
-        }
-
-        Request request = CommandJson.getRequest(json);
-        HadoopModule.getTaskRunner().executeTask(task, callback);
-        return request;
-    }
-
-    public static Request createRequest(String command, String taskName, HashMap<String, String> map, TaskCallback callback) {
-        return createRequest(command, taskName, map, callback, null);
-    }
 
     public static Request createRequest(String command, Task task, HashMap<String, String> map) {
         String json = command;
