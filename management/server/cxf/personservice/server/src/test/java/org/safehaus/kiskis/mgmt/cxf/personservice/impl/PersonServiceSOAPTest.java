6b3da4f1964c687474f7fa311f30091469983dfa
bahadyr <bahadyr@acer>
2014-03-11 14:42:12 +0600

    Deleted old REST test bundle.
    Former-commit-id: 429c00073a3e1825d976c524a50ba7ff44b375ac

---
+++ management/server/cxf/personservice/server/src/test/java/org/safehaus/kiskis/mgmt/cxf/personservice/impl/PersonServiceSOAPTest.java
@@ -0,0 +1,65 @@
+package org.safehaus.kiskis.mgmt.cxf.personservice.impl;
+
+import org.safehaus.kiskis.mgmt.cxf.personservice.impl.PersonServiceImpl;
+import org.safehaus.kiskis.mgmt.cxf.personservice.person.Person;
+import org.safehaus.kiskis.mgmt.cxf.personservice.person.PersonService;
+
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonServiceSOAPTest {
+    
+    private static final String PERSONSERVICE_TESTURL = "http://localhost:8282/personService";
+
+
+    @Test
+    public void testPutPerson() {
+        PersonService personService = new PersonServiceImpl();;
+        Server server = startPersonService(personService);
+        
+        JaxWsProxyFactoryBean proxy = new JaxWsProxyFactoryBean();
+        proxy.setServiceClass(PersonService.class);
+        proxy.setAddress(PERSONSERVICE_TESTURL);
+        PersonService personServiceProxy = (PersonService)proxy.create();
+        
+        personServiceProxy.addPerson(createPerson());
+
+        // Get from impl directly
+        Person person = personService.getPerson("1001");
+        assertCorrectPerson(person);
+
+        // Get through proxy
+        Person person2 = personServiceProxy.getPerson("1001");
+        assertCorrectPerson(person2);
+
+        server.stop();
+    }
+
+
+    private Person createPerson() {
+        Person person = new Person();
+        person.setId("1001");
+        person.setName("Christian Schneider");
+        return person;
+    }
+
+
+    private void assertCorrectPerson(Person person) {
+        Assert.assertNotNull(person);
+        Assert.assertEquals("Christian Schneider", person.getName());
+    }
+
+    
+    private Server startPersonService(PersonService personService) {
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setAddress(PERSONSERVICE_TESTURL);
+        factory.setServiceBean(personService);
+        Server server = factory.create();
+        server.start();
+        return server;
+    }
+
+}
