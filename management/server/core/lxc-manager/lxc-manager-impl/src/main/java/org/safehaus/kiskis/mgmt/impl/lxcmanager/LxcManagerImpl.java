8cb170122dc6989ed68127208873c32ab107fec0
dilshat <dilshat.aliev@safehaus.org>
2014-03-25 18:46:38 +0600

    prepare mongo for demo
    Former-commit-id: 140008e8f3c9382091094d05cd239a41976fb3dd

--- management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/lxcmanager/LxcManagerImpl.java
+++ management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/lxcmanager/LxcManagerImpl.java
@@ -39,7 +39,7 @@ public class LxcManagerImpl implements LxcManager {
     private final Pattern p = Pattern.compile("load average: (.*)");
     private final double MIN_HDD_LXC_MB = 15 * 1024;         // 15G
     private final double MIN_HDD_IN_RESERVE_MB = 100 * 1024; // 100G
-    private final double MIN_RAM_LXC_MB = 1.5 * 1024;        // 1.5G
+    private final double MIN_RAM_LXC_MB = 1 * 1024;          // 1G
     private final double MIN_RAM_IN_RESERVE_MB = 2 * 1024;   // 2G
     private final double MIN_CPU_LXC_PERCENT = 15;           // 15%
     private final double MIN_CPU_IN_RESERVE_PERCENT = 30;    // 30%
