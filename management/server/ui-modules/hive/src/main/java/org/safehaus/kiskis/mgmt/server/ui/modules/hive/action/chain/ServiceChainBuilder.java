97702cc7b8a6b76b18569fd231d308ab78d5f89b
Askat Asanaliev <aasanaliev@exadel.com>
2014-03-26 15:58:22 +0600

    Fixed SUBUTAI-506 - Hive - start derby service before starting hive service
    Former-commit-id: 0b8f22e9618b777599f944b7885112865e713be4

--- management/server/ui-modules/hive/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/hive/action/chain/ServiceChainBuilder.java
+++ management/server/ui-modules/hive/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/hive/action/chain/ServiceChainBuilder.java
@@ -7,45 +7,69 @@ import org.safehaus.kiskis.mgmt.server.ui.modules.hive.common.command.ActionList
 import org.safehaus.kiskis.mgmt.server.ui.modules.hive.common.command.CommandAction;
 import org.safehaus.kiskis.mgmt.server.ui.modules.hive.view.UILogger;
 import org.safehaus.kiskis.mgmt.shared.protocol.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServiceChainBuilder extends AbstractChainBuilder {
 
-    private static final String SERVICE_START_COMMAND = "service hive-thrift start";
-    private static final String SERVICE_STOP_COMMAND = "service hive-thrift stop";
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceChainBuilder.class);
 
-    private String command;
+    private static final String DERBY_START = "service derby start";
+    private static final String HIVE_START = ". /etc/profile && service hive-thrift start";
+
+    private static final String DERBY_STOP = "service derby stop";
+    private static final String HIVE_STOP = ". /etc/profile && service hive-thrift stop";
+
+    private String commands[];
     private String message;
 
-    private ServiceChainBuilder(UILogger logger, String command, String message) {
+    private ServiceChainBuilder(UILogger logger, String message, String ... commands) {
         super(logger);
-        this.command = command;
         this.message = message;
+        this.commands = commands;
     }
 
     public Chain getChain() {
-        return new Chain(agentInitAction,
-            new CommandAction(command, getActionListener())
-        );
+
+        Chain chain = new Chain(agentInitAction);
+
+        for (String command : commands) {
+            chain.add( new CommandAction( command, getActionListener() ) );
+        }
+
+        return chain;
     }
 
     private ActionListener getActionListener() {
+
         return new BasicListener(logger, message) {
             @Override
             protected boolean onComplete(Context context, String stdOut, String stdErr, Response response) {
+
+                LOG.info("response: {}", response);
+                LOG.info("stdOut: {}", stdOut);
+                LOG.info("stdErr: {}", stdErr);
+
                 logger.info(stdOut);
                 logger.info(stdErr);
-                logger.info("Completed");
-                return false;
+
+                return true;
             }
         };
     }
 
     public static Chain getStartChain(UILogger logger) {
-        return new ServiceChainBuilder(logger, SERVICE_START_COMMAND, "Starting the service, please wait...").getChain();
+        return new ServiceChainBuilder(logger, "Starting services...",
+                DERBY_START,
+                HIVE_START
+        ).getChain();
     }
 
     public static Chain getStopChain(UILogger logger) {
-        return new ServiceChainBuilder(logger, SERVICE_STOP_COMMAND, "Stopping the service, please wait...").getChain();
+        return new ServiceChainBuilder(logger, "Stopping services...",
+                HIVE_STOP,
+                DERBY_STOP
+        ).getChain();
     }
 
 }
