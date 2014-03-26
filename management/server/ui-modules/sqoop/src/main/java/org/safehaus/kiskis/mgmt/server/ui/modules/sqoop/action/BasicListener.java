4b9770c68c3521d775405720f71ebde6be3ca9a5
Askat Asanaliev <aasanaliev@exadel.com>
2014-03-26 14:31:03 +0600

    Fixed SUBUTAI-505 - Sqoop doesn't show status message after successful import
    Former-commit-id: 3c44aed9b7465a900f6ac0351baa72b7aae0382e

--- management/server/ui-modules/sqoop/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/sqoop/action/BasicListener.java
+++ management/server/ui-modules/sqoop/src/main/java/org/safehaus/kiskis/mgmt/server/ui/modules/sqoop/action/BasicListener.java
@@ -4,8 +4,12 @@ import org.safehaus.kiskis.mgmt.server.ui.modules.sqoop.common.chain.Context;
 import org.safehaus.kiskis.mgmt.server.ui.modules.sqoop.common.command.ActionListener;
 import org.safehaus.kiskis.mgmt.server.ui.modules.sqoop.view.UILogger;
 import org.safehaus.kiskis.mgmt.shared.protocol.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasicListener extends ActionListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BasicListener.class);
 
     protected UILogger logger;
     protected String executeMessage;
@@ -17,15 +21,22 @@ public class BasicListener extends ActionListener {
 
     @Override
     protected void onStart(Context context, String programLine) {
+        LOG.info(executeMessage);
         logger.info(executeMessage);
     }
 
     @Override
     protected boolean onComplete(Context context, String stdOut, String stdErr, Response response) {
 
-        logger.info(stdOut);
-        logger.info(stdErr);
-        logger.info("Completed");
+        LOG.info("response: {}", response);
+        LOG.info("stdOut: {}", stdOut);
+        LOG.info("stdErr: {}", stdErr);
+
+        String msg = response.getExitCode() == null || response.getExitCode() == 0
+                ? "Import performed successfully"
+                : "Error occurred. Please see the server logs for details.";
+
+        logger.complete(msg);
 
         return false;
     }
