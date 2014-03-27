809ad64b1ba06795507236710bc118c5f67ab4af
dilshat <dilshat.aliev@safehaus.org>
2014-03-27 22:19:54 +0600

    improvement of lxc
    Former-commit-id: bd7b6f2ec724ea9544bdcd9c3e38dc7501e94e35

--- management/server/core/communication-manager/communication-manager-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/communicationmanager/CommunicationManagerImpl.java
+++ management/server/core/communication-manager/communication-manager-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/communicationmanager/CommunicationManagerImpl.java
@@ -5,7 +5,6 @@ import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.safehaus.kiskis.mgmt.api.communicationmanager.CommandJson;
 import org.safehaus.kiskis.mgmt.api.communicationmanager.ResponseListener;
 import org.safehaus.kiskis.mgmt.api.communicationmanager.CommunicationManager;
 import javax.jms.*;
@@ -18,7 +17,6 @@ import org.apache.activemq.broker.region.policy.PolicyMap;
 import org.apache.activemq.broker.region.policy.SharedDeadLetterStrategy;
 import org.apache.activemq.pool.PooledConnectionFactory;
 import org.safehaus.kiskis.mgmt.shared.protocol.Request;
-import org.safehaus.kiskis.mgmt.shared.protocol.enums.RequestType;
 
 public class CommunicationManagerImpl implements CommunicationManager {
 
@@ -40,6 +38,14 @@ public class CommunicationManagerImpl implements CommunicationManager {
     private int amqMaxPooledConnections;
     private int amqMaxSenderPoolSize;
     private int amqInactiveQueuesDropTimeoutSec;
+
+    PooledConnectionFactory getPooledConnectionFactory() {
+        return pooledConnectionFactory;
+    }
+
+    int getAmqMaxMessageToAgentTtlSec() {
+        return amqMaxMessageToAgentTtlSec;
+    }
 
     public void setAmqPort(int amqPort) {
         this.amqPort = amqPort;
@@ -94,58 +100,7 @@ public class CommunicationManagerImpl implements CommunicationManager {
     }
 
     public void sendRequest(Request request) {
-        exec.submit(new CommandProducer(request));
-    }
-
-    public class CommandProducer implements Runnable {
-
-        Request command;
-
-        public CommandProducer(Request command) {
-            this.command = command;
-        }
-
-        public void run() {
-            Connection connection = null;
-            Session session = null;
-            MessageProducer producer = null;
-            try {
-                connection = pooledConnectionFactory.createConnection();
-                connection.start();
-                session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                Destination destination = session.createQueue(command.getUuid().toString());
-                producer = session.createProducer(destination);
-                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-                producer.setTimeToLive(amqMaxMessageToAgentTtlSec * 1000);
-                String json = CommandJson.getJson(command);
-                if (command.getType() != RequestType.HEARTBEAT_REQUEST) {
-                    LOG.log(Level.INFO, "\nSending: {0}", json);
-                }
-                TextMessage message = session.createTextMessage(json);
-                producer.send(message);
-            } catch (JMSException ex) {
-                LOG.log(Level.SEVERE, "Error in CommandProducer.run", ex);
-            } finally {
-                if (producer != null) {
-                    try {
-                        producer.close();
-                    } catch (Exception e) {
-                    }
-                }
-                if (session != null) {
-                    try {
-                        session.close();
-                    } catch (Exception e) {
-                    }
-                }
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (Exception e) {
-                    }
-                }
-            }
-        }
+        exec.submit(new CommandProducer(request, this));
     }
 
     public void init() {
@@ -241,6 +196,8 @@ public class CommunicationManagerImpl implements CommunicationManager {
                 } catch (Exception e) {
                 }
             }
+            exec.shutdown();
+
             LOG.log(Level.INFO, "ActiveMQ stopped...");
         } catch (Exception ex) {
             LOG.log(Level.SEVERE, "Error in destroy", ex);
