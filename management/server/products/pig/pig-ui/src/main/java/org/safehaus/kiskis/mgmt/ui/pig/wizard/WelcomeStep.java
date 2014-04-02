e39af55ade4f1845251f97b0d3bf0191be7664c2
dilshat <dilshat.aliev@safehaus.org>
2014-04-02 14:51:35 +0600

    splitting pig to api and impl
    Former-commit-id: 4e1bbabae7557e6b183ce447854f4e34168a01b0

--- management/server/products/pig/pig-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/pig/wizard/WelcomeStep.java
+++ management/server/products/pig/pig-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/pig/wizard/WelcomeStep.java
@@ -33,10 +33,10 @@ public class WelcomeStep extends Panel {
         grid.addComponent(welcomeMsg, 3, 1, 6, 2);
 
         Label logoImg = new Label();
-        logoImg.setIcon(new ThemeResource("icons/modules/pig.png"));
+        logoImg.setIcon(new ThemeResource("icons/modules/pig.gif"));
         logoImg.setContentMode(Label.CONTENT_XHTML);
-        logoImg.setHeight(150, Sizeable.UNITS_PIXELS);
-        logoImg.setWidth(150, Sizeable.UNITS_PIXELS);
+        logoImg.setHeight(200, Sizeable.UNITS_PIXELS);
+        logoImg.setWidth(180, Sizeable.UNITS_PIXELS);
         grid.addComponent(logoImg, 1, 3, 2, 5);
 
         Button next = new Button("Start");
