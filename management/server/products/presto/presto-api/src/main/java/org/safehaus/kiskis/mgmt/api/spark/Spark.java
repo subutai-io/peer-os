62a8133cef746c23f70d21d9a8da20aaa948768b
dilshat <dilshat.aliev@safehaus.org>
2014-04-08 16:15:25 +0600

    Implementing presto module
    Former-commit-id: 21493d397d6da4ec1967d76f757296c7ca0f6f70

---
+++ management/server/products/presto/presto-api/src/main/java/org/safehaus/kiskis/mgmt/api/spark/Spark.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.kiskis.mgmt.api.spark;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author dilshat
+ */
+public interface Spark {
+
+    public UUID installCluster(Config config);
+
+    public UUID uninstallCluster(String clusterName);
+
+    public UUID addSlaveNode(String clusterName, String lxcHostname);
+
+    public UUID destroySlaveNode(String clusterName, String lxcHostname);
+
+    public UUID changeMasterNode(String clusterName, String newMasterHostname, boolean keepSlave);
+
+    /**
+     * Returns list of configurations of installed clusters
+     *
+     * @return - list of configurations of installed clusters
+     *
+     */
+    public List<Config> getClusters();
+
+    /**
+     * Starts the specified node
+     *
+     * @param clusterName - name of cluster
+     * @param lxcHostName - hostname of node
+     * @param master - specifies if this commands affects master or slave
+     * running on this node true - master, false - slave
+     * @return - UUID of operation to track
+     *
+     */
+    public UUID startNode(String clusterName, String lxcHostName, boolean master);
+
+    /**
+     * Stops the specified node
+     *
+     * @param clusterName - name of cluster
+     * @param lxcHostName - hostname of node
+     * @param master - specifies if this commands affects master or slave
+     * running on this node true - master, false - slave
+     * @return - UUID of operation to track
+     *
+     */
+    public UUID stopNode(String clusterName, String lxcHostName, boolean master);
+
+    /**
+     * Checks status of the specified node
+     *
+     * @param clusterName - name of cluster
+     * @param lxcHostName - hostname of node
+     * @return - UUID of operation to track
+     *
+     */
+    public UUID checkNode(String clusterName, String lxcHostName);
+}
---
+++ management/server/products/presto/presto-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/spark/wizard/WelcomeStep.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.kiskis.mgmt.ui.spark.wizard;
+
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+
+/**
+ *
+ * @author dilshat
+ */
+public class WelcomeStep extends Panel {
+
+    public WelcomeStep(final Wizard wizard) {
+
+        setSizeFull();
+
+        GridLayout grid = new GridLayout(10, 6);
+        grid.setSpacing(true);
+        grid.setMargin(true);
+        grid.setSizeFull();
+
+        Label welcomeMsg = new Label("<center><h2>Welcome to Spark Installation Wizard!</h2>");
+        welcomeMsg.setContentMode(Label.CONTENT_XHTML);
+        grid.addComponent(welcomeMsg, 3, 1, 6, 2);
+
+        Label logoImg = new Label();
+        logoImg.setIcon(new ThemeResource("icons/modules/spark.png"));
+        logoImg.setContentMode(Label.CONTENT_XHTML);
+        logoImg.setHeight(100, Sizeable.UNITS_PIXELS);
+        logoImg.setWidth(192, Sizeable.UNITS_PIXELS);
+        grid.addComponent(logoImg, 1, 3, 2, 5);
+
+        Button next = new Button("Start");
+        next.setWidth(100, Sizeable.UNITS_PIXELS);
+        grid.addComponent(next, 6, 4, 6, 4);
+        grid.setComponentAlignment(next, Alignment.BOTTOM_RIGHT);
+
+        next.addListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                wizard.init();
+                wizard.next();
+            }
+        });
+
+        addComponent(grid);
+    }
+
+}
