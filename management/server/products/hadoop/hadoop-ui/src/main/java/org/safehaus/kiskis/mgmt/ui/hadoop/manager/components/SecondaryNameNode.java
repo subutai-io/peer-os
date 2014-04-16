bf584f3ef83dd76d55b01208dbf958282c0b29e5
daralbaev <daralbaev@gmail.com>
2014-04-16 18:45:10 +0600

    -m SUBUTAI-295 Hadoop Manager in progress
    Former-commit-id: 17459e31c2f9544e7d4fd681af5a9e9c0a8aaf9b

--- management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/SecondaryNameNode.java
+++ management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/SecondaryNameNode.java
@@ -1,6 +1,9 @@
 package org.safehaus.kiskis.mgmt.ui.hadoop.manager.components;
 
 import org.safehaus.kiskis.mgmt.api.hadoop.Config;
+import org.safehaus.kiskis.mgmt.shared.protocol.CompleteEvent;
+import org.safehaus.kiskis.mgmt.shared.protocol.enums.NodeState;
+import org.safehaus.kiskis.mgmt.ui.hadoop.HadoopUI;
 
 import java.util.UUID;
 
@@ -11,10 +14,40 @@ public class SecondaryNameNode extends ClusterNode {
 
     public SecondaryNameNode(Config cluster) {
         super(cluster);
+
+        restartButton.setVisible(false);
+        startButton.setEnabled(false);
+        stopButton.setEnabled(false);
+    }
+
+
+    @Override
+    protected void getStatus(UUID trackID) {
+        setLoading(true);
+
+        HadoopUI.getExecutor().execute(new CheckTask(cluster, new CompleteEvent() {
+
+            public void onComplete(NodeState state) {
+                synchronized (progressIcon) {
+                    boolean isRunning = false;
+                    if (state == NodeState.RUNNING) {
+                        isRunning = true;
+                    } else if (state == NodeState.STOPPED) {
+                        isRunning = false;
+                    }
+
+                    setLoading(false);
+                    startButton.setVisible(isRunning);
+                    stopButton.setVisible(!isRunning);
+                }
+            }
+        }, trackID, cluster.getJobTracker()));
     }
 
     @Override
-    public void getStatus(UUID trackID) {
-
+    protected void setLoading(boolean isLoading) {
+        startButton.setVisible(!isLoading);
+        stopButton.setVisible(!isLoading);
+        progressIcon.setVisible(isLoading);
     }
 }
