554434c54aeb0eea47e089aa5a1c0916e965b161
daralbaev <daralbaev@gmail.com>
2014-04-18 20:13:30 +0600

    -m SUBUTAI-295 Hadoop Manager adding and removing new nodes
    Former-commit-id: 43029b8c3d6ef6098e0197391d3840f036f8f4c6

--- management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/ClusterNode.java
+++ management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/ClusterNode.java
@@ -1,9 +1,7 @@
 package org.safehaus.kiskis.mgmt.ui.hadoop.manager.components;
 
-import com.vaadin.event.MouseEvents;
 import com.vaadin.terminal.Sizeable;
 import com.vaadin.terminal.ThemeResource;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -20,8 +18,7 @@ public class ClusterNode extends HorizontalLayout {
 
     protected Label progressIcon;
     protected Config cluster;
-    protected Button startButton, stopButton, restartButton;
-    protected Embedded image;
+    protected Embedded startButton, stopButton, restartButton;
     protected List<ClusterNode> slaveNodes;
 
     public ClusterNode(Config cluster) {
@@ -35,58 +32,39 @@ public class ClusterNode extends HorizontalLayout {
         addComponent(getStartButton());
         addComponent(getStopButton());
         addComponent(getRestartButton());
-        addComponent(getImage());
-    }
-
-    private Embedded getImage() {
-        image = new Embedded("", new ThemeResource("icons/buttons/start.png"));
-        image.setWidth(16, Sizeable.UNITS_PIXELS);
-        image.setHeight(16, Sizeable.UNITS_PIXELS);
-        image.addListener(new MouseEvents.ClickListener() {
-
-            @Override
-            public void click(com.vaadin.event.MouseEvents.ClickEvent event) {
-                setLoading(true);
-            }
-        });
-
-        return image;
     }
 
     private Label getProgressIcon() {
         progressIcon = new Label();
         progressIcon.setIcon(new ThemeResource("../base/common/img/loading-indicator.gif"));
         progressIcon.setContentMode(Label.CONTENT_XHTML);
-        progressIcon.setHeight(11, Sizeable.UNITS_PIXELS);
-        progressIcon.setWidth(50, Sizeable.UNITS_PIXELS);
+        progressIcon.setHeight(20, Sizeable.UNITS_PIXELS);
+        progressIcon.setWidth(20, Sizeable.UNITS_PIXELS);
         progressIcon.setVisible(false);
 
         return progressIcon;
     }
 
-    private Button getStartButton() {
-        startButton = new Button();
-        startButton.setIcon(new ThemeResource("icons/buttons/start.png"));
-        startButton.setWidth(50, Sizeable.UNITS_PIXELS);
-        startButton.setHeight(11, Sizeable.UNITS_PIXELS);
+    private Embedded getStartButton() {
+        startButton = new Embedded("", new ThemeResource("icons/buttons/start.png"));
+        startButton.setWidth(20, Sizeable.UNITS_PIXELS);
+        startButton.setHeight(20, Sizeable.UNITS_PIXELS);
 
         return startButton;
     }
 
-    private Button getStopButton() {
-        stopButton = new Button();
-        stopButton.setIcon(new ThemeResource("icons/buttons/stop.png"));
-        stopButton.setWidth(50, Sizeable.UNITS_PIXELS);
-        stopButton.setHeight(11, Sizeable.UNITS_PIXELS);
+    private Embedded getStopButton() {
+        stopButton = new Embedded("", new ThemeResource("icons/buttons/stop.png"));
+        stopButton.setWidth(20, Sizeable.UNITS_PIXELS);
+        stopButton.setHeight(20, Sizeable.UNITS_PIXELS);
 
         return stopButton;
     }
 
-    private Button getRestartButton() {
-        restartButton = new Button();
-        restartButton.setIcon(new ThemeResource("icons/buttons/restart.png"));
-        restartButton.setWidth(50, Sizeable.UNITS_PIXELS);
-        restartButton.setHeight(11, Sizeable.UNITS_PIXELS);
+    private Embedded getRestartButton() {
+        restartButton = new Embedded("", new ThemeResource("icons/buttons/restart.png"));
+        restartButton.setWidth(20, Sizeable.UNITS_PIXELS);
+        restartButton.setHeight(20, Sizeable.UNITS_PIXELS);
 
         return restartButton;
     }
