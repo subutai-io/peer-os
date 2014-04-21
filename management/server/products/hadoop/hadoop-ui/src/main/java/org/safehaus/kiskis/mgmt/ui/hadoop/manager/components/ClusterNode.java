108fc7d9ce49df09624c1bb67f25dd1a14649cf5
daralbaev <daralbaev@gmail.com>
2014-04-21 14:45:40 +0600

    -m SUBUTAI-295 Hadoop Manager adding and removing new nodes
    Former-commit-id: e6798174c2a3410f78b61ff2a522c5a1eef76a41

--- management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/ClusterNode.java
+++ management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hadoop/manager/components/ClusterNode.java
@@ -1,6 +1,7 @@
 package org.safehaus.kiskis.mgmt.ui.hadoop.manager.components;
 
 import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
 import org.safehaus.kiskis.mgmt.api.hadoop.Config;
@@ -14,6 +15,8 @@ import java.util.UUID;
  */
 public class ClusterNode extends HorizontalLayout {
 
+    public static final int ICON_SIZE = 30;
+
     protected Config cluster;
     protected Embedded progressButton, startButton, stopButton, restartButton;
     protected List<ClusterNode> slaveNodes;
@@ -26,15 +29,19 @@ public class ClusterNode extends HorizontalLayout {
         setSpacing(true);
 
         addComponent(getProgressButton());
+        setComponentAlignment(progressButton, Alignment.MIDDLE_CENTER);
         addComponent(getStartButton());
+        setComponentAlignment(startButton, Alignment.MIDDLE_CENTER);
         addComponent(getStopButton());
+        setComponentAlignment(stopButton, Alignment.MIDDLE_CENTER);
         addComponent(getRestartButton());
+        setComponentAlignment(restartButton, Alignment.MIDDLE_CENTER);
     }
 
     private Embedded getProgressButton() {
         progressButton = new Embedded("", new ThemeResource("../base/common/img/loading-indicator.gif"));
-        progressButton.setWidth(24, UNITS_PIXELS);
-        progressButton.setHeight(24, UNITS_PIXELS);
+        progressButton.setWidth(ICON_SIZE, UNITS_PIXELS);
+        progressButton.setHeight(ICON_SIZE, UNITS_PIXELS);
         progressButton.setVisible(false);
 
         return progressButton;
@@ -42,24 +49,24 @@ public class ClusterNode extends HorizontalLayout {
 
     private Embedded getStartButton() {
         startButton = new Embedded("", new ThemeResource("icons/buttons/start.png"));
-        startButton.setWidth(24, UNITS_PIXELS);
-        startButton.setHeight(24, UNITS_PIXELS);
+        startButton.setWidth(ICON_SIZE, UNITS_PIXELS);
+        startButton.setHeight(ICON_SIZE, UNITS_PIXELS);
 
         return startButton;
     }
 
     private Embedded getStopButton() {
         stopButton = new Embedded("", new ThemeResource("icons/buttons/stop.png"));
-        stopButton.setWidth(24, UNITS_PIXELS);
-        stopButton.setHeight(24, UNITS_PIXELS);
+        stopButton.setWidth(ICON_SIZE, UNITS_PIXELS);
+        stopButton.setHeight(ICON_SIZE, UNITS_PIXELS);
 
         return stopButton;
     }
 
     private Embedded getRestartButton() {
         restartButton = new Embedded("", new ThemeResource("icons/buttons/stop.png"));
-        restartButton.setWidth(24, UNITS_PIXELS);
-        restartButton.setHeight(24, UNITS_PIXELS);
+        restartButton.setWidth(ICON_SIZE, UNITS_PIXELS);
+        restartButton.setHeight(ICON_SIZE, UNITS_PIXELS);
 
         return restartButton;
     }
