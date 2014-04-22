17c13fb5744fed028ce69f640e62e4ba98859fa0
Askat Asanaliev <aasanaliev@exadel.com>
2014-04-22 18:06:39 +0600

    SUBUTAI-616 - Monitoring. Replace the chart lib b/c of the license issue
    Former-commit-id: a0b2c840fec102a2104cd24613f7368fb2723b48

--- management/server/core/monitor/monitor-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/monitor/ModuleView.java
+++ management/server/core/monitor/monitor-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/monitor/ModuleView.java
@@ -103,7 +103,7 @@ public class ModuleView extends CustomComponent {
         AbsoluteLayout chartLayout = new AbsoluteLayout();
         chartLayout.setWidth(800, Sizeable.UNITS_PIXELS);
         chartLayout.setHeight(400, Sizeable.UNITS_PIXELS);
-        chartLayout.setDebugId("placeholder");
+        chartLayout.setDebugId("chart");
 
         layout.addComponent(chartLayout, "left: 250px; top: 50px;");
     }
