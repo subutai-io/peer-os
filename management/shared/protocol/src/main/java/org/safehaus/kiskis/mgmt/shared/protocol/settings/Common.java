9de4c1833634e3a16a82ea0dcb689d0082f3adb0
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-04-25 22:37:45 +0600

    implementing new agent parent node resolve strategy
    Former-commit-id: 9c91a879f6da386d3b9c844a859ff67be2d5f1cb

--- management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/settings/Common.java
+++ management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/settings/Common.java
@@ -5,6 +5,7 @@ public class Common {
     public static final String UNKNOWN_LXC_PARENT_NAME = "UNKNOWN";
     public static final String BASE_CONTAINER_NAME = "base-container";
     public static final int REFRESH_UI_SEC = 3;
+    public static final int LXC_AGENT_WAIT_TIMEOUT_SEC = 90;
     public static final int AGENT_FRESHNESS_MIN = 5;
     public static final int MAX_COMMAND_TIMEOUT_SEC = 100 * 60 * 60; // 100 hours
     public static final String IP_MASK = "^10\\.10\\.10\\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$";
