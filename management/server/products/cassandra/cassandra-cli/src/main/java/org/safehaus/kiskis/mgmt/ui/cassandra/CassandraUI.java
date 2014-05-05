3515d3185c2c88a9e7a1003b29314144dfdf09aa
bahadyr <bahadyr@safehaus.org>
2014-05-05 11:06:29 +0600

    Added Cassandra CLI module.
    Former-commit-id: 9fc59a0f0528de4c9224fb4dd94a3f31e36dafc9

---
+++ management/server/products/cassandra/cassandra-cli/src/main/java/org/safehaus/kiskis/mgmt/ui/cassandra/CassandraUI.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.kiskis.mgmt.ui.cassandra;
+
+import org.safehaus.kiskis.mgmt.api.agentmanager.AgentManager;
+import org.safehaus.kiskis.mgmt.api.cassandra.Cassandra;
+import org.safehaus.kiskis.mgmt.api.cassandra.Config;
+import org.safehaus.kiskis.mgmt.api.tracker.Tracker;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author dilshat
+ */
+public class CassandraUI {
+
+    private static Cassandra cassandraManager;
+    private static AgentManager agentManager;
+    private static Tracker tracker;
+    private static ExecutorService executor;
+
+    public static Tracker getTracker() {
+        return tracker;
+    }
+
+    public void setTracker(Tracker tracker) {
+        CassandraUI.tracker = tracker;
+    }
+
+    public static Cassandra getCassandraManager() {
+        return cassandraManager;
+    }
+
+    public void setCassandraManager(Cassandra cassandraManager) {
+        CassandraUI.cassandraManager = cassandraManager;
+    }
+
+    public static ExecutorService getExecutor() {
+        return executor;
+    }
+
+    public static AgentManager getAgentManager() {
+        return agentManager;
+    }
+
+    public void setAgentManager(AgentManager agentManager) {
+        CassandraUI.agentManager = agentManager;
+    }
+
+    public void init() {
+        executor = Executors.newCachedThreadPool();
+    }
+
+    public void destroy() {
+        cassandraManager = null;
+        agentManager = null;
+        tracker = null;
+        executor.shutdown();
+    }
+
+    public String getName() {
+        return Config.PRODUCT_KEY;
+    }
+
+}
