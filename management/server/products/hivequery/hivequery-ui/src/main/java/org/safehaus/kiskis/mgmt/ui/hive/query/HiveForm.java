54e398f5a90608b58dc04a017f7195b213150056
daralbaev <daralbaev@gmail.com>
2014-05-05 20:34:53 +0600

    -m Adding Hive Query API SUBUTAI-673 SUBUTAI-664
    Former-commit-id: 7e39623364cea5cb6d90f705639c0a22e55cd7ce

---
+++ management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/HiveForm.java
@@ -0,0 +1,23 @@
+package org.safehaus.kiskis.mgmt.ui.hive.query;
+
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Runo;
+
+public class HiveForm extends CustomComponent {
+
+    public HiveForm() {
+        setSizeFull();
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSpacing(true);
+        verticalLayout.setSizeFull();
+        TabSheet mongoSheet = new TabSheet();
+        mongoSheet.setStyleName(Runo.TABSHEET_SMALL);
+        mongoSheet.setSizeFull();
+
+        verticalLayout.addComponent(mongoSheet);
+        setCompositionRoot(verticalLayout);
+    }
+
+}
