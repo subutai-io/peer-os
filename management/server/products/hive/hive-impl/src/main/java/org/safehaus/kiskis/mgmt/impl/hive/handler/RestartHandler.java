9c8a21913b1fc491ae9e2790fc4a461a73cc248d
Azilet Beishenaliev <aziletb@gmail.com>
2014-05-06 18:26:54 +0600

    refactor Hive impl module to use AbstractOperationHandler in shared module
    Former-commit-id: 034d6c6b92bee42b00547de0d2b768f30dcaa6c3

--- management/server/products/hive/hive-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/hive/handler/RestartHandler.java
+++ management/server/products/hive/hive-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/hive/handler/RestartHandler.java
@@ -6,14 +6,16 @@ import org.safehaus.kiskis.mgmt.api.commandrunner.AgentResult;
 import org.safehaus.kiskis.mgmt.api.commandrunner.Command;
 import org.safehaus.kiskis.mgmt.api.commandrunner.RequestBuilder;
 import org.safehaus.kiskis.mgmt.api.hive.Config;
-import org.safehaus.kiskis.mgmt.api.tracker.ProductOperation;
 import org.safehaus.kiskis.mgmt.impl.hive.*;
 import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
 
 public class RestartHandler extends AbstractHandler {
 
-    public RestartHandler(HiveImpl manager, String clusterName, ProductOperation po) {
-        super(manager, clusterName, po);
+    private final String hostname;
+
+    public RestartHandler(HiveImpl manager, String clusterName, String hostname) {
+        super(manager, clusterName, "Restart node " + hostname);
+        this.hostname = hostname;
     }
 
     public void run() {
