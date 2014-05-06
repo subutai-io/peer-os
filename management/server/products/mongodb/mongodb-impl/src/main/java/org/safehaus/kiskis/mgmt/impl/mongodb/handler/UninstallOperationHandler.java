52a50cbf2b2bd54cb66255c1baa065fc3ef28246
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-05-06 23:21:50 +0600

    Refactoring Mongo Impl to use handlers
    Former-commit-id: 809f1bf869b7cf5a040e32953c05741544926b94

---
+++ management/server/products/mongodb/mongodb-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/mongodb/handler/UninstallOperationHandler.java
@@ -0,0 +1,50 @@
+package org.safehaus.kiskis.mgmt.impl.mongodb.handler;
+
+import org.safehaus.kiskis.mgmt.api.lxcmanager.LxcDestroyException;
+import org.safehaus.kiskis.mgmt.api.mongodb.Config;
+import org.safehaus.kiskis.mgmt.api.tracker.ProductOperation;
+import org.safehaus.kiskis.mgmt.impl.mongodb.MongoImpl;
+import org.safehaus.kiskis.mgmt.shared.protocol.AbstractOperationHandler;
+
+import java.util.UUID;
+
+/**
+ * Created by dilshat on 5/6/14.
+ */
+public class UninstallOperationHandler extends AbstractOperationHandler<MongoImpl> {
+    private final ProductOperation po;
+
+    public UninstallOperationHandler(MongoImpl manager, String clusterName) {
+        super(manager, clusterName);
+        po = manager.getTracker().createProductOperation(Config.PRODUCT_KEY,
+                String.format("Destroying cluster %s", clusterName));
+    }
+
+    @Override
+    public UUID getTrackerId() {
+        return po.getId();
+    }
+
+    @Override
+    public void run() {
+        Config config = manager.getCluster(clusterName);
+        if (config == null) {
+            po.addLogFailed(String.format("Cluster with name %s does not exist", clusterName));
+            return;
+        }
+
+        po.addLog("Destroying lxc containers");
+        try {
+            manager.getLxcManager().destroyLxcs(config.getAllNodes());
+            po.addLog("Lxc containers successfully destroyed");
+        } catch (LxcDestroyException ex) {
+            po.addLog(String.format("%s, skipping...", ex.getMessage()));
+        }
+        po.addLog("Updating db...");
+        if (manager.getDbManager().deleteInfo(Config.PRODUCT_KEY, config.getClusterName())) {
+            po.addLogDone("Cluster info deleted from DB\nDone");
+        } else {
+            po.addLogFailed("Error while deleting cluster info from DB. Check logs\nFailed");
+        }
+    }
+}
