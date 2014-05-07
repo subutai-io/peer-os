ab6d98f588aa55bba3096758c182c48c25132900
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-05-07 15:10:38 +0600

    Refactoring Products Impl to use handlers
    Former-commit-id: 1dd1ce9d2ebc37d8cfde6152f4ffc7da9d19e59f

--- management/server/products/solr/solr-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/solr/SolrImpl.java
+++ management/server/products/solr/solr-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/solr/SolrImpl.java
@@ -1,23 +1,18 @@
 package org.safehaus.kiskis.mgmt.impl.solr;
 
-import com.google.common.base.Strings;
+import com.google.common.base.Preconditions;
 import org.safehaus.kiskis.mgmt.api.agentmanager.AgentManager;
-import org.safehaus.kiskis.mgmt.api.commandrunner.AgentResult;
-import org.safehaus.kiskis.mgmt.api.commandrunner.Command;
 import org.safehaus.kiskis.mgmt.api.commandrunner.CommandRunner;
 import org.safehaus.kiskis.mgmt.api.dbmanager.DbManager;
-import org.safehaus.kiskis.mgmt.api.lxcmanager.LxcCreateException;
-import org.safehaus.kiskis.mgmt.api.lxcmanager.LxcDestroyException;
 import org.safehaus.kiskis.mgmt.api.lxcmanager.LxcManager;
 import org.safehaus.kiskis.mgmt.api.solr.Config;
 import org.safehaus.kiskis.mgmt.api.solr.Solr;
-import org.safehaus.kiskis.mgmt.api.tracker.ProductOperation;
 import org.safehaus.kiskis.mgmt.api.tracker.Tracker;
-import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
-import org.safehaus.kiskis.mgmt.shared.protocol.Util;
-import org.safehaus.kiskis.mgmt.shared.protocol.enums.NodeState;
+import org.safehaus.kiskis.mgmt.impl.solr.handler.*;
+import org.safehaus.kiskis.mgmt.shared.protocol.AbstractOperationHandler;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -41,6 +36,26 @@ public class SolrImpl implements Solr {
         Commands.init(commandRunner);
     }
 
+    public CommandRunner getCommandRunner() {
+        return commandRunner;
+    }
+
+    public AgentManager getAgentManager() {
+        return agentManager;
+    }
+
+    public DbManager getDbManager() {
+        return dbManager;
+    }
+
+    public Tracker getTracker() {
+        return tracker;
+    }
+
+    public LxcManager getLxcManager() {
+        return lxcManager;
+    }
+
     public void init() {
         executor = Executors.newCachedThreadPool();
     }
@@ -50,368 +65,68 @@ public class SolrImpl implements Solr {
     }
 
     public UUID installCluster(final Config config) {
-        final ProductOperation po = tracker.createProductOperation(Config.PRODUCT_KEY, String.format("Installing %s", Config.PRODUCT_KEY));
 
-        executor.execute(new Runnable() {
+        Preconditions.checkNotNull(config, "Configuration is null");
 
-            public void run() {
+        AbstractOperationHandler operationHandler = new InstallOperationHandler(this, config);
 
-                if (config == null || Strings.isNullOrEmpty(config.getClusterName()) || config.getNumberOfNodes() <= 0) {
-                    po.addLogFailed("Malformed configuration\nInstallation aborted");
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                if (getCluster(config.getClusterName()) != null) {
-                    po.addLogFailed(String.format("Cluster with name '%s' already exists\nInstallation aborted", config.getClusterName()));
-                    return;
-                }
-
-                try {
-                    po.addLog(String.format("Creating %d lxc containers...", config.getNumberOfNodes()));
-                    Map<Agent, Set<Agent>> lxcAgentsMap = lxcManager.createLxcs(config.getNumberOfNodes());
-                    config.setNodes(new HashSet<Agent>());
-
-                    for (Map.Entry<Agent, Set<Agent>> entry : lxcAgentsMap.entrySet()) {
-                        config.getNodes().addAll(entry.getValue());
-                    }
-                    po.addLog("Lxc containers created successfully\nUpdating db...");
-                    if (dbManager.saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
-
-                        po.addLog("Cluster info saved to DB\nInstalling Solr...");
-
-                        //install
-                        Command installCommand = Commands.getInstallCommand(config.getNodes());
-                        commandRunner.runCommand(installCommand);
-
-                        if (installCommand.hasSucceeded()) {
-                            po.addLogDone("Installation succeeded");
-                        } else {
-                            po.addLogFailed(String.format("Installation failed, %s", installCommand.getAllErrors()));
-                        }
-
-                    } else {
-                        //destroy all lxcs also
-                        try {
-                            lxcManager.destroyLxcs(lxcAgentsMap);
-                        } catch (LxcDestroyException ex) {
-                            po.addLogFailed("Could not save cluster info to DB! Please see logs. Use LXC module to cleanup\nInstallation aborted");
-                        }
-                        po.addLogFailed("Could not save cluster info to DB! Please see logs\nInstallation aborted");
-                    }
-                } catch (LxcCreateException ex) {
-                    po.addLogFailed(ex.getMessage());
-                }
-
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID uninstallCluster(final String clusterName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Destroying cluster %s", clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new UninstallOperationHandler(this, clusterName);
 
-            public void run() {
-                Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                po.addLog("Destroying lxc containers...");
-
-                try {
-                    lxcManager.destroyLxcs(config.getNodes());
-                    po.addLog("Lxc containers successfully destroyed");
-                } catch (LxcDestroyException ex) {
-                    po.addLog(String.format("%s, skipping...", ex.getMessage()));
-                }
-                po.addLog("Updating db...");
-                if (dbManager.deleteInfo(Config.PRODUCT_KEY, config.getClusterName())) {
-                    po.addLogDone("Cluster info deleted from DB\nDone");
-                } else {
-                    po.addLogFailed("Error while deleting cluster info from DB. Check logs.\nFailed");
-                }
-
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID startNode(final String clusterName, final String lxcHostName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Starting node %s in %s", lxcHostName, clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new StartNodeOperationHandler(this, clusterName, lxcHostName);
 
-            public void run() {
-                Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                final Agent node = agentManager.getAgentByHostname(lxcHostName);
-                if (node == null) {
-                    po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostName));
-                    return;
-                }
-
-                if (!config.getNodes().contains(node)) {
-                    po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostName, clusterName));
-                    return;
-                }
-
-                po.addLog("Starting node...");
-
-                Command startCommand = Commands.getStartCommand(node);
-                commandRunner.runCommand(startCommand);
-                Command statusCommand = Commands.getStatusCommand(node);
-                commandRunner.runCommand(statusCommand);
-                AgentResult result = statusCommand.getResults().get(node.getUuid());
-                NodeState nodeState = NodeState.UNKNOWN;
-                if (result != null) {
-                    if (result.getStdOut().contains("is running")) {
-                        nodeState = NodeState.RUNNING;
-                    } else if (result.getStdOut().contains("is not running")) {
-                        nodeState = NodeState.STOPPED;
-                    }
-                }
-
-                if (NodeState.RUNNING.equals(nodeState)) {
-                    po.addLogDone(String.format("Node on %s started", lxcHostName));
-                } else {
-                    po.addLogFailed(String.format("Failed to start node %s. %s",
-                            lxcHostName, startCommand.getAllErrors()
-                    ));
-                }
-
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID stopNode(final String clusterName, final String lxcHostName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Stopping node %s in %s", lxcHostName, clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new StopNodeOperationHandler(this, clusterName, lxcHostName);
 
-            public void run() {
-                Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                final Agent node = agentManager.getAgentByHostname(lxcHostName);
-                if (node == null) {
-                    po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostName));
-                    return;
-                }
-                if (!config.getNodes().contains(node)) {
-                    po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostName, clusterName));
-                    return;
-                }
-                po.addLog("Stopping node...");
-
-                Command stopCommand = Commands.getStopCommand(node);
-                commandRunner.runCommand(stopCommand);
-                Command statusCommand = Commands.getStatusCommand(node);
-                commandRunner.runCommand(statusCommand);
-                AgentResult result = statusCommand.getResults().get(node.getUuid());
-                NodeState nodeState = NodeState.UNKNOWN;
-                if (result != null) {
-                    if (result.getStdOut().contains("is running")) {
-                        nodeState = NodeState.RUNNING;
-                    } else if (result.getStdOut().contains("is not running")) {
-                        nodeState = NodeState.STOPPED;
-                    }
-                }
-
-                if (NodeState.STOPPED.equals(nodeState)) {
-                    po.addLogDone(String.format("Node on %s stopped", lxcHostName));
-                } else {
-                    po.addLogFailed(String.format("Failed to stop node %s. %s",
-                            lxcHostName, stopCommand.getAllErrors()
-                    ));
-                }
-
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID checkNode(final String clusterName, final String lxcHostName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Checking node %s in %s", lxcHostName, clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new CheckNodeOperationHandler(this, clusterName, lxcHostName);
 
-            public void run() {
-                Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                final Agent node = agentManager.getAgentByHostname(lxcHostName);
-                if (node == null) {
-                    po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostName));
-                    return;
-                }
-                if (!config.getNodes().contains(node)) {
-                    po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostName, clusterName));
-                    return;
-                }
-                po.addLog("Checking node...");
-
-                Command checkNodeCommand = Commands.getStatusCommand(node);
-                commandRunner.runCommand(checkNodeCommand);
-
-                NodeState nodeState = NodeState.UNKNOWN;
-                if (checkNodeCommand.hasCompleted()) {
-                    AgentResult result = checkNodeCommand.getResults().get(node.getUuid());
-                    if (result.getStdOut().contains("is running")) {
-                        nodeState = NodeState.RUNNING;
-                    } else if (result.getStdOut().contains("is not running")) {
-                        nodeState = NodeState.STOPPED;
-                    }
-                }
-
-                if (NodeState.UNKNOWN.equals(nodeState)) {
-                    po.addLogFailed(String.format("Failed to check status of %s, %s",
-                            lxcHostName, checkNodeCommand.getAllErrors()
-                    ));
-                } else {
-                    po.addLogDone(String.format("Node %s is %s",
-                            lxcHostName, nodeState
-                    ));
-                }
-
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID destroyNode(final String clusterName, final String lxcHostName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Destroying %s in %s", lxcHostName, clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new DestroyNodeOperationHandler(this, clusterName, lxcHostName);
 
-            public void run() {
-                final Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                Agent agent = agentManager.getAgentByHostname(lxcHostName);
-                if (agent == null) {
-                    po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostName));
-                    return;
-                }
-                if (!config.getNodes().contains(agent)) {
-                    po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostName, clusterName));
-                    return;
-                }
-
-                if (config.getNodes().size() == 1) {
-                    po.addLogFailed("This is the last node in the cluster. Please, destroy cluster instead\nOperation aborted");
-                    return;
-                }
-
-                //destroy lxc
-                po.addLog("Destroying lxc container...");
-                Agent physicalAgent = agentManager.getAgentByHostname(agent.getParentHostName());
-                if (physicalAgent == null) {
-                    po.addLog(
-                            String.format("Could not determine physical parent of %s. Use LXC module to cleanup, skipping...",
-                                    agent.getHostname())
-                    );
-                } else {
-                    if (!lxcManager.destroyLxcOnHost(physicalAgent, agent.getHostname())) {
-                        po.addLog("Could not destroy lxc container. Use LXC module to cleanup, skipping...");
-                    } else {
-                        po.addLog("Lxc container destroyed successfully");
-                    }
-                }
-                //update db
-                po.addLog("Updating db...");
-                config.getNodes().remove(agent);
-                if (!dbManager.saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
-                    po.addLogFailed(String.format("Error while updating cluster info [%s] in DB. Check logs\nFailed",
-                            config.getClusterName()));
-                } else {
-                    po.addLogDone("Done");
-                }
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public UUID addNode(final String clusterName) {
-        final ProductOperation po
-                = tracker.createProductOperation(Config.PRODUCT_KEY,
-                String.format("Adding node to %s", clusterName));
 
-        executor.execute(new Runnable() {
+        AbstractOperationHandler operationHandler = new AddNodeOperationHandler(this, clusterName);
 
-            public void run() {
-                Config config = getCluster(clusterName);
-                if (config == null) {
-                    po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
-                    return;
-                }
+        executor.execute(operationHandler);
 
-                try {
-
-                    po.addLog("Creating lxc container...");
-
-                    Map<Agent, Set<Agent>> lxcAgentsMap = lxcManager.createLxcs(1);
-
-                    Agent lxcAgent = lxcAgentsMap.entrySet().iterator().next().getValue().iterator().next();
-
-                    config.getNodes().add(lxcAgent);
-                    po.addLog("Lxc container created successfully\nUpdating db...");
-                    if (dbManager.saveInfo(Config.PRODUCT_KEY, clusterName, config)) {
-                        po.addLog("Cluster info updated in DB\nInstalling Solr...");
-
-                        Command installCommand = Commands.getInstallCommand(Util.wrapAgentToSet(lxcAgent));
-                        commandRunner.runCommand(installCommand);
-
-                        if (installCommand.hasSucceeded()) {
-                            po.addLogDone("Installation succeeded\nDone");
-
-                        } else {
-                            po.addLogFailed(String.format("Installation failed, %s",
-                                    installCommand.getAllErrors()));
-                        }
-                    } else {
-                        po.addLogFailed("Error while updating cluster info in DB. Check logs. Use LXC Module to cleanup\nFailed");
-                    }
-
-                } catch (LxcCreateException ex) {
-                    po.addLogFailed(ex.getMessage());
-                }
-            }
-        });
-
-        return po.getId();
+        return operationHandler.getTrackerId();
     }
 
     public List<Config> getClusters() {
---
+++ management/server/products/solr/solr-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/solr/handler/DestroyNodeOperationHandler.java
@@ -0,0 +1,78 @@
+package org.safehaus.kiskis.mgmt.impl.solr.handler;
+
+import org.safehaus.kiskis.mgmt.api.solr.Config;
+import org.safehaus.kiskis.mgmt.api.tracker.ProductOperation;
+import org.safehaus.kiskis.mgmt.impl.solr.SolrImpl;
+import org.safehaus.kiskis.mgmt.shared.protocol.AbstractOperationHandler;
+import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
+
+import java.util.UUID;
+
+/**
+ * Created by dilshat on 5/7/14.
+ */
+public class DestroyNodeOperationHandler extends AbstractOperationHandler<SolrImpl> {
+    private final ProductOperation po;
+    private final String lxcHostname;
+
+    public DestroyNodeOperationHandler(SolrImpl manager, String clusterName, String lxcHostname) {
+        super(manager, clusterName);
+        this.lxcHostname = lxcHostname;
+        po = manager.getTracker().createProductOperation(Config.PRODUCT_KEY,
+                String.format("Destroying %s in %s", lxcHostname, clusterName));
+    }
+
+    @Override
+    public UUID getTrackerId() {
+        return po.getId();
+    }
+
+    @Override
+    public void run() {
+        final Config config = manager.getCluster(clusterName);
+        if (config == null) {
+            po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
+            return;
+        }
+
+        Agent agent = manager.getAgentManager().getAgentByHostname(lxcHostname);
+        if (agent == null) {
+            po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostname));
+            return;
+        }
+        if (!config.getNodes().contains(agent)) {
+            po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostname, clusterName));
+            return;
+        }
+
+        if (config.getNodes().size() == 1) {
+            po.addLogFailed("This is the last node in the cluster. Please, destroy cluster instead\nOperation aborted");
+            return;
+        }
+
+        //destroy lxc
+        po.addLog("Destroying lxc container...");
+        Agent physicalAgent = manager.getAgentManager().getAgentByHostname(agent.getParentHostName());
+        if (physicalAgent == null) {
+            po.addLog(
+                    String.format("Could not determine physical parent of %s. Use LXC module to cleanup, skipping...",
+                            agent.getHostname())
+            );
+        } else {
+            if (!manager.getLxcManager().destroyLxcOnHost(physicalAgent, agent.getHostname())) {
+                po.addLog("Could not destroy lxc container. Use LXC module to cleanup, skipping...");
+            } else {
+                po.addLog("Lxc container destroyed successfully");
+            }
+        }
+        //update db
+        po.addLog("Updating db...");
+        config.getNodes().remove(agent);
+        if (!manager.getDbManager().saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
+            po.addLogFailed(String.format("Error while updating cluster info [%s] in DB. Check logs\nFailed",
+                    config.getClusterName()));
+        } else {
+            po.addLogDone("Done");
+        }
+    }
+}
