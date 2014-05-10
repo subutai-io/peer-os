09c2d2666eb0c6f038844dac145b5c787685c462
daralbaev <daralbaev@gmail.com>
2014-05-11 00:42:18 +0600

    -m Adding Hive Query API SUBUTAI-673 SUBUTAI-664
    Former-commit-id: e70fe69de98d2f1bc89cda1f1862c69416f07b10

---
+++ management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/components/QueryList.java
@@ -0,0 +1,43 @@
+package org.safehaus.kiskis.mgmt.ui.hive.query.components;
+
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.ui.AbstractSelect;
+import com.vaadin.ui.Select;
+import org.safehaus.kiskis.mgmt.api.hive.query.Config;
+import org.safehaus.kiskis.mgmt.ui.hive.query.HiveQueryUI;
+
+import java.util.List;
+
+/**
+ * Created by daralbaev on 11.05.14.
+ */
+public class QueryList extends Select {
+
+    public QueryList() {
+
+        setFilteringMode(AbstractSelect.Filtering.FILTERINGMODE_CONTAINS);
+        setNullSelectionAllowed(false);
+
+        this.setWidth("100%");
+        this.setHeight(100, Sizeable.UNITS_PERCENTAGE);
+
+        this.setImmediate(true);
+        refreshDataSource();
+    }
+
+    public void refreshDataSource() {
+        List<Config> list = HiveQueryUI.getManager().load();
+
+        BeanItemContainer<Config> beans =
+                new BeanItemContainer<>(Config.class);
+
+        for (Config item : list) {
+            beans.addBean(item);
+        }
+
+        setContainerDataSource(beans);
+        setItemCaptionMode(Select.ITEM_CAPTION_MODE_PROPERTY);
+        setItemCaptionPropertyId("name");
+    }
+}
