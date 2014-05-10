16b9aebe27f48e23596febec2dacec4a81f10e44
daralbaev <daralbaev@gmail.com>
2014-05-11 01:04:59 +0600

    -m Adding Hive Query API SUBUTAI-673 SUBUTAI-664
    Former-commit-id: 0f7ebeb55632428cc6b4f051123ee80f81577f07

--- management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/components/QueryList.java
+++ management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/components/QueryList.java
@@ -1,7 +1,7 @@
 package org.safehaus.kiskis.mgmt.ui.hive.query.components;
 
 import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.ui.AbstractSelect;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Select;
 import org.safehaus.kiskis.mgmt.api.hive.query.Config;
 import org.safehaus.kiskis.mgmt.ui.hive.query.HiveQueryUI;
@@ -11,24 +11,32 @@ import java.util.List;
 /**
  * Created by daralbaev on 11.05.14.
  */
-public class QueryList extends Select {
+public class QueryList extends ListSelect {
+
+    public static final int ROW_SIZE = 10;
 
     public QueryList() {
 
-        setFilteringMode(AbstractSelect.Filtering.FILTERINGMODE_CONTAINS);
+        //setFilteringMode(AbstractSelect.Filtering.FILTERINGMODE_CONTAINS);
         setNullSelectionAllowed(false);
-        this.setImmediate(true);
-        refreshDataSource();
+        setImmediate(true);
+        setRows(QueryList.ROW_SIZE);
+
+        refreshDataSource(null);
     }
 
-    public void refreshDataSource() {
+    public void refreshDataSource(String filter) {
         List<Config> list = HiveQueryUI.getManager().load();
 
         BeanItemContainer<Config> beans =
                 new BeanItemContainer<Config>(Config.class);
 
         for (Config item : list) {
-            beans.addBean(item);
+            if (filter == null) {
+                beans.addBean(item);
+            } else if (item.getName().contains(filter) || item.getDescription().contains(filter)) {
+                beans.addBean(item);
+            }
         }
 
         setContainerDataSource(beans);
