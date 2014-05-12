ab803c6e143191c01a515eaed3c714780223579f
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-05-12 16:01:20 +0600

    Modifying Accumulo module to handle correct settings and fixed addNode operation, made AbstractHandler strict typecheck passed manager
    Former-commit-id: fb5e762e585e1dc45e3ad33404f9fe05d02ed26c

--- management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/AbstractOperationHandler.java
+++ management/shared/protocol/src/main/java/org/safehaus/kiskis/mgmt/shared/protocol/AbstractOperationHandler.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 /**
  * Created by dilshat on 5/6/14.
  */
-public abstract class AbstractOperationHandler<T> implements Runnable {
+public abstract class AbstractOperationHandler<T extends ApiBase> implements Runnable {
 
     protected final T manager;
     protected final String clusterName;
