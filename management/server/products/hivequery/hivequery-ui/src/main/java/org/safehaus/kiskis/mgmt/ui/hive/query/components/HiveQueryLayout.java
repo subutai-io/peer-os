d9f0caee1a55a2d0f11a69db81358f91a651261d
daralbaev <daralbaev@gmail.com>
2014-05-12 14:27:32 +0600

    -m Adding Hive Query API SUBUTAI-673 SUBUTAI-664
    Former-commit-id: 3d6d104b4801ef7ceedb827209754bd3d5cfcd40

--- management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/components/HiveQueryLayout.java
+++ management/server/products/hivequery/hivequery-ui/src/main/java/org/safehaus/kiskis/mgmt/ui/hive/query/components/HiveQueryLayout.java
@@ -12,33 +12,36 @@ public class HiveQueryLayout extends GridLayout {
 
     private HadoopTreeTable table;
     private QueryList list;
+    private TextField searchTextField;
     private TextField nameTextField;
     private TextArea queryTextArea;
     private TextArea descriptionTextArea;
+    private TextArea resultTextArea;
 
     public HiveQueryLayout() {
-        super(3, 3);
+        super(12, 12);
 
         setSpacing(true);
         setSizeFull();
 
         table = new HadoopTreeTable();
-        addComponent(table, 0, 0, 1, 0);
+        addComponent(table, 0, 0, 5, 3);
         setComponentAlignment(table, Alignment.MIDDLE_CENTER);
 
         list = new QueryList();
-        addComponent(list, 2, 0);
+        addComponent(list, 6, 2, 11, 3);
         setComponentAlignment(list, Alignment.MIDDLE_CENTER);
 
         nameTextField = new TextField("Query Name");
-        addComponent(nameTextField, 0, 1, 2, 1);
+        addComponent(nameTextField, 0, 4, 5, 4);
         setComponentAlignment(nameTextField, Alignment.MIDDLE_CENTER);
 
         queryTextArea = new TextArea("Query");
-        addComponent(queryTextArea, 0, 2, 1, 2);
+        addComponent(queryTextArea, 0, 5, 5, 7);
         setComponentAlignment(queryTextArea, Alignment.MIDDLE_CENTER);
 
         descriptionTextArea = new TextArea("Description");
-        addComponent(descriptionTextArea, 2, 2);
+        addComponent(descriptionTextArea, 6, 5, 11, 7);
+        setComponentAlignment(descriptionTextArea, Alignment.MIDDLE_CENTER);
     }
 }
