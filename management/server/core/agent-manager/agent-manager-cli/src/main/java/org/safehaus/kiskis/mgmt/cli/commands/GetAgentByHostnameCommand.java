8f0059b1ef6f01b10aa360f413ba5bf7f307b61a
bahadyr <bahadyr@safehaus.org>
2014-05-13 12:41:53 +0600

    Get agent by hostname command implemented.
    Former-commit-id: 9b2ac0290d3d344080ab3a00824f5ee5aca9a838

--- management/server/core/agent-manager/agent-manager-cli/src/main/java/org/safehaus/kiskis/mgmt/cli/commands/GetAgentByHostnameCommand.java
+++ management/server/core/agent-manager/agent-manager-cli/src/main/java/org/safehaus/kiskis/mgmt/cli/commands/GetAgentByHostnameCommand.java
@@ -1,7 +1,10 @@
 package org.safehaus.kiskis.mgmt.cli.commands;
 
+import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.kiskis.mgmt.api.agentmanager.AgentManager;
+import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
 
 
 /**
@@ -10,7 +13,32 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "agent", name = "get-agent-by-hostname", description = "get agent by hostname")
 public class GetAgentByHostnameCommand extends OsgiCommandSupport {
 
+    private AgentManager agentManager;
+
+    public AgentManager getAgentManager() {
+        return agentManager;
+    }
+
+    public void setAgentManager(AgentManager agentManager) {
+        this.agentManager = agentManager;
+    }
+
+    @Argument(index = 0, name = "hostname", required = true, multiValued = false, description = "agent hostname")
+    String hostname;
+
     protected Object doExecute() {
+
+        Agent agent = agentManager.getAgentByHostname(hostname);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hostname: ").append(agent.getHostname());
+        for (String ip : agent.getListIP()) {
+            sb.append("IP: ").append(ip);
+        }
+        sb.append("MAC address: ").append(agent.getMacAddress());
+        sb.append("Parent hostname: ").append(agent.getParentHostName());
+        sb.append("Transport ID: ").append(agent.getTransportId());
+        sb.append("UUID: ").append(agent.getUuid());
+        System.out.println(sb.toString());
         System.out.println("get-agent-by-hostname command executed");
         return null;
     }
