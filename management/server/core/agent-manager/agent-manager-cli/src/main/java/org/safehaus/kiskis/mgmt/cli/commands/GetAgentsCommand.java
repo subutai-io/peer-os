48ee820cf2a7ac6a7eb2c8d4c8c0dfa17b379d64
bahadyr <bahadyr@safehaus.org>
2014-05-13 12:46:30 +0600

    Get agents command implemented.
    Former-commit-id: 68bdc431261018ed03f78a096daa082da84bb0d0

--- management/server/core/agent-manager/agent-manager-cli/src/main/java/org/safehaus/kiskis/mgmt/cli/commands/GetAgentsCommand.java
+++ management/server/core/agent-manager/agent-manager-cli/src/main/java/org/safehaus/kiskis/mgmt/cli/commands/GetAgentsCommand.java
@@ -2,6 +2,10 @@ package org.safehaus.kiskis.mgmt.cli.commands;
 
 import org.apache.felix.gogo.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.kiskis.mgmt.api.agentmanager.AgentManager;
+import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
+
+import java.util.Set;
 
 
 /**
@@ -10,7 +14,26 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "agent", name = "get-agents", description = "get the list of agents")
 public class GetAgentsCommand extends OsgiCommandSupport {
 
+    private AgentManager agentManager;
+
+    public AgentManager getAgentManager() {
+        return agentManager;
+    }
+
+    public void setAgentManager(AgentManager agentManager) {
+        this.agentManager = agentManager;
+    }
+
     protected Object doExecute() {
+        Set<Agent> agentList = agentManager.getAgents();
+        StringBuilder sb = new StringBuilder();
+
+        for(Agent agent : agentList) {
+            sb.append(agent.getHostname());
+        }
+
+        System.out.println(sb.toString());
+
         System.out.println("get-agents command executed");
         return null;
     }
