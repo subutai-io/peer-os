c1a7799f0e508dae685abbda9e2d25c5fe44baee
Askat Asanaliev <aasanaliev@exadel.com>
2014-05-16 03:49:28 +0600

    SUBUTAI-702 - Cover the Pig plugin with unit test - Destroy operation
    Former-commit-id: 30bbade9d6c7178ef0f9f6f31b1a567d750cd424

--- management/server/products/pig/pig-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/pig/handler/DestroyNodeOperationHandler.java
+++ management/server/products/pig/pig-impl/src/main/java/org/safehaus/kiskis/mgmt/impl/pig/handler/DestroyNodeOperationHandler.java
@@ -1,88 +1,88 @@
 package org.safehaus.kiskis.mgmt.impl.pig.handler;
 
+
 import org.safehaus.kiskis.mgmt.api.commandrunner.AgentResult;
 import org.safehaus.kiskis.mgmt.api.commandrunner.Command;
 import org.safehaus.kiskis.mgmt.api.pig.Config;
 import org.safehaus.kiskis.mgmt.shared.operation.ProductOperation;
-import org.safehaus.kiskis.mgmt.impl.pig.Commands;
 import org.safehaus.kiskis.mgmt.impl.pig.PigImpl;
 import org.safehaus.kiskis.mgmt.shared.operation.AbstractOperationHandler;
 import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
 import org.safehaus.kiskis.mgmt.shared.protocol.Util;
 
-import java.util.UUID;
 
-/**
- * Created by dilshat on 5/6/14.
- */
 public class DestroyNodeOperationHandler extends AbstractOperationHandler<PigImpl> {
     private final String lxcHostname;
-    private final ProductOperation po;
 
-    public DestroyNodeOperationHandler(PigImpl manager, String clusterName, String lxcHostname) {
-        super(manager, clusterName);
+
+    public DestroyNodeOperationHandler( PigImpl manager, String clusterName, String lxcHostname ) {
+        super( manager, clusterName );
         this.lxcHostname = lxcHostname;
-        po = manager.getTracker().createProductOperation(Config.PRODUCT_KEY,
-                String.format("Destroying %s in %s", lxcHostname, clusterName));
+        productOperation = manager.getTracker().createProductOperation( Config.PRODUCT_KEY,
+                String.format( "Destroying %s in %s", lxcHostname, clusterName ) );
     }
 
-    @Override
-    public UUID getTrackerId() {
-        return po.getId();
-    }
 
     @Override
     public void run() {
-        Config config = manager.getCluster(clusterName);
-        if (config == null) {
-            po.addLogFailed(String.format("Cluster with name %s does not exist\nOperation aborted", clusterName));
+        Config config = manager.getCluster( clusterName );
+        if ( config == null ) {
+            productOperation.addLogFailed(
+                    String.format( "Cluster with name %s does not exist\nOperation aborted", clusterName ) );
             return;
         }
 
-        Agent agent = manager.getAgentManager().getAgentByHostname(lxcHostname);
-        if (agent == null) {
-            po.addLogFailed(String.format("Agent with hostname %s is not connected\nOperation aborted", lxcHostname));
+        Agent agent = manager.getAgentManager().getAgentByHostname( lxcHostname );
+        if ( agent == null ) {
+            productOperation.addLogFailed(
+                    String.format( "Agent with hostname %s is not connected\nOperation aborted", lxcHostname ) );
             return;
         }
 
-        if (!config.getNodes().contains(agent)) {
-            po.addLogFailed(String.format("Agent with hostname %s does not belong to cluster %s", lxcHostname, clusterName));
+        if ( !config.getNodes().contains( agent ) ) {
+            productOperation.addLogFailed(
+                    String.format( "Agent with hostname %s does not belong to cluster %s", lxcHostname, clusterName ) );
             return;
         }
 
-        if (config.getNodes().size() == 1) {
-            po.addLogFailed("This is the last node in the cluster. Please, destroy cluster instead\nOperation aborted");
+        if ( config.getNodes().size() == 1 ) {
+            productOperation.addLogFailed(
+                    "This is the last node in the cluster. Please, destroy cluster instead\nOperation aborted" );
             return;
         }
-        po.addLog("Uninstalling Pig...");
-        Command uninstallCommand = manager.getCommands().getUninstallCommand(Util.wrapAgentToSet(agent));
-        manager.getCommandRunner().runCommand(uninstallCommand);
 
-        if (uninstallCommand.hasCompleted()) {
-            AgentResult result = uninstallCommand.getResults().get(agent.getUuid());
-            if (result.getExitCode() != null && result.getExitCode() == 0) {
-                if (result.getStdOut().contains("Package ksks-pig is not installed, so not removed")) {
-                    po.addLog(String.format("Pig is not installed, so not removed on node %s",
-                            agent.getHostname()));
-                } else {
-                    po.addLog(String.format("Pig is removed from node %s",
-                            agent.getHostname()));
+        productOperation.addLog( "Uninstalling Pig..." );
+        Command uninstallCommand = manager.getCommands().getUninstallCommand( Util.wrapAgentToSet( agent ) );
+        manager.getCommandRunner().runCommand( uninstallCommand );
+
+        if ( uninstallCommand.hasCompleted() ) {
+            AgentResult result = uninstallCommand.getResults().get( agent.getUuid() );
+            if ( result.getExitCode() != null && result.getExitCode() == 0 ) {
+                if ( result.getStdOut().contains( "Package ksks-pig is not installed, so not removed" ) ) {
+                    productOperation.addLog(
+                            String.format( "Pig is not installed, so not removed on node %s", agent.getHostname() ) );
                 }
-            } else {
-                po.addLog(String.format("Error %s on node %s", result.getStdErr(),
-                        agent.getHostname()));
+                else {
+                    productOperation.addLog( String.format( "Pig is removed from node %s", agent.getHostname() ) );
+                }
+            }
+            else {
+                productOperation
+                        .addLog( String.format( "Error %s on node %s", result.getStdErr(), agent.getHostname() ) );
             }
 
-            config.getNodes().remove(agent);
-            po.addLog("Updating db...");
+            config.getNodes().remove( agent );
+            productOperation.addLog( "Updating db..." );
 
-            if (manager.getDbManager().saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
-                po.addLogDone("Cluster info update in DB\nDone");
-            } else {
-                po.addLogFailed("Error while updating cluster info in DB. Check logs.\nFailed");
+            if ( manager.getDbManager().saveInfo( Config.PRODUCT_KEY, config.getClusterName(), config ) ) {
+                productOperation.addLogDone( "Cluster info update in DB\nDone" );
             }
-        } else {
-            po.addLogFailed("Uninstallation failed, command timed out");
+            else {
+                productOperation.addLogFailed( "Error while updating cluster info in DB. Check logs.\nFailed" );
+            }
+        }
+        else {
+            productOperation.addLogFailed( "Uninstallation failed, command timed out" );
         }
     }
 }
--- management/server/products/pig/pig-impl/src/test/java/org/safehaus/kiskis/mgmt/impl/pig/handler/DestroyOperationHandlerTest.java
+++ management/server/products/pig/pig-impl/src/test/java/org/safehaus/kiskis/mgmt/impl/pig/handler/DestroyOperationHandlerTest.java
@@ -4,7 +4,6 @@ package org.safehaus.kiskis.mgmt.impl.pig.handler;
 import org.junit.Test;
 import org.safehaus.kiskis.mgmt.api.pig.Config;
 import org.safehaus.kiskis.mgmt.impl.pig.PigImpl;
-import org.safehaus.kiskis.mgmt.product.common.test.unit.mock.CommonMockBuilder;
 import org.safehaus.kiskis.mgmt.shared.operation.AbstractOperationHandler;
 import org.safehaus.kiskis.mgmt.shared.operation.ProductOperationState;
 
@@ -14,35 +13,27 @@ import static junit.framework.Assert.assertTrue;
 
 public class DestroyOperationHandlerTest {
 
-    @Test( expected = NullPointerException.class )
-    public void testWithNullConfig() {
-        new PigImplMock().installCluster( null );
-    }
-
 
     @Test
-    public void testWithMalformedConfiguration() {
-        AbstractOperationHandler operationHandler = new InstallOperationHandler( new PigImplMock(), new Config() );
+    public void testWithoutCluster() {
+        AbstractOperationHandler operationHandler = new DestroyNodeOperationHandler( new PigImplMock(), "test-cluster",
+                "lxc-host" );
 
         operationHandler.run();
 
-        assertTrue( operationHandler.getProductOperation().getLog().contains( "Malformed configuration" ) );
+        assertTrue( operationHandler.getProductOperation().getLog().contains( "not exist" ) );
         assertEquals( operationHandler.getProductOperation().getState(), ProductOperationState.FAILED );
     }
 
 
     @Test
     public void testWithExistingCluster() {
-        Config config = new Config().setClusterName( "test-cluster" );
-        config.getNodes().add( CommonMockBuilder.createAgent() );
-
         PigImpl pigImpl = new PigImplMock().setClusterConfig( new Config() );
-        AbstractOperationHandler operationHandler = new InstallOperationHandler( pigImpl, config );
+        AbstractOperationHandler operationHandler = new DestroyNodeOperationHandler( pigImpl, "test-cluster", "lxc-host" );
 
         operationHandler.run();
 
-        assertTrue( operationHandler.getProductOperation().getLog().contains( "test-cluster" ) );
-        assertTrue( operationHandler.getProductOperation().getLog().contains( "already exists" ) );
+        assertTrue( operationHandler.getProductOperation().getLog().contains( "not connected" ) );
         assertEquals( operationHandler.getProductOperation().getState(), ProductOperationState.FAILED );
     }
 
