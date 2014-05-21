ef64fac19168c3f951a88a5ec751fe4a6caedc94
Azilet Beishenaliev <aziletb@gmail.com>
2014-05-21 16:25:47 +0600

    add tests for status/start/stop actions in Hive impl
    Former-commit-id: cc26f2c2ac9a23ee8ad434978002fa201ee2468f

---
+++ management/server/products/hive/hive-impl/src/test/java/org/safehaus/kiskis/mgmt/impl/hive/handler/StatusHandlerTest.java
@@ -0,0 +1,41 @@
+package org.safehaus.kiskis.mgmt.impl.hive.handler;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.safehaus.kiskis.mgmt.api.hive.Config;
+import org.safehaus.kiskis.mgmt.impl.hive.mock.HiveImplMock;
+import org.safehaus.kiskis.mgmt.shared.operation.ProductOperation;
+import org.safehaus.kiskis.mgmt.shared.operation.ProductOperationState;
+
+public class StatusHandlerTest {
+
+    HiveImplMock mock;
+    AbstractHandler handler;
+
+    @Before
+    public void setUp() {
+        mock = new HiveImplMock();
+        handler = new StatusHandler(mock, "test-cluster", "test-host");
+    }
+
+    @Test
+    public void testWithoutCluster() {
+        handler.run();
+
+        ProductOperation po = handler.getProductOperation();
+        assertTrue(po.getLog().toLowerCase().contains("not exist"));
+        assertEquals(po.getState(), ProductOperationState.FAILED);
+    }
+
+    @Test
+    public void testFail() {
+        mock.setConfig(new Config());
+        handler.run();
+
+        ProductOperation po = handler.getProductOperation();
+        assertTrue(po.getLog().toLowerCase().contains("not connected"));
+        assertEquals(po.getState(), ProductOperationState.FAILED);
+    }
+
+}
