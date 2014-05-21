1bef9bb1b97f8a654c5a381e8211c4e6bf124ad3
Azilet Beishenaliev <aziletb@gmail.com>
2014-05-21 16:10:41 +0600

    add commands and abstract handler tests in Hive impl module
    Former-commit-id: ee67d2c24bf89f7f712f721c2f661a18ef7c3bf5

--- management/server/products/hive/hive-impl/src/test/java/org/safehaus/kiskis/mgmt/impl/hive/CommandsTest.java
+++ management/server/products/hive/hive-impl/src/test/java/org/safehaus/kiskis/mgmt/impl/hive/CommandsTest.java
@@ -1,50 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.safehaus.kiskis.mgmt.impl.hive;
 
-import java.util.Arrays;
-import java.util.UUID;
 import org.junit.*;
+import org.safehaus.kiskis.mgmt.product.common.test.unit.mock.CommonMockBuilder;
 import org.safehaus.kiskis.mgmt.shared.protocol.Agent;
 
-/**
- *
- * @author azilet
- */
 public class CommandsTest {
 
-    private String hiveServerIp = "127.0.0.1";
     private Agent hiveServer;
-
-    public CommandsTest() {
-    }
 
     @Before
     public void before() throws Exception {
-        hiveServer = new Agent(UUID.randomUUID(), "hostname", "parenthost",
-                "MAC-addr", Arrays.asList(hiveServerIp), true, "transportId");
-    }
-
-    @After
-    public void after() throws Exception {
+        hiveServer = CommonMockBuilder.createAgent();
     }
 
     @Test
     public void testMake() {
-        System.out.println("make");
         for(CommandType t : CommandType.values()) {
             for(Product p : Product.values()) {
-                Assert.assertNotNull(Commands.make(t, p));
+                String s = Commands.make(t, p);
+                Assert.assertNotNull(s);
+                if(t != CommandType.LIST)
+                    Assert.assertTrue(s.contains(t.toString().toLowerCase()));
             }
         }
     }
 
     @Test
     public void testConfigureHiveServer() {
-        System.out.println("configureHiveServer");
+        String hiveServerIp = hiveServer.getListIP().get(0);
         String s = Commands.configureHiveServer(hiveServerIp);
         Assert.assertNotNull(s);
         Assert.assertTrue(s.contains(hiveServerIp));
@@ -52,14 +35,12 @@ public class CommandsTest {
 
     @Test
     public void testConfigureClient() {
-        System.out.println("configureClient");
         String s = Commands.configureClient(hiveServer);
         Assert.assertNotNull(s);
     }
 
     @Test
     public void testAddHivePoperty() {
-        System.out.println("addHivePoperty");
         String s = Commands.addHivePoperty("add", "file.xml", "property", null);
         Assert.assertNotNull(s);
 
