d0cd677df3fed5282c5cc65282cd1454a092c06c
daralbaev <daralbaev@gmail.com>
2014-05-25 19:42:06 +0600

    renaming packages
    Former-commit-id: 398116b6f3916c733d3068e12f8f05dc8fc87996

---
+++ management/server/products/oozie/oozie-ui/src/main/java/org/safehaus/subutai/ui/oozie/wizard/VerificationStep.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.subutai.ui.oozie.wizard;
+
+import com.vaadin.ui.*;
+import org.safehaus.subutai.api.oozie.Config;
+import org.safehaus.subutai.server.ui.MgmtApplication;
+import org.safehaus.subutai.shared.protocol.Agent;
+import org.safehaus.subutai.ui.oozie.OozieUI;
+
+import java.util.UUID;
+
+/**
+ * @author dilshat
+ */
+public class VerificationStep extends Panel {
+
+    public VerificationStep(final Wizard wizard) {
+
+        setSizeFull();
+
+        GridLayout grid = new GridLayout(1, 5);
+        grid.setSpacing(true);
+        grid.setMargin(true);
+        grid.setSizeFull();
+
+        Label confirmationLbl = new Label("<strong>Please verify the installation settings "
+                + "(you may change them by clicking on Back button)</strong><br/>");
+        confirmationLbl.setContentMode(Label.CONTENT_XHTML);
+
+        ConfigView cfgView = new ConfigView("Installation configuration");
+        cfgView.addStringCfg("Cluster Name", wizard.getConfig().getClusterName());
+        cfgView.addStringCfg("Server", wizard.getConfig().getServer().getHostname() + "\n");
+        for (Agent agent : wizard.getConfig().getClients()) {
+            cfgView.addStringCfg("Clients", agent.getHostname() + "\n");
+        }
+
+        Button install = new Button("Install");
+        install.addListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+
+                UUID trackID = OozieUI.getOozieManager().installCluster(wizard.getConfig());
+                MgmtApplication.showProgressWindow(Config.PRODUCT_KEY, trackID, null);
+                wizard.init();
+            }
+        });
+
+        Button back = new Button("Back");
+        back.addListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                wizard.back();
+            }
+        });
+
+        HorizontalLayout buttons = new HorizontalLayout();
+        buttons.addComponent(back);
+        buttons.addComponent(install);
+
+        grid.addComponent(confirmationLbl, 0, 0);
+
+        grid.addComponent(cfgView.getCfgTable(), 0, 1, 0, 3);
+
+        grid.addComponent(buttons, 0, 4);
+
+        addComponent(grid);
+
+    }
+
+}
