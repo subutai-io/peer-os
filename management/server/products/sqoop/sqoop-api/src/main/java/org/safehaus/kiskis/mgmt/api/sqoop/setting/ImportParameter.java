0793ef177ea7d22a4a3e7bdad8b3809862ba0550
daralbaev <daralbaev@gmail.com>
2014-05-25 19:55:01 +0600

    renaming packages
    Former-commit-id: 95fe66516562d29b075c9b30771db92004575a3c

--- management/server/products/sqoop/sqoop-api/src/main/java/org/safehaus/kiskis/mgmt/api/sqoop/setting/ImportParameter.java
+++
@@ -1,9 +0,0 @@
-package org.safehaus.kiskis.mgmt.api.sqoop.setting;
-
-public enum ImportParameter {
-
-    IMPORT_ALL_TABLES,
-    DATASOURCE_TABLE_NAME,
-    DATASOURCE_COLUMN_FAMILY,
-    DATASOURCE_DATABASE;
-}
