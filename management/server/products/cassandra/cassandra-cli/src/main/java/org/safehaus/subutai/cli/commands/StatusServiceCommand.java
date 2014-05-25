3ff1d51a07b3f7900b98fa2427e09f5f0e56caa8
daralbaev <daralbaev@gmail.com>
2014-05-25 19:18:56 +0600

    renaming packages
    Former-commit-id: 4dead19b658fd39c4bee5325cac950f98ede609e

---
+++ management/server/products/cassandra/cassandra-cli/src/main/java/org/safehaus/subutai/cli/commands/StatusServiceCommand.java
@@ -0,0 +1,51 @@
+package org.safehaus.subutai.cli.commands;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.subutai.api.cassandra.Cassandra;
+import org.safehaus.subutai.api.cassandra.Config;
+import org.safehaus.subutai.api.tracker.Tracker;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+/**
+ * Displays the last log entries
+ */
+@Command(scope = "cassandra", name = "service-cassandra-status", description = "Command to check Cassandra service")
+public class StatusServiceCommand extends OsgiCommandSupport {
+
+    private static Cassandra cassandraManager;
+    private static Tracker tracker;
+
+    public Tracker getTracker() {
+        return tracker;
+    }
+
+    public void setTracker(Tracker tracker) {
+        StatusServiceCommand.tracker = tracker;
+    }
+
+    public void setCassandraManager(Cassandra cassandraManager) {
+        StatusServiceCommand.cassandraManager = cassandraManager;
+    }
+
+    public static Cassandra getCassandraManager() {
+        return cassandraManager;
+    }
+
+
+    @Argument(index = 0, name = "agentUUID", description = "UUID of the agent.", required = true, multiValued = false)
+    String agentUUID = null;
+
+
+    protected Object doExecute() throws IOException {
+
+        UUID uuid = cassandraManager.statusCassandraService(agentUUID);
+        tracker.printOperationLog(Config.PRODUCT_KEY, uuid, 30000);
+
+        return null;
+    }
+}
