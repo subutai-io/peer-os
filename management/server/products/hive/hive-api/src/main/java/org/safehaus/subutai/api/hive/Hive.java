3406165b6e84eac85269aaa5ca1861fd00958f3f
daralbaev <daralbaev@gmail.com>
2014-05-25 19:30:20 +0600

    renaming packages
    Former-commit-id: e451fc9fee37292306421ade4b7ff41ce60f8f04

---
+++ management/server/products/hive/hive-api/src/main/java/org/safehaus/subutai/api/hive/Hive.java
@@ -0,0 +1,20 @@
+package org.safehaus.subutai.api.hive;
+
+import org.safehaus.subutai.shared.protocol.ApiBase;
+
+import java.util.UUID;
+
+public interface Hive extends ApiBase<Config> {
+
+    public UUID statusCheck(String clusterName, String hostname);
+
+    public UUID startNode(String clusterName, String hostname);
+
+    public UUID stopNode(String clusterName, String hostname);
+
+    public UUID restartNode(String clusterName, String hostname);
+
+    public UUID addNode(String clusterName, String hostname);
+
+    public UUID destroyNode(String clusterName, String hostname);
+}
