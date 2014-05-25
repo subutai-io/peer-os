80e7e30c0b44825b7ca7012292bfb105c4b816f1
daralbaev <daralbaev@gmail.com>
2014-05-25 19:44:29 +0600

    renaming packages
    Former-commit-id: db01812626cf684ae6286f83366653b4f5ab3534

---
+++ management/server/products/pig/pig-impl/src/main/java/org/safehaus/subutai/impl/pig/handler/UninstallOperationHandler.java
@@ -0,0 +1,76 @@
+package org.safehaus.subutai.impl.pig.handler;
+
+
+import org.safehaus.subutai.api.commandrunner.AgentResult;
+import org.safehaus.subutai.api.commandrunner.Command;
+import org.safehaus.subutai.api.pig.Config;
+import org.safehaus.subutai.impl.pig.PigImpl;
+import org.safehaus.subutai.shared.operation.AbstractOperationHandler;
+import org.safehaus.subutai.shared.protocol.Agent;
+
+
+public class UninstallOperationHandler extends AbstractOperationHandler<PigImpl> {
+
+
+    public UninstallOperationHandler( PigImpl manager, String clusterName ) {
+        super( manager, clusterName );
+        productOperation = manager.getTracker().createProductOperation( Config.PRODUCT_KEY,
+                String.format( "Destroying cluster %s", clusterName ) );
+    }
+
+
+    @Override
+    public void run() {
+        Config config = manager.getCluster( clusterName );
+        if ( config == null ) {
+            productOperation.addLogFailed(
+                    String.format( "Cluster with name %s does not exist\nOperation aborted", clusterName ) );
+            return;
+        }
+
+        for ( Agent node : config.getNodes() ) {
+            if ( manager.getAgentManager().getAgentByHostname( node.getHostname() ) == null ) {
+                productOperation.addLogFailed(
+                        String.format( "Node %s is not connected\nOperation aborted", node.getHostname() ) );
+                return;
+            }
+        }
+
+        productOperation.addLog( "Uninstalling Pig..." );
+        Command uninstallCommand = manager.getCommands().getUninstallCommand( config.getNodes() );
+        manager.getCommandRunner().runCommand( uninstallCommand );
+
+        if ( uninstallCommand.hasCompleted() ) {
+            for ( AgentResult result : uninstallCommand.getResults().values() ) {
+                Agent agent = manager.getAgentManager().getAgentByUUID( result.getAgentUUID() );
+
+                if ( result.getExitCode() != null && result.getExitCode() == 0 ) {
+                    if ( result.getStdOut().contains( "Package ksks-pig is not installed, so not removed" ) ) {
+                        productOperation.addLog( String.format( "Pig is not installed, so not removed on node %s",
+                                agent == null ? result.getAgentUUID() : agent.getHostname() ) );
+                    }
+                    else {
+                        productOperation.addLog( String.format( "Pig is removed from node %s",
+                                agent == null ? result.getAgentUUID() : agent.getHostname() ) );
+                    }
+                }
+                else {
+                    productOperation.addLog( String.format( "Error %s on node %s", result.getStdErr(),
+                            agent == null ? result.getAgentUUID() : agent.getHostname() ) );
+                }
+            }
+
+            productOperation.addLog( "Updating db..." );
+
+            if ( manager.getDbManager().deleteInfo( Config.PRODUCT_KEY, config.getClusterName() ) ) {
+                productOperation.addLogDone( "Cluster info deleted from DB\nDone" );
+            }
+            else {
+                productOperation.addLogFailed( "Error while deleting cluster info from DB. Check logs.\nFailed" );
+            }
+        }
+        else {
+            productOperation.addLogFailed( "Uninstallation failed, command timed out" );
+        }
+    }
+}
---
+++ management/server/products/pig/pig-impl/src/test/java/org/safehaus/subutai/impl/pig/handler/UninstallOperationHandlerTest.java
@@ -0,0 +1,39 @@
+package org.safehaus.subutai.impl.pig.handler;
+
+
+import org.junit.Test;
+import org.safehaus.subutai.api.pig.Config;
+import org.safehaus.subutai.impl.pig.PigImpl;
+import org.safehaus.subutai.impl.pig.handler.mock.PigImplMock;
+import org.safehaus.subutai.shared.operation.AbstractOperationHandler;
+import org.safehaus.subutai.shared.operation.ProductOperationState;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+
+public class UninstallOperationHandlerTest {
+
+
+    @Test
+    public void testWithoutCluster() {
+        AbstractOperationHandler operationHandler = new UninstallOperationHandler( new PigImplMock(), "test-cluster" );
+
+        operationHandler.run();
+
+        assertTrue( operationHandler.getProductOperation().getLog().contains( "not exist" ) );
+        assertEquals( operationHandler.getProductOperation().getState(), ProductOperationState.FAILED );
+    }
+
+
+    @Test
+    public void testWithExistingCluster() {
+        PigImpl pigImpl = new PigImplMock().setClusterConfig( new Config() );
+        AbstractOperationHandler operationHandler = new UninstallOperationHandler( pigImpl, "test-cluster" );
+
+        operationHandler.run();
+
+        assertTrue( operationHandler.getProductOperation().getLog().contains( "Uninstallation failed" ) );
+        assertEquals( operationHandler.getProductOperation().getState(), ProductOperationState.FAILED );
+    }
+}
