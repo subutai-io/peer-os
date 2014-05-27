ae59c13afb3e46fa11fa3fe4ade26fa09055cede
Azilet Beishenaliev <aziletb@gmail.com>
2014-05-27 18:22:45 +0600

    add initial CLI files for Package Manager
    Former-commit-id: 7289ea3d0820d6a0155419551859189ddbf783f1

---
+++ management/server/core/package-manager/package-manager-cli/src/main/java/org/safehaus/subutai/cli/commands/ListPackages.java
@@ -0,0 +1,37 @@
+package org.safehaus.subutai.cli.commands;
+
+import java.util.Collection;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.subutai.api.packagemanager.PackageInfo;
+import org.safehaus.subutai.api.packagemanager.PackageManager;
+
+@Command(scope = "package", name = "list", description = "list packages")
+public class ListPackages extends OsgiCommandSupport {
+
+    private PackageManager packageManager;
+    @Argument(index = 0, required = true)
+    private String hostname;
+
+    public PackageManager getPackageManager() {
+        return packageManager;
+    }
+
+    public void setPackageManager(PackageManager packageManager) {
+        this.packageManager = packageManager;
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        if(hostname != null) {
+            Collection<PackageInfo> ls = packageManager.listPackages(hostname);
+            for(PackageInfo pi : ls) {
+                System.out.println(pi);
+            }
+        } else System.out.println("Target host is not defined");
+
+        return null;
+    }
+
+}
