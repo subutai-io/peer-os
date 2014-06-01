3405cb8e89b808b4f8918bb33a1a1eaee48f59c4
daralbaev <daralbaev@gmail.com>
2014-06-02 03:42:03 +0600

    Styling main application
    Former-commit-id: c7d43cba900a5b732bfb406450dbe9a26b6aa2c5

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
@@ -1,11 +1,11 @@
 /**
  * DISCLAIMER
- * 
+ *
  * The quality of the code is such that you should not copy any of it as best
  * practice how to build Vaadin applications.
- * 
+ *
  * @author jouni@vaadin.com
- * 
+ *
  */
 
 package org.safehaus.subutai.server.ui.views;
@@ -16,16 +16,26 @@ import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.safehaus.subutai.server.ui.MainUI;
+import org.safehaus.subutai.server.ui.api.PortalModule;
+import org.safehaus.subutai.server.ui.api.PortalModuleListener;
+import org.safehaus.subutai.server.ui.api.PortalModuleService;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
-public class ModulesView extends VerticalLayout implements View {
+public class ModulesView extends VerticalLayout implements View, PortalModuleListener {
 
     private static final Logger LOG = Logger.getLogger(MainUI.class.getName());
     private TabSheet editors;
+    private HorizontalLayout modulesLayout;
+    private List<VerticalLayout> modulesList = new ArrayList<VerticalLayout>();
 
     @Override
     public void enter(ViewChangeEvent event) {
@@ -33,6 +43,7 @@ public class ModulesView extends VerticalLayout implements View {
         addStyleName("reports");
 
         addComponent(buildDraftsView());
+        getPortalModuleService().addListener(this);
     }
 
     private Component buildDraftsView() {
@@ -59,24 +70,13 @@ public class ModulesView extends VerticalLayout implements View {
         titleAndDrafts.addComponent(draftsTitle);
         titleAndDrafts.setComponentAlignment(draftsTitle, Alignment.TOP_CENTER);
 
-        HorizontalLayout drafts = new HorizontalLayout();
-        drafts.setSpacing(true);
-        titleAndDrafts.addComponent(drafts);
+        modulesLayout = new HorizontalLayout();
+        modulesLayout.setSpacing(true);
+        titleAndDrafts.addComponent(modulesLayout);
 
-        VerticalLayout createBox = new VerticalLayout();
-        createBox.setWidth(null);
-        createBox.addStyleName("create");
-        Button create = new Button("Create New");
-        create.addStyleName("default");
-        createBox.addComponent(create);
-        createBox.setComponentAlignment(create, Alignment.MIDDLE_CENTER);
-        drafts.addComponent(createBox);
-        create.addClickListener(new ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                autoCreate(0, null, null);
-            }
-        });
+        for (PortalModule module : getPortalModuleService().getModules()) {
+            addModule(module);
+        }
 
         return editors;
     }
@@ -97,7 +97,59 @@ public class ModulesView extends VerticalLayout implements View {
         editors.addTab(createEditorInstance(which, items, table)).setClosable(
                 true);
         editors.setSelectedTab(editors.getComponentCount() - 1);
-    };
+    }
 
+    ;
+
+    @Override
+    public void moduleRegistered(PortalModule module) {
+        addModule(module);
+    }
+
+    private void addModule(PortalModule module) {
+        VerticalLayout moduleLayout = new VerticalLayout();
+        moduleLayout.setId(module.getId());
+        moduleLayout.setWidth(null);
+        moduleLayout.addStyleName("create");
+        Button create = new Button(module.getName());
+        create.addStyleName("default");
+        moduleLayout.addComponent(create);
+        moduleLayout.setComponentAlignment(create, Alignment.MIDDLE_CENTER);
+
+        modulesLayout.addComponent(moduleLayout);
+        create.addClickListener(new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                autoCreate(0, null, null);
+            }
+        });
+
+        modulesList.add(moduleLayout);
+    }
+
+    @Override
+    public void moduleUnregistered(PortalModule module) {
+
+        for (Component component : modulesList) {
+            if (component.getId().equals(component.getId())) {
+                modulesLayout.removeComponent(component);
+                modulesList.remove(component);
+                break;
+            }
+        }
+    }
+
+    public static PortalModuleService getPortalModuleService() {
+        // get bundle instance via the OSGi Framework Util class
+        BundleContext ctx = FrameworkUtil.getBundle(PortalModuleService.class).getBundleContext();
+        if (ctx != null) {
+            ServiceReference serviceReference = ctx.getServiceReference(PortalModuleService.class.getName());
+            if (serviceReference != null) {
+                return PortalModuleService.class.cast(ctx.getService(serviceReference));
+            }
+        }
+
+        return null;
+    }
 }
 
