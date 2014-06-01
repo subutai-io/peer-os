b91b5443221b4e6149ed5668633c54af191ebdc0
daralbaev <daralbaev@gmail.com>
2014-06-02 02:55:47 +0600

    Styling main application
    Former-commit-id: a05a6c2b631cdee5203dcd242b8e72fda5283505

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
@@ -10,14 +10,9 @@
 
 package org.safehaus.subutai.server.ui.views;
 
-import com.vaadin.event.LayoutEvents.LayoutClickEvent;
-import com.vaadin.event.LayoutEvents.LayoutClickListener;
 import com.vaadin.event.Transferable;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.ThemeResource;
-import com.vaadin.shared.MouseEventDetails.MouseButton;
-import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
@@ -46,82 +41,20 @@ public class ModulesView extends VerticalLayout implements View {
         editors.addStyleName("borderless");
         editors.addStyleName("editors");
 
-        /*editors.setCloseHandler(new TabSheet.CloseHandler() {
-            @Override
-            public void onTabClose(TabSheet tabsheet, final Component tabContent) {
-
-            }
-        });*/
-
         final VerticalLayout center = new VerticalLayout();
         center.setSizeFull();
-        center.setCaption("All Drafts");
+        center.setCaption("Modules");
         editors.addComponent(center);
-
-        VerticalLayout titleAndDrafts = new VerticalLayout();
-        titleAndDrafts.setSizeUndefined();
-        titleAndDrafts.setSpacing(true);
-        titleAndDrafts.addStyleName("drafts");
-        center.addComponent(titleAndDrafts);
-        center.setComponentAlignment(titleAndDrafts, Alignment.MIDDLE_CENTER);
-
-        Label draftsTitle = new Label("Drafts");
-        draftsTitle.addStyleName("h1");
-        draftsTitle.setSizeUndefined();
-        titleAndDrafts.addComponent(draftsTitle);
-        titleAndDrafts.setComponentAlignment(draftsTitle, Alignment.TOP_CENTER);
-
-        HorizontalLayout drafts = new HorizontalLayout();
-        drafts.setSpacing(true);
-        titleAndDrafts.addComponent(drafts);
-
-        CssLayout draftThumb = new CssLayout();
-        draftThumb.addStyleName("draft-thumb");
-        Image draft = new Image(null, new ThemeResource(
-                "img/draft-report-thumb.png"));
-        draftThumb.addComponent(draft);
-        Label draftTitle = new Label(
-                "Monthly revenue<br><span>Last modified 1 day ago</span>",
-                ContentMode.HTML);
-        draftTitle.setSizeUndefined();
-        draftThumb.addComponent(draftTitle);
-        drafts.addComponent(draftThumb);
-        // TODO layout bug, we need to set the alignment also for the first
-        // child
-        drafts.setComponentAlignment(draftThumb, Alignment.MIDDLE_CENTER);
-
-        final Button delete = new Button("×");
-        delete.setPrimaryStyleName("delete-button");
-        draftThumb.addComponent(delete);
-        delete.addClickListener(new ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                Notification.show("Not implemented in this demo");
-            }
-        });
-
-        draftThumb.addLayoutClickListener(new LayoutClickListener() {
-            @Override
-            public void layoutClick(LayoutClickEvent event) {
-                if (event.getButton() == MouseButton.LEFT
-                        && event.getChildComponent() != delete) {
-                    editors.addTab(createEditorInstance(1, null, null))
-                            .setClosable(true);
-                    editors.setSelectedTab(editors.getComponentCount() - 1);
-                }
-            }
-        });
-        draft.setDescription("Click to edit");
-        delete.setDescription("Delete draft");
 
         VerticalLayout createBox = new VerticalLayout();
         createBox.setWidth(null);
         createBox.addStyleName("create");
+
         Button create = new Button("Create New");
         create.addStyleName("default");
         createBox.addComponent(create);
         createBox.setComponentAlignment(create, Alignment.MIDDLE_CENTER);
-        drafts.addComponent(createBox);
+
         create.addClickListener(new ClickListener() {
             @Override
             public void buttonClick(ClickEvent event) {
