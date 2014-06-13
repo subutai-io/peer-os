b52c6e338de62699598238c26ade92b740541930
Emin Inal <emininal@gmail.com>
2014-06-13 09:55:24 +0300

    added MQTT agent v1.0.6 code
    Former-commit-id: 88a9cfffd939df95601a518fd3ae93440722b4a3

--- agent/KiskisAgent/src/KAConnection.cpp
+++ agent/KiskisAgent/src/KAConnection.cpp
@@ -18,18 +18,79 @@
 /**
  *  \details   Default constructor of KAConnection class.
  */
-KAConnection::KAConnection(string brokerURI, string serverURI, string clientURI)
+KAConnection::KAConnection(const char * id, const char * subscribedTopic, const char * publishedTopic
+		, const char * broadcastTopic, const char * host, int port) : mosqpp::mosquittopp(id)
 {
-	this->brokerURI=brokerURI;
-	this->connection=NULL;
-	this->session=NULL;
-	this->sender=NULL;
-	this->serverURI=serverURI;
-	this->senderdestination=NULL;
+	this->keepalive = 60;
+	this->id = id;
+	this->port = port;
+	this->host = host;
+	this->subscribedTopic = subscribedTopic;
+	this->broadcastTopic = broadcastTopic;
+	this->publishedTopic = publishedTopic;
+	this->reveivedMessage=false;
+	this->connectionStatus=false;
+	this->bufferSize = 10000;
+	this->certpath = "/etc/ksks-agent/config/";
+};
 
-	this->receiver=NULL;
-	this->clientURI=clientURI;
-	this->receiverdestination=NULL;
+/**
+ *  \details   Callback method for getting passwords for client key.
+ */
+static int password_callback(char* buf, int size, int rwflag, void* userdata)
+{
+	pugi::xml_document doc;
+	if(doc.load_file("/etc/ksks-agent/config/settings.xml").status)		//if the settings file does not exist
+	{
+		return 100;
+		exit(1);
+	}
+	string clientpasswd = doc.child("Settings").child_value("clientpasswd") ;		//reading cleintpassword
+	strncpy(buf,clientpasswd.c_str(),size);
+	buf[size-1] = '\0';
+	return strlen(buf);
+}
+
+/**
+ *  \details   openSession method is one of the most important function of the KAConnection class.
+ *  		   It tries to open a connection to MQTT Broker.
+ *  		   if this operation is successfully done it returns true. Otherwise it returns false.
+ */
+bool KAConnection::openSession()
+{
+	tls_opts_set(1, "tlsv1.1", NULL);
+	string ca = certpath + "ca.crt";
+	string clientCrt = certpath + "client.crt";
+	string clientKey = certpath + "client.key";
+	int sslresult = tls_set(ca.c_str(),NULL,clientCrt.c_str(),clientKey.c_str(),password_callback);
+	int result = connect(this->host, this->port, this->keepalive);
+	if(result == MOSQ_ERR_SUCCESS && sslresult == MOSQ_ERR_SUCCESS)
+	{
+		subscribe(NULL,this->subscribedTopic,2); //subscribed to agent own Topic.
+		subscribe(NULL,this->broadcastTopic,2); //subscribed to broadcastTopic.
+		this->connectionStatus=true;
+		return true;
+		//	if(result == MOSQ_ERR_SUCCESS)
+	}
+	else
+		return false;
+}
+
+/**
+ *  \details   This method is tries to reconnect to MQTT Broker.
+ *  		   if this operation is successfully done it returns true. Otherwise it returns false.
+ */
+bool KAConnection::reConnect()
+{
+	int result;
+	result = reconnect();
+	if(result == MOSQ_ERR_SUCCESS)
+	{
+		cout << "Successfully reconnected to server.." << endl;
+		return true;
+	}
+	else
+		return false;
 }
 
 /**
@@ -37,323 +98,116 @@ KAConnection::KAConnection(string brokerURI, string serverURI, string clientURI)
  */
 KAConnection::~KAConnection()
 {
-	//closeConnection();								//destroy all pointers
+	loop_stop();              // Kill the thread
+	mosqpp::lib_cleanup();    // Mosquitto library cleanup
 }
 
 /**
- *  \details   openSession method is one of the most important function of the KAConnection class.
- *  		   It tries to open a connection to ActiveMQ Broker.
- *  		   if this operation is successfully done it returns true. Otherwise it returns false.
+ *  \details   This method sends the given strings to MQTT Broker.
  */
-bool KAConnection::openSession()
-{							//opening a TCP session on Broker
-	try
+bool KAConnection::sendMessage(string message)
+{
+	const  char * _message = message.c_str();
+	int ret = publish(NULL,this->publishedTopic,strlen(_message),_message,2,true);
+	return ( ret == MOSQ_ERR_SUCCESS );
+}
+
+/**
+ *  \details   This method informs the user about the disconnecting from MQTT Broker.
+ */
+void KAConnection::on_disconnect(int rc)
+{
+	std::cout << " KAConnection - disconnection(" << rc << ")" << std::endl;
+	this->connectionStatus=false;
+}
+
+/**
+ *  \details   This method informs the user about the connecting to MQTT Broker.
+ */
+void KAConnection::on_connect(int rc)
+{
+	if ( rc == 0 )
 	{
-		ActiveMQConnectionFactory* connectionFactory =  new ActiveMQConnectionFactory( brokerURI ); 	//Create a ConnectionFactory
-		if(connectionFactory != NULL)
+		cout << " KAConnection - connected with server" << endl;
+		if(connectionStatus==false)
 		{
-			connection = connectionFactory->createConnection();						// Creating Connection
-			connection->start();													// Starting Connection
-			connection->setExceptionListener(this);
-			session = connection->createSession( Session::AUTO_ACKNOWLEDGE );		// Create a Session
-
-			senderdestination = session->createQueue( serverURI );
-			sender = session->createProducer(senderdestination);
-			sender->setDeliveryMode( DeliveryMode::NON_PERSISTENT );
-
-			receiverdestination = session->createQueue( clientURI );
-			receiver = session->createConsumer(receiverdestination);
+			subscribe(NULL,this->subscribedTopic,2); // resubscribe to agent own Topic.
+			subscribe(NULL,this->broadcastTopic,2); //resubscribe to broadcastTopic.
 		}
-		else
-		{
-			return false;
-		}
-		return true;
-	}
-	catch ( CMSException& error )
-	{
-		error.printStackTrace();
-		cout << error.what()<< endl;
-	}
-	return false;
-}
-
-/**
- *  \details   This method sends the given strings to ActiveMQ Broker.
- */
-void KAConnection::sendMessage(string mymessage)
-{				//sending a message to queue
-	try
-	{
-		sendmessage = session->createTextMessage(mymessage);
-		sender->send(sendmessage);
-		delete sendmessage;
-	}
-	catch(const std::exception& error)
-	{
-		cout << error.what()<< endl;;
-	}
-}
-
-/**
- *  \details   fetchMessage method is one of the most important function of the KAConnection class.
- *  		   It tries to fetch a message from ActiveMQ Broker in blocking mode.
- *  		   It reads the message from SERVICE_QUEUE sequentially
- *  		   if new message comes, it returns true and returning reference textmessage which has a real message.
- *  		   otherwise it just return false.
- */
-bool KAConnection::fetchMessage(string& textmessage)
-{		//fetch a message from queue with no wait
-	Message* message = receiver->receiveNoWait();				//return true if the message exist on queue
-	if(message!=NULL)
-	{											//return false if the messsage does not exist on queue
-		receivedmessage = dynamic_cast< TextMessage* >(message);
-		textmessage =  receivedmessage->getText();
-		delete receivedmessage;
-		return true;
+		connectionStatus==true;
 	}
 	else
 	{
-		return false;
+		cout << " KAConnection - Impossible to connect with server(" << rc << ")" << endl;
 	}
 }
 
 /**
- *  \details   setting "senderdestination" private variable of the KAConnection class.
+ *  \details   This method informs the user about the published message to MQTT Broker.
  */
-void KAConnection::setSenderDestination(Destination* mydestination)
-{	//setting sender pointer
-	senderdestination = mydestination;
-}
-
-/**
- *  \details   getting "senderdestination" private variable of the KAConnection class.
- */
-Destination* KAConnection::getSenderDestination()
-{						//gettin sender pointer
-	return senderdestination;
-}
-
-/**
- *  \details   getting "sender" private variable of the KAConnection class.
- */
-MessageProducer* KAConnection::getSender()
-{						//getting sender pointer
-	return sender;
-}
-
-/**
- *  \details   setting "sender" private variable of the KAConnection class.
- */
-void KAConnection::setSender(MessageProducer* mysender)
-{		//setting sender pointer
-	sender = mysender;
-}
-
-/**
- *  \details   getting "sendmessage" private variable of the KAConnection class.
- */
-TextMessage* KAConnection::getSendMessage()
-{						//getting sendmessage pointer
-	return sendmessage;
-}
-
-/**
- *  \details   setting "sendmessage" private variable of the KAConnection class.
- */
-void KAConnection::setSendMessage(TextMessage* mymessage)
-{			//setting sendmessage pointer
-	this->sendmessage=mymessage;
-}
-
-/**
- *  \details   getting "session" private variable of the KAConnection class.
- */
-Session* KAConnection::getSession()
-{					//getting session pointer
-	return session;
-}
-
-/**
- *  \details   setting "session" private variable of the KAConnection class.
- */
-void KAConnection::setSesion(Session* mysession)
-{ 		//setting session pointer
-	session = mysession;
-}
-
-/**
- *  \details   getting "brokerURI" private variable of the KAConnection class.
- */
-string KAConnection::getBrokerURI()
-{					//getting Broker URI
-	return brokerURI;
-}
-
-/**
- *  \details   setting "brokerURI" private variable of the KAConnection class.
- */
-void KAConnection::setBrokerURI(string mybrokerURI)
-{	//setting Broker URI
-	brokerURI=mybrokerURI;
-}
-
-/**
- *  \details   getting "connection" private variable of the KAConnection class.
- */
-Connection* KAConnection::getConnection()
-{				//getting Connection pointer
-	return connection;
-}
-
-/**
- *  \details   setting "connection" private variable of the KAConnection class.
- */
-void KAConnection::setConnection(Connection* myconnection)
-{		//setting connection pointer
-	connection = myconnection;
-}
-
-/**
- *  \details   getting "receiver" private variable of the KAConnection class.
- */
-MessageConsumer * KAConnection::getReceiver()
-{						//getting receiver
-	return receiver;
-}
-
-/**
- *  \details   setting "receiver" private variable of the KAConnection class.
- */
-void KAConnection::setReciever(MessageConsumer* myreceiver)
-{			//setting receiver
-	receiver = myreceiver;
-}
-
-/**
- *  \details   getting "receivedmessage" private variable of the KAConnection class.
- */
-TextMessage* KAConnection::getReceivedMessage()
-{						//getting receivedmesssage pointer
-	return receivedmessage;
-}
-
-/**
- *  \details   setting "receivedmessage" private variable of the KAConnection class.
- */
-void KAConnection::setReceivedMessage(TextMessage* mymessage)
-{			//setting receivedmesssage pointer
-	this->receivedmessage=mymessage;
-}
-
-/**
- *  \details   setting "receiverdestination" private variable of the KAConnection class.
- */
-void KAConnection::setReceiverDestination(Destination* mydestination)
+void KAConnection::on_publish(int mid)
 {
-	receiverdestination = mydestination;
+	cout << " KAConnection - Message (" << mid << ") succeed to be published " << endl;
 }
 
 /**
- *  \details   getting "receiverdestination" private variable of the KAConnection class.
+ *  \details   This method informs the user about the published message to MQTT Broker.
  */
-Destination* KAConnection::getReceiverDestination()
+void KAConnection::on_subscribe(int mid)
 {
-	return receiverdestination;
+	cout << "Subscriptions succesfully Done!"  << endl;
 }
 
 /**
- *  \details   This method close the connections and session to the ActiveMQ Broker
- *   		   It also clear the all pointers in the called KAConnection instance.
+ *  \details   This method gets the message from buffer asynchronously and changes status of the received message flag.
  */
-void KAConnection::close()
-{			// Destroy all resources.
-	// Close open resources such as session and connection.
-	try
+void KAConnection::on_message(const struct mosquitto_message *message)
+{
+	char buf[this->bufferSize+1];
+
+	if(!strcmp(message->topic, this->subscribedTopic) || !strcmp(message->topic, this->broadcastTopic))
 	{
-		if( session != NULL ) session->close();
-		if( connection != NULL ) connection->close();
+		memset(buf, 0, (this->bufferSize+1)*sizeof(char));
+		/* Copy N-1 bytes to ensure always 0 terminated. */
+		memcpy(buf, message->payload, this->bufferSize*sizeof(char));
+
+		this->reveivedMessage = true; //setting message status true
+		setMessage((string)buf); //setting message from buffer
 	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	try
-	{
-		if( session != NULL )
-			delete session;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	session = NULL;
-	try
-	{
-		if( connection != NULL )
-			delete connection;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	connection = NULL;
-	try
-	{
-		if( sender != NULL )
-			delete sender;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	sender = NULL;
-	try
-	{
-		if( sendmessage != NULL )
-			delete sendmessage;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	sendmessage = NULL;
-	try{
-		if( senderdestination != NULL )
-			delete senderdestination;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	senderdestination = NULL;
-	try
-	{
-		if( receiver != NULL )
-			delete receiver;
-	}catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	receiver = NULL;
-	try
-	{
-		if( receivedmessage != NULL )
-			delete receivedmessage;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	receivedmessage = NULL;
-	try
-	{
-		if( receiverdestination != NULL )
-			delete receiverdestination;
-	}
-	catch ( CMSException& e )
-	{
-		e.printStackTrace();
-	}
-	receiverdestination = NULL;
+	else
+		cout << "Error!" << endl;
+}
+
+/**
+ *  \details   This method returns the status of the new message.
+ */
+bool KAConnection::checkMessageStatus()
+{
+	return reveivedMessage;
+}
+
+/**
+ *  \details   This method resets the status of the new message flag.
+ */
+void KAConnection::resetMessageStatus()
+{
+	this->reveivedMessage = false;
+}
+
+/**
+ *  \details   This method is one of the most important function of the KAConnection class.
+ *  		   It tries to return the message from MQTT Broker in none-blocking mode.
+ */
+string KAConnection::getMessage()
+{
+	return this->messsage;
+}
+
+/**
+ *  \details   setting "message" private variable of the KAConnection class.
+ *
+ */
+void KAConnection::setMessage(string message)
+{
+	this->messsage = message;
 }
