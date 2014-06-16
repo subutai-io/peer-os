cc4230046ecf3e3eacdb44386c097d0d3784d236
daralbaev <daralbaev@gmail.com>
2014-06-16 13:35:17 +0600

    -m SUBUTAI-873 Mahout UI
    Former-commit-id: c7c23fa39910c8ed2a8c0620d6dca442b29a51b9

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
@@ -10,11 +10,10 @@
 
 package org.safehaus.subutai.server.ui.views;
 
+import com.vaadin.event.LayoutEvents;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.*;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
@@ -88,20 +87,29 @@ public class ModulesView extends VerticalLayout implements View, PortalModuleLis
 
     private void addModule(final PortalModule module) {
         CssLayout moduleLayout = new CssLayout();
+        moduleLayout.setCaption(module.getName());
         moduleLayout.setId(module.getId());
         moduleLayout.setWidth(null);
         moduleLayout.addStyleName("create");
-        Button create = new Button(module.getName());
+
+        moduleLayout.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
+            @Override
+            public void layoutClick(LayoutEvents.LayoutClickEvent layoutClickEvent) {
+                autoCreate(module);
+            }
+        });
+
+        /*Button create = new Button(module.getName());
         create.addStyleName("default");
-        moduleLayout.addComponent(create);
+        moduleLayout.addComponent(create);*/
 
         modulesLayout.addComponent(moduleLayout);
-        create.addClickListener(new ClickListener() {
+        /*create.addClickListener(new ClickListener() {
             @Override
             public void buttonClick(ClickEvent event) {
                 autoCreate(module);
             }
-        });
+        });*/
 
         modulesList.add(moduleLayout);
     }
