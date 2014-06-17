1924e13e2e7b1c44121eebc14de6d479fbac8a70
daralbaev <daralbaev@gmail.com>
2014-06-17 18:33:24 +0600

    -m SUBUTAI-883 Zookeeper UI migration
    Former-commit-id: bdc757655c9a775823a2b4559ee780b93b935048

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
@@ -33,7 +33,7 @@ public class ModulesView extends VerticalLayout implements View, PortalModuleLis
     private static final Logger LOG = Logger.getLogger(MainUI.class.getName());
     private TabSheet editors;
     private CssLayout modulesLayout;
-    private List<Component> modulesList = new ArrayList<Component>();
+    private List<Component> modulesList = new ArrayList<>();
 
     @Override
     public void enter(ViewChangeEvent event) {
