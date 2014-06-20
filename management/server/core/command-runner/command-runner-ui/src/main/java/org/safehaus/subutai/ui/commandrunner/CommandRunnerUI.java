463ded85540ae1fd23d0aa687c6b97e45f5306f4
daralbaev <daralbaev@gmail.com>
2014-06-21 03:52:01 +0600

    -m SUBUTAI-864 Accumulo UI External resources to load with FileResource
    Former-commit-id: fec04340831e928ec87fd7a1209265159d579d4b

--- management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/CommandRunnerUI.java
+++ management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/CommandRunnerUI.java
@@ -5,53 +5,55 @@ import com.vaadin.ui.Component;
 import org.safehaus.subutai.api.agentmanager.AgentManager;
 import org.safehaus.subutai.api.commandrunner.CommandRunner;
 import org.safehaus.subutai.server.ui.api.PortalModule;
+import org.safehaus.subutai.shared.protocol.FileUtil;
 
 import java.io.File;
 
 
 public class CommandRunnerUI implements PortalModule {
 
-    public static final String MODULE_NAME = "Terminal";
-    private CommandRunner commandRunner;
-    private AgentManager agentManager;
+	public static final String MODULE_IMAGE = "terminal.jpg";
+	public static final String MODULE_NAME = "Terminal";
+	private CommandRunner commandRunner;
+	private AgentManager agentManager;
 
 
-    public void setCommandRunner( CommandRunner commandRunner ) {
-        this.commandRunner = commandRunner;
-    }
+	public void setCommandRunner(CommandRunner commandRunner) {
+		this.commandRunner = commandRunner;
+	}
 
 
-    public void setAgentManager( AgentManager agentManager ) {
-        this.agentManager = agentManager;
-    }
+	public void setAgentManager(AgentManager agentManager) {
+		this.agentManager = agentManager;
+	}
 
 
-    public void init() {
-    }
+	public void init() {
+	}
 
 
-    public void destroy() {
-    }
-
-
-    @Override
-    public String getId() {
-        return CommandRunnerUI.MODULE_NAME;
-    }
-
-    @Override
-    public String getName() {
-        return CommandRunnerUI.MODULE_NAME;
-    }
-
-	@Override
-	public File getImage() {
-		return null;
+	public void destroy() {
 	}
 
 
 	@Override
-    public Component createComponent() {
-        return new TerminalForm( commandRunner, agentManager );
-    }
+	public String getId() {
+		return CommandRunnerUI.MODULE_NAME;
+	}
+
+	@Override
+	public String getName() {
+		return CommandRunnerUI.MODULE_NAME;
+	}
+
+	@Override
+	public File getImage() {
+		return FileUtil.getFile(CommandRunnerUI.MODULE_IMAGE, this);
+	}
+
+
+	@Override
+	public Component createComponent() {
+		return new TerminalForm(commandRunner, agentManager);
+	}
 }
--- management/server/core/monitor/monitor-ui/src/main/java/org/safehaus/subutai/ui/monitor/MonitorModule.java
+++ management/server/core/monitor/monitor-ui/src/main/java/org/safehaus/subutai/ui/monitor/MonitorModule.java
@@ -4,41 +4,43 @@ import com.vaadin.ui.Component;
 import org.safehaus.subutai.api.agentmanager.AgentManager;
 import org.safehaus.subutai.api.monitor.Monitor;
 import org.safehaus.subutai.server.ui.api.PortalModule;
+import org.safehaus.subutai.shared.protocol.FileUtil;
 
 import java.io.File;
 
 public class MonitorModule implements PortalModule {
 
-    private static final String MODULE_NAME = "Monitor";
+	public static final String MODULE_IMAGE = "monitor.jpg";
+	private static final String MODULE_NAME = "Monitor";
 
-    private Monitor monitor;
-    private AgentManager agentManager;
+	private Monitor monitor;
+	private AgentManager agentManager;
 
-    @Override
-    public String getId() {
-        return MODULE_NAME;
-    }
+	@Override
+	public String getId() {
+		return MODULE_NAME;
+	}
 
-    @Override
-    public String getName() {
-        return MODULE_NAME;
-    }
+	@Override
+	public String getName() {
+		return MODULE_NAME;
+	}
 
 	@Override
 	public File getImage() {
-		return null;
+		return FileUtil.getFile(MonitorModule.MODULE_IMAGE, this);
+	}
+
+	@Override
+	public Component createComponent() {
+		return new ModuleView(monitor, agentManager);
 	}
 
 	public void setMonitor(Monitor monitor) {
-        this.monitor = monitor;
-    }
+		this.monitor = monitor;
+	}
 
-    public void setAgentManager( AgentManager agentManager ) {
-        this.agentManager = agentManager;
-    }
-
-    @Override
-    public Component createComponent() {
-        return new ModuleView(monitor, agentManager);
-    }
+	public void setAgentManager(AgentManager agentManager) {
+		this.agentManager = agentManager;
+	}
 }
