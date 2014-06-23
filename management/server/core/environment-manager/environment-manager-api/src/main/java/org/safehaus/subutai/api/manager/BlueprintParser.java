74667d4b9a043fc6ab860377be31822b78789d72
bahadyr <bahadyr@safehaus.org>
2014-06-24 04:28:47 +0600

    Environment Manager refactor.
    Former-commit-id: 47208a8fed4245497250d886be4dedd51b52856c

--- management/server/core/environment-manager/environment-manager-api/src/main/java/org/safehaus/subutai/api/manager/BlueprintParser.java
+++ management/server/core/environment-manager/environment-manager-api/src/main/java/org/safehaus/subutai/api/manager/BlueprintParser.java
@@ -1,13 +1,21 @@
 package org.safehaus.subutai.api.manager;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
 /**
  * Created by bahadyr on 6/24/14.
  */
 public class BlueprintParser {
 
+    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+
     public Blueprint parseBlueprint( final String blueprintStr ) {
 
-        return null;
+        Blueprint blueprint = gson.fromJson( blueprintStr, Blueprint.class );
+        return blueprint;
     }
 }
