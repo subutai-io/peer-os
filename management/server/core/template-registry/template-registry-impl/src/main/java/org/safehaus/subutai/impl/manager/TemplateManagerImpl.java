5ec6ed79d4d6b93d803cf9f6789a0002f69ee726
bahadyr <bahadyr@safehaus.org>
2014-06-23 16:47:50 +0600

    Added Template Registry module.
    Former-commit-id: f9b9cfe8f5a9dd2aea89e3843abd8ed9865936c2

---
+++ management/server/core/template-registry/template-registry-impl/src/main/java/org/safehaus/subutai/impl/manager/TemplateManagerImpl.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.safehaus.subutai.impl.manager;
+
+
+import org.safehaus.subutai.api.manager.TemplateManager;
+
+
+/**
+ * This is an implementation of LxcManager
+ */
+public class TemplateManagerImpl implements TemplateManager {
+
+
+    @Override
+    public void getTemplates() {
+
+    }
+
+
+    @Override
+    public void getInstances() {
+
+    }
+
+
+    @Override
+    public void createNewTemplate() {
+
+    }
+
+
+    @Override
+    public void commitTemplate() {
+
+    }
+
+
+    @Override
+    public void destoryTemplate() {
+
+    }
+
+
+    @Override
+    public void getTsarFilesInfo() {
+
+    }
+
+
+    @Override
+    public void getTsarFileDescriptor() {
+
+    }
+
+
+    @Override
+    public void deleteTsarFile() {
+
+    }
+
+
+    @Override
+    public void pushTsarFileAsDebPackageIntoRepository() {
+
+    }
+}
