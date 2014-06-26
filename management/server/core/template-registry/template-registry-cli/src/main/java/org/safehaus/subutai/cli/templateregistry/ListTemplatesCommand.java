07110920cc78b6f609c20771bc2aa2d3d1f59484
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-06-26 21:03:55 +0600

    Added CLI GetParentTemplate, ListTemplates UnregisterTemplate
    Former-commit-id: 5951f6a69733aedf78da8f7813c592ecd3c41c77

--- management/server/core/template-registry/template-registry-cli/src/main/java/org/safehaus/subutai/cli/templateregistry/ListTemplatesCommand.java
+++ management/server/core/template-registry/template-registry-cli/src/main/java/org/safehaus/subutai/cli/templateregistry/ListTemplatesCommand.java
@@ -1,6 +1,9 @@
 package org.safehaus.subutai.cli.templateregistry;
 
 
+import java.util.List;
+
+import org.safehaus.subutai.api.templateregistry.Template;
 import org.safehaus.subutai.api.templateregistry.TemplateRegistryManager;
 import org.safehaus.subutai.api.templateregistry.TemplateTree;
 
@@ -17,6 +20,21 @@ public class ListTemplatesCommand extends OsgiCommandSupport {
     private TemplateRegistryManager templateRegistryManager;
 
 
+    private void listFamily( int level, TemplateTree tree, Template currentTemplate ) {
+        System.out.println(
+                String.format( "%" + ( level > 0 ? level : "" ) + "s %s", "", currentTemplate.getTemplateName() ) );
+        List<Template> children = tree.getChildrenTemplates( currentTemplate );
+        if ( children == null || children.isEmpty() ) {
+            return;
+        }
+        else {
+            for ( Template child : children ) {
+                listFamily( level + 1, tree, child );
+            }
+        }
+    }
+
+
     public void setTemplateRegistryManager( final TemplateRegistryManager templateRegistryManager ) {
         this.templateRegistryManager = templateRegistryManager;
     }
@@ -27,7 +45,7 @@ public class ListTemplatesCommand extends OsgiCommandSupport {
 
         TemplateTree tree = templateRegistryManager.getTemplateTree();
 
-        //print template tree with indentation
+        listFamily( 0, tree, Template.getMasterTemplate() );
 
         return null;
     }
