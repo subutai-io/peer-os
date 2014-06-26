821fe56eea1e5a3f341d3a41da9c7cdecf73995d
daralbaev <daralbaev@gmail.com>
2014-06-26 17:42:55 +0600

    -m Hadoop modifications
    Former-commit-id: aef8afc1dfe4d364bdda574fe7b1eca66c0e8608

--- management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/subutai/ui/hadoop/manager/Manager.java
+++ management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/subutai/ui/hadoop/manager/Manager.java
@@ -8,71 +8,79 @@ import org.safehaus.subutai.ui.hadoop.manager.components.ClusterNode;
  * Created by daralbaev on 12.04.14.
  */
 public class Manager extends VerticalLayout {
-    private HorizontalLayout horizontalLayout, buttonsLayout;
-    private Embedded indicator;
-    private Button refreshButton;
-    private HadoopTable table;
+	private HorizontalLayout horizontalLayout, buttonsLayout;
+	private Embedded indicator;
+	private Button refreshButton;
+	private HadoopTable table;
 
-    public Manager() {
-        setSizeFull();
+	public Manager() {
+		setSizeFull();
 
-        horizontalLayout = new HorizontalLayout();
-        horizontalLayout.setMargin(true);
-        horizontalLayout.setSpacing(true);
+		GridLayout grid = new GridLayout();
+		grid.setColumns(1);
+		grid.setRows(7);
+		grid.setSizeFull();
 
-        horizontalLayout.addComponent(getButtonRefresh());
-        horizontalLayout.addComponent(getIndicator());
+		horizontalLayout = new HorizontalLayout();
+		horizontalLayout.setMargin(true);
+		horizontalLayout.setSpacing(true);
 
-        buttonsLayout = new HorizontalLayout();
-        buttonsLayout.setMargin(true);
-        buttonsLayout.setSpacing(true);
+		horizontalLayout.addComponent(getButtonRefresh());
+		horizontalLayout.addComponent(getIndicator());
 
-        Embedded startedButton = new Embedded("", new ThemeResource("icons/buttons/start.png"));
-        startedButton.setWidth(ClusterNode.ICON_SIZE, Unit.PIXELS);
-        startedButton.setHeight(ClusterNode.ICON_SIZE, Unit.PIXELS);
-        startedButton.setEnabled(false);
-        buttonsLayout.addComponent(startedButton);
-        buttonsLayout.addComponent(new Label("Started node"));
+		buttonsLayout = new HorizontalLayout();
+		buttonsLayout.setMargin(true);
+		buttonsLayout.setSpacing(true);
 
-        Embedded stoppedButton = new Embedded("", new ThemeResource("icons/buttons/stop.png"));
-        stoppedButton.setWidth(ClusterNode.ICON_SIZE, Unit.PIXELS);
-        stoppedButton.setHeight(ClusterNode.ICON_SIZE, Unit.PIXELS);
-        stoppedButton.setEnabled(false);
-        buttonsLayout.addComponent(stoppedButton);
-        buttonsLayout.addComponent(new Label("Stopped node"));
+		Embedded startedButton = new Embedded("", new ThemeResource("icons/buttons/start.png"));
+		startedButton.setWidth(ClusterNode.ICON_SIZE, Unit.PIXELS);
+		startedButton.setHeight(ClusterNode.ICON_SIZE, Unit.PIXELS);
+		startedButton.setEnabled(false);
+		buttonsLayout.addComponent(startedButton);
+		buttonsLayout.addComponent(new Label("Started node"));
 
-        addComponent(horizontalLayout);
-        addComponent(getHadoopTable());
-        addComponent(buttonsLayout);
-    }
+		Embedded stoppedButton = new Embedded("", new ThemeResource("icons/buttons/stop.png"));
+		stoppedButton.setWidth(ClusterNode.ICON_SIZE, Unit.PIXELS);
+		stoppedButton.setHeight(ClusterNode.ICON_SIZE, Unit.PIXELS);
+		stoppedButton.setEnabled(false);
+		buttonsLayout.addComponent(stoppedButton);
+		buttonsLayout.addComponent(new Label("Stopped node"));
 
-    private Button getButtonRefresh() {
-        refreshButton = new Button("Refresh");
-        refreshButton.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent clickEvent) {
-                table.refreshDataSource();
-            }
-        });
+		grid.addComponent(horizontalLayout, 0, 0);
+		grid.addComponent(getHadoopTable(), 0, 1, 0, 5);
+		grid.addComponent(buttonsLayout, 0, 6);
 
-        return refreshButton;
-    }
+		addComponent(grid);
+	}
 
-    private Embedded getIndicator() {
-        indicator = new Embedded("", new ThemeResource("icons/indicator.gif"));
-        indicator.setHeight(11, Unit.PIXELS);
-        indicator.setWidth(50, Unit.PIXELS);
-        indicator.setVisible(true);
+	private Button getButtonRefresh() {
+		refreshButton = new Button("Refresh");
+		refreshButton.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(Button.ClickEvent clickEvent) {
+				table.refreshDataSource();
+			}
+		});
 
-        return indicator;
-    }
+		return refreshButton;
+	}
 
-    private HadoopTable getHadoopTable() {
-        if (table == null) {
-            table = new HadoopTable("Hadoop Clusters", indicator);
-            table.setMultiSelect(false);
-        }
+	private Embedded getIndicator() {
+		indicator = new Embedded("", new ThemeResource("icons/indicator.gif"));
+		indicator.setHeight(11, Unit.PIXELS);
+		indicator.setWidth(50, Unit.PIXELS);
+		indicator.setVisible(true);
 
-        return table;
-    }
+		return indicator;
+	}
+
+	private HadoopTable getHadoopTable() {
+		if (table == null) {
+			table = new HadoopTable("Hadoop Clusters", indicator);
+			table.setMultiSelect(false);
+			table.setSizeFull();
+		}
+
+		return table;
+	}
 }
