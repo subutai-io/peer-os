8fbca0f599fa455cf23dda026877c7f37d6a0729
daralbaev <daralbaev@gmail.com>
2014-06-26 14:30:00 +0600

    -m Only one tab for module can be opened.
    Former-commit-id: 951a67e551085715e94c55e71b9a62e7df57d379

--- management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/CommandRunnerUI.java
+++ management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/CommandRunnerUI.java
@@ -29,7 +29,6 @@ public class CommandRunnerUI implements PortalModule {
 
 
 	public void init() {
-
 	}
 
 
--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/ModulesView.java
@@ -89,28 +89,27 @@ public class ModulesView extends VerticalLayout implements View, PortalModuleLis
 
 	private void addModule(final PortalModule module) {
 		if (module.getImage() != null) {
-			if (!contains(module)) {
-				CssLayout moduleLayout = new CssLayout();
-				moduleLayout.setId(module.getId());
-				moduleLayout.setWidth(150, Unit.PIXELS);
-				moduleLayout.setHeight(200, Unit.PIXELS);
-				moduleLayout.addStyleName("create");
+			CssLayout moduleLayout = new CssLayout();
+			moduleLayout.setId(module.getId());
+			moduleLayout.setWidth(150, Unit.PIXELS);
+			moduleLayout.setHeight(200, Unit.PIXELS);
+			moduleLayout.addStyleName("create");
 
-				moduleLayout.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
-					@Override
-					public void layoutClick(LayoutEvents.LayoutClickEvent layoutClickEvent) {
+			moduleLayout.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
+				@Override
+				public void layoutClick(LayoutEvents.LayoutClickEvent layoutClickEvent) {
+					if (!contains(module)) {
 						autoCreate(module);
 					}
-				});
+				}
+			});
 
-				Image image = new Image("", new FileResource(module.getImage()));
-				image.setWidth(90, Unit.PERCENTAGE);
-				image.setDescription(module.getName());
-				moduleLayout.addComponent(image);
+			Image image = new Image("", new FileResource(module.getImage()));
+			image.setWidth(90, Unit.PERCENTAGE);
+			image.setDescription(module.getName());
+			moduleLayout.addComponent(image);
 
-				modulesLayout.addComponent(moduleLayout);
-				modulesList.add(moduleLayout);
-			}
+			modulesLayout.addComponent(moduleLayout);
 		}
 	}
 
@@ -139,13 +138,13 @@ public class ModulesView extends VerticalLayout implements View, PortalModuleLis
 	@Override
 	public void moduleUnregistered(PortalModule module) {
 
-		for (Component component : modulesList) {
+		/*for (Component component : modulesList) {
 			if (component.getId().equals(module.getId())) {
 				modulesLayout.removeComponent(component);
 				modulesList.remove(component);
 				break;
 			}
-		}
+		}*/
 	}
 }
 
