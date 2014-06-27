c0d32d4e10c6cf6f511dce4ce2c17d14e82e4945
daralbaev <daralbaev@gmail.com>
2014-06-28 03:11:21 +0600

    -m SUBUTAI-877 Styling Presto module
    Former-commit-id: 10162ae32a7092ab6d0d524029cdd0d0a5fe87c2

--- management/server/products/presto/presto-ui/src/main/java/org/safehaus/subutai/ui/presto/PrestoUI.java
+++ management/server/products/presto/presto-ui/src/main/java/org/safehaus/subutai/ui/presto/PrestoUI.java
@@ -13,7 +13,9 @@ import org.safehaus.subutai.api.presto.Config;
 import org.safehaus.subutai.api.presto.Presto;
 import org.safehaus.subutai.api.tracker.Tracker;
 import org.safehaus.subutai.server.ui.api.PortalModule;
+import org.safehaus.subutai.shared.protocol.FileUtil;
 
+import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -22,68 +24,75 @@ import java.util.concurrent.Executors;
  */
 public class PrestoUI implements PortalModule {
 
-    private static Presto prestoManager;
-    private static AgentManager agentManager;
-    private static Tracker tracker;
-    private static Hadoop hadoopManager;
-    private static CommandRunner commandRunner;
-    private static ExecutorService executor;
+	public static final String MODULE_IMAGE = "presto.png";
 
-    public PrestoUI(AgentManager agentManager, Tracker tracker, Hadoop hadoopManager, Presto prestoManager, CommandRunner commandRunner) {
-        PrestoUI.agentManager = agentManager;
-        PrestoUI.tracker = tracker;
-        PrestoUI.hadoopManager = hadoopManager;
-        PrestoUI.prestoManager = prestoManager;
-        PrestoUI.commandRunner = commandRunner;
-    }
+	private static Presto prestoManager;
+	private static AgentManager agentManager;
+	private static Tracker tracker;
+	private static Hadoop hadoopManager;
+	private static CommandRunner commandRunner;
+	private static ExecutorService executor;
 
-    public static Tracker getTracker() {
-        return tracker;
-    }
+	public PrestoUI(AgentManager agentManager, Tracker tracker, Hadoop hadoopManager, Presto prestoManager, CommandRunner commandRunner) {
+		PrestoUI.agentManager = agentManager;
+		PrestoUI.tracker = tracker;
+		PrestoUI.hadoopManager = hadoopManager;
+		PrestoUI.prestoManager = prestoManager;
+		PrestoUI.commandRunner = commandRunner;
+	}
 
-    public static Presto getPrestoManager() {
-        return prestoManager;
-    }
+	public static Tracker getTracker() {
+		return tracker;
+	}
 
-    public static Hadoop getHadoopManager() {
-        return hadoopManager;
-    }
+	public static Presto getPrestoManager() {
+		return prestoManager;
+	}
 
-    public static ExecutorService getExecutor() {
-        return executor;
-    }
+	public static Hadoop getHadoopManager() {
+		return hadoopManager;
+	}
 
-    public static AgentManager getAgentManager() {
-        return agentManager;
-    }
+	public static ExecutorService getExecutor() {
+		return executor;
+	}
 
-    public static CommandRunner getCommandRunner() {
-        return commandRunner;
-    }
+	public static AgentManager getAgentManager() {
+		return agentManager;
+	}
 
-    public void init() {
-        executor = Executors.newCachedThreadPool();
-    }
+	public static CommandRunner getCommandRunner() {
+		return commandRunner;
+	}
 
-    public void destroy() {
-        prestoManager = null;
-        agentManager = null;
-        hadoopManager = null;
-        tracker = null;
-        executor.shutdown();
-    }
+	public void init() {
+		executor = Executors.newCachedThreadPool();
+	}
 
-    @Override
-    public String getId() {
-        return Config.PRODUCT_KEY;
-    }
+	public void destroy() {
+		prestoManager = null;
+		agentManager = null;
+		hadoopManager = null;
+		tracker = null;
+		executor.shutdown();
+	}
 
-    public String getName() {
-        return Config.PRODUCT_KEY;
-    }
+	@Override
+	public String getId() {
+		return Config.PRODUCT_KEY;
+	}
 
-    public Component createComponent() {
-        return new PrestoForm();
-    }
+	public String getName() {
+		return Config.PRODUCT_KEY;
+	}
+
+	@Override
+	public File getImage() {
+		return FileUtil.getFile(PrestoUI.MODULE_IMAGE, this);
+	}
+
+	public Component createComponent() {
+		return new PrestoForm();
+	}
 
 }
