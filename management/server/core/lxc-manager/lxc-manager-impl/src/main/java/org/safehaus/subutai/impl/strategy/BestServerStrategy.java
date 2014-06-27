e633a4eaa9d0673e79e1112ec2028e5ed5ae397f
Azilet Beishenaliev <aziletb@gmail.com>
2014-06-27 17:41:54 +0600

    SUBUTAI-1000: update placement strategy to accept count of nodes
    Former-commit-id: 1b009ac99c17ee2fdd4b406826eb162f03026c20

--- management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/subutai/impl/strategy/BestServerStrategy.java
+++ management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/subutai/impl/strategy/BestServerStrategy.java
@@ -11,11 +11,25 @@ public class BestServerStrategy extends LxcPlacementStrategy {
 
     public static final String defaultNodeType = "default";
 
+    private final int nodesCount;
     private Set<PlacementStrategyENUM> strategyFactors;
 
-    public BestServerStrategy(PlacementStrategyENUM... strategyFactors) {
+    public BestServerStrategy(int nodesCount, PlacementStrategyENUM... strategyFactors) {
+        this.nodesCount = nodesCount;
         this.strategyFactors = EnumSet.noneOf(PlacementStrategyENUM.class);
         this.strategyFactors.addAll(Arrays.asList(strategyFactors));
+    }
+
+    @Override
+    public Map<Agent, Integer> calculateSlots(Map<Agent, ServerMetric> serverMetrics) {
+        Map<Agent, Integer> res = new HashMap<>();
+        Map<Agent, Map<String, Integer>> map = getPlacementInfoMap();
+        for(Map.Entry<Agent, Map<String, Integer>> e : map.entrySet()) {
+            int total = 0;
+            for(Integer i : e.getValue().values()) total += i;
+            res.put(e.getKey(), total);
+        }
+        return res;
     }
 
     @Override
@@ -23,20 +37,18 @@ public class BestServerStrategy extends LxcPlacementStrategy {
 
         // using each startegy criteria, grade servers one by one
         Map<Agent, Integer> grades = new HashMap<>();
+        for(Agent a : serverMetrics.keySet()) grades.put(a, 0);
         for(PlacementStrategyENUM sf : strategyFactors) {
             try {
                 Agent a = getBestMatch(serverMetrics, MetricComparator.create(sf));
-                if(a != null) {
-                    Integer g = grades.get(a);
-                    grades.put(a, (g != null ? g : 0) + 1);
-                }
+                if(a != null) grades.put(a, grades.get(a) + 1);
             } catch(Exception ex) {
                 // comparator not defined for strategy
                 // TODO: log
             }
         }
 
-        // sort servers by their grades
+        // sort servers by their grades in decreasing order
         ArrayList<Map.Entry<Agent, Integer>> ls = new ArrayList<>(grades.entrySet());
         Collections.sort(ls, new Comparator<Map.Entry>() {
 
@@ -44,12 +56,20 @@ public class BestServerStrategy extends LxcPlacementStrategy {
             public int compare(Map.Entry o1, Map.Entry o2) {
                 Integer v1 = (Integer)o1.getValue();
                 Integer v2 = (Integer)o2.getValue();
-                return v1.compareTo(v2);
+                return -1 * v1.compareTo(v2);
             }
         });
 
-        Agent best = ls.get(ls.size() - 1).getKey();
-        addPlacementInfo(best, defaultNodeType, 1);
+        // distribute nodes count among server with best server first
+        Map<Agent, Integer> slots = new HashMap<>();
+        for(int i = 0; i < nodesCount; i++) {
+            Agent best = ls.get(i % nodesCount).getKey();
+            if(slots.containsKey(best)) slots.put(best, slots.get(best) + 1);
+            else slots.put(best, 1);
+        }
+        for(Map.Entry<Agent, Integer> e : slots.entrySet()) {
+            addPlacementInfo(e.getKey(), defaultNodeType, e.getValue());
+        }
     }
 
     private Agent getBestMatch(Map<Agent, ServerMetric> serverMetrics, final MetricComparator mc) {
--- management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/subutai/impl/strategy/PlacementStrategyFactory.java
+++ management/server/core/lxc-manager/lxc-manager-impl/src/main/java/org/safehaus/subutai/impl/strategy/PlacementStrategyFactory.java
@@ -5,11 +5,11 @@ import org.safehaus.subutai.api.manager.helper.PlacementStrategyENUM;
 
 public class PlacementStrategyFactory {
 
-    public static LxcPlacementStrategy create(PlacementStrategyENUM... strategy) {
+    public static LxcPlacementStrategy create(int nodesCount, PlacementStrategyENUM... strategy) {
         if(strategy == null || strategy.length == 0)
             return new DefaultLxcPlacementStrategy(1);
 
-        return new BestServerStrategy(strategy);
+        return new BestServerStrategy(nodesCount, strategy);
     }
 
 }
