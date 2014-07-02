806b039684fe1b3caae2816c4b358eb262252994
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-02 23:51:31 +0600

    merge with main
    Former-commit-id: 0613a13090304d9107aa6e94fc8099ff5f534cc5

---
+++ management/server/core/template-registry/template-registry-rest/src/main/java/org/safehaus/subutai/rest/templateregistry/RestServiceImpl.java
@@ -0,0 +1,80 @@
+package org.safehaus.subutai.rest.templateregistry;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.safehaus.subutai.api.templateregistry.Template;
+import org.safehaus.subutai.api.templateregistry.TemplateRegistryManager;
+import org.safehaus.subutai.api.templateregistry.TemplateTree;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+/**
+ *
+ */
+
+public class RestServiceImpl implements RestService {
+
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private TemplateRegistryManager templateRegistryManager;
+
+
+    public void setTemplateRegistryManager( TemplateRegistryManager templateRegistryManager ) {
+        this.templateRegistryManager = templateRegistryManager;
+    }
+
+
+    @Override
+    public String getTemplate( final String templateName ) {
+        return gson.toJson( templateRegistryManager.getTemplate( templateName ) );
+    }
+
+
+    @Override
+    public String getParentTemplate( final String childName ) {
+        return gson.toJson( templateRegistryManager.getParentTemplate( childName ) );
+    }
+
+
+    @Override
+    public String getParentTemplates( final String childTemplateName ) {
+        List<String> parents = new ArrayList<>();
+        for ( Template template : templateRegistryManager.getParentTemplates( childTemplateName ) ) {
+            parents.add( template.getTemplateName() );
+        }
+        return gson.toJson( parents );
+    }
+
+
+    @Override
+    public String getChildTemplates( final String parentTemplateName ) {
+        return gson.toJson( templateRegistryManager.getChildTemplates( parentTemplateName ) );
+    }
+
+
+    @Override
+    public String getTemplateTree() {
+        TemplateTree tree = templateRegistryManager.getTemplateTree();
+        Template master = Template.getMasterTemplate();
+        addChildren( tree, master );
+
+        return gson.toJson( master );
+    }
+
+
+    private void addChildren( TemplateTree tree, Template currentTemplate ) {
+        List<Template> children = tree.getChildrenTemplates( currentTemplate );
+        if ( children == null || children.isEmpty() ) {
+            return;
+        }
+        else {
+            currentTemplate.addChildren( children );
+            for ( Template child : children ) {
+                addChildren( tree, child );
+            }
+        }
+    }
+}
