6ebb0d57c0e492d70a50b4d32e73733e87da2328
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-04 10:32:45 +0300

    SUBUTAI-1024: Move hadoop configurations under /etc directory
    Former-commit-id: 96973f82661c73dfc59977027990bf77c4821975

--- big-data/hadoop/hadoop/etc/profile.d/hadoop-home.sh
+++ big-data/hadoop/hadoop/etc/profile.d/hadoop-home.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+set -e
+
 hadoopHome="/opt/hadoop-1.2.1"
 export HADOOP_HOME=$hadoopHome
-export PATH=$PATH:$HADOOP_HOME/bin
+export HADOOP_CONF_DIR="/etc/hadoop"
+path_content=$(echo $PATH)
+
+pattern="$hadoopHome/bin"
+if [[ $path_content != *$pattern* ]];
+then
+        export PATH=$PATH:$pattern
+fi
+
+pattern="$hadoopHome/sbin"
+if [[ $path_content != *$pattern* ]];
+then
+        export PATH=$PATH:$pattern
+fi
