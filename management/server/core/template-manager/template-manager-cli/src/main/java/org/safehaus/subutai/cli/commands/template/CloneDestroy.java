3a0604bd93464240862f8f89f8699719b13afb41
Azilet Beishenaliev <aziletb@gmail.com>
2014-07-06 12:23:32 +0600

    SUBUTAI-993: fix cli classed
    Former-commit-id: e093d1df1b0c18f1cb2987e0f8eac3c656e26609

---
+++ management/server/core/template-manager/template-manager-cli/src/main/java/org/safehaus/subutai/cli/commands/template/CloneDestroy.java
@@ -0,0 +1,34 @@
+package org.safehaus.subutai.cli.commands.template;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.subutai.api.template.manager.TemplateManager;
+
+@Command(scope = "template-man", name = "clone-destroy", description = "destroy clone")
+public class CloneDestroy extends OsgiCommandSupport {
+
+    private TemplateManager templateManager;
+
+    @Argument(index = 0, required = true)
+    private String hostName;
+    @Argument(index = 1, required = true)
+    private String cloneName;
+
+    public TemplateManager getTemplateManaget() {
+        return templateManager;
+    }
+
+    public void setTemplateManaget(TemplateManager templateManaget) {
+        this.templateManager = templateManaget;
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        boolean b = templateManager.cloneDestroy(hostName, cloneName);
+        if(b) System.out.println("Clone successfully destroyed");
+        else System.out.println("Failed to destroy");
+        return null;
+    }
+
+}
---
+++ management/server/core/template-manager/template-manager-cli/src/main/java/org/safehaus/subutai/cli/commands/template/PromoteClone.java
@@ -0,0 +1,34 @@
+package org.safehaus.subutai.cli.commands.template;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.subutai.api.template.manager.TemplateManager;
+
+@Command(scope = "template-man", name = "promote", description = "promote clone to template")
+public class PromoteClone extends OsgiCommandSupport {
+
+    private TemplateManager templateManager;
+
+    @Argument(index = 0, required = true)
+    private String hostName;
+    @Argument(index = 1, required = true)
+    private String cloneName;
+
+    public TemplateManager getTemplateManaget() {
+        return templateManager;
+    }
+
+    public void setTemplateManaget(TemplateManager templateManaget) {
+        this.templateManager = templateManaget;
+    }
+
+    @Override
+    protected Object doExecute() throws Exception {
+        boolean b = templateManager.promoteClone(hostName, cloneName);
+        if(b) System.out.println("Clone successfully promoted to a template");
+        else System.out.println("Failed to promote clone");
+        return null;
+    }
+
+}
