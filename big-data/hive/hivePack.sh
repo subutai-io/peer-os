dadb63b4df937d4b6b7cc072cfe96153ad1c950e
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-07 15:47:26 +0300

    SUBUTAI-1030: Move hive configurations under /etc directory
    Former-commit-id: 4461f685668d46b9c9e90e8a62d6925dbd7815ee

---
+++ big-data/hive/hivePack.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+
+. /var/lib/jenkins/jobs/master.get_branch_repo/workspace/big-data/pack-funcs
+
+productName=hive
+downloadHiveAndMakeChanges() {
+	initializeVariables $1
+	confDirectory=$BASE/$fileName/etc/$productName
+	tarFile=apache-hive-0.13.1-bin.tar.gz
+	# Create directories that are required for the debian package
+        mkdir -p $confDirectory
+
+	wget http://archive.apache.org/dist/hive/hive-0.13.1/$tarFile -P $tempDirectory
+	if [ -f $BASE/$fileName/opt/README ]; then
+	        rm $BASE/$fileName/opt/README
+	fi
+	# unpack tar ball and make changes 
+	pushd $tempDirectory
+	# Rename existing directories if any
+	mv *$productName* $productName
+	tar -xpf $tarFile -C .
+	rm $tarFile
+	mv *$productName*/conf/* $confDirectory
+	# Move extracted tar file contents under relevant directory
+	mv *$productName*/* $productName
+	popd
+}
+
+# 2) Get the sources which are downloaded from version control system to local machine to relevant directories to generate the debian package
+getSourcesToRelevantDirectories $productName
+# 3) Download hadoop tar file and make necessary changes
+downloadHadoopAndMakeChanges $productName
+# 4) Create the Debian package
+generateDebianPackage $productName
