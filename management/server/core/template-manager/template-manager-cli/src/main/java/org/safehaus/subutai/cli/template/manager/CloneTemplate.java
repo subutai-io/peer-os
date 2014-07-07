52b255b05df86ca0e208988971cbd2e7181da891
Azilet Beishenaliev <aziletb@gmail.com>
2014-07-07 16:12:48 +0600

    SUBUTAI-993: fix cli (misspelled setter names)
    Former-commit-id: c2c099c968a082883fb2be0f04e5dad917d4f641

--- management/server/core/template-manager/template-manager-cli/src/main/java/org/safehaus/subutai/cli/template/manager/CloneTemplate.java
+++ management/server/core/template-manager/template-manager-cli/src/main/java/org/safehaus/subutai/cli/template/manager/CloneTemplate.java
@@ -14,22 +14,23 @@ public class CloneTemplate extends OsgiCommandSupport {
     private String hostName;
     @Argument(index = 1, required = true)
     private String templateName;
-    @Argument(index = 3, required = true)
+    @Argument(index = 2, required = true)
     private String cloneName;
 
-    public TemplateManager getTemplateManaget() {
+    public TemplateManager getTemplateManager() {
         return templateManager;
     }
 
-    public void setTemplateManaget(TemplateManager templateManaget) {
-        this.templateManager = templateManaget;
+    public void setTemplateManager(TemplateManager templateManager) {
+        this.templateManager = templateManager;
     }
 
     @Override
     protected Object doExecute() throws Exception {
         boolean b = templateManager.clone(hostName, templateName, cloneName);
         if(b)
-            System.out.println(String.format("New instance '%s' is clone in %s", cloneName, hostName));
+            System.out.println(String.format("New instance '%s' is clone in %s",
+                    cloneName, hostName));
         else
             System.out.println("Failed to clone new instance");
         return null;
