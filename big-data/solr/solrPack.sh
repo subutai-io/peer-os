89926e0e1d0ad8e7dd54a397cde0e8ddff5655f9
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-09 13:33:59 +0300

    SUBUTAI-1041: Edit packaging script of Solr
    Former-commit-id: 92ca8fe0d4f49af564d470e92396b334d399de8c

--- big-data/solr/solrPack.sh
+++ big-data/solr/solrPack.sh
@@ -1,49 +1,28 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
+. /var/lib/jenkins/jobs/master.get_branch_repo/workspace/big-data/pack-funcs
 
-if ls | grep .deb ; then
-        rm  *.deb
-fi
+productName=hadoop
+downloadFileAndMakeChanges() {
+        initializeVariables $1
+        optDirectory=$BASE/$fileName/opt
+	tarFile=solr-4.4.0.tgz
+	mkdir -p $optDirectory
 
-rm -rf ksks-solr*/*
-cp -r  ../workspace/big-data/solr/solr/* ksks-solr*/
-
-cd ksks-solr*
-wget -P opt http://archive.apache.org/dist/lucene/solr/4.4.0/solr-4.4.0.tgz
-cd -
-
-fileName=`ls | grep ksks | awk '{print $1}' | head -1`
-
-lineNumberVersion=$(sed -n '/Version:/=' $fileName/DEBIAN/control)
-lineNumberPackage=$(sed -n '/Package:/=' $fileName/DEBIAN/control)
-
-
-lineVersion=$(sed $lineNumberVersion!d $fileName/DEBIAN/control)
-linePackage=$(sed $lineNumberPackage!d $fileName/DEBIAN/control)
-
-version=$(echo $lineVersion | awk -F":" '{split($2,a," ");print a[1]}')
-
-versionFirst=$(echo $version | awk -F"." '{print $1}')
-versionSecond=$(echo $version | awk -F"." '{print $2}')
-versionThird=$(echo $version | awk -F"." '{print $3}')
-updatedVersion=$(echo `expr $versionThird + 1`)
-
-updatedRelease=$versionFirst.$versionSecond.$updatedVersion
-updatedFileName="ksks-solr-"$updatedRelease"-amd64"
-
-replaceVersion="Version: $updatedRelease"
-sed -i $fileName/DEBIAN/control -e $lineNumberVersion's!.*!'"$replaceVersion"'!'
-
-{
-        mv $fileName $updatedFileName
-} || {
-        echo "Version not changed"
+	wget http://archive.apache.org/dist/lucene/solr/4.4.0/$tarFile -P $optDirectpry
+        if  ls $optDirectory/README* ; then
+                rm $optDirectory/README*
+        fi
+	# unpack tar ball and make changes 
+        pushd $optDirectory
+        tar -xpf $tarFile -C .
+        rm $tarFile
+        popd
 }
-find ./$updatedFileName -name "*~" -print0 | xargs -0 rm -rf
-rm $updatedFileName/DEBIAN/md5sums
-md5sum `find ./$updatedFileName -type f | awk '/.\//{ print substr($0, 3) }'` >> $updatedFileName/DEBIAN/md5sums
-dpkg-deb -z8 -Zgzip --build $updatedFileName/
 
-cp *.deb ~/Automation/Bigdata/solr
-
+# 2) Get the sources which are downloaded from version control system to local machine to relevant directories to generate the debian package
+getSourcesToRelevantDirectories $productName
+# 3) Download hadoop tar file and make necessary changes
+downloadFileAndMakeChanges $productName
+# 4) Create the Debian package
+generateDebianPackage $productName
