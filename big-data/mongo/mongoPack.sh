34be60fcd0725a2976135cbf1a1087f30196858f
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-09 08:04:16 +0300

    SUBUTAI-1034:Move mongo configurations under /etc directory
    Former-commit-id: 5d4a457d2e2b49199ed468782c4e32178452579e

--- big-data/mongo/mongoPack.sh
+++ big-data/mongo/mongoPack.sh
@@ -6,7 +6,5 @@ productName=mongo
 
 # 2) Get the sources which are downloaded from version control system to local machine to relevant directories to generate the debian package
 getSourcesToRelevantDirectories $productName
-# 3) Download hadoop tar file and make necessary changes
-downloadFileAndMakeChanges $productName
-# 4) Create the Debian package
+# 3) Create the Debian package
 generateDebianPackage $productName
