b6ebf713cead5c9dcbc303a7b9a7ac798b7284b7
daralbaev <daralbaev@gmail.com>
2014-07-09 18:12:24 +0600

    Terminal module modifications
    Former-commit-id: ab8ef573a734ecdc892a920fdfea8ae97fad6567

--- management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/TerminalControl.java
+++ management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/TerminalControl.java
@@ -1,6 +1,8 @@
 package org.safehaus.subutai.ui.commandrunner;
 
 import com.google.common.base.Strings;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.server.VaadinService;
 import com.vaadin.ui.TextArea;
 
@@ -9,21 +11,64 @@ import com.vaadin.ui.TextArea;
  */
 public class TerminalControl extends TextArea {
 
-	private String username;
+	private String inputPrompt;
+	private String username, currentPath, machineName;
 
 	public TerminalControl() {
 		username = (String) VaadinService.getCurrentRequest().getWrappedSession().getAttribute("username");
+		currentPath = "/";
+		machineName = "";
 
 		this.setSizeFull();
 		this.setImmediate(true);
 		this.setWordwrap(true);
 		this.addStyleName("terminal");
-		if (!Strings.isNullOrEmpty(username)) {
-			this.setValue(String.format("%s#", username));
+
+		setInputPrompt();
+		addShortcutListener(new ShortcutListener("Shortcut Name", ShortcutAction.KeyCode.ENTER, null) {
+			@Override
+			public void handleAction(Object sender, Object target) {
+				getCommand();
+			}
+		});
+	}
+
+	public void setInputPrompt() {
+		inputPrompt = String.format("%s@%s:%s#", username, machineName, currentPath);
+		if (Strings.isNullOrEmpty(getValue())) {
+			setValue(String.format("%s", inputPrompt));
 		} else {
-			this.setValue("#");
+			setValue(String.format("%s\n$s", getValue(), inputPrompt));
 		}
 		this.setCursorPosition(this.getValue().length());
 		this.focus();
+	}
+
+	public String getCurrentPath() {
+		return currentPath;
+	}
+
+	public void setCurrentPath(String currentPath) {
+		this.currentPath = currentPath;
+	}
+
+	public String getMachineName() {
+		return machineName;
+	}
+
+	public void setMachineName(String machineName) {
+		this.machineName = machineName;
+	}
+
+	public String getCommand() {
+		String value = getValue();
+
+		if (Strings.isNullOrEmpty(value)) {
+			String[] args = value.split(inputPrompt);
+			value = args[args.length - 1];
+		}
+
+		System.out.println(value);
+		return value;
 	}
 }
