db2d95f7163d1b2b03251a58ed84041f1fdacbc8
Salih Kardan <salihkardan@gmail.com>
2014-07-10 08:45:05 +0300

    SUBUTAI-1049 move sqoop configuration files under /etc directory
    Former-commit-id: a0ca2ab15a9ddea3e2275810d9348ca19999a4a5

--- big-data/sqoop/sqoopPack.sh
+++ big-data/sqoop/sqoopPack.sh
@@ -17,6 +17,25 @@ downloadFileAndMakeChanges() {
 
     # download sqoop
 	wget http://archive.apache.org/dist/sqoop/$sqoopVersion/sqoop-$sqoopVersion.bin__hadoop-1.0.0.tar.gz -P $tempDirectory
+
+	# download ojdbc and mysql jar files and place them under sqoop/lib folder
+	# ojdbc 
+	wget http://www.java2s.com/Code/JarDownload/ojdbc6/ojdbc6.jar.zip -P $tempDirectory
+	pushd $tempDirectory
+	unzip ojdbc6.jar.zip
+	cp ojdbc6.jar $tempDirectory/sqoop-$sqoopVersion/lib
+	rm -r ojdbc6.jar
+	rm -r ojdbc6.jar.zip
+	popd
+	# mysql
+	wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.0.8.tar.gz -P $tempDirectory
+	tar -xpf mysql-connector-java-5.0.8.tar.gz
+	pushd mysql-connector-java-5.0.8
+	cp mysql-connector-java-5.0.8-bin.jar $tempDirectory/sqoop-$sqoopVersion/lib
+	popd
+	rm -r mysql-connector-java-5.0.8.tar.gz 
+	rm -r mysql-connector-java-5.0.8
+ 
 	pushd $tempDirectory
 	# unpack tar file
 	tar -xpf sqoop-*.tar.gz
@@ -29,9 +48,6 @@ downloadFileAndMakeChanges() {
 
 	# move configuration files of sqoop under etc/
 	mv sqoop-*/conf/* $confDirectory/
-
-	# copy extra jar files under sqoop/lib folder
-	cp $BASE/extraJar/*.jar $tempDirectory/sqoop-$sqoopVersion/lib
 	popd
 }
 # 1) Get the sources which are downloaded from version control system
