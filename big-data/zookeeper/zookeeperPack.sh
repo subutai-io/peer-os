d233a28e40f41708d2b61e03968f3b60201b5475
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-10 07:50:03 +0300

    SUBUTAI-1045: Edit packaging script of Zookeeper
    Former-commit-id: 9c83999f5ff45bfd81d2ae82bc8673140c47a595

--- big-data/zookeeper/zookeeperPack.sh
+++ big-data/zookeeper/zookeeperPack.sh
@@ -1,49 +1,43 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
+. /var/lib/jenkins/jobs/master.get_branch_repo/workspace/big-data/pack-funcs
 
-if ls | grep .deb ; then
-        rm  *.deb
-fi
+productName=zookeeper
+downloadFileAndMakeChanges() {
+        initializeVariables $1
+	tempDirectory=$BASE/$fileName/opt
+        optDirectory=$BASE/$fileName/opt
+        confDirectory=$BASE/$fileName/etc/$productName
+        tarFile=zookeeper-3.4.5.tar.gz
+        mkdir -p $optDirectory
+	mkdir -p $confDirectory
 
-rm -rf ksks-zookeeper*/*
-cp -r  ../workspace/big-data/zookeeper/zookeeper/* ksks-zookeeper*/
+	wget  http://archive.apache.org/dist/zookeeper/zookeeper-3.4.5/$tarFile -P $tempDirectory
+        if  ls $optDirectory/README* ; then
+                rm $optDirectory/README*
+        fi
+        # unpack tar ball and make changes 
+        pushd $tempDirectory
+        tar -xpf $tarFile -C .
+        rm $tarFile
+	mv zookeeper*/conf/* $confDirectory
+        popd
+	
+	# Create zookeeper related folders into "/var" directory
+	pushd $optDirectory/..
+	if [ -d "var/zookeeper" ]; then
+	        mkdir -p "var/zookeeper"
+	fi
 
-cd ksks-zookeeper*
-wget -P opt http://apache.bilkent.edu.tr/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz
-cd -
-
-fileName=`ls | grep ksks | awk '{print $1}' | head -1`
-
-lineNumberVersion=$(sed -n '/Version:/=' $fileName/DEBIAN/control)
-lineNumberPackage=$(sed -n '/Package:/=' $fileName/DEBIAN/control)
+	# edit zkServer.sh script to clean warning "[: /var/zookeeper/: unexpected operator"
+	sed -i "s/if \[ \-f \$ZOOPIDFILE \]; then/    if [ -f \"\$ZOOPIDFILE\" ]; then/g" $optDirectory/bin/zkServer.sh
 
 
-lineVersion=$(sed $lineNumberVersion!d $fileName/DEBIAN/control)
-linePackage=$(sed $lineNumberPackage!d $fileName/DEBIAN/control)
-
-version=$(echo $lineVersion | awk -F":" '{split($2,a," ");print a[1]}')
-
-versionFirst=$(echo $version | awk -F"." '{print $1}')
-versionSecond=$(echo $version | awk -F"." '{print $2}')
-versionThird=$(echo $version | awk -F"." '{print $3}')
-updatedVersion=$(echo `expr $versionThird + 1`)
-
-updatedRelease=$versionFirst.$versionSecond.$updatedVersion
-updatedFileName="ksks-zookeeper-"$updatedRelease"-amd64"
-
-replaceVersion="Version: $updatedRelease"
-sed -i $fileName/DEBIAN/control -e $lineNumberVersion's!.*!'"$replaceVersion"'!'
-
-{
-        mv $fileName $updatedFileName
-} || {
-        echo "Version not changed"
 }
-find ./$updatedFileName -name "*~" -print0 | xargs -0 rm -rf
-rm $updatedFileName/DEBIAN/md5sums
-md5sum `find ./$updatedFileName -type f | awk '/.\//{ print substr($0, 3) }'` >> $updatedFileName/DEBIAN/md5sums
-dpkg-deb -z8 -Zgzip --build $updatedFileName/
 
-cp *.deb ~/Automation/Bigdata/zookeeper
-
+# 2) Get the sources which are downloaded from version control system to local machine to relevant directories to generate the debian package
+getSourcesToRelevantDirectories $productName
+# 3) Download hadoop tar file and make necessary changes
+downloadFileAndMakeChanges $productName
+# 4) Create the Debian package
+generateDebianPackage $productName
