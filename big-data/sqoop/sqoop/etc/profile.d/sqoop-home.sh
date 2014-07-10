1313e47ceb27a1198f138fe04fac99945c722140
Salih Kardan <salihkardan@gmail.com>
2014-07-10 08:19:48 +0300

    SUBUTAI-1049 move sqoop configuration files under /etc directory
    Former-commit-id: 8ddc3c1b029a720ea590f4cfc2b46a66ad002a55

--- big-data/sqoop/sqoop/etc/profile.d/sqoop-home.sh
+++ big-data/sqoop/sqoop/etc/profile.d/sqoop-home.sh
@@ -1,7 +1,12 @@
-sqoopHome="/opt/sqoop-1.4.3"
+sqoopHome="/opt/sqoop-1.4.4"
 hadoopHome="/opt/hadoop-1.2.1"
 export SQOOP_HOME=$sqoopHome
-export PATH=$PATH:$SQOOP_HOME/bin
 export HADOOP_COMMON_HOME=$hadoopHome
 export HADOOP_MAPRED_HOME=$hadoopHome
 
+path_content=$(echo $PATH)
+pattern="$sqoopHome/bin"
+if [[ $path_content != *$pattern* ]];
+then
+	export PATH=$PATH:$pattern
+fi
