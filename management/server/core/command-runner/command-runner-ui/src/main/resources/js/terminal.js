ea55c7452564d5a6423eec975f971b782a0a39ff
daralbaev <daralbaev@gmail.com>
2014-07-11 03:50:04 +0600

    Terminal module modifications
    Former-commit-id: b96532cb18f209b2ff2bc9e762c1af0b24e24240

--- management/server/core/command-runner/command-runner-ui/src/main/resources/js/terminal.js
+++ management/server/core/command-runner/command-runner-ui/src/main/resources/js/terminal.js
@@ -1,4 +1,18 @@
-function escapeInput(target) {
-    alert("key pressed in " + target.charCode);
+function escapeInput(e) {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+        case 37:
+            // left key pressed
+            break;
+        case 38:
+            // up key pressed
+            break;
+        case 39:
+            // right key pressed
+            break;
+        case 40:
+            // down key pressed
+            break;
+    }
 }
 document.getElementById("terminal").onkeydown = escapeInput;
