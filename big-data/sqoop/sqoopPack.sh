c37b33ce8b1856d3780c0d35b0541f05c23eea41
Salih Kardan <salihkardan@gmail.com>
2014-07-10 08:52:10 +0300

    SUBUTAI-1049 move sqoop configuration files under /etc directory
    Former-commit-id: 9b5af51b8fdd8b3ef5ec4cadb0bc9eb6bf9efe0b

--- big-data/sqoop/sqoopPack.sh
+++ big-data/sqoop/sqoopPack.sh
@@ -18,33 +18,31 @@ downloadFileAndMakeChanges() {
     # download sqoop
 	wget http://archive.apache.org/dist/sqoop/$sqoopVersion/sqoop-$sqoopVersion.bin__hadoop-1.0.0.tar.gz -P $tempDirectory
 
-	# download ojdbc and mysql jar files and place them under sqoop/lib folder
-	# ojdbc 
-	wget http://www.java2s.com/Code/JarDownload/ojdbc6/ojdbc6.jar.zip -P $tempDirectory
-	pushd $tempDirectory
-	unzip ojdbc6.jar.zip
-	cp ojdbc6.jar $tempDirectory/sqoop-$sqoopVersion/lib
-	rm -r ojdbc6.jar
-	rm -r ojdbc6.jar.zip
-	popd
-	# mysql
-	wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.0.8.tar.gz -P $tempDirectory
-	tar -xpf mysql-connector-java-5.0.8.tar.gz
-	pushd mysql-connector-java-5.0.8
-	cp mysql-connector-java-5.0.8-bin.jar $tempDirectory/sqoop-$sqoopVersion/lib
-	popd
-	rm -r mysql-connector-java-5.0.8.tar.gz 
-	rm -r mysql-connector-java-5.0.8
- 
 	pushd $tempDirectory
 	# unpack tar file
-	tar -xpf sqoop-*.tar.gz
+	tar -xzpf sqoop-*.tar.gz
 
 	# remove tar file
 	rm sqoop-*.tar.gz
 
 	# remane downloaded file -- remove hadoop-1 from file name -- 
 	mv sqoop-$sqoopVersion.bin__hadoop-1* sqoop-$sqoopVersion
+
+	# download ojdbc and mysql jar files and place them under sqoop/lib folder
+	# ojdbc 
+	wget http://www.java2s.com/Code/JarDownload/ojdbc6/ojdbc6.jar.zip -P $tempDirectory
+	unzip ojdbc6.jar.zip
+	cp ojdbc6.jar $tempDirectory/sqoop-$sqoopVersion/lib
+	rm -r ojdbc6.jar
+	rm -r ojdbc6.jar.zip
+	# mysql
+	wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.0.8.tar.gz -P $tempDirectory
+	tar -xzpf mysql-connector-java-5.0.8.tar.gz
+	pushd mysql-connector-java-5.0.8
+	cp mysql-connector-java-5.0.8-bin.jar $tempDirectory/sqoop-$sqoopVersion/lib
+	popd
+	rm -r mysql-connector-java-5.0.8.tar.gz 
+	rm -r mysql-connector-java-5.0.8
 
 	# move configuration files of sqoop under etc/
 	mv sqoop-*/conf/* $confDirectory/
