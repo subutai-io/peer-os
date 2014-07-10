a12f4238eeffa03e9f1cc8546c7615b5b1ef6adf
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-10 08:13:18 +0300

    SUBUTAI-1045: Edit packaging script of Zookeeper
    Former-commit-id: b7abfeb1ed0299ba30d5b50b703f63ea5e67d4db

--- big-data/zookeeper/zookeeper/opt/zookeeper-3.4.5/bin/zookeeper-conf.sh
+++ big-data/zookeeper/zookeeper/opt/zookeeper-3.4.5/bin/zookeeper-conf.sh
@@ -1,4 +1,12 @@
-ZOOKEEPER_CONF=/opt/zookeeper-3.4.5/conf
+#!/bin/bash
+set -e
+
+. /etc/profile
+
+if [ "x$ZOOKEEPER_CONF_DIR" = "x" ];
+then
+  ZOOKEEPER_CONF_DIR="/etc/zookeeper"
+fi
 
 usage="Usage: zookeeper-conf.sh \"List of machine names which will be HQuorumPeer, do NOT use comma between machine names\""
 
@@ -9,11 +17,11 @@ if [ $# -le 0 ]; then
 fi
 
 # clean content of zoo.cfg file
-sed -i '/2888/d' $ZOOKEEPER_CONF/zoo.cfg
+sed -i '/2888/d' $ZOOKEEPER_CONF_DIR/zoo.cfg
 
 # write to zoo.cfg file
 i=1
 for arg; do
-   	echo "$i=$arg:2888:3888" >> $ZOOKEEPER_CONF/zoo.cfg
+   	echo "$i=$arg:2888:3888" >> $ZOOKEEPER_CONF_DIR/zoo.cfg
 	i=$((i + 1))
 done
