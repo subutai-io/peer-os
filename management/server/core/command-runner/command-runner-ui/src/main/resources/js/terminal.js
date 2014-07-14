54393e8f6c37c334e8c1028db8d79d4996456b16
daralbaev <daralbaev@gmail.com>
2014-07-15 02:55:44 +0600

    Terminal module modifications
    Former-commit-id: 11824b0296b06c639ad095af171b02e6e16149c9

--- management/server/core/command-runner/command-runner-ui/src/main/resources/js/terminal.js
+++ management/server/core/command-runner/command-runner-ui/src/main/resources/js/terminal.js
@@ -1,15 +1,12 @@
-var jqconsole;
-$(function () {
-    jqconsole = $('#terminal').jqconsole('Subutai Terminal\n', '$prompt');
-    var startPrompt = function () {
-        // Start the prompt with history enabled.
-        jqconsole.Prompt(true, function (input) {
-            // Output input with the class jqconsole-output.
-            jqconsole.Write(input + '\n', 'jqconsole-output');
-            callback(input);
-            // Restart the prompt.
-            startPrompt();
-        });
-    };
-    startPrompt();
-});
+var jqconsole = $('#terminal').jqconsole('Subutai Terminal\n', '$prompt');
+var startPrompt = function () {
+    // Start the prompt with history enabled.
+    jqconsole.Prompt(true, function (input) {
+        // Output input with the class jqconsole-output.
+        jqconsole.Write(input + '\n', 'jqconsole-output');
+        callback(input);
+        // Restart the prompt.
+        startPrompt();
+    });
+};
+startPrompt();
