7d20416575abebadc45e7bf3565f15b454327282
daralbaev <daralbaev@gmail.com>
2014-07-15 15:14:08 +0600

    Terminal module modifications
    Former-commit-id: 16be8af835f0f4c0988edf3f7c7531562f26ad88

--- management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/TerminalForm.java
+++ management/server/core/command-runner/command-runner-ui/src/main/java/org/safehaus/subutai/ui/commandrunner/TerminalForm.java
@@ -235,6 +235,7 @@ public class TerminalForm extends CustomComponent implements Disposable {
 	}
 
 	private void show(String notification) {
+		System.out.println(notification);
 		commandOutputTxtArea.setOutputPrompt(notification);
 //		Notification.show(notification);
 	}
