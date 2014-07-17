0470acb865449ea339628ef09651dea87d8cf0ac
bahadyr <bahadyr@safehaus.org>
2014-07-17 16:02:50 +0600

    Configuration Manager code refactor.
    Former-commit-id: 40524cbabcfad4b5180ea4bccac0e63b3cead8d8

--- management/server/core/configuration-manager/configuration-manager-impl/src/test/java/utils/PropertiesConfigurationLoaderTest.java
+++ management/server/core/configuration-manager/configuration-manager-impl/src/test/java/utils/PropertiesConfigurationLoaderTest.java
@@ -1,12 +1,16 @@
 package utils;
 
 
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 import org.safehaus.subutai.configuration.manager.impl.utils.PropertiesConfigurationLoader;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 
@@ -41,8 +45,11 @@ public class PropertiesConfigurationLoaderTest {
         Gson gsonNew = new GsonBuilder().setPrettyPrinting().create();
         System.out.println(gson.toJson( jo ));
 
+        Set<Map.Entry<String, JsonElement>> set = jo.entrySet();
+        for ( Map.Entry<String, JsonElement> stringJsonElementEntry : set ) {
+            System.out.println(stringJsonElementEntry.getKey() + " " + stringJsonElementEntry.getValue());
 
-
+        }
 
     }
 }
