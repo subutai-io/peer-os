642f03aa22267370c088194a8c61d85e522f147f
daralbaev <daralbaev@gmail.com>
2014-07-20 02:56:39 +0600

    Vaadin Json comm. testing

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/MonitorView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/MonitorView.java
@@ -18,96 +18,98 @@ import com.vaadin.ui.VerticalLayout;
 
 public class MonitorView extends VerticalLayout implements View {
 
-	private static String jsonString =
-			"    {\n" +
-					"    \"file\": \"core-site.xml\",\n" +
-					"    \"type\": \"xml\",     \n" +
-					"    \"fieldName\": \"fs.default.name\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"FS Default Name\",\n" +
-					"    \"required\": true,\n" +
-					"    \"tooltip\" : \"Enter fs default name\",\n" +
-					"    \"uiType\": \"TextField\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"hdfs-site.xml\",\n" +
-					"    \"type\": \"xml\",     \n" +
-					"    \"fieldName\": \"hadoop.tmp.dir\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Hadoop Temp Dir\",\n" +
-					"    \"required\": false,\n" +
-					"    \"tooltip\" : \"Enter replication factor\",\n" +
-					"    \"uiType\": \"TextField\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"hdfs-site.xml\",\n" +
-					"    \"type\": \"xml\",     \n" +
-					"    \"fieldName\": \"dfs.replication\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Replication\",\n" +
-					"    \"required\": true,\n" +
-					"    \"tooltip\" : \"Enter excluded nodes, one per line\",\n" +
-					"    \"uiType\": \"TextField\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"dfs.include\",\n" +
-					"    \"type\": \"plain\",       \n" +
-					"    \"fieldName\": \"\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Include\",\n" +
-					"    \"required\": false,\n" +
-					"    \"tooltip\" : \"Enter included nodes, one per line\",\n" +
-					"    \"uiType\": \"TextArea\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"dfs.exclude\",\n" +
-					"    \"type\": \"plain\",       \n" +
-					"    \"fieldName\": \"\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Include\",\n" +
-					"    \"required\": false,\n" +
-					"    \"tooltip\" : \"Enter excluded nodes, one per line\",\n" +
-					"    \"uiType\": \"TextArea\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"masters\",\n" +
-					"    \"type\": \"plain\",       \n" +
-					"    \"fieldName\": \"\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Masters\",\n" +
-					"    \"required\": false,\n" +
-					"    \"tooltip\" : \"Enter masters, one per line\",\n" +
-					"    \"uiType\": \"TextArea\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"slaves\",\n" +
-					"    \"type\": \"plain\",       \n" +
-					"    \"fieldName\": \"\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Slaves\",\n" +
-					"    \"required\": false,\n" +
-					"    \"tooltip\" : \"Enter slaves, one per line\",\n" +
-					"    \"uiType\": \"TextArea\",\n" +
-					"    \"value\": null\n" +
-					"    },\n" +
-					"    {\n" +
-					"    \"file\": \"mapred-site.xml\",\n" +
-					"    \"type\": \"xml\",     \n" +
-					"    \"fieldName\": \"mapred.job.tracker\",\n" +
-					"    \"fieldPath\": \"\",\n" +
-					"    \"label\": \"Job Tracker\",\n" +
-					"    \"required\": true,\n" +
-					"    \"tooltip\" : \"Enter Job Tracker\",\n" +
-					"    \"uiType\": \"TextField\",\n" +
-					"    \"value\": null\n" +
-					"    }\n" +
-					"";
+	private static String jsonString = "{\n" +
+			"    \"parameters\": [\n" +
+			"        {\n" +
+			"            \"file\": \"core-site.xml\",\n" +
+			"            \"type\": \"xml\",\n" +
+			"            \"fieldName\": \"fs.default.name\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"FS Default Name\",\n" +
+			"            \"required\": true,\n" +
+			"            \"tooltip\": \"Enter fs default name\",\n" +
+			"            \"uiType\": \"TextField\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"hdfs-site.xml\",\n" +
+			"            \"type\": \"xml\",\n" +
+			"            \"fieldName\": \"hadoop.tmp.dir\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Hadoop Temp Dir\",\n" +
+			"            \"required\": false,\n" +
+			"            \"tooltip\": \"Enter replication factor\",\n" +
+			"            \"uiType\": \"TextField\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"hdfs-site.xml\",\n" +
+			"            \"type\": \"xml\",\n" +
+			"            \"fieldName\": \"dfs.replication\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Replication\",\n" +
+			"            \"required\": true,\n" +
+			"            \"tooltip\": \"Enter excluded nodes, one per line\",\n" +
+			"            \"uiType\": \"TextField\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"dfs.include\",\n" +
+			"            \"type\": \"plain\",\n" +
+			"            \"fieldName\": \"\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Include\",\n" +
+			"            \"required\": false,\n" +
+			"            \"tooltip\": \"Enter included nodes, one per line\",\n" +
+			"            \"uiType\": \"TextArea\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"dfs.exclude\",\n" +
+			"            \"type\": \"plain\",\n" +
+			"            \"fieldName\": \"\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Include\",\n" +
+			"            \"required\": false,\n" +
+			"            \"tooltip\": \"Enter excluded nodes, one per line\",\n" +
+			"            \"uiType\": \"TextArea\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"masters\",\n" +
+			"            \"type\": \"plain\",\n" +
+			"            \"fieldName\": \"\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Masters\",\n" +
+			"            \"required\": false,\n" +
+			"            \"tooltip\": \"Enter masters, one per line\",\n" +
+			"            \"uiType\": \"TextArea\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"slaves\",\n" +
+			"            \"type\": \"plain\",\n" +
+			"            \"fieldName\": \"\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Slaves\",\n" +
+			"            \"required\": false,\n" +
+			"            \"tooltip\": \"Enter slaves, one per line\",\n" +
+			"            \"uiType\": \"TextArea\",\n" +
+			"            \"value\": null\n" +
+			"        },\n" +
+			"        {\n" +
+			"            \"file\": \"mapred-site.xml\",\n" +
+			"            \"type\": \"xml\",\n" +
+			"            \"fieldName\": \"mapred.job.tracker\",\n" +
+			"            \"fieldPath\": \"\",\n" +
+			"            \"label\": \"Job Tracker\",\n" +
+			"            \"required\": true,\n" +
+			"            \"tooltip\": \"Enter Job Tracker\",\n" +
+			"            \"uiType\": \"TextField\",\n" +
+			"            \"value\": null\n" +
+			"        }\n" +
+			"    ]\n" +
+			"}";
 
 	@Override
 	public void enter(ViewChangeEvent event) {
