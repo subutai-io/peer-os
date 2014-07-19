b93dd147567e451918155cd98723fe3b01670e80
daralbaev <daralbaev@gmail.com>
2014-07-20 02:24:09 +0600

    Vaadin Json comm. testing

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/MonitorView.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/views/MonitorView.java
@@ -10,20 +10,117 @@
 
 package org.safehaus.subutai.server.ui.views;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 public class MonitorView extends VerticalLayout implements View {
 
+	private static String jsonString = "\"parameters\": [\n" +
+			"    {\n" +
+			"    \"file\": \"core-site.xml\",\n" +
+			"    \"type\": \"xml\",     \n" +
+			"    \"fieldName\": \"fs.default.name\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"FS Default Name\",\n" +
+			"    \"required\": true,\n" +
+			"    \"tooltip\" : \"Enter fs default name\",\n" +
+			"    \"uiType\": \"TextField\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"hdfs-site.xml\",\n" +
+			"    \"type\": \"xml\",     \n" +
+			"    \"fieldName\": \"hadoop.tmp.dir\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Hadoop Temp Dir\",\n" +
+			"    \"required\": false,\n" +
+			"    \"tooltip\" : \"Enter replication factor\",\n" +
+			"    \"uiType\": \"TextField\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"hdfs-site.xml\",\n" +
+			"    \"type\": \"xml\",     \n" +
+			"    \"fieldName\": \"dfs.replication\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Replication\",\n" +
+			"    \"required\": true,\n" +
+			"    \"tooltip\" : \"Enter excluded nodes, one per line\",\n" +
+			"    \"uiType\": \"TextField\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"dfs.include\",\n" +
+			"    \"type\": \"plain\",       \n" +
+			"    \"fieldName\": \"\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Include\",\n" +
+			"    \"required\": false,\n" +
+			"    \"tooltip\" : \"Enter included nodes, one per line\",\n" +
+			"    \"uiType\": \"TextArea\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"dfs.exclude\",\n" +
+			"    \"type\": \"plain\",       \n" +
+			"    \"fieldName\": \"\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Include\",\n" +
+			"    \"required\": false,\n" +
+			"    \"tooltip\" : \"Enter excluded nodes, one per line\",\n" +
+			"    \"uiType\": \"TextArea\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"masters\",\n" +
+			"    \"type\": \"plain\",       \n" +
+			"    \"fieldName\": \"\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Masters\",\n" +
+			"    \"required\": false,\n" +
+			"    \"tooltip\" : \"Enter masters, one per line\",\n" +
+			"    \"uiType\": \"TextArea\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"slaves\",\n" +
+			"    \"type\": \"plain\",       \n" +
+			"    \"fieldName\": \"\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Slaves\",\n" +
+			"    \"required\": false,\n" +
+			"    \"tooltip\" : \"Enter slaves, one per line\",\n" +
+			"    \"uiType\": \"TextArea\",\n" +
+			"    \"value\": null\n" +
+			"    },\n" +
+			"    {\n" +
+			"    \"file\": \"mapred-site.xml\",\n" +
+			"    \"type\": \"xml\",     \n" +
+			"    \"fieldName\": \"mapred.job.tracker\",\n" +
+			"    \"fieldPath\": \"\",\n" +
+			"    \"label\": \"Job Tracker\",\n" +
+			"    \"required\": true,\n" +
+			"    \"tooltip\" : \"Enter Job Tracker\",\n" +
+			"    \"uiType\": \"TextField\",\n" +
+			"    \"value\": null\n" +
+			"    }\n" +
+			"]";
     @Override
     public void enter(ViewChangeEvent event) {
         setSizeFull();
         addStyleName("timeline");
 
-        Label header = new Label("Revenue by Movie Title");
-        header.addStyleName("h1");
-        addComponent(header);
+	    VerticalLayout panel = new VerticalLayout();
+	    panel.setSizeFull();
+	    panel.setMargin(true);
+	    panel.setSpacing(true);
+
+        addComponent(panel);
+
+	    JsonObject o = new JsonParser().parse(jsonString).getAsJsonObject();
+	    System.out.println(o);
     }
 }
