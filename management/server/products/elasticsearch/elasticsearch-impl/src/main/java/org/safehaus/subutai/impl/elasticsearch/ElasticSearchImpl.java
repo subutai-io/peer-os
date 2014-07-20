dbd66939bc71475c2c553c4633b3d9b67a94b719
Askat Asanaliev <aasanaliev@safehaus.org>
2014-07-20 13:37:42 +0600

    Implemented commands

--- management/server/products/elasticsearch/elasticsearch-impl/src/main/java/org/safehaus/subutai/impl/elasticsearch/ElasticSearchImpl.java
+++ management/server/products/elasticsearch/elasticsearch-impl/src/main/java/org/safehaus/subutai/impl/elasticsearch/ElasticSearchImpl.java
@@ -17,22 +17,16 @@ public class ElasticsearchImpl implements Elasticsearch {
 
     public static final String REMOVE = "apt-get --force-yes --assume-yes purge ksks-elasticsearch";
 
+    // success: 0; unrecognized: 256;
     public static final String SERVICE_START = "service elasticsearch start";
-    // unrecognized: 256; success: 0;
 
-    public static final String SERVICE_STOP = "service elasticsearch stop";
     // success: 0;
+    public static final String SERVICE_STOP = "service elasticsearch stop";
 
+    // running: 0; not running: 768; unrecognized: 256;
     public static final String SERVICE_STATUS = "service elasticsearch status";
-    // not running: 768; running: 0; unrecognized: 256
 
     public static final String CONFIG = "es-conf.sh %s %s";
-//    $ es-conf.sh cluster.name my_cluster
-//    $ es-conf.sh node.name my_node1
-//    $ es-conf.sh node.master true | false
-//    $ es-conf.sh node.data true | false
-//    $ es-conf.sh index.number_of_shards 4
-//    $ es-conf.sh index.number_of_replicas 2
 
     private CommandRunner commandRunner;
 
@@ -46,79 +40,50 @@ public class ElasticsearchImpl implements Elasticsearch {
     public AgentResult config( Agent agent, String param, String value ) {
 
         String configCommand = String.format( CONFIG, param, value );
-        System.out.println( "configCommand: " + configCommand );
 
-        Command command = commandRunner.createCommand(
-                new RequestBuilder( configCommand ),
-                Sets.newHashSet( agent ) );
-
-        commandRunner.runCommand( command );
-
-        return command.getResults().get( agent.getUuid() );
+        return runCommand( agent, configCommand );
     }
 
 
     @Override
     public AgentResult install( Agent agent ) {
-
-        Command command = commandRunner.createCommand(
-                new RequestBuilder( INSTALL ),
-                Sets.newHashSet( agent ) );
-
-        commandRunner.runCommand( command );
-
-        return command.getResults().get( agent.getUuid() );
+        return runCommand( agent, INSTALL );
     }
 
 
     @Override
     public AgentResult remove( Agent agent ) {
-
-        Command command = commandRunner.createCommand(
-                new RequestBuilder( REMOVE ),
-                Sets.newHashSet( agent ) );
-
-        commandRunner.runCommand( command );
-
-        return command.getResults().get( agent.getUuid() );
+        return runCommand( agent, REMOVE );
     }
 
 
     @Override
     public AgentResult serviceStart( Agent agent ) {
-
-        Command command = commandRunner.createCommand(
-                new RequestBuilder( SERVICE_START ),
-                Sets.newHashSet( agent ) );
-
-        commandRunner.runCommand( command );
-
-        return command.getResults().get( agent.getUuid() );
+        return runCommand( agent, SERVICE_START );
     }
 
 
     @Override
     public AgentResult serviceStop( Agent agent ) {
-
-        Command command = commandRunner.createCommand(
-                new RequestBuilder( SERVICE_STOP ),
-                Sets.newHashSet( agent ) );
-
-        commandRunner.runCommand( command );
-
-        return command.getResults().get( agent.getUuid() );
+        return runCommand( agent, SERVICE_STOP );
     }
 
 
     @Override
     public AgentResult serviceStatus( Agent agent ) {
+        return runCommand( agent, SERVICE_STATUS );
+    }
+
+
+    private AgentResult runCommand( Agent agent, String commandLine ) {
 
         Command command = commandRunner.createCommand(
-                new RequestBuilder( SERVICE_STATUS ),
+                new RequestBuilder( commandLine ),
                 Sets.newHashSet( agent ) );
 
         commandRunner.runCommand( command );
 
         return command.getResults().get( agent.getUuid() );
     }
+
 }
