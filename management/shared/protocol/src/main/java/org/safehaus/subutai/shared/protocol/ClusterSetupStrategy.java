5e47ecdd4b6f92ddaf82223116f3d5497aa71625
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-21 14:30:05 +0600

    minor bugfix

---
+++ management/shared/protocol/src/main/java/org/safehaus/subutai/shared/protocol/ClusterSetupStrategy.java
@@ -0,0 +1,12 @@
+package org.safehaus.subutai.shared.protocol;
+
+
+/**
+ * This interface must be implemented by all product modules.
+ *
+ * This is a strategy used to setup a product cluster
+ */
+public interface ClusterSetupStrategy {
+
+    public ConfigBase setup();
+}
