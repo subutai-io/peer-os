0e2bb46d9871d45c210ccb51d607b613d1ab0d61
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-22 16:10:28 +0600

    added zookeeper module for conversion

---
+++ management/server/plugins/zookeeper/zookeeper-cli/src/main/java/org/safehaus/subutai/plugin/zookeeper/cli/ListClustersCommand.java
@@ -0,0 +1,37 @@
+package org.safehaus.subutai.plugin.zookeeper.cli;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.safehaus.subutai.plugin.zookeeper.api.Config;
+import org.safehaus.subutai.plugin.zookeeper.api.Zookeeper;
+
+import java.util.List;
+
+
+/**
+ * Displays the last log entries
+ */
+@Command(scope = "zookeeper", name = "list-clusters", description = "mydescription")
+public class ListClustersCommand extends OsgiCommandSupport {
+
+    private Zookeeper zookeeperManager;
+
+    public Zookeeper getZookeeperManager() {
+        return zookeeperManager;
+    }
+
+    public void setZookeeperManager(Zookeeper zookeeperManager) {
+        this.zookeeperManager = zookeeperManager;
+    }
+
+    protected Object doExecute() {
+        List<Config> configList = zookeeperManager.getClusters();
+        if (!configList.isEmpty())
+            for (Config config : configList) {
+                System.out.println(config.getClusterName());
+            }
+        else System.out.println("No Zookeeper cluster");
+
+        return null;
+    }
+}
