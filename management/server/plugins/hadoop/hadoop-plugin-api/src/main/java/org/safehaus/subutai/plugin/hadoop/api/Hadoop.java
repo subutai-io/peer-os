1f9ca25cc756943726f09b697f79c8b725940b18
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-24 16:44:36 +0600

    Converting Hadoop

---
+++ management/server/plugins/hadoop/hadoop-plugin-api/src/main/java/org/safehaus/subutai/plugin/hadoop/api/Hadoop.java
@@ -0,0 +1,48 @@
+package org.safehaus.subutai.plugin.hadoop.api;
+
+import org.safehaus.subutai.shared.operation.ProductOperation;
+import org.safehaus.subutai.shared.protocol.Agent;
+import org.safehaus.subutai.shared.protocol.ApiBase;
+import org.safehaus.subutai.shared.protocol.ClusterSetupStrategy;
+
+import java.util.UUID;
+
+/**
+ * Created by daralbaev on 02.04.14.
+ */
+public interface Hadoop extends ApiBase<HadoopClusterConfig> {
+
+	public UUID startNameNode(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID stopNameNode(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID restartNameNode(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID statusNameNode(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID statusSecondaryNameNode(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID statusDataNode(Agent agent);
+
+	public UUID startJobTracker(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID stopJobTracker(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID restartJobTracker(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID statusJobTracker(HadoopClusterConfig hadoopClusterConfig );
+
+	public UUID statusTaskTracker(Agent agent);
+
+	public UUID addNode(String clusterName);
+
+	public UUID blockDataNode(HadoopClusterConfig hadoopClusterConfig, Agent agent);
+
+	public UUID blockTaskTracker(HadoopClusterConfig hadoopClusterConfig, Agent agent);
+
+	public UUID unblockDataNode(HadoopClusterConfig hadoopClusterConfig, Agent agent);
+
+	public UUID unblockTaskTracker(HadoopClusterConfig hadoopClusterConfig, Agent agent);
+
+	public ClusterSetupStrategy getClusterSetupStrategy(ProductOperation po, HadoopClusterConfig hadoopClusterConfig );
+}
---
+++ management/server/plugins/hadoop/hadoop-plugin-ui/src/main/java/org/safehaus/subutai/plugin/hadoop/ui/manager/components/SecondaryNameNode.java
@@ -0,0 +1,56 @@
+package org.safehaus.subutai.plugin.hadoop.ui.manager.components;
+
+import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
+import org.safehaus.subutai.plugin.hadoop.ui.HadoopUI;
+import org.safehaus.subutai.shared.protocol.CompleteEvent;
+import org.safehaus.subutai.shared.protocol.enums.NodeState;
+
+import java.util.UUID;
+
+/**
+ * Created by daralbaev on 12.04.14.
+ */
+public class SecondaryNameNode extends ClusterNode {
+
+	public SecondaryNameNode(HadoopClusterConfig cluster) {
+		super(cluster);
+		setHostname(cluster.getSecondaryNameNode().getHostname());
+
+		restartButton.setVisible(false);
+		startButton.setEnabled(false);
+		stopButton.setEnabled(false);
+
+		getStatus(null);
+	}
+
+
+	@Override
+	protected void getStatus(UUID trackID) {
+		setLoading(true);
+
+		HadoopUI.getExecutor().execute(new CheckTask(cluster, new CompleteEvent() {
+
+			public void onComplete(NodeState state) {
+				synchronized (progressButton) {
+					boolean isRunning = false;
+					if (state == NodeState.RUNNING) {
+						isRunning = true;
+					} else if (state == NodeState.STOPPED) {
+						isRunning = false;
+					}
+
+					setLoading(false);
+					startButton.setVisible(isRunning);
+					stopButton.setVisible(!isRunning);
+				}
+			}
+		}, trackID, cluster.getSecondaryNameNode()));
+	}
+
+	@Override
+	protected void setLoading(boolean isLoading) {
+		startButton.setVisible(!isLoading);
+		stopButton.setVisible(!isLoading);
+		progressButton.setVisible(isLoading);
+	}
+}
