ae32c4189aaa3301ccac394c5eeb287801868522
Furkan Bicak <bicak.cs@safehaus.org>
2014-07-24 22:05:31 +0300

    Disable make when there are uncommitted changes

--- cli/debian/update-version.sh
+++ cli/debian/update-version.sh
@@ -24,22 +24,16 @@ function isEmpty {
 
 function exitIfNoChange {
   local changelogFile=$1
-  # Ignore changes in this file before decision
-  git checkout -- $changelogFile > /dev/null 2>&1
   branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
   branch_name="(unnamed branch)"     # detached HEAD
   branch_name=${branch_name##refs/heads/}
-  # Check if there are uncommitted changes
-  git_status=$(git status | grep "cli/")
-  isStatusEmpty=$(isEmpty $git_status)
   
-  # Check if there are local commits not pushed
+  # Check if there are local commits
   git_diff=$(git diff origin/$branch_name..HEAD)
   isDiffEmpty=$(isEmpty $git_diff)
-  echo "isStatusEmpty: $isStatusEmpty"
   echo "isDiffEmpty: $isDiffEmpty"
   echo "Checking if there is a change for branch: $branch_name"
-  if [ $isStatusEmpty = "true" -a $isDiffEmpty = "true" ]; then
+  if [ $isDiffEmpty = "true" ]; then
     echo "No change is made on debian package"
     exit 0
   fi
@@ -78,8 +72,8 @@ require_clean_work_tree () {
 changelogFile="debian/changelog"
 # Switch to root .git directory to see changes properly
 pushd ..
-#exitIfNoChange $changelogFile
 require_clean_work_tree
+exitIfNoChange $changelogFile
 popd
 
 #----------------UPDATE_VERSION-----------------------
@@ -114,15 +108,12 @@ sed -i "s/$version/$updatedVersion/1" $changelogFile
 #------------------------------------------------------
 #(3) commit and push with incremented patch version number (X+1) if there are uncommitted changes
 #------------------------------------------------------
-if [ $isStatusEmpty = "false" ]; then
-  git add .
-  git commit -m "Auto commit while building subutai-cli package"
-  isSuccesful=$?
-else
-  isSuccesful=0
-fi
+git add .
+git commit -m "Auto commit while building subutai-cli package"
+isSuccesful=$?
 git push origin $branch_name
 isSuccesful=`expr $? + $isSuccesful`
+
 #------------------------------------------------------
 #(4) if commit and push worked then generate the package with the new (X+1) version number which must be unique, else exit
 #------------------------------------------------------
