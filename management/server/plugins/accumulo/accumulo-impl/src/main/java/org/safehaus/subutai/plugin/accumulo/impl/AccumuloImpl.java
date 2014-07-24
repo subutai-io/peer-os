f02a4a881c626c27d1ea1aa50e7912256ef2508b
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-24 20:23:55 +0600

    Refactoring EnvironmentBuilder

--- management/server/plugins/accumulo/accumulo-impl/src/main/java/org/safehaus/subutai/plugin/accumulo/impl/AccumuloImpl.java
+++ management/server/plugins/accumulo/accumulo-impl/src/main/java/org/safehaus/subutai/plugin/accumulo/impl/AccumuloImpl.java
@@ -12,7 +12,7 @@ import org.safehaus.subutai.plugin.accumulo.api.NodeType;
 import org.safehaus.subutai.api.agentmanager.AgentManager;
 import org.safehaus.subutai.api.commandrunner.CommandRunner;
 import org.safehaus.subutai.api.dbmanager.DbManager;
-import org.safehaus.subutai.api.hadoop.Hadoop;
+import org.safehaus.subutai.plugin.hadoop.api.Hadoop;
 import org.safehaus.subutai.api.tracker.Tracker;
 import org.safehaus.subutai.plugin.zookeeper.api.Zookeeper;
 import org.safehaus.subutai.plugin.accumulo.impl.handler.AddNodeOperationHandler;
