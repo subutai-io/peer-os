95c410832dd98a52a14e010c737a7f3863834289
daralbaev <daralbaev@gmail.com>
2014-07-25 22:47:43 +0600

    Just last commit before rest

--- management/server/plugins/hadoop/hadoop-plugin-impl/src/main/java/org/safehaus/subutai/plugin/hadoop/impl/HadoopDbSetupStrategy.java
+++ management/server/plugins/hadoop/hadoop-plugin-impl/src/main/java/org/safehaus/subutai/plugin/hadoop/impl/HadoopDbSetupStrategy.java
@@ -147,23 +147,23 @@ public class HadoopDbSetupStrategy implements ClusterSetupStrategy {
         }*/
 
 		po.addLog("Hadoop installation started");
-		InstallHadoopOperation installOperation = new InstallHadoopOperation(hadoopClusterConfig);
-		for (Command command : installOperation.getCommandList()) {
-			po.addLog((String.format("%s started...", command.getDescription())));
-			HadoopImpl.getCommandRunner().runCommand(command);
-
-			if (command.hasSucceeded()) {
-				po.addLogDone(String.format("%s succeeded", command.getDescription()));
-			} else {
-				po.addLogFailed(
-						String.format("%s failed, %s", command.getDescription(), command.getAllErrors()));
-			}
-		}
-
 		if (HadoopImpl.getDbManager()
 				.saveInfo(HadoopClusterConfig.PRODUCT_KEY, hadoopClusterConfig.getClusterName(),
 						hadoopClusterConfig)) {
 			po.addLog("Cluster info saved to DB");
+
+			InstallHadoopOperation installOperation = new InstallHadoopOperation(hadoopClusterConfig);
+			for (Command command : installOperation.getCommandList()) {
+				po.addLog((String.format("%s started...", command.getDescription())));
+				HadoopImpl.getCommandRunner().runCommand(command);
+
+				if (command.hasSucceeded()) {
+					po.addLogDone(String.format("%s succeeded", command.getDescription()));
+				} else {
+					po.addLogFailed(
+							String.format("%s failed, %s", command.getDescription(), command.getAllErrors()));
+				}
+			}
 		} else {
 			destroyLXC(po, "Could not save cluster info to DB! Please see logs\nInstallation aborted");
 		}
