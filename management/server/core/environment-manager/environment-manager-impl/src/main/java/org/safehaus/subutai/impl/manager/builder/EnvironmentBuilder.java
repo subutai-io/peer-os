2509ab86ddeff5f98c13984387dfafd242ae58dd
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-07-25 18:51:13 +0600

    Refactoring EnvironmentBuilder

--- management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/impl/manager/builder/EnvironmentBuilder.java
+++ management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/impl/manager/builder/EnvironmentBuilder.java
@@ -7,14 +7,15 @@ import java.util.Set;
 import org.safehaus.subutai.api.agentmanager.AgentManager;
 import org.safehaus.subutai.api.container.ContainerManager;
 import org.safehaus.subutai.api.lxcmanager.LxcCreateException;
+import org.safehaus.subutai.api.manager.exception.EnvironmentBuildException;
 import org.safehaus.subutai.api.manager.exception.EnvironmentDestroyException;
 import org.safehaus.subutai.api.manager.helper.Environment;
 import org.safehaus.subutai.api.manager.helper.EnvironmentBlueprint;
 import org.safehaus.subutai.api.manager.helper.Node;
 import org.safehaus.subutai.api.manager.helper.NodeGroup;
 import org.safehaus.subutai.api.manager.helper.PlacementStrategy;
+import org.safehaus.subutai.api.templateregistry.Template;
 import org.safehaus.subutai.api.templateregistry.TemplateRegistryManager;
-import org.safehaus.subutai.api.manager.exception.EnvironmentBuildException;
 import org.safehaus.subutai.shared.protocol.Agent;
 
 
@@ -34,27 +35,35 @@ public class EnvironmentBuilder {
     }
 
 
-    //@todo Baha handle all exceptional situations like if physical agent is not connected, if template is not found etc
     public Environment build( final EnvironmentBlueprint blueprint, ContainerManager containerManager )
             throws EnvironmentBuildException {
-        Environment environment = new Environment();
-        environment.setName( blueprint.getName() );
+        Environment environment = new Environment( blueprint.getName() );
         for ( NodeGroup nodeGroup : blueprint.getNodeGroups() ) {
             PlacementStrategy placementStrategy = nodeGroup.getPlacementStrategy();
-            int nodeCount = nodeGroup.getNumberOfNodes();
+            if ( nodeGroup.getNumberOfNodes() <= 0 ) {
+
+            }
 
             Set<Node> nodes = new HashSet<>();
             Set<Agent> physicalAgents = new HashSet<>();
             for ( String host : nodeGroup.getPhysicalNodes() ) {
-                physicalAgents.add( agentManager.getAgentByHostname( host ) );
+                Agent pAgent = agentManager.getAgentByHostname( host );
+                if ( pAgent == null ) {
+                    throw new EnvironmentBuildException( String.format( "Physical agent %s is not connected", host ) );
+                }
+                physicalAgents.add( pAgent );
+            }
+            Template template = templateRegistryManager.getTemplate( nodeGroup.getTemplateName() );
+            if ( template == null ) {
+                throw new EnvironmentBuildException(
+                        String.format( "Template %s not registered", nodeGroup.getTemplateName() ) );
             }
             try {
                 Set<Agent> agents = containerManager
-                        .clone( environment.getUuid(), nodeGroup.getTemplateName(), nodeCount, physicalAgents,
-                                placementStrategy );
-
+                        .clone( environment.getUuid(), template.getTemplateName(), nodeGroup.getNumberOfNodes(),
+                                physicalAgents, placementStrategy );
                 for ( Agent agent : agents ) {
-                    nodes.add( new Node( agent, templateRegistryManager.getTemplate( nodeGroup.getTemplateName() ) ) );
+                    nodes.add( new Node( agent, template, nodeGroup.getName() ) );
                 }
             }
             catch ( LxcCreateException ex ) {
