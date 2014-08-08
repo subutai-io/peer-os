27b3d0a45b89642f4ae2e1a132fba9cd074c6744
Salih Kardan <salihkardan@gmail.com>
2014-08-08 16:19:31 +0300

    fixing scripts of all debian packages that exports PATH variable

--- big-data/hive/hive/etc/profile.d/hive-home.sh
+++ big-data/hive/hive/etc/profile.d/hive-home.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
 hiveHome="/opt/hive"
 hiveConf="/etc/hive"
 export HIVE_HOME=$hiveHome
 export HIVE_CONF_DIR=$hiveConf
-export PATH=$PATH:$HIVE_HOME/bin
+
+path_content=$(echo $PATH)
+
+pattern="$hiveHome/bin"
+if test "${path_content#*$pattern}" = "$path_content"
+then
+	export PATH=$PATH:$pattern
+fi
--- big-data/oozie-server/oozie-server/etc/profile.d/oozie-server-home.sh
+++ big-data/oozie-server/oozie-server/etc/profile.d/oozie-server-home.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
 oozieHome="/opt/oozie-3.3.2"
 export OOZIE_HOME=$oozieHome
-export PATH=$PATH:$OOZIE_HOME/bin
+
+path_content=$(echo $PATH)
+
+pattern="$oozieHome/bin"
+if test "${path_content#*$pattern}" = "$path_content"
+then
+	export PATH=$PATH:$pattern
+fi
