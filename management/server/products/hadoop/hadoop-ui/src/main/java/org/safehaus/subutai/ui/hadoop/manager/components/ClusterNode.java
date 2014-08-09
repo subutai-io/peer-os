837a9bcebe6b7b4956ecb64c7ce1acf97ad00578
daralbaev <daralbaev@gmail.com>
2014-08-10 01:09:39 +0600

    hadoop hostnames delimiter is \n

--- management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/subutai/ui/hadoop/manager/components/ClusterNode.java
+++ management/server/products/hadoop/hadoop-ui/src/main/java/org/safehaus/subutai/ui/hadoop/manager/components/ClusterNode.java
@@ -48,7 +48,7 @@ public class ClusterNode extends HorizontalLayout {
 	}
 
 	public void setHostname(String value) {
-		hostname.setValue(value.replace("-", "\\n"));
+		hostname.setValue(value.replaceAll("-", "\n") + "\n");
 	}
 
 	private Embedded getProgressButton() {
