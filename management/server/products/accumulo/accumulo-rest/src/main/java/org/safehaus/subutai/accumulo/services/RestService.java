cd479219601603c758605a498bbe293f5c4c61c0
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-08-11 20:51:48 +0600

    Added REST to tracker

--- management/server/products/accumulo/accumulo-rest/src/main/java/org/safehaus/subutai/accumulo/services/RestService.java
+++ management/server/products/accumulo/accumulo-rest/src/main/java/org/safehaus/subutai/accumulo/services/RestService.java
@@ -3,14 +3,54 @@ package org.safehaus.subutai.accumulo.services;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 
 public interface RestService {
 
     @GET
-    @Path("list_clusters")
+    @Path( "list_clusters" )
     @Produces( { MediaType.APPLICATION_JSON } )
     public String listClusters();
+
+    @GET
+    @Path( "get_cluster/{clustername}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String getCluster( @PathParam( "clustername" ) String source );
+
+    @GET
+    @Path( "destroy_cluster/{clustername}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String destroyCluster( @PathParam( "clustername" ) String clusterName );
+
+    @GET
+    @Path( "start_cluster/{clustername}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String startCluster( @PathParam( "clustername" ) String clusterName );
+
+    @GET
+    @Path( "stop_cluster/{clustername}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String stopCluster( @PathParam( "clustername" ) String clusterName );
+
+    @GET
+    @Path( "create_cluster" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String createCluster( @QueryParam( "config" ) String config );
+
+    @GET
+    @Path( "add_node" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String addNode( @QueryParam( "clustername" ) String clustername,
+                           @QueryParam( "lxchostname" ) String lxchostname, @QueryParam( "nodetype" ) String nodetype );
+
+    @GET
+    @Path( "destroy_node" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String destroyNode( @QueryParam( "clustername" ) String clustername,
+                               @QueryParam( "lxchostname" ) String lxchostname,
+                               @QueryParam( "nodetype" ) String nodetype );
 }
--- management/server/products/accumulo/accumulo-rest/src/main/java/org/safehaus/subutai/accumulo/services/RestServiceImpl.java
+++ management/server/products/accumulo/accumulo-rest/src/main/java/org/safehaus/subutai/accumulo/services/RestServiceImpl.java
@@ -1,14 +1,24 @@
 package org.safehaus.subutai.accumulo.services;
 
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 import org.safehaus.subutai.api.accumulo.Accumulo;
+import org.safehaus.subutai.api.accumulo.Config;
+import org.safehaus.subutai.api.accumulo.NodeType;
+import org.safehaus.subutai.api.agentmanager.AgentManager;
+import org.safehaus.subutai.shared.protocol.Agent;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 
 /**
- * Created by bahadyr on 5/6/14.
+ * REST implementation of Accumulo API
  */
 
 public class RestServiceImpl implements RestService {
@@ -16,6 +26,12 @@ public class RestServiceImpl implements RestService {
     private static final Gson gson =
             new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     private Accumulo accumuloManager;
+    private AgentManager agentManager;
+
+
+    public void setAgentManager( final AgentManager agentManager ) {
+        this.agentManager = agentManager;
+    }
 
 
     public void setAccumuloManager( final Accumulo accumuloManager ) {
@@ -23,8 +39,82 @@ public class RestServiceImpl implements RestService {
     }
 
 
+    private String wrapUUID( UUID uuid ) {
+        Map map = new HashMap<>();
+        map.put( "OPERATION_ID", uuid );
+        return gson.toJson( map );
+    }
+
+
     @Override
     public String listClusters() {
         return gson.toJson( accumuloManager.getClusters() );
+    }
+
+
+    @Override
+    public String getCluster( final String source ) {
+        return gson.toJson( accumuloManager.getCluster( source ) );
+    }
+
+
+    @Override
+    public String destroyCluster( final String clusterName ) {
+        return wrapUUID( accumuloManager.uninstallCluster( clusterName ) );
+    }
+
+
+    @Override
+    public String startCluster( final String clusterName ) {
+        return wrapUUID( accumuloManager.startCluster( clusterName ) );
+    }
+
+
+    @Override
+    public String stopCluster( final String clusterName ) {
+        return wrapUUID( accumuloManager.stopCluster( clusterName ) );
+    }
+
+
+    @Override
+    public String createCluster( final String config ) {
+        TrimmedAccumuloConfig trimmedAccumuloConfig = gson.fromJson( config, TrimmedAccumuloConfig.class );
+        Config expandedConfig = new Config();
+        expandedConfig.setClusterName( trimmedAccumuloConfig.getClusterName() );
+        expandedConfig.setInstanceName( trimmedAccumuloConfig.getInstanceName() );
+        expandedConfig.setPassword( trimmedAccumuloConfig.getPassword() );
+        expandedConfig.setMasterNode( agentManager.getAgentByHostname( trimmedAccumuloConfig.getMasterNode() ) );
+        expandedConfig.setGcNode( agentManager.getAgentByHostname( trimmedAccumuloConfig.getGcNode() ) );
+        expandedConfig.setMonitor( agentManager.getAgentByHostname( trimmedAccumuloConfig.getMonitor() ) );
+
+        Set<Agent> tracers = new HashSet<>();
+        Set<Agent> slaves = new HashSet<>();
+        for ( String tracer : trimmedAccumuloConfig.getTracers() ) {
+            tracers.add( agentManager.getAgentByHostname( tracer ) );
+        }
+        for ( String slave : trimmedAccumuloConfig.getSlaves() ) {
+            slaves.add( agentManager.getAgentByHostname( slave ) );
+        }
+
+        expandedConfig.setTracers( tracers );
+        expandedConfig.setSlaves( slaves );
+
+        return wrapUUID( accumuloManager.installCluster( expandedConfig ) );
+    }
+
+
+    @Override
+    public String addNode( final String clustername, final String lxchostname, final String nodetype ) {
+        NodeType accumuloNodeType = NodeType.valueOf( nodetype.toUpperCase() );
+
+        return wrapUUID( accumuloManager.addNode( clustername, lxchostname, accumuloNodeType ) );
+    }
+
+
+    @Override
+    public String destroyNode( final String clustername, final String lxchostname, final String nodetype ) {
+        NodeType accumuloNodeType = NodeType.valueOf( nodetype.toUpperCase() );
+
+        return wrapUUID( accumuloManager.destroyNode( clustername, lxchostname, accumuloNodeType ) );
     }
 }
