c7c0b79a7b0d7f003ffa1d89bc4061281c572e16
daralbaev <daralbaev@gmail.com>
2014-08-12 14:47:14 +0600

    1) Remove hostname entry from 127.0.0.1 2) Add ip address info to /etc/hosts of container itself

--- management/server/products/hadoop/hadoop-impl/src/main/java/org/safehaus/subutai/impl/hadoop/operation/Installation.java
+++ management/server/products/hadoop/hadoop-impl/src/main/java/org/safehaus/subutai/impl/hadoop/operation/Installation.java
@@ -5,114 +5,119 @@ import org.safehaus.subutai.api.commandrunner.Command;
 import org.safehaus.subutai.api.hadoop.Config;
 import org.safehaus.subutai.api.lxcmanager.LxcCreateException;
 import org.safehaus.subutai.api.lxcmanager.LxcDestroyException;
-import org.safehaus.subutai.shared.operation.ProductOperation;
 import org.safehaus.subutai.impl.hadoop.HadoopImpl;
 import org.safehaus.subutai.impl.hadoop.operation.common.InstallHadoopOperation;
+import org.safehaus.subutai.shared.operation.ProductOperation;
 import org.safehaus.subutai.shared.protocol.Agent;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by daralbaev on 08.04.14.
  */
 public class Installation {
-    private HadoopImpl parent;
-    private Config config;
+	private HadoopImpl parent;
+	private Config config;
 
-    public Installation(HadoopImpl parent, Config config) {
-        this.parent = parent;
-        this.config = config;
-    }
+	public Installation(HadoopImpl parent, Config config) {
+		this.parent = parent;
+		this.config = config;
+	}
 
-    private void destroyLXC(ProductOperation po, String log) {
-        //destroy all lxcs also
-        try {
-            parent.getLxcManager().destroyLxcs(new HashSet<Agent>(config.getAllNodes()));
-            if (parent.getDbManager().deleteInfo(Config.PRODUCT_KEY, config.getClusterName())) {
-                po.addLogDone("Cluster info deleted from DB\nDone");
-            } else {
-                po.addLogFailed("Error while deleting cluster info from DB. Check logs.\nFailed");
-            }
-        } catch (LxcDestroyException ex) {
-            po.addLogFailed(log + "\nUse LXC module to cleanup");
-        }
-        po.addLogFailed(log);
-    }
+	private void destroyLXC(ProductOperation po, String log) {
+		//destroy all lxcs also
+		try {
+			parent.getLxcManager().destroyLxcs(new HashSet<>(config.getAllNodes()));
+			if (parent.getDbManager().deleteInfo(Config.PRODUCT_KEY, config.getClusterName())) {
+				po.addLogDone("Cluster info deleted from DB\nDone");
+			} else {
+				po.addLogFailed("Error while deleting cluster info from DB. Check logs.\nFailed");
+			}
+		} catch (LxcDestroyException ex) {
+			po.addLogFailed(log + "\nUse LXC module to cleanup");
+		}
+		po.addLogFailed(log);
+	}
 
-    private void setMasterNodes(Set<Agent> agents) {
-        if (agents != null && agents.size() >= 3) {
-            Agent[] arr = agents.toArray(new Agent[agents.size()]);
-            config.setNameNode(arr[0]);
-            config.setJobTracker(arr[1]);
-            config.setSecondaryNameNode(arr[2]);
-        }
-    }
+	private void setMasterNodes(Set<Agent> agents) {
+		if (agents != null && agents.size() >= 3) {
+			Agent[] arr = agents.toArray(new Agent[agents.size()]);
+			config.setNameNode(arr[0]);
+			config.setJobTracker(arr[1]);
+			config.setSecondaryNameNode(arr[2]);
+		}
+	}
 
-    private void setSlaveNodes(Set<Agent> agents) {
-        if(agents != null) {
-            config.getDataNodes().addAll(agents);
-            config.getTaskTrackers().addAll(agents);
-        }
-    }
+	private void setSlaveNodes(Set<Agent> agents) {
+		if (agents != null) {
+			config.getDataNodes().addAll(agents);
+			config.getTaskTrackers().addAll(agents);
+		}
+	}
 
-    public UUID execute() {
-        final ProductOperation po = parent.getTracker().createProductOperation(Config.PRODUCT_KEY, "Installation of Hadoop");
+	public UUID execute() {
+		final ProductOperation po = parent.getTracker().createProductOperation(Config.PRODUCT_KEY, "Installation of Hadoop");
 
-        parent.getExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
+		parent.getExecutor().execute(new Runnable() {
+			@Override
+			public void run() {
 
-                if (config == null ||
-                        Strings.isNullOrEmpty(config.getClusterName()) ||
-                        Strings.isNullOrEmpty(config.getDomainName())) {
-                    po.addLogFailed("Malformed configuration\nHadoop installation aborted");
-                    return;
-                }
+				if (config == null ||
+						Strings.isNullOrEmpty(config.getClusterName()) ||
+						Strings.isNullOrEmpty(config.getDomainName())) {
+					po.addLogFailed("Malformed configuration\nHadoop installation aborted");
+					return;
+				}
 
-                try {
-                    po.addLog(String.format("Creating %d lxc containers...", config.getCountOfSlaveNodes() + 3));
-                    Map<String, Set<Agent>> nodes = CustomPlacementStrategy.getNodes(
-                            parent.getLxcManager(), 3, config.getCountOfSlaveNodes());
+				try {
+					po.addLog(String.format("Creating %d lxc containers...", config.getCountOfSlaveNodes() + 3));
+					Map<String, Set<Agent>> nodes = CustomPlacementStrategy.getNodes(
+							parent.getLxcManager(), 3, config.getCountOfSlaveNodes());
 
-                    setMasterNodes(nodes.get(CustomPlacementStrategy.MASTER_NODE_TYPE));
-                    setSlaveNodes(nodes.get(CustomPlacementStrategy.SLAVE_NODE_TYPE));
-                    po.addLog("Lxc containers created successfully\nConfiguring network...");
+					setMasterNodes(nodes.get(CustomPlacementStrategy.MASTER_NODE_TYPE));
+					setSlaveNodes(nodes.get(CustomPlacementStrategy.SLAVE_NODE_TYPE));
+					po.addLog("Lxc containers created successfully\nConfiguring network...");
 
-                    if ( parent.getNetworkManager().configHostsOnAgents( config.getAllNodes(), config.getDomainName() )
-                            && parent.getNetworkManager().configSshOnAgents( config.getAllNodes() ) ) {
+					if (parent.getNetworkManager().configHostsOnAgents(config.getAllNodes(), config.getDomainName())
+							&& parent.getNetworkManager().configSshOnAgents(config.getAllNodes())) {
 
-                        po.addLog( "Cluster network configured" );
-                        po.addLog( "Hadoop installation started" );
+						po.addLog("Cluster network configured");
+						po.addLog("Hadoop installation started");
 
-                        InstallHadoopOperation installOperation = new InstallHadoopOperation( config );
+						if (parent.getDbManager().saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
+							po.addLog("Cluster info saved to DB");
+						} else {
+							destroyLXC(po, "Could not save cluster info to DB! Please see logs\nInstallation aborted");
+						}
 
-                        for ( Command command : installOperation.getCommandList() ) {
-                            po.addLog( ( String.format( "%s started...", command.getDescription() ) ) );
-                            HadoopImpl.getCommandRunner().runCommand( command );
+						InstallHadoopOperation installOperation = new InstallHadoopOperation(config);
 
-                            if ( command.hasSucceeded() ) {
-                                po.addLogDone( String.format( "%s succeeded", command.getDescription() ) );
-                            }
-                            else {
-                                po.addLogFailed( String.format( "%s failed, %s", command.getDescription(),
-                                        command.getAllErrors() ) );
-                            }
-                        }
+						for (Command command : installOperation.getCommandList()) {
+							po.addLog((String.format("%s started...", command.getDescription())));
+							HadoopImpl.getCommandRunner().runCommand(command);
 
-                        if (parent.getDbManager().saveInfo(Config.PRODUCT_KEY, config.getClusterName(), config)) {
-                            po.addLog("Cluster info saved to DB");
-                        } else {
-                            destroyLXC(po, "Could not save cluster info to DB! Please see logs\nInstallation aborted");
-                        }
-                    } else {
-                        destroyLXC(po, "Could not configure network! Please see logs\nLXC creation aborted");
-                    }
-                } catch (LxcCreateException ex) {
-                    po.addLogFailed(ex.getMessage());
-                }
-            }
-        });
+							if (command.hasSucceeded()) {
+								po.addLog(String.format("%s succeeded", command.getDescription()));
+							} else {
+								po.addLogFailed(String.format("%s failed, %s", command.getDescription(),
+										command.getAllErrors()));
+							}
+						}
 
-        return po.getId();
-    }
+						po.addLogDone(String.format("Cluster '%s' \nInstallation finished",
+								config.getClusterName()));
+					} else {
+						destroyLXC(po, "Could not configure network! Please see logs\nLXC creation aborted");
+					}
+				} catch (LxcCreateException ex) {
+					po.addLogFailed(ex.getMessage());
+				}
+			}
+		});
+
+		return po.getId();
+	}
 }
