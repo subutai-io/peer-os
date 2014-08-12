32066c5ee7352497723144c2a24b1c43afbc7b89
Askat Asanaliev <aasanaliev@safehaus.org>
2014-08-12 18:49:09 +0600

    Configured REST config

--- management/server/products/elasticsearch/elasticsearch-rest/src/main/java/org/safehaus/subutai/elasticsearch/rest/RestService.java
+++ management/server/products/elasticsearch/elasticsearch-rest/src/main/java/org/safehaus/subutai/elasticsearch/rest/RestService.java
@@ -1,19 +1,48 @@
 package org.safehaus.subutai.elasticsearch.rest;
 
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.safehaus.subutai.api.agentmanager.AgentManager;
+import org.safehaus.subutai.api.elasticsearch.Elasticsearch;
+import org.safehaus.subutai.shared.protocol.Agent;
 
-public interface RestService {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+public class RestService {
+
+    private static final Gson gson =
+            new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+
+    private Elasticsearch elasticsearch;
+    private AgentManager agentManager;
+
+
+    public void setAgentManager( AgentManager agentManager ) {
+        this.agentManager = agentManager;
+    }
+
+
+    public void setElasticsearch( Elasticsearch elasticsearch ) {
+        this.elasticsearch = elasticsearch;
+    }
+
 
     @GET
-    @Path( "list_clusters" )
+    @Path( "list" )
     @Produces( { MediaType.APPLICATION_JSON } )
-    public String listClusters();
-
+    public String listClusters() {
+        return gson.toJson( "list clusters" );
+    }
 }
