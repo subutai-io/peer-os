bb16e968573a6c245d89eac031ea1e4201b9b023
Askat Asanaliev <aasanaliev@safehaus.org>
2014-08-12 20:19:56 +0600

    REST service for cluster installation

--- management/server/products/elasticsearch/elasticsearch-rest/src/main/java/org/safehaus/subutai/elasticsearch/rest/RestService.java
+++ management/server/products/elasticsearch/elasticsearch-rest/src/main/java/org/safehaus/subutai/elasticsearch/rest/RestService.java
@@ -2,11 +2,15 @@ package org.safehaus.subutai.elasticsearch.rest;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.safehaus.subutai.api.agentmanager.AgentManager;
@@ -36,6 +40,14 @@ public class RestService {
     }
 
 
+    private String wrapUUID( UUID uuid ) {
+        HashMap map = new HashMap();
+        map.put( "OPERATION_ID", uuid );
+
+        return gson.toJson( map );
+    }
+
+
     @GET
     @Path( "list-clusters" )
     @Produces( { MediaType.APPLICATION_JSON } )
@@ -49,5 +61,31 @@ public class RestService {
         }
 
         return gson.toJson( clusterNames );
+    }
+
+
+    @GET
+    @Path( "installCluster" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public String installCluster(
+            @QueryParam( "clusterName" ) String clusterName,
+            @QueryParam( "numberOfNodes" ) int numberOfNodes,
+            @QueryParam( "numberOfMasterNodes" ) int numberOfMasterNodes,
+            @QueryParam( "numberOfDataNodes" ) int numberOfDataNodes,
+            @QueryParam( "numberOfShards" ) int numberOfShards,
+            @QueryParam( "numberOfReplicas" ) int numberOfReplicas
+    ) {
+
+        Config config = new Config();
+        config.setClusterName( clusterName );
+        config.setNumberOfNodes( numberOfNodes );
+        config.setNumberOfMasterNodes( numberOfMasterNodes );
+        config.setNumberOfDataNodes( numberOfDataNodes );
+        config.setNumberOfShards( numberOfShards );
+        config.setNumberOfReplicas( numberOfReplicas );
+
+        UUID uuid = elasticsearch.installCluster( config );
+
+        return wrapUUID( uuid );
     }
 }
