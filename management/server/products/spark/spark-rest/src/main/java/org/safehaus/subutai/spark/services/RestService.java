3bf78b65afc44cf45cc07dc8ea2048fe5431fe85
daralbaev <daralbaev@gmail.com>
2014-08-13 23:07:00 +0600

    Spark REST service added

--- management/server/products/spark/spark-rest/src/main/java/org/safehaus/subutai/spark/services/RestService.java
+++ management/server/products/spark/spark-rest/src/main/java/org/safehaus/subutai/spark/services/RestService.java
@@ -1,19 +1,138 @@
 package org.safehaus.subutai.spark.services;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import org.safehaus.subutai.api.agentmanager.AgentManager;
+import org.safehaus.subutai.api.spark.Config;
+import org.safehaus.subutai.api.spark.Spark;
+import org.safehaus.subutai.common.JsonUtil;
+import org.safehaus.subutai.shared.protocol.Agent;
 
-@Path("spark")
-public interface RestService {
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-    @GET
-    @Path("install/{clusterName}")
-    public String installCluster(@PathParam("clusterName") String clusterName);
+public class RestService {
 
-    @GET
-    @Path("uninstall/{clusterName}") //Maps for the `hello/John` in the URL
-    public String uninstallCluster(@PathParam("clusterName") String clusterName);
+	private static final String OPERATION_ID = "OPERATION_ID";
 
+	private Spark sparkManager;
+	private AgentManager agentManager;
 
+	public Spark getSparkManager() {
+		return sparkManager;
+	}
+
+	public AgentManager getAgentManager() {
+		return agentManager;
+	}
+
+	@GET
+	@Path ("list_clusters")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String listClusters() {
+
+		List<Config> configList = sparkManager.getClusters();
+		ArrayList<String> clusterNames = new ArrayList();
+
+		for (Config config : configList) {
+			clusterNames.add(config.getClusterName());
+		}
+
+		return JsonUtil.GSON.toJson(clusterNames);
+	}
+
+	@GET
+	@Path ("get_cluster/{clusterName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String getCluster(
+			@PathParam ("clusterName") String clusterName
+	) {
+		return JsonUtil.GSON.toJson(sparkManager.getCluster(clusterName));
+	}
+
+	@GET
+	@Path ("install")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String installCluster(@QueryParam ("config") String config) {
+		TrimmedSparkConfig trimmedPrestoConfig = JsonUtil.GSON.fromJson(config, TrimmedSparkConfig.class);
+		Config expandedConfig = new Config();
+
+		expandedConfig.setClusterName(trimmedPrestoConfig.getClusterName());
+		expandedConfig.setMasterNode(agentManager.getAgentByHostname(trimmedPrestoConfig.getMasterNodeHostName()));
+		if (trimmedPrestoConfig.getSlavesHostName() != null && !trimmedPrestoConfig.getSlavesHostName().isEmpty()) {
+			Set<Agent> nodes = new HashSet<>();
+			for (String node : trimmedPrestoConfig.getSlavesHostName()) {
+				nodes.add(agentManager.getAgentByHostname(node));
+			}
+			expandedConfig.setSlaveNodes(nodes);
+		}
+
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.installCluster(expandedConfig));
+	}
+
+	@GET
+	@Path ("uninstall/{clusterName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String uninstallCluster(@PathParam ("clusterName") String clusterName) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.uninstallCluster(clusterName));
+	}
+
+	@GET
+	@Path ("add_slave_node/{clusterName}/{lxcHostName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String addSlaveNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.addSlaveNode(clusterName, lxcHostName));
+	}
+
+	@GET
+	@Path ("destroy_slave_node/{clusterName}/{lxcHostName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String destroySlaveNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.destroySlaveNode(clusterName, lxcHostName));
+	}
+
+	@GET
+	@Path ("change_master_node/{clusterName}/{lxcHostName}/{keepSlave}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String changeMasterNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName,
+			@PathParam ("keepSlave") boolean keepSlave) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.changeMasterNode(clusterName, lxcHostName, keepSlave));
+	}
+
+	@GET
+	@Path ("start_node/{clusterName}/{lxcHostName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String startNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName,
+			@PathParam ("keepSlave") boolean keepSlave) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.startNode(clusterName, lxcHostName, keepSlave));
+	}
+
+	@GET
+	@Path ("stop_node/{clusterName}/{lxcHostName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String stopNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName,
+			@PathParam ("keepSlave") boolean keepSlave) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.stopNode(clusterName, lxcHostName, keepSlave));
+	}
+
+	@GET
+	@Path ("check_node/{clusterName}/{lxcHostName}")
+	@Produces ( {MediaType.APPLICATION_JSON})
+	public String checkNode(
+			@PathParam ("clusterName") String clusterName,
+			@PathParam ("lxcHostName") String lxcHostName) {
+		return JsonUtil.toJson(OPERATION_ID, sparkManager.checkNode(clusterName, lxcHostName));
+	}
 }
