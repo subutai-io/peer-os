efba6dc4a40b3eb42c4c4791601971ddfead3217
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-08-19 20:10:41 +0600

    Accumulo conversion in progress

--- management/server/plugins/mongodb/mongodb-impl/src/main/java/org/safehaus/subutai/plugin/mongodb/impl/handler/DestroyNodeOperationHandler.java
+++ management/server/plugins/mongodb/mongodb-impl/src/main/java/org/safehaus/subutai/plugin/mongodb/impl/handler/DestroyNodeOperationHandler.java
@@ -80,6 +80,7 @@ public class DestroyNodeOperationHandler extends AbstractOperationHandler<MongoI
 
         if ( nodeType == NodeType.CONFIG_NODE ) {
             config.getConfigServers().remove( agent );
+            config.setNumberOfConfigServers(config.getNumberOfConfigServers() - 1);
             //restart routers
             po.addLog( "Restarting routers..." );
             Command stopRoutersCommand = Commands.getStopNodeCommand( config.getRouterServers() );
@@ -114,6 +115,7 @@ public class DestroyNodeOperationHandler extends AbstractOperationHandler<MongoI
         }
         else if ( nodeType == NodeType.DATA_NODE ) {
             config.getDataNodes().remove( agent );
+            config.setNumberOfDataNodes(config.getNumberOfDataNodes() - 1);
             //unregister from primary
             po.addLog( "Unregistering this node from replica set..." );
             Command findPrimaryNodeCommand = Commands.getFindPrimaryNodeCommand( agent, config.getDataNodePort() );
@@ -151,6 +153,7 @@ public class DestroyNodeOperationHandler extends AbstractOperationHandler<MongoI
             }
         }
         else if ( nodeType == NodeType.ROUTER_NODE ) {
+            config.setNumberOfRouters( config.getNumberOfRouters() - 1 );
             config.getRouterServers().remove( agent );
         }
         //destroy lxc
