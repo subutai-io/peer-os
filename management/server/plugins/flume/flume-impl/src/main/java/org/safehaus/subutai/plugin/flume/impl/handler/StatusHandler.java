e9f54e75d625b6e5975be7bb5e8ee083649fed9c
Azilet Beishenaliev <aziletb@gmail.com>
2014-08-25 14:16:07 +0600

    SUBUTAI-1067: fix status check operation

--- management/server/plugins/flume/flume-impl/src/main/java/org/safehaus/subutai/plugin/flume/impl/handler/StatusHandler.java
+++ management/server/plugins/flume/flume-impl/src/main/java/org/safehaus/subutai/plugin/flume/impl/handler/StatusHandler.java
@@ -1,16 +1,15 @@
 package org.safehaus.subutai.plugin.flume.impl.handler;
 
-import org.safehaus.subutai.plugin.flume.impl.FlumeImpl;
-import org.safehaus.subutai.plugin.flume.impl.Commands;
-import org.safehaus.subutai.plugin.flume.impl.CommandType;
 import java.util.Arrays;
 import java.util.HashSet;
 import org.safehaus.subutai.api.commandrunner.*;
 import org.safehaus.subutai.plugin.flume.api.FlumeConfig;
+import org.safehaus.subutai.plugin.flume.impl.CommandType;
+import org.safehaus.subutai.plugin.flume.impl.Commands;
+import org.safehaus.subutai.plugin.flume.impl.FlumeImpl;
 import org.safehaus.subutai.shared.operation.AbstractOperationHandler;
 import org.safehaus.subutai.shared.operation.ProductOperation;
 import org.safehaus.subutai.shared.protocol.Agent;
-import org.safehaus.subutai.shared.protocol.enums.NodeState;
 
 public class StatusHandler extends AbstractOperationHandler<FlumeImpl> {
 
@@ -43,21 +42,16 @@ public class StatusHandler extends AbstractOperationHandler<FlumeImpl> {
                 new HashSet<>(Arrays.asList(node)));
         manager.getCommandRunner().runCommand(cmd);
 
-        NodeState nodeState = NodeState.UNKNOWN;
         if(cmd.hasSucceeded()) {
             AgentResult result = cmd.getResults().get(node.getUuid());
-            if(result.getStdOut().contains("is running"))
-                nodeState = NodeState.RUNNING;
-            else if(result.getStdOut().contains("is not running"))
-                nodeState = NodeState.STOPPED;
+            if(result.getStdOut().contains(Commands.PACKAGE_NAME))
+                po.addLogDone("Flume installed on " + node.getHostname());
+            else
+                po.addLogFailed("Flume not installed on " + node.getHostname());
+        } else {
+            po.addLog(cmd.getAllErrors());
+            po.addLogFailed("Failed to check Flume on " + hostname);
         }
-
-        if(NodeState.UNKNOWN.equals(nodeState))
-            po.addLogFailed(String.format("Failed to check status of %s, %s",
-                    hostname, cmd.getAllErrors()));
-        else
-            po.addLogDone(String.format("Node %s is %s", hostname,
-                    nodeState));
     }
 
 }
