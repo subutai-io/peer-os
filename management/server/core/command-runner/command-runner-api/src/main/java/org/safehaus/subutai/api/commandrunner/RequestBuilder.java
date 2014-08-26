f524036971e370478f657c0b890d5e90da918df3
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-08-26 16:51:55 +0600

    Bug fix

--- management/server/core/command-runner/command-runner-api/src/main/java/org/safehaus/subutai/api/commandrunner/RequestBuilder.java
+++ management/server/core/command-runner/command-runner-api/src/main/java/org/safehaus/subutai/api/commandrunner/RequestBuilder.java
@@ -6,16 +6,17 @@
 package org.safehaus.subutai.api.commandrunner;
 
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import org.safehaus.subutai.shared.protocol.Request;
 import org.safehaus.subutai.shared.protocol.enums.OutputRedirection;
 import org.safehaus.subutai.shared.protocol.enums.RequestType;
 import org.safehaus.subutai.shared.protocol.settings.Common;
 
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 
 /**
@@ -24,177 +25,178 @@ import java.util.UUID;
  */
 public class RequestBuilder {
 
-	//source of command
-	private final String source = "COMMAND-RUNNER";
+    //source of command
+    private final static String source = "COMMAND-RUNNER";
 
-	//the same for all commands
-	private final Integer requestSequenceNumber = 1;
+    //the same for all commands
+    private final Integer requestSequenceNumber = 1;
 
-	//the command to execute, e.g. ls
-	private final String command;
+    //the command to execute, e.g. ls
+    private final String command;
 
-	//current working directory
-	private String cwd = "/";
+    //current working directory
+    private String cwd = "/";
 
-	//type of command
-	private RequestType type = RequestType.EXECUTE_REQUEST;
+    //type of command
+    private RequestType type = RequestType.EXECUTE_REQUEST;
 
-	//std out redirection
-	private OutputRedirection outputRedirection = OutputRedirection.RETURN;
+    //std out redirection
+    private OutputRedirection outputRedirection = OutputRedirection.RETURN;
 
-	//std err redirection
-	private OutputRedirection errRedirection = OutputRedirection.RETURN;
+    //std err redirection
+    private OutputRedirection errRedirection = OutputRedirection.RETURN;
 
-	//command timeout interval
-	private Integer timeout = 30;
+    //command timeout interval
+    private Integer timeout = 30;
 
-	//file path for std out redirection if any
-	private String stdOutPath;
+    //file path for std out redirection if any
+    private String stdOutPath;
 
-	//file path for std err redirection if any
-	private String stdErrPath;
+    //file path for std err redirection if any
+    private String stdErrPath;
 
-	//user under which to run the command
-	private String runAs = "root";
+    //user under which to run the command
+    private String runAs = "root";
 
-	//command arguments
-	private List<String> cmdArgs;
+    //command arguments
+    private List<String> cmdArgs;
 
-	//environment variables
-	private Map<String, String> envVars;
+    //environment variables
+    private Map<String, String> envVars;
 
-	//PID for terminate_request
-	private int pid;
+    //PID for terminate_request
+    private int pid;
 
-	// Config points for inotify
-	private String[] confPoints;
+    // Config points for inotify
+    private String[] confPoints;
 
 
-	/**
-	 * Constructor
-	 *
-	 * @param command - command to run
-	 */
-	public RequestBuilder(String command) {
-		Preconditions.checkArgument(!Strings.isNullOrEmpty(command), "Command is null or empty");
+    /**
+     * Constructor
+     *
+     * @param command - command to run
+     */
+    public RequestBuilder( String command ) {
+        Preconditions.checkArgument( !Strings.isNullOrEmpty( command ), "Command is null or empty" );
 
-		this.command = command;
-	}
+        this.command = command;
+    }
 
 
-	public Integer getTimeout() {
-		return timeout;
-	}
+    public Integer getTimeout() {
+        return timeout;
+    }
 
 
-	public RequestBuilder withCwd(String cwd) {
-		Preconditions.checkArgument(!Strings.isNullOrEmpty(cwd), "Current working directory is null or empty");
+    public RequestBuilder withCwd( String cwd ) {
+        Preconditions.checkArgument( !Strings.isNullOrEmpty( cwd ), "Current working directory is null or empty" );
 
-		this.cwd = cwd;
+        this.cwd = cwd;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withType(RequestType type) {
-		Preconditions.checkNotNull(type, "Request Type is null");
+    public RequestBuilder withType( RequestType type ) {
+        Preconditions.checkNotNull( type, "Request Type is null" );
 
-		this.type = type;
+        this.type = type;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withStdOutRedirection(OutputRedirection outputRedirection) {
-		Preconditions.checkNotNull(outputRedirection, "Std Out Redirection is null");
+    public RequestBuilder withStdOutRedirection( OutputRedirection outputRedirection ) {
+        Preconditions.checkNotNull( outputRedirection, "Std Out Redirection is null" );
 
-		this.outputRedirection = outputRedirection;
+        this.outputRedirection = outputRedirection;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withStdErrRedirection(OutputRedirection errRedirection) {
-		Preconditions.checkNotNull(errRedirection, "Std Err Redirection is null");
+    public RequestBuilder withStdErrRedirection( OutputRedirection errRedirection ) {
+        Preconditions.checkNotNull( errRedirection, "Std Err Redirection is null" );
 
-		this.errRedirection = errRedirection;
+        this.errRedirection = errRedirection;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withTimeout(int timeout) {
-		Preconditions.checkArgument(timeout > 0 && timeout <= Common.MAX_COMMAND_TIMEOUT_SEC,
-				String.format("Timeout is not in range 1 to %s", Common.MAX_COMMAND_TIMEOUT_SEC));
+    public RequestBuilder withTimeout( int timeout ) {
+        Preconditions.checkArgument( timeout > 0 && timeout <= Common.MAX_COMMAND_TIMEOUT_SEC,
+                String.format( "Timeout is not in range 1 to %s", Common.MAX_COMMAND_TIMEOUT_SEC ) );
 
-		this.timeout = timeout;
+        this.timeout = timeout;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withStdOutPath(String stdOutPath) {
+    public RequestBuilder withStdOutPath( String stdOutPath ) {
 
-		this.stdOutPath = stdOutPath;
+        this.stdOutPath = stdOutPath;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withErrPath(String stdErrPath) {
+    public RequestBuilder withErrPath( String stdErrPath ) {
 
-		this.stdErrPath = stdErrPath;
+        this.stdErrPath = stdErrPath;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withRunAs(String runAs) {
+    public RequestBuilder withRunAs( String runAs ) {
 
-		Preconditions.checkArgument(!Strings.isNullOrEmpty(runAs), "Run As is null or empty");
+        Preconditions.checkArgument( !Strings.isNullOrEmpty( runAs ), "Run As is null or empty" );
 
-		this.runAs = runAs;
+        this.runAs = runAs;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withCmdArgs(List<String> cmdArgs) {
+    public RequestBuilder withCmdArgs( List<String> cmdArgs ) {
 
-		this.cmdArgs = cmdArgs;
+        this.cmdArgs = cmdArgs;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withEnvVars(Map<String, String> envVars) {
+    public RequestBuilder withEnvVars( Map<String, String> envVars ) {
 
-		this.envVars = envVars;
+        this.envVars = envVars;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withPid(int pid) {
-		Preconditions.checkArgument(pid > 0, "PID is less then or equal to 0");
+    public RequestBuilder withPid( int pid ) {
+        Preconditions.checkArgument( pid > 0, "PID is less then or equal to 0" );
 
-		this.pid = pid;
+        this.pid = pid;
 
-		return this;
-	}
+        return this;
+    }
 
 
-	public RequestBuilder withConfPoints(String confPoints[]) {
+    public RequestBuilder withConfPoints( String confPoints[] ) {
 
-		this.confPoints = confPoints;
+        this.confPoints = confPoints.clone();
 
-		return this;
-	}
+        return this;
+    }
 
-	public Request build(UUID agentUUID, UUID taskUUID) {
 
-		return new Request(source, type, agentUUID, taskUUID, requestSequenceNumber, cwd, command, outputRedirection,
-				errRedirection, stdOutPath, stdErrPath, runAs, cmdArgs, envVars, pid, timeout)
-				.setConfPoints(confPoints);
-	}
+    public Request build( UUID agentUUID, UUID taskUUID ) {
+
+        return new Request( source, type, agentUUID, taskUUID, requestSequenceNumber, cwd, command, outputRedirection,
+                errRedirection, stdOutPath, stdErrPath, runAs, cmdArgs, envVars, pid, timeout )
+                .setConfPoints( confPoints );
+    }
 }
