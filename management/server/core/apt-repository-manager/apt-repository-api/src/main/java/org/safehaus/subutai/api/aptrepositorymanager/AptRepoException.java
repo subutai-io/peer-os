a142300652d38e503fddafc7050afdeddd821b97
dilshat.aliev@safehaus.org <dilshat.aliev@safehaus.org>
2014-08-27 14:20:42 +0600

    Documenting core

--- management/server/core/apt-repository-manager/apt-repository-api/src/main/java/org/safehaus/subutai/api/aptrepositorymanager/AptRepoException.java
+++ management/server/core/apt-repository-manager/apt-repository-api/src/main/java/org/safehaus/subutai/api/aptrepositorymanager/AptRepoException.java
@@ -1,9 +1,12 @@
 package org.safehaus.subutai.api.aptrepositorymanager;
 
 
+/**
+ * Represents exception thrown by AptRepositoryManager
+ */
 public class AptRepoException extends Exception {
 
-	public AptRepoException(final String message) {
-		super(message);
-	}
+    public AptRepoException( final String message ) {
+        super( message );
+    }
 }
