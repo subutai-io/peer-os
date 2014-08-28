24655733d6e75a5c7a893633ce055e9c9eb40bf8
Askat Asanaliev <aasanaliev@exadel.com>
2014-08-28 21:25:44 +0600

    Core refactor

--- management/server/products/presto/presto-impl/src/main/java/org/safehaus/subutai/impl/presto/handler/StartNodeOperationHandler.java
+++ management/server/products/presto/presto-impl/src/main/java/org/safehaus/subutai/impl/presto/handler/StartNodeOperationHandler.java
@@ -1,5 +1,7 @@
 package org.safehaus.subutai.impl.presto.handler;
 
+
+import com.google.common.collect.Sets;
 import org.safehaus.subutai.api.commandrunner.AgentResult;
 import org.safehaus.subutai.api.commandrunner.Command;
 import org.safehaus.subutai.api.commandrunner.CommandCallback;
@@ -10,69 +12,86 @@ import org.safehaus.subutai.shared.operation.AbstractOperationHandler;
 import org.safehaus.subutai.shared.operation.ProductOperation;
 import org.safehaus.subutai.shared.protocol.Agent;
 import org.safehaus.subutai.shared.protocol.Response;
-import org.safehaus.subutai.shared.protocol.Util;
 
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+
 /**
  * Created by dilshat on 5/7/14.
  */
-public class StartNodeOperationHandler extends AbstractOperationHandler<PrestoImpl> {
-	private final ProductOperation po;
-	private final String lxcHostname;
+public class StartNodeOperationHandler extends AbstractOperationHandler<PrestoImpl>
+{
+    private final ProductOperation po;
+    private final String lxcHostname;
 
-	public StartNodeOperationHandler(PrestoImpl manager, String clusterName, String lxcHostname) {
-		super(manager, clusterName);
-		this.lxcHostname = lxcHostname;
-		po = manager.getTracker().createProductOperation(Config.PRODUCT_KEY,
-				String.format("Starting node %s in %s", lxcHostname, clusterName));
-	}
 
-	@Override
-	public UUID getTrackerId() {
-		return po.getId();
-	}
+    public StartNodeOperationHandler( PrestoImpl manager, String clusterName, String lxcHostname )
+    {
+        super( manager, clusterName );
+        this.lxcHostname = lxcHostname;
+        po = manager.getTracker().createProductOperation( Config.PRODUCT_KEY,
+            String.format( "Starting node %s in %s", lxcHostname, clusterName ) );
+    }
 
-	@Override
-	public void run() {
-		Config config = manager.getCluster(clusterName);
-		if (config == null) {
-			po.addLogFailed(String.format("Cluster with name %s does not exist", clusterName));
-			return;
-		}
 
-		Agent node = manager.getAgentManager().getAgentByHostname(lxcHostname);
-		if (node == null) {
-			po.addLogFailed(String.format("Agent with hostname %s is not connected", lxcHostname));
-			return;
-		}
+    @Override
+    public UUID getTrackerId()
+    {
+        return po.getId();
+    }
 
-		if (!config.getAllNodes().contains(node)) {
-			po.addLogFailed(String.format("Node %s does not belong to this cluster", lxcHostname));
-			return;
-		}
 
-		po.addLog(String.format("Starting node %s...", node.getHostname()));
+    @Override
+    public void run()
+    {
+        Config config = manager.getCluster( clusterName );
+        if ( config == null )
+        {
+            po.addLogFailed( String.format( "Cluster with name %s does not exist", clusterName ) );
+            return;
+        }
 
-		Command startNodeCommand = Commands.getStartCommand(Util.wrapAgentToSet(node));
-		final AtomicBoolean ok = new AtomicBoolean();
-		manager.getCommandRunner().runCommand(startNodeCommand, new CommandCallback() {
+        Agent node = manager.getAgentManager().getAgentByHostname( lxcHostname );
+        if ( node == null )
+        {
+            po.addLogFailed( String.format( "Agent with hostname %s is not connected", lxcHostname ) );
+            return;
+        }
 
-			@Override
-			public void onResponse(Response response, AgentResult agentResult, Command command) {
-				if (agentResult.getStdOut().contains("Started")) {
-					ok.set(true);
-					stop();
-				}
-			}
+        if ( !config.getAllNodes().contains( node ) )
+        {
+            po.addLogFailed( String.format( "Node %s does not belong to this cluster", lxcHostname ) );
+            return;
+        }
 
-		});
+        po.addLog( String.format( "Starting node %s...", node.getHostname() ) );
 
-		if (ok.get()) {
-			po.addLogDone(String.format("Node %s started", node.getHostname()));
-		} else {
-			po.addLogFailed(String.format("Starting node %s failed, %s", node.getHostname(), startNodeCommand.getAllErrors()));
-		}
-	}
+        Command startNodeCommand = Commands.getStartCommand( Sets.newHashSet( node ) );
+        final AtomicBoolean ok = new AtomicBoolean();
+        manager.getCommandRunner().runCommand( startNodeCommand, new CommandCallback()
+        {
+
+            @Override
+            public void onResponse( Response response, AgentResult agentResult, Command command )
+            {
+                if ( agentResult.getStdOut().contains( "Started" ) )
+                {
+                    ok.set( true );
+                    stop();
+                }
+            }
+
+        } );
+
+        if ( ok.get() )
+        {
+            po.addLogDone( String.format( "Node %s started", node.getHostname() ) );
+        }
+        else
+        {
+            po.addLogFailed(
+                String.format( "Starting node %s failed, %s", node.getHostname(), startNodeCommand.getAllErrors() ) );
+        }
+    }
 }
--- management/server/products/storm/storm-ui/src/main/java/org/safehaus/subutai/ui/storm/wizard/NodeSelectionStep.java
+++ management/server/products/storm/storm-ui/src/main/java/org/safehaus/subutai/ui/storm/wizard/NodeSelectionStep.java
@@ -1,169 +1,213 @@
 package org.safehaus.subutai.ui.storm.wizard;
 
+
+import com.google.common.base.Strings;
 import com.vaadin.data.Property;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import org.safehaus.subutai.api.storm.Config;
 import org.safehaus.subutai.shared.protocol.Agent;
-import org.safehaus.subutai.shared.protocol.Util;
 import org.safehaus.subutai.ui.storm.StormUI;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class NodeSelectionStep extends Panel {
 
-	public NodeSelectionStep(final Wizard wizard) {
+public class NodeSelectionStep extends Panel
+{
 
-		setSizeFull();
+    public NodeSelectionStep( final Wizard wizard )
+    {
 
-		GridLayout content = new GridLayout(1, 2);
-		content.setSizeFull();
-		content.setSpacing(true);
-		content.setMargin(true);
+        setSizeFull();
 
-		TextField clusterNameTxt = new TextField("Cluster name");
-		clusterNameTxt.setRequired(true);
-		clusterNameTxt.addValueChangeListener(new Property.ValueChangeListener() {
+        GridLayout content = new GridLayout( 1, 2 );
+        content.setSizeFull();
+        content.setSpacing( true );
+        content.setMargin( true );
 
-			@Override
-			public void valueChange(Property.ValueChangeEvent e) {
-				wizard.getConfig().setClusterName(e.getProperty().getValue().toString().trim());
-			}
-		});
+        TextField clusterNameTxt = new TextField( "Cluster name" );
+        clusterNameTxt.setRequired( true );
+        clusterNameTxt.addValueChangeListener( new Property.ValueChangeListener()
+        {
 
-		Component nimbusElem;
-		if (wizard.getConfig().isExternalZookeeper()) {
-			ComboBox zkClustersCombo = new ComboBox("Zookeeper cluster");
-			final ComboBox masterNodeCombo = makeMasterNodeComboBox(wizard);
+            @Override
+            public void valueChange( Property.ValueChangeEvent e )
+            {
+                wizard.getConfig().setClusterName( e.getProperty().getValue().toString().trim() );
+            }
+        } );
 
-			zkClustersCombo.setImmediate(true);
-			zkClustersCombo.setTextInputAllowed(false);
-			zkClustersCombo.setRequired(true);
-			zkClustersCombo.setNullSelectionAllowed(false);
-			zkClustersCombo.addValueChangeListener(new Property.ValueChangeListener() {
-				@Override
-				public void valueChange(Property.ValueChangeEvent e) {
-					masterNodeCombo.removeAllItems();
-					if (e.getProperty().getValue() != null) {
-						org.safehaus.subutai.api.zookeeper.Config zk
-								= (org.safehaus.subutai.api.zookeeper.Config) e.getProperty().getValue();
-						for (Agent a : zk.getNodes()) {
-							masterNodeCombo.addItem(a);
-							masterNodeCombo.setItemCaption(a, a.getHostname());
-						}
-						// do select if values exist
-						if (wizard.getConfig().getNimbus() != null)
-							masterNodeCombo.setValue(wizard.getConfig().getNimbus());
+        Component nimbusElem;
+        if ( wizard.getConfig().isExternalZookeeper() )
+        {
+            ComboBox zkClustersCombo = new ComboBox( "Zookeeper cluster" );
+            final ComboBox masterNodeCombo = makeMasterNodeComboBox( wizard );
 
-						wizard.getConfig().setZookeeperClusterName(zk.getClusterName());
-					}
-				}
-			});
-			List<org.safehaus.subutai.api.zookeeper.Config> zk_list = StormUI.getZookeeper().getClusters();
-			for (org.safehaus.subutai.api.zookeeper.Config zkc : zk_list) {
-				zkClustersCombo.addItem(zkc);
-				zkClustersCombo.setItemCaption(zkc, zkc.getClusterName());
-				if (zkc.getClusterName().equals(wizard.getConfig().getZookeeperClusterName()))
-					zkClustersCombo.setValue(zkc);
-			}
-			if (wizard.getConfig().getNimbus() != null)
-				masterNodeCombo.setValue(wizard.getConfig().getNimbus());
+            zkClustersCombo.setImmediate( true );
+            zkClustersCombo.setTextInputAllowed( false );
+            zkClustersCombo.setRequired( true );
+            zkClustersCombo.setNullSelectionAllowed( false );
+            zkClustersCombo.addValueChangeListener( new Property.ValueChangeListener()
+            {
+                @Override
+                public void valueChange( Property.ValueChangeEvent e )
+                {
+                    masterNodeCombo.removeAllItems();
+                    if ( e.getProperty().getValue() != null )
+                    {
+                        org.safehaus.subutai.api.zookeeper.Config zk
+                            = ( org.safehaus.subutai.api.zookeeper.Config ) e.getProperty().getValue();
+                        for ( Agent a : zk.getNodes() )
+                        {
+                            masterNodeCombo.addItem( a );
+                            masterNodeCombo.setItemCaption( a, a.getHostname() );
+                        }
+                        // do select if values exist
+                        if ( wizard.getConfig().getNimbus() != null )
+                        {
+                            masterNodeCombo.setValue( wizard.getConfig().getNimbus() );
+                        }
 
-			HorizontalLayout hl = new HorizontalLayout(zkClustersCombo, masterNodeCombo);
-			nimbusElem = new Panel("Nimbus node", hl);
-			nimbusElem.setSizeUndefined();
-			nimbusElem.setStyleName("default");
-		} else {
-			String s = "<b>A new nimbus node will be created with Zookeeper instance installed</b>";
-			nimbusElem = new Label(s, ContentMode.HTML);
-		}
+                        wizard.getConfig().setZookeeperClusterName( zk.getClusterName() );
+                    }
+                }
+            } );
+            List<org.safehaus.subutai.api.zookeeper.Config> zk_list = StormUI.getZookeeper().getClusters();
+            for ( org.safehaus.subutai.api.zookeeper.Config zkc : zk_list )
+            {
+                zkClustersCombo.addItem( zkc );
+                zkClustersCombo.setItemCaption( zkc, zkc.getClusterName() );
+                if ( zkc.getClusterName().equals( wizard.getConfig().getZookeeperClusterName() ) )
+                {
+                    zkClustersCombo.setValue( zkc );
+                }
+            }
+            if ( wizard.getConfig().getNimbus() != null )
+            {
+                masterNodeCombo.setValue( wizard.getConfig().getNimbus() );
+            }
 
-		ComboBox nodesCountCmb = new ComboBox("Number of supervisor nodes",
-				Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-		nodesCountCmb.setImmediate(true);
-		nodesCountCmb.setRequired(true);
-		nodesCountCmb.setTextInputAllowed(false);
-		nodesCountCmb.setNullSelectionAllowed(false);
-		nodesCountCmb.setValue(wizard.getConfig().getSupervisorsCount());
-		nodesCountCmb.addValueChangeListener(new Property.ValueChangeListener() {
-			@Override
-			public void valueChange(Property.ValueChangeEvent event) {
-				wizard.getConfig().setSupervisorsCount((Integer) event.getProperty().getValue());
-			}
-		});
+            HorizontalLayout hl = new HorizontalLayout( zkClustersCombo, masterNodeCombo );
+            nimbusElem = new Panel( "Nimbus node", hl );
+            nimbusElem.setSizeUndefined();
+            nimbusElem.setStyleName( "default" );
+        }
+        else
+        {
+            String s = "<b>A new nimbus node will be created with Zookeeper instance installed</b>";
+            nimbusElem = new Label( s, ContentMode.HTML );
+        }
 
-		// set selected values
-		if (wizard.getConfig().getClusterName() != null)
-			clusterNameTxt.setValue(wizard.getConfig().getClusterName());
-		if (wizard.getConfig().getSupervisorsCount() > 0)
-			nodesCountCmb.setValue(wizard.getConfig().getSupervisorsCount());
+        ComboBox nodesCountCmb = new ComboBox( "Number of supervisor nodes",
+            Arrays.asList( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ) );
+        nodesCountCmb.setImmediate( true );
+        nodesCountCmb.setRequired( true );
+        nodesCountCmb.setTextInputAllowed( false );
+        nodesCountCmb.setNullSelectionAllowed( false );
+        nodesCountCmb.setValue( wizard.getConfig().getSupervisorsCount() );
+        nodesCountCmb.addValueChangeListener( new Property.ValueChangeListener()
+        {
+            @Override
+            public void valueChange( Property.ValueChangeEvent event )
+            {
+                wizard.getConfig().setSupervisorsCount( ( Integer ) event.getProperty().getValue() );
+            }
+        } );
 
-		Button next = new Button("Next");
-		next.addStyleName("default");
-		next.addClickListener(new Button.ClickListener() {
+        // set selected values
+        if ( wizard.getConfig().getClusterName() != null )
+        {
+            clusterNameTxt.setValue( wizard.getConfig().getClusterName() );
+        }
+        if ( wizard.getConfig().getSupervisorsCount() > 0 )
+        {
+            nodesCountCmb.setValue( wizard.getConfig().getSupervisorsCount() );
+        }
 
-			@Override
-			public void buttonClick(Button.ClickEvent event) {
-				Config config = wizard.getConfig();
-				if (Util.isStringEmpty(config.getClusterName()))
-					show("Enter cluster name");
-				else if (config.isExternalZookeeper() && config.getNimbus() == null)
-					show("Select master node");
-				else if (config.getSupervisorsCount() <= 0)
-					show("Select supervisor nodes count");
-				else
-					wizard.next();
-			}
-		});
+        Button next = new Button( "Next" );
+        next.addStyleName( "default" );
+        next.addClickListener( new Button.ClickListener()
+        {
 
-		Button back = new Button("Back");
-		back.addStyleName("default");
-		back.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(Button.ClickEvent event) {
-				wizard.back();
-			}
-		});
+            @Override
+            public void buttonClick( Button.ClickEvent event )
+            {
+                Config config = wizard.getConfig();
+                if ( Strings.isNullOrEmpty( config.getClusterName() ) )
+                {
+                    show( "Enter cluster name" );
+                }
+                else if ( config.isExternalZookeeper() && config.getNimbus() == null )
+                {
+                    show( "Select master node" );
+                }
+                else if ( config.getSupervisorsCount() <= 0 )
+                {
+                    show( "Select supervisor nodes count" );
+                }
+                else
+                {
+                    wizard.next();
+                }
+            }
+        } );
 
-		VerticalLayout layout = new VerticalLayout();
-		layout.setSpacing(true);
-		layout.addComponent(new Label("Please, specify installation settings"));
-		layout.addComponent(content);
+        Button back = new Button( "Back" );
+        back.addStyleName( "default" );
+        back.addClickListener( new Button.ClickListener()
+        {
+            @Override
+            public void buttonClick( Button.ClickEvent event )
+            {
+                wizard.back();
+            }
+        } );
 
-		HorizontalLayout buttons = new HorizontalLayout();
-		buttons.addComponent(back);
-		buttons.addComponent(next);
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSpacing( true );
+        layout.addComponent( new Label( "Please, specify installation settings" ) );
+        layout.addComponent( content );
 
-		content.addComponent(clusterNameTxt);
-		content.addComponent(nimbusElem);
-		content.addComponent(nodesCountCmb);
-		content.addComponent(buttons);
+        HorizontalLayout buttons = new HorizontalLayout();
+        buttons.addComponent( back );
+        buttons.addComponent( next );
 
-		setContent(layout);
+        content.addComponent( clusterNameTxt );
+        content.addComponent( nimbusElem );
+        content.addComponent( nodesCountCmb );
+        content.addComponent( buttons );
 
-	}
+        setContent( layout );
 
-	private ComboBox makeMasterNodeComboBox(final Wizard wizard) {
-		ComboBox cb = new ComboBox("Nodes");
+    }
 
-		cb.setImmediate(true);
-		cb.setTextInputAllowed(false);
-		cb.setRequired(true);
-		cb.setNullSelectionAllowed(false);
-		cb.addValueChangeListener(new Property.ValueChangeListener() {
 
-			@Override
-			public void valueChange(Property.ValueChangeEvent event) {
-				Agent serverNode = (Agent) event.getProperty().getValue();
-				wizard.getConfig().setNimbus(serverNode);
-			}
-		});
-		return cb;
-	}
+    private ComboBox makeMasterNodeComboBox( final Wizard wizard )
+    {
+        ComboBox cb = new ComboBox( "Nodes" );
 
-	private void show(String notification) {
-		Notification.show(notification);
-	}
+        cb.setImmediate( true );
+        cb.setTextInputAllowed( false );
+        cb.setRequired( true );
+        cb.setNullSelectionAllowed( false );
+        cb.addValueChangeListener( new Property.ValueChangeListener()
+        {
+
+            @Override
+            public void valueChange( Property.ValueChangeEvent event )
+            {
+                Agent serverNode = ( Agent ) event.getProperty().getValue();
+                wizard.getConfig().setNimbus( serverNode );
+            }
+        } );
+        return cb;
+    }
+
+
+    private void show( String notification )
+    {
+        Notification.show( notification );
+    }
 
 }
