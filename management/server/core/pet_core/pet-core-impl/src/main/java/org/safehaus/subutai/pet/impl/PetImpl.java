e0fee4988bff0dd584c9545c37e5bcbac82d6ce2
bahadyr <bahadyr@safehaus.org>
2014-08-28 19:09:03 +0600

    Pet Core module added. Can be used for tests.

--- management/server/core/pet_core/pet-core-impl/src/main/java/org/safehaus/subutai/pet/impl/PetImpl.java
+++ management/server/core/pet_core/pet-core-impl/src/main/java/org/safehaus/subutai/pet/impl/PetImpl.java
@@ -8,6 +8,7 @@ import org.safehaus.subutai.pet.api.PetManager;
  * Created by bahadyr on 8/28/14.
  */
 public class PetImpl implements PetManager {
+
     @Override
     public String getPetName() {
         return "hello kitty!";
