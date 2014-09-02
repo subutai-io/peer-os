6edc5065f703ee974900cf02320d66d5e7996d45
daralbaev <daralbaev@gmail.com>
2014-09-03 02:19:56 +0600

    -m SUBUTAI-1194 Presto plugin is added to karaf instead of product

--- management/server/plugins/hadoop/hadoop-plugin-api/src/main/java/org/safehaus/subutai/plugin/hadoop/api/HadoopClusterConfig.java
+++ management/server/plugins/hadoop/hadoop-plugin-api/src/main/java/org/safehaus/subutai/plugin/hadoop/api/HadoopClusterConfig.java
@@ -18,7 +18,7 @@ public class HadoopClusterConfig implements ConfigBase {
     public static final int DEFAULT_HADOOP_MASTER_NODES_QUANTITY = 3;
     public static final String PRODUCT_NAME = "hadoop";
     private String templateName = PRODUCT_NAME;
-    public static final String PRODUCT_KEY = "Hadoop2";
+    public static final String PRODUCT_KEY = "Hadoop";
     public static final int NAME_NODE_PORT = 8020, JOB_TRACKER_PORT = 9000;
 
     private String clusterName, domainName;
--- management/server/plugins/hadoop/hadoop-plugin-ui/src/main/java/org/safehaus/subutai/plugin/hadoop/ui/HadoopUI.java
+++ management/server/plugins/hadoop/hadoop-plugin-ui/src/main/java/org/safehaus/subutai/plugin/hadoop/ui/HadoopUI.java
@@ -1,6 +1,10 @@
 package org.safehaus.subutai.plugin.hadoop.ui;
 
-import com.vaadin.ui.Component;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.safehaus.subutai.api.agentmanager.AgentManager;
 import org.safehaus.subutai.api.tracker.Tracker;
 import org.safehaus.subutai.common.util.FileUtil;
@@ -8,82 +12,79 @@ import org.safehaus.subutai.plugin.hadoop.api.Hadoop;
 import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
 import org.safehaus.subutai.server.ui.api.PortalModule;
 
-import java.io.File;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.vaadin.ui.Component;
 
-/**
- * Created by daralbaev on 08.04.14.
- */
+
 public class HadoopUI implements PortalModule {
 
-	public static final String MODULE_IMAGE = "hadoop.png";
+    public static final String MODULE_IMAGE = "hadoop.png";
 
-	private static Hadoop hadoopManager;
-	private static AgentManager agentManager;
-	private static ExecutorService executor;
-	private static Tracker tracker;
+    private static Hadoop hadoopManager;
+    private static AgentManager agentManager;
+    private static ExecutorService executor;
+    private static Tracker tracker;
 
-	public static Hadoop getHadoopManager() {
-		return hadoopManager;
-	}
 
-	public void setHadoopManager(Hadoop hadoopManager) {
-		HadoopUI.hadoopManager = hadoopManager;
-	}
+    public HadoopUI( AgentManager agentManager, Hadoop hadoopManager, Tracker tracker ) {
+        HadoopUI.agentManager = agentManager;
+        HadoopUI.hadoopManager = hadoopManager;
+        HadoopUI.tracker = tracker;
+    }
 
-	public static AgentManager getAgentManager() {
-		return agentManager;
-	}
 
-	public void setAgentManager(AgentManager agentManager) {
-		HadoopUI.agentManager = agentManager;
-	}
+    public static Hadoop getHadoopManager() {
+        return hadoopManager;
+    }
 
-	public static ExecutorService getExecutor() {
-		return executor;
-	}
 
-	public void setExecutor(ExecutorService executor) {
-		HadoopUI.executor = executor;
-	}
+    public static AgentManager getAgentManager() {
+        return agentManager;
+    }
 
-	public static Tracker getTracker() {
-		return tracker;
-	}
 
-	public void setTracker(Tracker tracker) {
-		HadoopUI.tracker = tracker;
-	}
+    public static ExecutorService getExecutor() {
+        return executor;
+    }
 
-	public void init() {
-		executor = Executors.newCachedThreadPool();
-	}
 
-	public void destroy() {
-		tracker = null;
-		hadoopManager = null;
-		agentManager = null;
-		executor.shutdown();
-	}
+    public static Tracker getTracker() {
+        return tracker;
+    }
 
-	@Override
-	public String getId() {
-		return HadoopClusterConfig.PRODUCT_KEY;
-	}
 
-	@Override
-	public String getName() {
-		return HadoopClusterConfig.PRODUCT_KEY;
-	}
+    public void init() {
+        executor = Executors.newCachedThreadPool();
+    }
 
-	@Override
-	public File getImage() {
-		return FileUtil.getFile( HadoopUI.MODULE_IMAGE, this );
-	}
 
-	@Override
-	public Component createComponent() {
-		return new HadoopForm();
-	}
+    public void destroy() {
+        tracker = null;
+        hadoopManager = null;
+        agentManager = null;
+        executor.shutdown();
+    }
+
+
+    @Override
+    public String getId() {
+        return HadoopClusterConfig.PRODUCT_KEY;
+    }
+
+
+    @Override
+    public String getName() {
+        return HadoopClusterConfig.PRODUCT_KEY;
+    }
+
+
+    @Override
+    public File getImage() {
+        return FileUtil.getFile( HadoopUI.MODULE_IMAGE, this );
+    }
+
+
+    @Override
+    public Component createComponent() {
+        return new HadoopForm();
+    }
 }
