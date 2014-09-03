35917f2d8917b63ea9733d0b56779e4a4c47dfa1
Azilet Beishenaliev <aziletb@gmail.com>
2014-09-03 20:04:10 +0600

    SUBUTAI-1077: modify ui

--- management/server/plugins/hive/hive-ui/src/main/java/org/safehaus/subutai/plugin/hive/ui/wizard/NodeSelectionStep.java
+++ management/server/plugins/hive/hive-ui/src/main/java/org/safehaus/subutai/plugin/hive/ui/wizard/NodeSelectionStep.java
@@ -1,6 +1,5 @@
 package org.safehaus.subutai.plugin.hive.ui.wizard;
 
-
 import com.google.common.base.Strings;
 import com.vaadin.data.Property;
 import com.vaadin.ui.*;
@@ -8,225 +7,184 @@ import java.util.*;
 import org.safehaus.subutai.common.protocol.Agent;
 import org.safehaus.subutai.common.util.CollectionUtil;
 import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
+import org.safehaus.subutai.plugin.hive.api.HiveConfig;
+import org.safehaus.subutai.plugin.hive.api.SetupType;
 import org.safehaus.subutai.plugin.hive.ui.HiveUI;
 
+public class NodeSelectionStep extends Panel {
 
-public class NodeSelectionStep extends Panel
-{
-
-    private final ComboBox hadoopClusters;
-    private final ComboBox serverNode;
     private int controlWidth = 350;
 
-
-    public NodeSelectionStep( final Wizard wizard )
-    {
+    public NodeSelectionStep(final Wizard wizard) {
 
         setSizeFull();
 
-        GridLayout content = new GridLayout( 1, 2 );
+        GridLayout content = new GridLayout(1, 2);
         content.setSizeFull();
-        content.setSpacing( true );
-        content.setMargin( true );
+        content.setSpacing(true);
+        content.setMargin(true);
 
-        TextField nameTxt = new TextField( "Cluster name" );
-        nameTxt.setRequired( true );
-        nameTxt.setWidth( controlWidth, Unit.POINTS );
-        nameTxt.addValueChangeListener( new Property.ValueChangeListener()
-        {
+        TextField nameTxt = new TextField("Cluster name");
+        nameTxt.setRequired(true);
+        nameTxt.setWidth(controlWidth, Unit.POINTS);
+        nameTxt.addValueChangeListener(new Property.ValueChangeListener() {
 
             @Override
-            public void valueChange( Property.ValueChangeEvent e )
-            {
-                wizard.getConfig().setClusterName( e.getProperty().getValue().toString().trim() );
+            public void valueChange(Property.ValueChangeEvent e) {
+                wizard.getConfig().setClusterName(e.getProperty().getValue().toString().trim());
             }
-        } );
-        nameTxt.setValue( wizard.getConfig().getClusterName() );
+        });
+        nameTxt.setValue(wizard.getConfig().getClusterName());
 
-        hadoopClusters = new ComboBox( "Hadoop cluster" );
-        serverNode = makeServerNodeComboBox( wizard );
-
-        hadoopClusters.setImmediate( true );
-        hadoopClusters.setWidth( controlWidth, Unit.POINTS );
-        hadoopClusters.setTextInputAllowed( false );
-        hadoopClusters.setRequired( true );
-        hadoopClusters.setNullSelectionAllowed( false );
-        hadoopClusters.addValueChangeListener( new Property.ValueChangeListener()
-        {
+        Button next = new Button("Next");
+        next.addStyleName("default");
+        next.addClickListener(new Button.ClickListener() {
             @Override
-            public void valueChange( Property.ValueChangeEvent event )
-            {
-                if ( event.getProperty().getValue() != null )
-                {
-                    HadoopClusterConfig hadoopInfo = (HadoopClusterConfig)event.getProperty().getValue();
-                    Agent selected = wizard.getConfig().getServer() != null
-                        ? wizard.getConfig().getServer()
-                        : hadoopInfo.getNameNode();
-                    fillServerNodeComboBox( hadoopInfo, selected );
-                    filterNodes();
-
-                    wizard.getConfig().setHadoopClusterName( hadoopInfo.getClusterName() );
-                }
-            }
-        } );
-
-        List<HadoopClusterConfig> clusters = HiveUI.getHadoopManager().getClusters();
-        if ( clusters.size() > 0 )
-        {
-            for(HadoopClusterConfig hci : clusters)            {
-                hadoopClusters.addItem( hci );
-                hadoopClusters.setItemCaption( hci, hci.getClusterName() );
-            }
-        }
-
-        HadoopClusterConfig info = HiveUI.getHadoopManager().getCluster(wizard.getConfig().getHadoopClusterName());
-        if ( info != null )
-        {
-            hadoopClusters.setValue( info );
-        }
-
-        Button next = new Button( "Next" );
-        next.addStyleName( "default" );
-        next.addClickListener( new Button.ClickListener()
-        {
-            @Override
-            public void buttonClick( Button.ClickEvent clickEvent )
-            {
-                if ( Strings.isNullOrEmpty( wizard.getConfig().getClusterName() ) )
-                {
-                    show( "Enter name for Hive installation" );
-                }
-                else if ( Strings.isNullOrEmpty( wizard.getConfig().getHadoopClusterName() ) )
-                {
-                    show( "Select Hadoop cluster" );
-                }
-                else if ( wizard.getConfig().getServer() == null )
-                {
-                    show( "Select server node" );
-                }
-                else if ( CollectionUtil.isCollectionEmpty( wizard.getConfig().getClients() ) )
-                {
-                    show( "Select client nodes" );
-                }
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                if(Strings.isNullOrEmpty(wizard.getConfig().getClusterName()))
+                    show("Enter name for Hive installation");
+                else if(Strings.isNullOrEmpty(wizard.getConfig().getHadoopClusterName()))
+                    show("Select Hadoop cluster");
+                else if(wizard.getConfig().getServer() == null)
+                    show("Select server node");
+                else if(CollectionUtil.isCollectionEmpty(wizard.getConfig().getClients()))
+                    show("Select client nodes");
                 else
-                {
                     wizard.next();
-                }
             }
-        } );
+        });
 
-        Button back = new Button( "Back" );
-        back.addStyleName( "default" );
-        back.addClickListener( new Button.ClickListener()
-        {
+        Button back = new Button("Back");
+        back.addStyleName("default");
+        back.addClickListener(new Button.ClickListener() {
             @Override
-            public void buttonClick( Button.ClickEvent clickEvent )
-            {
+            public void buttonClick(Button.ClickEvent clickEvent) {
                 wizard.back();
             }
-        } );
+        });
 
         VerticalLayout layout = new VerticalLayout();
-        layout.setSpacing( true );
-        layout.addComponent( new Label( "Please, specify installation settings" ) );
-        layout.addComponent( content );
+        layout.setSpacing(true);
+        layout.addComponent(new Label("Please, specify installation settings"));
+        layout.addComponent(content);
 
         HorizontalLayout buttons = new HorizontalLayout();
-        buttons.addComponent( back );
-        buttons.addComponent( next );
+        buttons.addComponent(back);
+        buttons.addComponent(next);
 
-        content.addComponent( nameTxt );
-        content.addComponent( hadoopClusters );
-        content.addComponent( serverNode );
-        content.addComponent( buttons );
+        content.addComponent(nameTxt);
+        if(wizard.getConfig().getSetupType() == SetupType.OVER_HADOOP)
+            addOverHadoopComponents(content, wizard.getConfig());
+        else if(wizard.getConfig().getSetupType() == SetupType.WITH_HADOOP)
+            addWithHadoopComponents(content, wizard.getConfig());
+        content.addComponent(buttons);
 
-        setContent( layout );
+        setContent(layout);
 
     }
 
+    private void addOverHadoopComponents(ComponentContainer parent, final HiveConfig config) {
+        ComboBox hadoopClusters = new ComboBox("Hadoop cluster");
+        final ComboBox cmbServerNode = makeServerNodeComboBox(config);
 
-    private ComboBox makeServerNodeComboBox( final Wizard wizard )
-    {
-        ComboBox cb = new ComboBox( "Server node" );
-        cb.setImmediate( true );
-        cb.setTextInputAllowed( false );
-        cb.setRequired( true );
-        cb.setNullSelectionAllowed( false );
-        cb.setWidth( controlWidth, Unit.POINTS );
-        cb.addValueChangeListener( new Property.ValueChangeListener()
-        {
+        hadoopClusters.setImmediate(true);
+        hadoopClusters.setWidth(controlWidth, Unit.POINTS);
+        hadoopClusters.setTextInputAllowed(false);
+        hadoopClusters.setRequired(true);
+        hadoopClusters.setNullSelectionAllowed(false);
+        hadoopClusters.addValueChangeListener(new Property.ValueChangeListener() {
             @Override
-            public void valueChange( Property.ValueChangeEvent event )
-            {
-                Agent hiveMaster = ( Agent ) event.getProperty().getValue();
-                wizard.getConfig().setServer( hiveMaster );
+            public void valueChange(Property.ValueChangeEvent event) {
+                if(event.getProperty().getValue() != null) {
+                    HadoopClusterConfig hc = (HadoopClusterConfig)event.getProperty().getValue();
+                    Agent selected = config.getServer() != null
+                            ? config.getServer()
+                            : hc.getNameNode();
+                    fillServerNodeComboBox(cmbServerNode, hc, selected);
+                    filterNodes(cmbServerNode);
 
-                HadoopClusterConfig hci = (HadoopClusterConfig)hadoopClusters.getValue();
-                wizard.getConfig().setClients( new HashSet<>( hci.getAllNodes() ) );
-                wizard.getConfig().getClients().remove( hiveMaster );
+                    config.setHadoopClusterName(hc.getClusterName());
+                    config.setHadoopNodes(new HashSet<>(hc.getAllNodes()));
+                }
             }
-        } );
+        });
+
+        List<HadoopClusterConfig> clusters = HiveUI.getHadoopManager().getClusters();
+        if(clusters.size() > 0)
+            for(HadoopClusterConfig hci : clusters) {
+                hadoopClusters.addItem(hci);
+                hadoopClusters.setItemCaption(hci, hci.getClusterName());
+            }
+
+        HadoopClusterConfig info = HiveUI.getHadoopManager().getCluster(config.getHadoopClusterName());
+        if(info != null)
+            hadoopClusters.setValue(info);
+
+        parent.addComponent(hadoopClusters);
+        parent.addComponent(cmbServerNode);
+    }
+
+    private void addWithHadoopComponents(ComponentContainer content, HiveConfig config) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private ComboBox makeServerNodeComboBox(final HiveConfig config) {
+        ComboBox cb = new ComboBox("Server node");
+        cb.setImmediate(true);
+        cb.setTextInputAllowed(false);
+        cb.setRequired(true);
+        cb.setNullSelectionAllowed(false);
+        cb.setWidth(controlWidth, Unit.POINTS);
+        cb.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent event) {
+                Agent hiveMaster = (Agent)event.getProperty().getValue();
+                config.setServer(hiveMaster);
+                config.setClients(config.getHadoopNodes());
+                config.getClients().remove(hiveMaster);
+            }
+        });
         return cb;
     }
 
-
-    private void fillServerNodeComboBox(HadoopClusterConfig hadoopInfo, Agent selected)    {
+    private void fillServerNodeComboBox(ComboBox serverNode, HadoopClusterConfig hadoopInfo, Agent selected) {
         serverNode.removeAllItems();
         List<Agent> slaves = hadoopInfo.getAllSlaveNodes();
-        for ( Agent a : hadoopInfo.getAllNodes() )
-        {
-            serverNode.addItem( a );
+        for(Agent a : hadoopInfo.getAllNodes()) {
+            serverNode.addItem(a);
             String caption = a.getHostname();
-            if ( hadoopInfo.getJobTracker().equals( a ) )
-            {
+            if(hadoopInfo.getJobTracker().equals(a))
                 caption += " [Job tracker]";
-            }
-            else if ( hadoopInfo.getNameNode().equals( a ) )
-            {
+            else if(hadoopInfo.getNameNode().equals(a))
                 caption += " [Name node]";
-            }
-            else if ( hadoopInfo.getSecondaryNameNode().equals( a ) )
-            {
+            else if(hadoopInfo.getSecondaryNameNode().equals(a))
                 caption += " [Name node 2]";
-            }
-            else if ( slaves.contains( a ) )
-            {
+            else if(slaves.contains(a))
                 caption += " [Slave node]";
-            }
-            serverNode.setItemCaption( a, caption );
+            serverNode.setItemCaption(a, caption);
         }
-        if ( selected != null )
-        {
-            serverNode.setValue( selected );
-        }
+        if(selected != null)
+            serverNode.setValue(selected);
     }
 
-
-    private void filterNodes()
-    {
-        Collection<Agent> items = ( Collection<Agent> ) serverNode.getItemIds();
-        final Set<Agent> set = new HashSet<>( items );
-        new Thread( new Runnable()
-        {
+    private void filterNodes(final ComboBox serverNode) {
+        Collection<Agent> items = (Collection<Agent>)serverNode.getItemIds();
+        final Set<Agent> set = new HashSet<>(items);
+        new Thread(new Runnable() {
 
             @Override
-            public void run()
-            {
-                Map<Agent, Boolean> map = HiveUI.getManager().isInstalled( set );
-                for ( Map.Entry<Agent, Boolean> e : map.entrySet() )
-                {
-                    if ( e.getValue() )
-                    {
-                        serverNode.removeItem( e.getKey() );
-                    }
+            public void run() {
+                Map<Agent, Boolean> map = HiveUI.getManager().isInstalled(set);
+                for(Map.Entry<Agent, Boolean> e : map.entrySet()) {
+                    if(e.getValue())
+                        serverNode.removeItem(e.getKey());
                 }
             }
-        } ).start();
+        }).start();
     }
 
-
-    private void show( String notification )
-    {
-        Notification.show( notification );
+    private void show(String notification) {
+        Notification.show(notification);
     }
+
 }
--- management/server/plugins/hive/hive-ui/src/main/java/org/safehaus/subutai/plugin/hive/ui/wizard/Wizard.java
+++ management/server/plugins/hive/hive-ui/src/main/java/org/safehaus/subutai/plugin/hive/ui/wizard/Wizard.java
@@ -2,71 +2,77 @@ package org.safehaus.subutai.plugin.hive.ui.wizard;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.GridLayout;
+import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
 import org.safehaus.subutai.plugin.hive.api.HiveConfig;
 
 public class Wizard {
 
-	private final GridLayout grid;
-	private int step = 1;
-	private HiveConfig config = new HiveConfig();
+    private final GridLayout grid;
+    private int step = 1;
+    private HiveConfig config = new HiveConfig();
+    private HadoopClusterConfig hadoopConfig = new HadoopClusterConfig();
 
-	public Wizard() {
-		grid = new GridLayout(1, 20);
-		grid.setMargin(true);
-		grid.setSizeFull();
+    public Wizard() {
+        grid = new GridLayout(1, 20);
+        grid.setMargin(true);
+        grid.setSizeFull();
 
-		putForm();
+        putForm();
 
-	}
+    }
 
-	private void putForm() {
-		grid.removeComponent(0, 1);
-		Component component = null;
-		switch (step) {
-			case 1: {
-				component = new WelcomeStep(this);
-				break;
-			}
-			case 2: {
-				component = new NodeSelectionStep(this);
-				break;
-			}
-			case 3: {
-				component = new VerificationStep(this);
-				break;
-			}
-			default: {
-				break;
-			}
-		}
+    private void putForm() {
+        grid.removeComponent(0, 1);
+        Component component = null;
+        switch(step) {
+            case 1: {
+                component = new WelcomeStep(this);
+                break;
+            }
+            case 2: {
+                component = new NodeSelectionStep(this);
+                break;
+            }
+            case 3: {
+                component = new VerificationStep(this);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
 
-		if (component != null) {
-			grid.addComponent(component, 0, 1, 0, 19);
-		}
-	}
+        if(component != null)
+            grid.addComponent(component, 0, 1, 0, 19);
+    }
 
-	public Component getContent() {
-		return grid;
-	}
+    public Component getContent() {
+        return grid;
+    }
 
-	protected void next() {
-		step++;
-		putForm();
-	}
+    protected void next() {
+        step++;
+        putForm();
+    }
 
-	protected void back() {
-		step--;
-		putForm();
-	}
+    protected void back() {
+        step--;
+        putForm();
+    }
 
-	protected void init() {
-		step = 1;
-		config = new HiveConfig();
-		putForm();
-	}
+    protected void init() {
+        step = 1;
+        config = new HiveConfig();
+        hadoopConfig = new HadoopClusterConfig();
+        putForm();
+    }
 
-	public HiveConfig getConfig() {
-		return config;
-	}
+    public HiveConfig getConfig() {
+        return config;
+    }
+
+    public HadoopClusterConfig getHadoopConfig() {
+        return hadoopConfig;
+    }
 
 }
