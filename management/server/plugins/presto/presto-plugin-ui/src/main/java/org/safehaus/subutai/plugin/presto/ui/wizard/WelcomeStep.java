067e3b7a39ff782ebf3eaa74011b5ebfe5fa3b9e
Azilet Beishenaliev <aziletb@gmail.com>
2014-09-06 18:37:00 +0600

    SUBUTAI-1211: modify ui

--- management/server/plugins/presto/presto-plugin-ui/src/main/java/org/safehaus/subutai/plugin/presto/ui/wizard/WelcomeStep.java
+++ management/server/plugins/presto/presto-plugin-ui/src/main/java/org/safehaus/subutai/plugin/presto/ui/wizard/WelcomeStep.java
@@ -1,56 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.safehaus.subutai.plugin.presto.ui.wizard;
 
 import com.vaadin.server.FileResource;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
-import org.safehaus.subutai.plugin.presto.ui.PrestoUI;
 import org.safehaus.subutai.common.util.FileUtil;
+import org.safehaus.subutai.plugin.presto.api.SetupType;
+import org.safehaus.subutai.plugin.presto.ui.PrestoUI;
 
-/**
- * @author dilshat
- */
 public class WelcomeStep extends Panel {
 
-	public WelcomeStep(final Wizard wizard) {
+    public WelcomeStep(final Wizard wizard) {
 
-		setSizeFull();
+        setSizeFull();
 
-		GridLayout grid = new GridLayout(10, 6);
-		grid.setSpacing(true);
-		grid.setMargin(true);
-		grid.setSizeFull();
+        GridLayout grid = new GridLayout(10, 6);
+        grid.setSpacing(true);
+        grid.setMargin(true);
+        grid.setSizeFull();
 
-		Label welcomeMsg = new Label("<center><h2>Welcome to Presto Installation Wizard!</h2>");
-		welcomeMsg.setContentMode(ContentMode.HTML);
-		grid.addComponent(welcomeMsg, 3, 1, 6, 2);
+        Label welcomeMsg = new Label("<center><h2>Welcome to Presto Installation Wizard!</h2>");
+        welcomeMsg.setContentMode(ContentMode.HTML);
+        grid.addComponent(welcomeMsg, 3, 1, 6, 2);
 
-		Label logoImg = new Label();
-		logoImg.setIcon(new FileResource(FileUtil.getFile(PrestoUI.MODULE_IMAGE, this)));
-		logoImg.setContentMode(ContentMode.HTML);
-		logoImg.setWidth(192, Unit.PIXELS);
-		logoImg.setHeight(63, Unit.PIXELS);
-		grid.addComponent(logoImg, 1, 3, 2, 5);
+        Label logoImg = new Label();
+        logoImg.setIcon(new FileResource(FileUtil.getFile(PrestoUI.MODULE_IMAGE, this)));
+        logoImg.setContentMode(ContentMode.HTML);
+        logoImg.setWidth(192, Unit.PIXELS);
+        logoImg.setHeight(63, Unit.PIXELS);
+        grid.addComponent(logoImg, 1, 3, 2, 5);
 
-		Button next = new Button("Start");
-		next.addStyleName("default");
-		next.setWidth(100, Unit.PIXELS);
-		grid.addComponent(next, 6, 4, 6, 4);
-		grid.setComponentAlignment(next, Alignment.BOTTOM_RIGHT);
+        Button next = new Button("Start over-Hadoop installation");
+        next.addStyleName("default");
+        next.addClickListener(new NextClickHandler(wizard, SetupType.OVER_HADOOP));
+        grid.addComponent(next, 6, 4, 6, 4);
+        grid.setComponentAlignment(next, Alignment.BOTTOM_RIGHT);
 
-		next.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(Button.ClickEvent clickEvent) {
-				wizard.init();
-				wizard.next();
-			}
-		});
+        Button next2 = new Button("Start with-Hadoop installation");
+        next2.addStyleName("default");
+        next2.addClickListener(new NextClickHandler(wizard, SetupType.OVER_HADOOP));
+        grid.addComponent(next2, 7, 4, 7, 4);
+        grid.setComponentAlignment(next2, Alignment.BOTTOM_RIGHT);
 
-		setContent(grid);
-	}
+        setContent(grid);
+    }
+
+    private class NextClickHandler implements Button.ClickListener {
+
+        private final Wizard wizard;
+        private final SetupType type;
+
+        public NextClickHandler(Wizard wizard, SetupType type) {
+            this.wizard = wizard;
+            this.type = type;
+        }
+
+        @Override
+        public void buttonClick(Button.ClickEvent event) {
+            wizard.init();
+            wizard.getConfig().setSetupType(type);
+            wizard.next();
+        }
+
+    }
 
 }
--- management/server/plugins/presto/presto-plugin-ui/src/main/java/org/safehaus/subutai/plugin/presto/ui/wizard/Wizard.java
+++ management/server/plugins/presto/presto-plugin-ui/src/main/java/org/safehaus/subutai/plugin/presto/ui/wizard/Wizard.java
@@ -1,49 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.safehaus.subutai.plugin.presto.ui.wizard;
-
-
-import org.safehaus.subutai.plugin.presto.api.PrestoClusterConfig;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.GridLayout;
+import org.safehaus.subutai.plugin.hadoop.api.HadoopClusterConfig;
+import org.safehaus.subutai.plugin.presto.api.PrestoClusterConfig;
 
-
-/**
- * @author dilshat
- */
 public class Wizard {
+
     private final GridLayout grid;
     private int step = 1;
     private PrestoClusterConfig config = new PrestoClusterConfig();
-
+    private HadoopClusterConfig hadoopConfig = new HadoopClusterConfig();
 
     public Wizard() {
-        grid = new GridLayout( 1, 20 );
-        grid.setMargin( true );
+        grid = new GridLayout(1, 20);
+        grid.setMargin(true);
         grid.setSizeFull();
 
         putForm();
     }
 
-
     private void putForm() {
-        grid.removeComponent( 0, 1 );
+        grid.removeComponent(0, 1);
         Component component = null;
-        switch ( step ) {
+        switch(step) {
             case 1: {
-                component = new WelcomeStep( this );
+                component = new WelcomeStep(this);
                 break;
             }
             case 2: {
-                component = new ConfigurationStep( this );
+                component = new ConfigurationStep(this);
                 break;
             }
             case 3: {
-                component = new VerificationStep( this );
+                component = new VerificationStep(this);
                 break;
             }
             default: {
@@ -51,37 +41,37 @@ public class Wizard {
             }
         }
 
-        if ( component != null ) {
-            grid.addComponent( component, 0, 1, 0, 19 );
-        }
+        if(component != null)
+            grid.addComponent(component, 0, 1, 0, 19);
     }
-
 
     public Component getContent() {
         return grid;
     }
-
 
     protected void next() {
         step++;
         putForm();
     }
 
-
     protected void back() {
         step--;
         putForm();
     }
 
-
     protected void init() {
         step = 1;
         config = new PrestoClusterConfig();
+        hadoopConfig = new HadoopClusterConfig();
         putForm();
     }
-
 
     public PrestoClusterConfig getConfig() {
         return config;
     }
+
+    public HadoopClusterConfig getHadoopConfig() {
+        return hadoopConfig;
+    }
+
 }
