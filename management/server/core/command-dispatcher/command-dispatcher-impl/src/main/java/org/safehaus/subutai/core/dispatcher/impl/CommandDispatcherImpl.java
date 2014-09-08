2931b04e950469eb500a824b19c3829740d6f8ed
dilshat.aliev <dilshat.aliev@safehaus.org>
2014-09-09 02:05:51 +0600

    Command dispatcher in progress

--- management/server/core/command-dispatcher/command-dispatcher-impl/src/main/java/org/safehaus/subutai/core/dispatcher/impl/CommandDispatcherImpl.java
+++ management/server/core/command-dispatcher/command-dispatcher-impl/src/main/java/org/safehaus/subutai/core/dispatcher/impl/CommandDispatcherImpl.java
@@ -72,7 +72,6 @@ public class CommandDispatcherImpl implements CommandDispatcher {
 
         //trigger CommandRunner.onResponse
         for ( Response response : responses ) {
-            LOG.warning( response.toString() );
             commandRunner.onResponse( response );
         }
     }
