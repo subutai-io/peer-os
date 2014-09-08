1cf78dd341c4e019c5de191ab0111402cb6c4125
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2014-09-08 19:56:19 +0600

    refactored sqoop-rest to return response object types and relevant status codes

--- management/server/plugins/sqoop/sqoop-rest/src/main/java/org/safehaus/subutai/plugin/sqoop/rest/RestService.java
+++ management/server/plugins/sqoop/sqoop-rest/src/main/java/org/safehaus/subutai/plugin/sqoop/rest/RestService.java
@@ -1,16 +1,5 @@
 package org.safehaus.subutai.plugin.sqoop.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
 import org.apache.commons.lang.StringUtils;
 import org.safehaus.subutai.common.protocol.Agent;
 import org.safehaus.subutai.common.util.JsonUtil;
@@ -21,6 +10,13 @@ import org.safehaus.subutai.plugin.sqoop.api.SqoopConfig;
 import org.safehaus.subutai.plugin.sqoop.api.setting.ExportSetting;
 import org.safehaus.subutai.plugin.sqoop.api.setting.ImportParameter;
 import org.safehaus.subutai.plugin.sqoop.api.setting.ImportSetting;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class RestService {
 
@@ -41,7 +37,7 @@ public class RestService {
     @GET
     @Path("clusters")
     @Produces({MediaType.APPLICATION_JSON})
-    public String getClusters() {
+    public Response getClusters() {
 
         List<SqoopConfig> configs = sqoopManager.getClusters();
         ArrayList<String> clusterNames = new ArrayList();
@@ -50,24 +46,26 @@ public class RestService {
             clusterNames.add(config.getClusterName());
         }
 
-        return JsonUtil.GSON.toJson(clusterNames);
+        String clusters = JsonUtil.GSON.toJson(clusterNames);
+        return Response.status(Response.Status.OK).entity(clusters).build();
     }
 
     @GET
     @Path("clusters/{clusterName}")
     @Produces({MediaType.APPLICATION_JSON})
-    public String getCluster(
+    public Response getCluster(
             @PathParam("clusterName") String clusterName
     ) {
         SqoopConfig config = sqoopManager.getCluster(clusterName);
 
-        return JsonUtil.GSON.toJson(config);
+        String cluster = JsonUtil.GSON.toJson(config);
+        return Response.status(Response.Status.OK).entity(cluster).build();
     }
 
     @POST
     @Path("clusters/{clusterName}")
     @Produces({MediaType.APPLICATION_JSON})
-    public String installCluster(
+    public Response installCluster(
             @PathParam("clusterName") String clusterName,
             @QueryParam("nodes") String nodes
     ) {
@@ -85,36 +83,39 @@ public class RestService {
 
         UUID uuid = sqoopManager.installCluster(config);
 
-        return JsonUtil.toJson(OPERATION_ID, uuid);
+        String operationId = JsonUtil.toJson(OPERATION_ID, uuid);
+        return Response.status(Response.Status.CREATED).entity(operationId).build();
     }
 
     @POST
     @Path("clusters/{clusterName}/nodes/{hostname}")
     @Produces({MediaType.APPLICATION_JSON})
     @Deprecated()
-    public String addNode(
+    public Response addNode(
             @PathParam("clusterName") String clusterName,
             @PathParam("hostname") String hostname
     ) {
-        return JsonUtil.toJson(OPERATION_ID, null);
+        String operationId = JsonUtil.toJson(OPERATION_ID, null);
+        return Response.status(Response.Status.CREATED).entity(operationId).build();
     }
 
     @DELETE
     @Path("clusters/{clusterName}/nodes/{hostname}")
     @Produces({MediaType.APPLICATION_JSON})
-    public String destroyNode(
+    public Response destroyNode(
             @PathParam("clusterName") String clusterName,
             @PathParam("hostname") String hostname
     ) {
         UUID uuid = sqoopManager.destroyNode(clusterName, hostname);
 
-        return JsonUtil.toJson(OPERATION_ID, uuid);
+        String operationId = JsonUtil.toJson(OPERATION_ID, uuid);
+        return Response.status(Response.Status.OK).entity(operationId).build();
     }
 
     @POST
     @Path("importData")
     @Produces({MediaType.APPLICATION_JSON})
-    public String importData(
+    public Response importData(
             @QueryParam("dataSourceType") String dataSourceType,
             @QueryParam("importAllTables") String importAllTables,
             @QueryParam("datasourceDatabase") String datasourceDatabase,
@@ -140,13 +141,14 @@ public class RestService {
 
         UUID uuid = sqoopManager.importData(settings);
 
-        return JsonUtil.toJson(OPERATION_ID, uuid);
+        String operationId = JsonUtil.toJson(OPERATION_ID, uuid);
+        return Response.status(Response.Status.CREATED).entity(operationId).build();
     }
 
     @GET
     @Path("exportData")
     @Produces({MediaType.APPLICATION_JSON})
-    public String exportData(
+    public Response exportData(
             @QueryParam("hdfsPath") String hdfsPath
     ) {
         ExportSetting setting = new ExportSetting();
@@ -154,6 +156,7 @@ public class RestService {
 
         UUID uuid = sqoopManager.exportData(setting);
 
-        return JsonUtil.toJson(OPERATION_ID, uuid);
+        String operationId = JsonUtil.toJson(OPERATION_ID, uuid);
+        return Response.status(Response.Status.OK).entity(operationId).build();
     }
 }
