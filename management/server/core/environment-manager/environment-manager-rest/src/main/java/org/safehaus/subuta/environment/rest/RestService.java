ba6ec38d8b975f98e90b1ed12c7d43cbd857e924
bahadyr <bahadyr@safehaus.org>
2014-09-10 04:33:05 +0600

    REST for Environment manager is added.

---
+++ management/server/core/environment-manager/environment-manager-rest/src/main/java/org/safehaus/subuta/environment/rest/RestService.java
@@ -0,0 +1,31 @@
+package org.safehaus.subuta.environment.rest;
+
+
+import javax.jws.WebService;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@WebService(serviceName = "peerServices")
+public interface RestService {
+
+
+    @POST
+    @Path("/peer")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String buildNodeGroup( String peer );
+
+    /*@GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPeerJsonFormat();
+
+    @GET
+    @Path( "/id" )
+    @Produces( MediaType.APPLICATION_JSON )
+    public String getId();*/
+}
