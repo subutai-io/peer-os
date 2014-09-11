e86256f82d961e40c91b8ebd97aefd094499277b
Timur Zhamakeev <ztimur@gmail.com>
2014-09-11 13:38:14 +0600

    extracted ContainerPlacementStrategy

--- management/server/core/strategy-manager/strategy-manager-api/src/main/java/org/safehaus/subutai/api/strategymanager/PlacementStrategyFactory.java
+++ management/server/core/strategy-manager/strategy-manager-api/src/main/java/org/safehaus/subutai/api/strategymanager/PlacementStrategyFactory.java
@@ -5,7 +5,7 @@ import org.safehaus.subutai.common.protocol.PlacementStrategy;
 import java.util.List;
 
 public interface PlacementStrategyFactory {
-    public ContainerPlacementStrategy create(int nodesCount, PlacementStrategy strategy, List<Criteria> criteria);
+    public AbstractContainerPlacementStrategy create(int nodesCount, PlacementStrategy strategy, List<Criteria> criteria);
     public PlacementStrategy getDefaultStrategyType();
-    public ContainerPlacementStrategy getDefaultStrategy(int nodesCount, List<Criteria> criteria);
+    public AbstractContainerPlacementStrategy getDefaultStrategy(int nodesCount, List<Criteria> criteria);
 }
