889f3de651ecaa6e52c9a11abec9179d6885662d
dilshat <daliev@critical-factor.com>
2014-09-12 20:02:46 +0600

    Tracker REST refactor

--- management/server/core/tracker/tracker-rest/src/main/java/org/safehaus/subutai/core/tracker/rest/RestService.java
+++ management/server/core/tracker/tracker-rest/src/main/java/org/safehaus/subutai/core/tracker/rest/RestService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
 public interface RestService {
@@ -13,17 +14,17 @@ public interface RestService {
     @GET
     @Path("operations/{source}/{uuid}")
     @Produces({ MediaType.APPLICATION_JSON })
-    public String getProductOperation( @PathParam("source") String source, @PathParam("uuid") String uuid );
+    public Response getProductOperation( @PathParam("source") String source, @PathParam("uuid") String uuid );
 
 
     @GET
     @Path("operations/{source}/{dateFrom}/{dateTo}/{limit}")
     @Produces({ MediaType.APPLICATION_JSON })
-    public String getProductOperations( @PathParam("source") String source, @PathParam("dateFrom") String fromDate,
+    public Response getProductOperations( @PathParam("source") String source, @PathParam("dateFrom") String fromDate,
                                         @PathParam("dateTo") String toDate, @PathParam("limit") int limit );
 
     @GET
     @Path("operations/sources")
     @Produces({ MediaType.APPLICATION_JSON })
-    public String getProductOperationSources();
+    public Response getProductOperationSources();
 }
