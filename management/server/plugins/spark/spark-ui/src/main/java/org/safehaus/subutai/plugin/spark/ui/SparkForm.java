57617a9dfd45fe73b57b8561d51cc667372d25fb
dilshat <daliev@critical-factor.com>
2014-09-15 18:59:33 +0600

    SUBUTAI-1357 SUBUTAI-1358 SUBUTAI-1359 SUBUTAI-1360 SUBUTAI-1361 SUBUTAI-1362 SUBUTAI-1363 SUBUTAI-1364 SUBUTAI-1365 SUBUTAI-1366 SUBUTAI-1367 SUBUTAI-1368

--- management/server/plugins/spark/spark-ui/src/main/java/org/safehaus/subutai/plugin/spark/ui/SparkForm.java
+++ management/server/plugins/spark/spark-ui/src/main/java/org/safehaus/subutai/plugin/spark/ui/SparkForm.java
@@ -1,30 +1,37 @@
 package org.safehaus.subutai.plugin.spark.ui;
 
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.VerticalLayout;
+
+import java.util.concurrent.ExecutorService;
+
+import javax.naming.NamingException;
+
+import org.safehaus.subutai.common.util.ServiceLocator;
 import org.safehaus.subutai.plugin.spark.ui.manager.Manager;
 import org.safehaus.subutai.plugin.spark.ui.wizard.Wizard;
 
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.VerticalLayout;
+
+
 public class SparkForm extends CustomComponent {
 
-    public SparkForm() {
+    public SparkForm( ExecutorService executor, ServiceLocator serviceLocator ) throws NamingException {
         setSizeFull();
 
         VerticalLayout verticalLayout = new VerticalLayout();
-        verticalLayout.setSpacing(true);
+        verticalLayout.setSpacing( true );
         verticalLayout.setSizeFull();
 
         TabSheet mongoSheet = new TabSheet();
         mongoSheet.setSizeFull();
-        Manager manager = new Manager();
-        Wizard wizard = new Wizard();
-        mongoSheet.addTab(wizard.getContent(), "Install");
-        mongoSheet.addTab(manager.getContent(), "Manage");
-        verticalLayout.addComponent(mongoSheet);
+        Manager manager = new Manager( executor, serviceLocator );
+        Wizard wizard = new Wizard( executor, serviceLocator );
+        mongoSheet.addTab( wizard.getContent(), "Install" );
+        mongoSheet.addTab( manager.getContent(), "Manage" );
+        verticalLayout.addComponent( mongoSheet );
 
-        setCompositionRoot(verticalLayout);
+        setCompositionRoot( verticalLayout );
         manager.refreshClustersInfo();
     }
-
 }
