1d265dcb2aa7f869a6d0c3e02f37e29a1fc515e3
dilshat <daliev@critical-factor.com>
2014-09-15 15:01:30 +0600

    Ger rid of static references to OSGi services in NetworkManager

--- management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/HostManager.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/HostManager.java
@@ -1,69 +1,77 @@
 package org.safehaus.subutai.core.network.impl;
 
 
-import com.google.common.collect.Sets;
+import java.util.List;
+
 import org.safehaus.subutai.common.command.Command;
 import org.safehaus.subutai.common.protocol.Agent;
+import org.safehaus.subutai.core.command.api.CommandRunner;
 
-import java.util.List;
+import com.google.common.collect.Sets;
 
 
 /**
  * Created by daralbaev on 04.04.14.
  */
 public class HostManager {
-	private List<Agent> agentList;
-	private String domainName;
+    private List<Agent> agentList;
+    private String domainName;
+    private CommandRunner commandRunner;
+    private Commands commands;
 
 
-	public HostManager(List<Agent> agentList, String domainName) {
-		this.agentList = agentList;
-		this.domainName = domainName;
-	}
+    public HostManager( CommandRunner commandRunner, List<Agent> agentList, String domainName ) {
+        this.agentList = agentList;
+        this.domainName = domainName;
+        this.commandRunner = commandRunner;
+        this.commands = new Commands( commandRunner );
+    }
 
 
-	public boolean execute() {
-		if (agentList != null && !agentList.isEmpty()) {
-			return write();
-		}
+    public boolean execute() {
+        if ( agentList != null && !agentList.isEmpty() ) {
+            return write();
+        }
 
-		return false;
-	}
-
-	private boolean write() {
-		//        String hosts = prepareHost();
-		//        Command command = Commands.getWriteHostsCommand(agentList, hosts);
-		Command command = Commands.getAddIpHostToEtcHostsCommand(domainName, Sets.newHashSet(agentList));
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
-
-		return command.hasSucceeded();
-	}
-
-	public boolean execute(Agent agent) {
-		if (agentList != null && !agentList.isEmpty() && agent != null) {
-			agentList.add(agent);
-			return write();
-		}
-
-		return false;
-	}
+        return false;
+    }
 
 
-	//    private String prepareHost() {
-	//        StringBuilder value = new StringBuilder();
-	//
-	//        for ( Agent agent : agentList ) {
-	//            value.append( agent.getListIP().get( 0 ) );
-	//            value.append( "\t" );
-	//            value.append( agent.getHostname() );
-	//            value.append( "." );
-	//            value.append( domainName );
-	//            value.append( "\t" );
-	//            value.append( agent.getHostname() );
-	//            value.append( "\n" );
-	//        }
-	//        value.append( "127.0.0.1\tlocalhost" );
-	//
-	//        return value.toString();
-	//    }
+    private boolean write() {
+        //        String hosts = prepareHost();
+        //        Command command = commands.getWriteHostsCommand(agentList, hosts);
+        Command command = commands.getAddIpHostToEtcHostsCommand( domainName, Sets.newHashSet( agentList ) );
+        commandRunner.runCommand( command );
+
+        return command.hasSucceeded();
+    }
+
+
+    public boolean execute( Agent agent ) {
+        if ( agentList != null && !agentList.isEmpty() && agent != null ) {
+            agentList.add( agent );
+            return write();
+        }
+
+        return false;
+    }
+
+
+    //    private String prepareHost() {
+    //        StringBuilder value = new StringBuilder();
+    //
+    //        for ( Agent agent : agentList ) {
+    //            value.append( agent.getListIP().get( 0 ) );
+    //            value.append( "\t" );
+    //            value.append( agent.getHostname() );
+    //            value.append( "." );
+    //            value.append( domainName );
+    //            value.append( "\t" );
+    //            value.append( agent.getHostname() );
+    //            value.append( "\n" );
+    //        }
+    //        value.append( "127.0.0.1\tlocalhost" );
+    //
+    //        return value.toString();
+    //    }
 }
--- management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/SshManager.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/SshManager.java
@@ -1,101 +1,118 @@
 package org.safehaus.subutai.core.network.impl;
 
-import com.google.common.base.Strings;
-import org.safehaus.subutai.common.command.AgentResult;
-import org.safehaus.subutai.common.command.Command;
-import org.safehaus.subutai.common.protocol.Agent;
 
 import java.util.Arrays;
 import java.util.List;
+
+import org.safehaus.subutai.common.command.AgentResult;
+import org.safehaus.subutai.common.command.Command;
+import org.safehaus.subutai.common.protocol.Agent;
+import org.safehaus.subutai.core.command.api.CommandRunner;
+
+import com.google.common.base.Strings;
+
 
 /**
  * Created by daralbaev on 04.04.14.
  */
 public class SshManager {
-	private List<Agent> agentList;
-	private String keys;
+    private List<Agent> agentList;
+    private String keys;
+    private CommandRunner commandRunner;
+    private Commands commands;
 
-	public SshManager(List<Agent> agentList) {
-		this.agentList = agentList;
-	}
 
-	public boolean execute() {
-		if (agentList != null && !agentList.isEmpty()) {
-			if (create()) {
-				if (read()) {
-					if (write()) {
-						return config();
-					}
-				}
-			}
-		}
+    public SshManager( CommandRunner commandRunner, List<Agent> agentList ) {
+        this.commandRunner = commandRunner;
+        this.agentList = agentList;
+        this.commands = new Commands( commandRunner );
+    }
 
-		return false;
-	}
 
-	private boolean create() {
-		Command command = Commands.getCreateSSHCommand(agentList);
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
+    public boolean execute() {
+        if ( agentList != null && !agentList.isEmpty() ) {
+            if ( create() ) {
+                if ( read() ) {
+                    if ( write() ) {
+                        return config();
+                    }
+                }
+            }
+        }
 
-		return command.hasSucceeded();
-	}
+        return false;
+    }
 
-	private boolean read() {
-		Command command = Commands.getReadSSHCommand(agentList);
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
 
-		StringBuilder value = new StringBuilder();
-		if (command.hasCompleted()) {
-			for (Agent agent : agentList) {
-				AgentResult result = command.getResults().get(agent.getUuid());
-				if (!Strings.isNullOrEmpty(result.getStdOut())) {
-					value.append(result.getStdOut());
-				}
-			}
-		}
-		keys = value.toString();
+    private boolean create() {
+        Command command = commands.getCreateSSHCommand( agentList );
+        commandRunner.runCommand( command );
 
-		if (!Strings.isNullOrEmpty(keys) && command.hasSucceeded()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        return command.hasSucceeded();
+    }
 
-	private boolean write() {
-		Command command = Commands.getWriteSSHCommand(agentList, keys);
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
 
-		return command.hasSucceeded();
-	}
+    private boolean read() {
+        Command command = commands.getReadSSHCommand( agentList );
+        commandRunner.runCommand( command );
 
-	private boolean config() {
-		Command command = Commands.getConfigSSHCommand(agentList);
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
+        StringBuilder value = new StringBuilder();
+        if ( command.hasCompleted() ) {
+            for ( Agent agent : agentList ) {
+                AgentResult result = command.getResults().get( agent.getUuid() );
+                if ( !Strings.isNullOrEmpty( result.getStdOut() ) ) {
+                    value.append( result.getStdOut() );
+                }
+            }
+        }
+        keys = value.toString();
 
-		return command.hasSucceeded();
-	}
+        if ( !Strings.isNullOrEmpty( keys ) && command.hasSucceeded() ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-	public boolean execute(Agent agent) {
-		if (agentList != null && !agentList.isEmpty() && agent != null) {
-			if (create(agent)) {
-				agentList.add(agent);
 
-				if (read()) {
-					if (write()) {
-						return config();
-					}
-				}
-			}
-		}
+    private boolean write() {
+        Command command = commands.getWriteSSHCommand( agentList, keys );
+        commandRunner.runCommand( command );
 
-		return false;
-	}
+        return command.hasSucceeded();
+    }
 
-	private boolean create(Agent agent) {
-		Command command = Commands.getCreateSSHCommand(Arrays.asList(agent));
-		NetwokManagerImpl.getCommandRunner().runCommand(command);
 
-		return command.hasSucceeded();
-	}
+    private boolean config() {
+        Command command = commands.getConfigSSHCommand( agentList );
+        commandRunner.runCommand( command );
+
+        return command.hasSucceeded();
+    }
+
+
+    public boolean execute( Agent agent ) {
+        if ( agentList != null && !agentList.isEmpty() && agent != null ) {
+            if ( create( agent ) ) {
+                agentList.add( agent );
+
+                if ( read() ) {
+                    if ( write() ) {
+                        return config();
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private boolean create( Agent agent ) {
+        Command command = commands.getCreateSSHCommand( Arrays.asList( agent ) );
+        commandRunner.runCommand( command );
+
+        return command.hasSucceeded();
+    }
 }
