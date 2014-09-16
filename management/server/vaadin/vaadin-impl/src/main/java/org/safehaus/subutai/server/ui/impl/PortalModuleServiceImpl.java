c538d23a1683cc0e04bbd4747ef80a3fcc99fc22
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2014-09-16 13:22:36 +0600

    -m SUBUTAI-1137 removing excessive logging for service registration

--- management/server/vaadin/vaadin-impl/src/main/java/org/safehaus/subutai/server/ui/impl/PortalModuleServiceImpl.java
+++ management/server/vaadin/vaadin-impl/src/main/java/org/safehaus/subutai/server/ui/impl/PortalModuleServiceImpl.java
@@ -7,66 +7,55 @@ import org.safehaus.subutai.server.ui.api.PortalModuleService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class PortalModuleServiceImpl implements PortalModuleService {
 
-    private static final Logger LOG = Logger.getLogger(PortalModuleServiceImpl.class.getName());
+    private List<PortalModule> modules = Collections.synchronizedList(new ArrayList<PortalModule>());
 
-	private List<PortalModule> modules = Collections.synchronizedList(new ArrayList<PortalModule>());
+    private List<PortalModuleListener> listeners = Collections.synchronizedList(new ArrayList<PortalModuleListener>());
 
-	private List<PortalModuleListener> listeners = Collections.synchronizedList(new ArrayList<PortalModuleListener>());
-
-	public void registerModule(PortalModule module) {
-        LOG.info("ModuleServiceImpl: Registering module: " + module.getName() + " " + module.getId());
-		System.out.println("ModuleServiceImpl: Registering module " + module.getId());
-		modules.add(module);
-		for (PortalModuleListener listener : listeners) {
-			listener.moduleRegistered(module);
-		}
-	}
-
-	public void unregisterModule(PortalModule module) {
-
-        LOG.log(Level.WARNING, "ModuleServiceImpl: Unregister module.");
-		if (module != null) {
-            LOG.info("ModuleServiceImpl: Unregister module: " + module.getName() + " " + module.getId());
-			System.out.println("ModuleServiceImpl: Unregister module " + module.getId());
-			modules.remove(module);
-			for (PortalModuleListener listener : listeners) {
-				listener.moduleUnregistered(module);
-			}
-		} else {
-            LOG.log(Level.WARNING, "ModuleServiceImpl: Unregister module is null");
+    public void registerModule(PortalModule module) {
+        System.out.println("ModuleServiceImpl: Registering module " + module.getId());
+        modules.add(module);
+        for (PortalModuleListener listener : listeners) {
+            listener.moduleRegistered(module);
         }
-	}
+    }
 
-	@Override
-	public PortalModule getModule(String pModuleId) {
-		for (PortalModule module : modules) {
-			if (pModuleId.equals(module.getId())) {
-				return module;
-			}
-		}
-		throw new IllegalArgumentException("Cannot find any module with the id given");
-	}
+    public void unregisterModule(PortalModule module) {
+        System.out.println("ModuleServiceImpl: Unregister module " + module.getId());
+        modules.remove(module);
+        for (PortalModuleListener listener : listeners) {
+            listener.moduleUnregistered(module);
+        }
+    }
 
-	public List<PortalModule> getModules() {
-		return Collections.unmodifiableList(modules);
-	}
+    @Override
+    public PortalModule getModule(String pModuleId) {
+        for (PortalModule module : modules) {
+            if (pModuleId.equals(module.getId())) {
+                return module;
+            }
+        }
+        throw new IllegalArgumentException("Cannot find any module with the id given");
+    }
 
-	public synchronized void addListener(PortalModuleListener listener) {
-		System.out.println("ModuleServiceImpl: Adding listener " + listener);
-		listeners.add(listener);
-	}
+    public List<PortalModule> getModules() {
+        return Collections.unmodifiableList(modules);
+    }
 
-	public synchronized void removeListener(PortalModuleListener listener) {
-		if (listener != null) {
-			System.out.println("ModuleServiceImpl: Removing listener " + listener);
-			listeners.remove(listener);
-		}
-	}
+    public synchronized void addListener(PortalModuleListener listener) {
+        System.out.println("ModuleServiceImpl: Adding listener " + listener);
+        listeners.add(listener);
+    }
+
+    public synchronized void removeListener(PortalModuleListener listener) {
+        if (listener != null) {
+            System.out.println("ModuleServiceImpl: Removing listener " + listener);
+            listeners.remove(listener);
+        }
+    }
 
 }
