9c371584bb73b830daa01a2e6d6c3f5f42fb9849
dilshat <daliev@critical-factor.com>
2014-09-16 19:37:18 +0600

    SUBUTAI-1473

--- management/server/plugins/zookeeper/zookeeper-ui/src/main/java/org/safehaus/subutai/plugin/zookeeper/ui/wizard/VerificationStep.java
+++ management/server/plugins/zookeeper/zookeeper-ui/src/main/java/org/safehaus/subutai/plugin/zookeeper/ui/wizard/VerificationStep.java
@@ -6,15 +6,23 @@
 package org.safehaus.subutai.plugin.zookeeper.ui.wizard;
 
 
-import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.ui.*;
-import org.safehaus.subutai.plugin.zookeeper.api.SetupType;
-import org.safehaus.subutai.plugin.zookeeper.api.ZookeeperClusterConfig;
-import org.safehaus.subutai.plugin.zookeeper.ui.ZookeeperUI;
-import org.safehaus.subutai.server.ui.component.ProgressWindow;
-import org.safehaus.subutai.common.protocol.Agent;
-
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+
+import org.safehaus.subutai.common.protocol.Agent;
+import org.safehaus.subutai.core.tracker.api.Tracker;
+import org.safehaus.subutai.plugin.zookeeper.api.SetupType;
+import org.safehaus.subutai.plugin.zookeeper.api.Zookeeper;
+import org.safehaus.subutai.plugin.zookeeper.api.ZookeeperClusterConfig;
+import org.safehaus.subutai.server.ui.component.ProgressWindow;
+
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Window;
 
 
 /**
@@ -22,83 +30,83 @@ import java.util.UUID;
  */
 public class VerificationStep extends Panel {
 
-	public VerificationStep(final Wizard wizard) {
+    public VerificationStep( final Zookeeper zookeeper, final ExecutorService executorService, final Tracker tracker,
+                             final Wizard wizard ) {
 
-		setSizeFull();
+        setSizeFull();
 
-		GridLayout grid = new GridLayout(1, 5);
-		grid.setSpacing(true);
-		grid.setMargin(true);
-		grid.setSizeFull();
+        GridLayout grid = new GridLayout( 1, 5 );
+        grid.setSpacing( true );
+        grid.setMargin( true );
+        grid.setSizeFull();
 
-		Label confirmationLbl = new Label("<strong>Please verify the installation settings "
-				+ "(you may change them by clicking on Back button)</strong><br/>");
-		confirmationLbl.setContentMode(ContentMode.HTML);
+        Label confirmationLbl = new Label( "<strong>Please verify the installation settings "
+                + "(you may change them by clicking on Back button)</strong><br/>" );
+        confirmationLbl.setContentMode( ContentMode.HTML );
 
-		ConfigView cfgView = new ConfigView("Installation configuration");
-		cfgView.addStringCfg("Cluster Name", wizard.getConfig().getClusterName());
-		if (wizard.getConfig().getSetupType() == SetupType.STANDALONE) {
-			cfgView.addStringCfg("Number of nodes", wizard.getConfig().getNumberOfNodes() + "");
-		} else if (wizard.getConfig().getSetupType() == SetupType.OVER_HADOOP) {
-			cfgView.addStringCfg("Hadoop cluster name", wizard.getConfig().getHadoopClusterName());
-			for (Agent node : wizard.getConfig().getNodes()) {
-				cfgView.addStringCfg("Nodes to install", node.getHostname());
-			}
-		} else if (wizard.getConfig().getSetupType() == SetupType.WITH_HADOOP) {
-			cfgView.addStringCfg("Zookeeper cluster name", wizard.getConfig().getClusterName());
-			cfgView.addStringCfg("Number of Zookeeper cluster nodes", wizard.getConfig().getNumberOfNodes() + "");
-			cfgView.addStringCfg("Hadoop cluster name", wizard.getHadoopClusterConfig().getClusterName());
-			cfgView.addStringCfg("Number of Hadoop slave nodes",
-					wizard.getHadoopClusterConfig().getCountOfSlaveNodes() + "");
-			cfgView.addStringCfg("Replication factor for Hadoop slave nodes",
-					wizard.getHadoopClusterConfig().getReplicationFactor() + "");
-			cfgView.addStringCfg("Hadoop cluster domain name", wizard.getHadoopClusterConfig().getDomainName());
-		}
+        ConfigView cfgView = new ConfigView( "Installation configuration" );
+        cfgView.addStringCfg( "Cluster Name", wizard.getConfig().getClusterName() );
+        if ( wizard.getConfig().getSetupType() == SetupType.STANDALONE ) {
+            cfgView.addStringCfg( "Number of nodes", wizard.getConfig().getNumberOfNodes() + "" );
+        }
+        else if ( wizard.getConfig().getSetupType() == SetupType.OVER_HADOOP ) {
+            cfgView.addStringCfg( "Hadoop cluster name", wizard.getConfig().getHadoopClusterName() );
+            for ( Agent node : wizard.getConfig().getNodes() ) {
+                cfgView.addStringCfg( "Nodes to install", node.getHostname() );
+            }
+        }
+        else if ( wizard.getConfig().getSetupType() == SetupType.WITH_HADOOP ) {
+            cfgView.addStringCfg( "Zookeeper cluster name", wizard.getConfig().getClusterName() );
+            cfgView.addStringCfg( "Number of Zookeeper cluster nodes", wizard.getConfig().getNumberOfNodes() + "" );
+            cfgView.addStringCfg( "Hadoop cluster name", wizard.getHadoopClusterConfig().getClusterName() );
+            cfgView.addStringCfg( "Number of Hadoop slave nodes",
+                    wizard.getHadoopClusterConfig().getCountOfSlaveNodes() + "" );
+            cfgView.addStringCfg( "Replication factor for Hadoop slave nodes",
+                    wizard.getHadoopClusterConfig().getReplicationFactor() + "" );
+            cfgView.addStringCfg( "Hadoop cluster domain name", wizard.getHadoopClusterConfig().getDomainName() );
+        }
 
-		Button install = new Button("Install");
-		install.addStyleName("default");
-		install.addClickListener(new Button.ClickListener() {
+        Button install = new Button( "Install" );
+        install.addStyleName( "default" );
+        install.addClickListener( new Button.ClickListener() {
 
-			@Override
-			public void buttonClick(Button.ClickEvent event) {
-				UUID trackID = wizard.getConfig().getSetupType() == SetupType.WITH_HADOOP ? ZookeeperUI.getManager()
-						.installCluster(
-								wizard.getConfig(),
-								wizard.getHadoopClusterConfig()) :
-						ZookeeperUI.getManager().installCluster(wizard.getConfig());
+            @Override
+            public void buttonClick( Button.ClickEvent event ) {
+                UUID trackID = wizard.getConfig().getSetupType() == SetupType.WITH_HADOOP ?
+                               zookeeper.installCluster( wizard.getConfig(), wizard.getHadoopClusterConfig() ) :
+                               zookeeper.installCluster( wizard.getConfig() );
 
-				ProgressWindow window =
-						new ProgressWindow(ZookeeperUI.getExecutor(), ZookeeperUI.getTracker(), trackID,
-								ZookeeperClusterConfig.PRODUCT_KEY);
-				window.getWindow().addCloseListener(new Window.CloseListener() {
-					@Override
-					public void windowClose(Window.CloseEvent closeEvent) {
-						wizard.init();
-					}
-				});
-				getUI().addWindow(window.getWindow());
-			}
-		});
+                ProgressWindow window =
+                        new ProgressWindow( executorService, tracker, trackID, ZookeeperClusterConfig.PRODUCT_KEY );
+                window.getWindow().addCloseListener( new Window.CloseListener() {
+                    @Override
+                    public void windowClose( Window.CloseEvent closeEvent ) {
+                        wizard.init();
+                    }
+                } );
+                getUI().addWindow( window.getWindow() );
+            }
+        } );
 
-		Button back = new Button("Back");
-		back.addStyleName("default");
-		back.addClickListener(new Button.ClickListener() {
-			@Override
-			public void buttonClick(Button.ClickEvent event) {
-				wizard.back();
-			}
-		});
+        Button back = new Button( "Back" );
+        back.addStyleName( "default" );
+        back.addClickListener( new Button.ClickListener() {
+            @Override
+            public void buttonClick( Button.ClickEvent event ) {
+                wizard.back();
+            }
+        } );
 
-		HorizontalLayout buttons = new HorizontalLayout();
-		buttons.addComponent(back);
-		buttons.addComponent(install);
+        HorizontalLayout buttons = new HorizontalLayout();
+        buttons.addComponent( back );
+        buttons.addComponent( install );
 
-		grid.addComponent(confirmationLbl, 0, 0);
+        grid.addComponent( confirmationLbl, 0, 0 );
 
-		grid.addComponent(cfgView.getCfgTable(), 0, 1, 0, 3);
+        grid.addComponent( cfgView.getCfgTable(), 0, 1, 0, 3 );
 
-		grid.addComponent(buttons, 0, 4);
+        grid.addComponent( buttons, 0, 4 );
 
-		setContent(grid);
-	}
+        setContent( grid );
+    }
 }
