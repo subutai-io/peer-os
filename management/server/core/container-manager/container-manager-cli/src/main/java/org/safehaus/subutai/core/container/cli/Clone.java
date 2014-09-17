f4220919d0cdbfdd16789f615717f212723f963a
Timur Zhamakeev <ztimur@gmail.com>
2014-09-17 12:55:44 +0600

    added ContainerEventListener to container clone CLI

--- management/server/core/container-manager/container-manager-cli/src/main/java/org/safehaus/subutai/core/container/cli/Clone.java
+++ management/server/core/container-manager/container-manager-cli/src/main/java/org/safehaus/subutai/core/container/cli/Clone.java
@@ -1,10 +1,14 @@
 package org.safehaus.subutai.core.container.cli;
 
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.UUID;
 
 import org.safehaus.subutai.common.protocol.Agent;
 import org.safehaus.subutai.core.container.api.ContainerCreateException;
+import org.safehaus.subutai.core.container.api.ContainerEvent;
+import org.safehaus.subutai.core.container.api.ContainerEventListener;
 import org.safehaus.subutai.core.container.api.ContainerManager;
 
 import org.apache.felix.gogo.commands.Argument;
@@ -13,7 +17,7 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 
 @Command( scope = "container", name = "clone" )
-public class Clone extends OsgiCommandSupport {
+public class Clone extends OsgiCommandSupport implements ContainerEventListener {
 
     ContainerManager containerManager;
 
@@ -34,17 +38,28 @@ public class Clone extends OsgiCommandSupport {
     @Override
     protected Object doExecute() throws Exception
     {
-
+        containerManager.addListener( this );
         UUID envId = UUID.randomUUID();
         try
         {
             Agent a = containerManager.clone( envId, hostname, templateName, cloneName );
-            System.out.println( String.format("Container cloned successfully. Agent %s", a) );
+            System.out.println( String.format( "Container cloned successfully. Agent %s", a ) );
         }
         catch ( ContainerCreateException cce )
         {
             System.out.println( "Container cloning failed!" );
         }
+        containerManager.removeListener( this );
         return null;
+    }
+
+
+    @Override
+    public void onContainerEvent( final ContainerEvent containerEvent )
+    {
+        Calendar cal = new GregorianCalendar();
+        cal.setTimeInMillis( containerEvent.getTimestamp() );
+        System.out.println( String.format( "%1s: %2s -< %3s %4$tm %4$te,%4$tY %4$tT", containerEvent.getEventType(),
+                containerEvent.getParentHostname(), containerEvent.getHostname(), cal ) );
     }
 }
