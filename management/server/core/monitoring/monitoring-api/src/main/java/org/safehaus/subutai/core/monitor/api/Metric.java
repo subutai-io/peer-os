52a53644eead49a64fe5ce9d549c2efe8ec6f86e
daralbaev <daralbaev@gmail.com>
2014-09-17 10:21:03 +0600

    -m SUBUTAI-1347 Monitoring API added

--- management/server/core/monitoring/monitoring-api/src/main/java/org/safehaus/subutai/core/monitor/api/Metric.java
+++ management/server/core/monitoring/monitoring-api/src/main/java/org/safehaus/subutai/core/monitor/api/Metric.java
@@ -1,35 +1,42 @@
 package org.safehaus.subutai.core.monitor.api;
 
+
 public enum Metric {
 
-	CPU_USER("CPU User", "%"),
-	CPU_SYSTEM("CPU System", "%"),
-	CPU_IDLE("CPU Idle", "%"),
-	CPU_WIO("CPU wio", "%"),
-	MEM_FREE("Free Memory", "KB"),
-	MEM_CACHED("Cached Memory", "KB"),
-	MEM_BUFFERS("Memory Buffers", "KB"),
-	SWAP_FREE("Free Swap Space", "KB"),
-	PKTS_IN("Packets Received", "packets/sec"),
-	PKTS_OUT("Packets Sent", "packets/sec"),
-	BYTES_IN("Bytes Received", "bytes/sec"),
-	BYTES_OUT("Bytes Sent", "bytes/sec"),
-	PART_MAX_USED("Maximum Disk Space Used", "%");
+    CPU_USER( "CPU User", "%" ),
+    CPU_SYSTEM( "CPU System", "%" ),
+    CPU_IDLE( "CPU Idle", "%" ),
+    CPU_WIO( "CPU wio", "%" ),
+    MEM_FREE( "Free Memory", "KB" ),
+    MEM_CACHED( "Cached Memory", "KB" ),
+    MEM_BUFFERS( "Memory Buffers", "KB" ),
+    SWAP_FREE( "Free Swap Space", "KB" ),
+    PKTS_IN( "Packets Received", "packets/sec" ),
+    PKTS_OUT( "Packets Sent", "packets/sec" ),
+    BYTES_IN( "Bytes Received", "bytes/sec" ),
+    BYTES_OUT( "Bytes Sent", "bytes/sec" ),
+    PART_MAX_USED( "Maximum Disk Space Used", "%" );
 
-	private String unit = "";
-	private String description = "";
+    private String unit = "";
+    private String description = "";
 
-	private Metric(String description, String unit) {
-		this.description = description;
-		this.unit = unit;
-	}
 
-	public String getUnit() {
-		return unit;
-	}
+    private Metric( String description, String unit )
+    {
+        this.description = description;
+        this.unit = unit;
+    }
 
-	@Override
-	public String toString() {
-		return description;
-	}
+
+    public String getUnit()
+    {
+        return unit;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return description;
+    }
 }
--- management/server/core/monitoring/monitoring-api/src/main/java/org/safehaus/subutai/core/monitor/api/Monitor.java
+++ management/server/core/monitoring/monitoring-api/src/main/java/org/safehaus/subutai/core/monitor/api/Monitor.java
@@ -1,12 +1,13 @@
 package org.safehaus.subutai.core.monitor.api;
 
+
 import java.util.Date;
 import java.util.Map;
 
+
 public interface Monitor {
 
-	public Map<Date, Double> getData(String host, Metric metric, Date startDate, Date endDate);
+    public Map<Date, Double> getData( String host, Metric metric, Date startDate, Date endDate );
 
-	public Map<Metric, Map<Date, Double>> getDataForAllMetrics(String host, Date startDate, Date endDate);
-
+    public Map<Metric, Map<Date, Double>> getDataForAllMetrics( String host, Date startDate, Date endDate );
 }
