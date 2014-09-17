02d5e039edddeb5ae8dc727ca5d3584190bcfae2
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2014-09-17 12:40:21 +0600

    -m SUBUTAI-1474 initial commit for separating core plugins from plugins

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/util/HelpManager.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/util/HelpManager.java
@@ -39,37 +39,7 @@ public class HelpManager {
 	}
 
 	public void showHelpFor(Class<? extends View> view) {
-		// if (view == DashboardView.class) {
-		//
-		// } else if (view == SalesView.class) {
-		// addOverlay(
-		// "Add new data sets",
-		// "You can add new data sets to the graph by choosing a title from the combo box and clicking \"Add\".",
-		// "timeline-add");
-		// addOverlay("Clear graph", "Clear all data sets from the graph",
-		// "timeline-clear");
-		// addOverlay(
-		// "Browse",
-		// "The Timeline org.safehaus.subutai.server.ui.component allows you to browse through and zoom the data sets infinitely.",
-		// "timeline-browse").center();
-		// } else if (view == TransactionsView.class) {
-		// addOverlay(
-		// "Unlimited Data",
-		// "You can scroll through any number of rows in the table with blazing speed",
-		// "table-lazy").center();
-		// addOverlay(
-		// "Filter",
-		// "Live filter the table contents (in this demo you can only filter the country field",
-		// "table-filter");
-		// addOverlay(
-		// "Create a report",
-		// "You can select some rows from the table, and then either drag them over to the \"Reports\" tab in the sidebar or click this button or open the context menu for the items and select \"Create Report\"",
-		// "table-rows");
-		// } else if (view == ScheduleView.class) {
-		// addOverlay("Drag'n'drop",
-		// "You can drag'n'drop the events to change the schedule. You can also click on the events to get more information of the event.",
-		// "event").center();
-		// }
+        // TODO need logic for showing help for View
 	}
 
 	public HelpOverlay addOverlay(String caption, String text, String style) {
