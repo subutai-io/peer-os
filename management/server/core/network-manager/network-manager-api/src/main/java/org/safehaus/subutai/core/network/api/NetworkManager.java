25bb4d1acb975ba2332146e587e766ecd5ca9c17
daralbaev <daralbaev@gmail.com>
2014-09-17 10:25:16 +0600

    -m SUBUTAI-1347
    Network manager API added

--- management/server/core/network-manager/network-manager-api/src/main/java/org/safehaus/subutai/core/network/api/NetworkManager.java
+++ management/server/core/network-manager/network-manager-api/src/main/java/org/safehaus/subutai/core/network/api/NetworkManager.java
@@ -1,15 +1,17 @@
 package org.safehaus.subutai.core.network.api;
 
-import org.safehaus.subutai.common.protocol.Agent;
 
 import java.util.List;
 
+import org.safehaus.subutai.common.protocol.Agent;
+
+
 public interface NetworkManager {
-	public boolean configSshOnAgents(List<Agent> agentList);
+    public boolean configSshOnAgents( List<Agent> agentList );
 
-	public boolean configSshOnAgents(List<Agent> agentList, Agent agent);
+    public boolean configSshOnAgents( List<Agent> agentList, Agent agent );
 
-	public boolean configHostsOnAgents(List<Agent> agentList, String domainName);
+    public boolean configHostsOnAgents( List<Agent> agentList, String domainName );
 
-	public boolean configHostsOnAgents(List<Agent> agentList, Agent agent, String domainName);
+    public boolean configHostsOnAgents( List<Agent> agentList, Agent agent, String domainName );
 }
