a554b36a38c0c03e2420fd1f4aa55215aabf5f00
daralbaev <daralbaev@gmail.com>
2014-09-18 15:47:30 +0600

    -m SUBUTAI-1347
    Config point tracker added

--- management/server/core/config-point-tracker/config-point-tracker-impl/src/main/java/org/safehaus/subutai/core/configpointtracker/impl/ConfigPointTrackerImpl.java
+++ management/server/core/config-point-tracker/config-point-tracker-impl/src/main/java/org/safehaus/subutai/core/configpointtracker/impl/ConfigPointTrackerImpl.java
@@ -1,52 +1,57 @@
 package org.safehaus.subutai.core.configpointtracker.impl;
 
 
-import org.safehaus.subutai.core.configpointtracker.api.ConfigPointTracker;
-
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.safehaus.subutai.core.configpointtracker.api.ConfigPointTracker;
+
 
 public class ConfigPointTrackerImpl implements ConfigPointTracker {
 
-	// <templateName, configPoints>
-	private final HashMap<String, Set<String>> configPoints = new HashMap<>();
+    // <templateName, configPoints>
+    private final HashMap<String, Set<String>> configPoints = new HashMap<>();
 
 
-	@Override
-	public void add(String templateName, String... configPaths) {
+    @Override
+    public void add( String templateName, String... configPaths )
+    {
 
-		Set<String> points = configPoints.get(templateName);
+        Set<String> points = configPoints.get( templateName );
 
-		if (points == null) {
-			points = new HashSet<>();
-			configPoints.put(templateName, points);
-		}
+        if ( points == null )
+        {
+            points = new HashSet<>();
+            configPoints.put( templateName, points );
+        }
 
-		for (String path : configPaths) {
-			points.add(path);
-		}
-	}
-
-
-	@Override
-	public void remove(String templateName, String... configPaths) {
-
-		Set<String> points = configPoints.get(templateName);
-
-		if (points == null) {
-			return;
-		}
-
-		for (String path : configPaths) {
-			points.remove(path);
-		}
-	}
+        Collections.addAll( points, configPaths );
+    }
 
 
-	@Override
-	public Set<String> get(String templateName) {
-		return configPoints.get(templateName);
-	}
+    @Override
+    public void remove( String templateName, String... configPaths )
+    {
+
+        Set<String> points = configPoints.get( templateName );
+
+        if ( points == null )
+        {
+            return;
+        }
+
+        for ( String path : configPaths )
+        {
+            points.remove( path );
+        }
+    }
+
+
+    @Override
+    public Set<String> get( String templateName )
+    {
+        return configPoints.get( templateName );
+    }
 }
