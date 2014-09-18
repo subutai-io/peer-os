e6bc228c410caecf0634bcb0ee238177497c4a4e
daralbaev <daralbaev@gmail.com>
2014-09-18 18:30:23 +0600

    -m SUBUTAI-1347
     Package manager CLI added

--- management/server/core/package-manager/cli/src/main/java/org/safehaus/subutai/core/packge/cli/FindPackagesInfo.java
+++ management/server/core/package-manager/cli/src/main/java/org/safehaus/subutai/core/packge/cli/FindPackagesInfo.java
@@ -1,37 +1,52 @@
 package org.safehaus.subutai.core.packge.cli;
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
-import org.safehaus.subutai.core.packge.api.PackageInfo;
-import org.safehaus.subutai.core.packge.api.PackageManager;
 
 import java.util.Collection;
 
-@Command (scope = "deb-package", name = "find", description = "find previously saved packages info")
+import org.safehaus.subutai.core.packge.api.PackageInfo;
+import org.safehaus.subutai.core.packge.api.PackageManager;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+
+@Command(scope = "deb-package", name = "find", description = "find previously saved packages info")
 public class FindPackagesInfo extends OsgiCommandSupport {
 
-	private PackageManager packageManager;
-	@Argument (index = 0, required = true)
-	private String hostname;
+    private PackageManager packageManager;
+    @Argument(index = 0, required = true)
+    private String hostname;
 
-	public PackageManager getPackageManager() {
-		return packageManager;
-	}
 
-	public void setPackageManager(PackageManager packageManager) {
-		this.packageManager = packageManager;
-	}
+    public PackageManager getPackageManager()
+    {
+        return packageManager;
+    }
 
-	@Override
-	protected Object doExecute() throws Exception {
-		Collection<PackageInfo> ls = packageManager.findPackagesInfo(hostname);
-		if (ls == null)
-			System.out.println("Package info not found for " + hostname);
-		else
-			for (PackageInfo pi : ls) System.out.println(pi);
 
-		return null;
-	}
+    public void setPackageManager( PackageManager packageManager )
+    {
+        this.packageManager = packageManager;
+    }
 
+
+    @Override
+    protected Object doExecute() throws Exception
+    {
+        Collection<PackageInfo> ls = packageManager.findPackagesInfo( hostname );
+        if ( ls == null )
+        {
+            System.out.println( "Package info not found for " + hostname );
+        }
+        else
+        {
+            for ( PackageInfo pi : ls )
+            {
+                System.out.println( pi );
+            }
+        }
+
+        return null;
+    }
 }
--- management/server/core/package-manager/cli/src/main/java/org/safehaus/subutai/core/packge/cli/ListPackages.java
+++ management/server/core/package-manager/cli/src/main/java/org/safehaus/subutai/core/packge/cli/ListPackages.java
@@ -1,39 +1,54 @@
 package org.safehaus.subutai.core.packge.cli;
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
-import org.safehaus.subutai.core.packge.api.PackageInfo;
-import org.safehaus.subutai.core.packge.api.PackageManager;
 
 import java.util.Collection;
 
-@Command (scope = "deb-package", name = "list", description = "list packages")
+import org.safehaus.subutai.core.packge.api.PackageInfo;
+import org.safehaus.subutai.core.packge.api.PackageManager;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+
+@Command(scope = "deb-package", name = "list", description = "list packages")
 public class ListPackages extends OsgiCommandSupport {
 
-	private PackageManager packageManager;
-	@Argument (index = 0, required = true)
-	private String hostname;
-	@Argument (index = 1, description = "package name pattern")
-	private String namePattern;
+    private PackageManager packageManager;
+    @Argument(index = 0, required = true)
+    private String hostname;
+    @Argument(index = 1, description = "package name pattern")
+    private String namePattern;
 
-	public PackageManager getPackageManager() {
-		return packageManager;
-	}
 
-	public void setPackageManager(PackageManager packageManager) {
-		this.packageManager = packageManager;
-	}
+    public PackageManager getPackageManager()
+    {
+        return packageManager;
+    }
 
-	@Override
-	protected Object doExecute() throws Exception {
-		Collection<PackageInfo> ls = packageManager.listPackages(hostname, namePattern);
-		if (ls == null)
-			System.out.println("Invalid hostname or agent is not connected");
-		else
-			for (PackageInfo pi : ls) System.out.println(pi);
 
-		return null;
-	}
+    public void setPackageManager( PackageManager packageManager )
+    {
+        this.packageManager = packageManager;
+    }
 
+
+    @Override
+    protected Object doExecute() throws Exception
+    {
+        Collection<PackageInfo> ls = packageManager.listPackages( hostname, namePattern );
+        if ( ls == null )
+        {
+            System.out.println( "Invalid hostname or agent is not connected" );
+        }
+        else
+        {
+            for ( PackageInfo pi : ls )
+            {
+                System.out.println( pi );
+            }
+        }
+
+        return null;
+    }
 }
