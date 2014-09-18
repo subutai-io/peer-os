0a2a3c2974aae2bdf576fcc37b3feda056514628
daralbaev <daralbaev@gmail.com>
2014-09-18 21:49:35 +0600

    -m SUBUTAI-1347
     Template registry CLI added

--- management/server/core/template-registry/template-registry-cli/src/main/java/org/safehaus/subutai/core/registry/cli/UnregisterTemplateCommand.java
+++ management/server/core/template-registry/template-registry-cli/src/main/java/org/safehaus/subutai/core/registry/cli/UnregisterTemplateCommand.java
@@ -1,37 +1,40 @@
 package org.safehaus.subutai.core.registry.cli;
 
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
 import org.safehaus.subutai.core.registry.api.TemplateRegistryManager;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 
 /**
  * CLI for TemplateRegistryManager.unregisterTemplate command
  */
-@Command (scope = "registry", name = "unregister-template", description = "Unregister template")
+@Command( scope = "registry", name = "unregister-template", description = "Unregister template" )
 public class UnregisterTemplateCommand extends OsgiCommandSupport {
-	@Argument (index = 0, name = "template name", required = true, multiValued = false,
-			description = "template name")
-	String templateName;
+    @Argument( index = 0, name = "template name", required = true, multiValued = false,
+            description = "template name" )
+    String templateName;
 
-	private TemplateRegistryManager templateRegistryManager;
-
-
-	public void setTemplateRegistryManager(final TemplateRegistryManager templateRegistryManager) {
-		this.templateRegistryManager = templateRegistryManager;
-	}
+    private TemplateRegistryManager templateRegistryManager;
 
 
-	@Override
-	protected Object doExecute() throws Exception {
-
-		templateRegistryManager.unregisterTemplate(templateName);
-
-		System.out.println(String.format("Template %s unregistered successfully", templateName));
+    public void setTemplateRegistryManager( final TemplateRegistryManager templateRegistryManager )
+    {
+        this.templateRegistryManager = templateRegistryManager;
+    }
 
 
-		return null;
-	}
+    @Override
+    protected Object doExecute() throws Exception
+    {
+
+        templateRegistryManager.unregisterTemplate( templateName );
+
+        System.out.println( String.format( "Template %s unregistered successfully", templateName ) );
+
+
+        return null;
+    }
 }
