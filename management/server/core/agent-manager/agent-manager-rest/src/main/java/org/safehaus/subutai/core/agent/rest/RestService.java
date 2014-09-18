9a1ef647166f6bea187a831a3b4b4c1fb1cc9769
daralbaev <daralbaev@gmail.com>
2014-09-18 22:23:04 +0600

    -m SUBUTAI-1347
     Agent manager REST added

--- management/server/core/agent-manager/agent-manager-rest/src/main/java/org/safehaus/subutai/core/agent/rest/RestService.java
+++ management/server/core/agent-manager/agent-manager-rest/src/main/java/org/safehaus/subutai/core/agent/rest/RestService.java
@@ -1,5 +1,6 @@
 package org.safehaus.subutai.core.agent.rest;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,95 +8,74 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+
 public interface RestService {
 
     // should return Set<Agent> in Json format
 
     /**
-     * Returns list of agents available in container in JSON format
-     * with HTTP response status 200
-     *
-     * @return
+     * Returns list of agents available in container in JSON format with HTTP response status 200
      */
     @GET
-    @Path("agents")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Path( "agents" )
+    @Produces( { MediaType.APPLICATION_JSON } )
     public Response getAgents();
 
     //should return Set<Agent> in Json format
 
     /**
-     * Returns list of physical agents available in container in JSON format
-     * with HTTP response status 200
-     *
-     * @return
+     * Returns list of physical agents available in container in JSON format with HTTP response status 200
      */
     @GET
-    @Path("agents/physical")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Path( "agents/physical" )
+    @Produces( { MediaType.APPLICATION_JSON } )
     public Response getPhysicalAgents();
 
     //Set<Agent>
 
     /**
-     * Returns list of lxc agents available in container in JSON format
-     * with HTTP response status 200
-     *
-     * @return
+     * Returns list of lxc agents available in container in JSON format with HTTP response status 200
      */
     @GET
-    @Path("agents/lxc")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Path( "agents/lxc" )
+    @Produces( { MediaType.APPLICATION_JSON } )
     public Response getLxcAgents();
 
     //Agent
 
     /**
-     * Returns list of agents available in container by hostname in JSON format
-     * this allows to differentiate remote and host agents
-     * with HTTP response status 200
-     *
-     * @param hostname
-     * @return
+     * Returns list of agents available in container by hostname in JSON format this allows to differentiate remote and
+     * host agents with HTTP response status 200
      */
     @GET
-    @Path("agents/by-hostname/{hostname}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getAgentByHostname(@PathParam("hostname") String hostname);
+    @Path( "agents/by-hostname/{hostname}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getAgentByHostname( @PathParam( "hostname" ) String hostname );
 
     //Agent
 
     /**
-     * Returns list of agents available in container by uuid in JSON format
-     * this allows to get specific agent
-     * with HTTP response status 200
-     *
-     * @param uuid
-     * @return
+     * Returns list of agents available in container by uuid in JSON format this allows to get specific agent with HTTP
+     * response status 200
      */
     @GET
-    @Path("agents/by-agent-id/{uuid}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getAgentByUUID(@PathParam("uuid") String uuid);
+    @Path( "agents/by-agent-id/{uuid}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getAgentByUUID( @PathParam( "uuid" ) String uuid );
 
     //Set<Agent>
 
     /**
-     * Returns list of agents available in container by parentHostname in JSON format
-     * this allows to differentiate remote and host agents
-     * with HTTP response status 200
-     *
-     * @param parentHostname
-     * @return
+     * Returns list of agents available in container by parentHostname in JSON format this allows to differentiate
+     * remote and host agents with HTTP response status 200
      */
     @GET
-    @Path("agents/{parentHostname}/children")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getLxcAgentsByParentHostname(@PathParam("parentHostname") String parentHostname);
+    @Path( "agents/{parentHostname}/children" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getLxcAgentsByParentHostname( @PathParam( "parentHostname" ) String parentHostname );
 
     @GET
-    @Path("agents/by-environment-id/{envId}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getAgentsByEnvironmentId(@PathParam("envId") String environmentId);
-
+    @Path( "agents/by-environment-id/{envId}" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getAgentsByEnvironmentId( @PathParam( "envId" ) String environmentId );
 }
