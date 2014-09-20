8111494b81cf741125645831e96c4a461b8638d2
daralbaev <daralbaev@gmail.com>
2014-09-20 21:32:06 +0600

    -m SUBUTAI-1347
    Fixing changes after merge

--- management/server/subutai-common/src/main/java/org/safehaus/subutai/common/util/UUIDUtil.java
+++ management/server/subutai-common/src/main/java/org/safehaus/subutai/common/util/UUIDUtil.java
@@ -3,7 +3,8 @@ package org.safehaus.subutai.common.util;
 
 import java.util.UUID;
 
-import org.doomdark.uuid.UUIDGenerator;
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
 
 
 /**
@@ -14,13 +15,30 @@ public class UUIDUtil
 
     public static UUID generateTimeBasedUUID()
     {
-        return java.util.UUID.fromString( UUIDGenerator.getInstance().generateTimeBasedUUID().toString() );
+        // http://www.cowtowncoder.com/blog/archives/2010/10/entry_429.html
+        /*
+        // need to pass Ethernet address; can either use real one (shown here)
+        EthernetAddress nic = EthernetAddress.fromInterface();
+        // or bogus which would be gotten with: EthernetAddress.constructMulticastAddress()
+        TimeBasedGenerator uuidGenerator = Generators.timeBasedGenerator( EthernetAddress.fromInterface() );
+        // also: we don't specify synchronizer, getting an intra-JVM syncer; there is
+        // also external file-locking-based synchronizer if multiple JVMs run JUG
+        UUID uuid = uuidGenerator.generate();*/
+
+
+        return Generators.timeBasedGenerator( EthernetAddress.fromInterface() ).generate();
     }
 
 
     public static UUID generateCassandraUUID()
     {
-        return UUID.fromString( new com.eaio.uuid.UUID().toString() );
+        return Generators.randomBasedGenerator().generate();
+    }
+
+
+    public static UUID generateStringUUID( String id )
+    {
+        return Generators.nameBasedGenerator().generate( id );
     }
 
 
