8b385f3f60f4b7f3bfdfb0f05ffc5e549faded0c
daralbaev <daralbaev@gmail.com>
2014-09-21 19:03:13 +0600

    -m SUBUTAI-1347
    Config Point Tracker added

--- management/server/core/config-point-tracker/config-point-tracker-cli/src/main/java/org/safehaus/subutai/core/configpointtracker/cli/GetCommand.java
+++ management/server/core/config-point-tracker/config-point-tracker-cli/src/main/java/org/safehaus/subutai/core/configpointtracker/cli/GetCommand.java
@@ -3,16 +3,16 @@ package org.safehaus.subutai.core.configpointtracker.cli;
 
 import org.safehaus.subutai.core.configpointtracker.api.ConfigPointTracker;
 
-import org.apache.felix.gogo.commands.Argument;
-import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 
-@Command(scope = "config-point-tracker", name = "get")
+@Command( scope = "config-point-tracker", name = "get" )
 public class GetCommand extends OsgiCommandSupport
 {
 
-    @Argument(index = 0, name = "templateName", required = true)
+    @Argument( index = 0, name = "templateName", required = true )
     private String templateName;
 
     private ConfigPointTracker configPointTracker;
