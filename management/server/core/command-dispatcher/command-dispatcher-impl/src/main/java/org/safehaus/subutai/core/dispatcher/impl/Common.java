96182a7ccdab27444b09de9f04426b28096e4368
dilshat <daliev@critical-factor.com>
2014-09-22 13:14:26 +0600

    sonar bugfix

--- management/server/core/command-dispatcher/command-dispatcher-impl/src/main/java/org/safehaus/subutai/core/dispatcher/impl/Common.java
+++ management/server/core/command-dispatcher/command-dispatcher-impl/src/main/java/org/safehaus/subutai/core/dispatcher/impl/Common.java
@@ -8,4 +8,7 @@ public class Common
 {
     public static final String DISPATCHER_NAME = "dispatcher";
     public static final String RESPONSE_URL = "http://%s:8181/cxf/dispatcher/responses";
+
+
+    private Common() {}
 }
