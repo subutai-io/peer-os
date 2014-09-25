2c2be2c87a1a8a69de327a39ca5eb82c49587807
bahadyr <bahadyr@safehaus.org>
2014-09-25 17:37:55 +0600

    Test coverage in progress.

--- management/server/core/peer-manager/peer-manager-cli/src/test/java/org/safehaus/subutai/core/peer/cli/ListCommandTest.java
+++ management/server/core/peer-manager/peer-manager-cli/src/test/java/org/safehaus/subutai/core/peer/cli/ListCommandTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.peer.cli;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class ListCommandTest
 {
+    ListCommand listCommand;
+
+
+    @Before
+    public void init()
+    {
+        listCommand = mock( ListCommand.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
--- management/server/core/peer-manager/peer-manager-cli/src/test/java/org/safehaus/subutai/core/peer/cli/RegisterCommandTest.java
+++ management/server/core/peer-manager/peer-manager-cli/src/test/java/org/safehaus/subutai/core/peer/cli/RegisterCommandTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.peer.cli;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class RegisterCommandTest
 {
+    RegisterCommand registerCommand;
+
+
+    @Before
+    public void init()
+    {
+        registerCommand = mock( RegisterCommand.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
