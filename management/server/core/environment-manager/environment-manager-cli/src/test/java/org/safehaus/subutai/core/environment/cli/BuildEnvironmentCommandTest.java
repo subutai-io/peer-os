ca0517a7c5d1aaf53ee851ff1d82101276f2b599
bahadyr <bahadyr@safehaus.org>
2014-09-25 17:29:25 +0600

    Test coverage in progress.

--- management/server/core/environment-manager/environment-manager-cli/src/test/java/org/safehaus/subutai/core/environment/cli/BuildEnvironmentCommandTest.java
+++ management/server/core/environment-manager/environment-manager-cli/src/test/java/org/safehaus/subutai/core/environment/cli/BuildEnvironmentCommandTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.environment.cli;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class BuildEnvironmentCommandTest
 {
+    BuildEnvironmentCommand buildEnvironmentCommand;
+
+
+    @Before
+    public void init()
+    {
+        buildEnvironmentCommand = mock( BuildEnvironmentCommand.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
