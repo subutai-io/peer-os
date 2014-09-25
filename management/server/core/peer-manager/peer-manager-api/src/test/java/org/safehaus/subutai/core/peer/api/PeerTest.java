8bcb9761ae1bdaf87b7885ab943bac25aa7294bc
bahadyr <bahadyr@safehaus.org>
2014-09-25 17:36:45 +0600

    Test coverage in progress.

--- management/server/core/peer-manager/peer-manager-api/src/test/java/org/safehaus/subutai/core/peer/api/PeerTest.java
+++ management/server/core/peer-manager/peer-manager-api/src/test/java/org/safehaus/subutai/core/peer/api/PeerTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.peer.api;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class PeerTest
 {
+    Peer peer;
+
+
+    @Before
+    public void init()
+    {
+        peer = mock( Peer.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
