184e3ad2f653799338b96dc78eef4b1c43d48a84
bahadyr <bahadyr@safehaus.org>
2014-09-25 17:36:00 +0600

    Test coverage in progress.

--- management/server/core/environment-manager/environment-manager-ui/src/test/java/org/safehaus/subutai/core/environment/ui/EnvironmentManagerPortalModuleTest.java
+++ management/server/core/environment-manager/environment-manager-ui/src/test/java/org/safehaus/subutai/core/environment/ui/EnvironmentManagerPortalModuleTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.environment.ui;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class EnvironmentManagerPortalModuleTest
 {
+    EnvironmentManagerPortalModule environmentManagerPortalModule;
+
+
+    @Before
+    public void init()
+    {
+        environmentManagerPortalModule = mock( EnvironmentManagerPortalModule.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
