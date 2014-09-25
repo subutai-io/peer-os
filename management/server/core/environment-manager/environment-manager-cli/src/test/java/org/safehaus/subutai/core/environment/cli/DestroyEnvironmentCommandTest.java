3d5018d7c5699e1d827d9281085cc4c793740ec3
bahadyr <bahadyr@safehaus.org>
2014-09-25 17:30:47 +0600

    Test coverage in progress.

--- management/server/core/environment-manager/environment-manager-cli/src/test/java/org/safehaus/subutai/core/environment/cli/DestroyEnvironmentCommandTest.java
+++ management/server/core/environment-manager/environment-manager-cli/src/test/java/org/safehaus/subutai/core/environment/cli/DestroyEnvironmentCommandTest.java
@@ -1,9 +1,30 @@
 package org.safehaus.subutai.core.environment.cli;
 
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+
 /**
  * Created by bahadyr on 9/25/14.
  */
 public class DestroyEnvironmentCommandTest
 {
+    DestroyEnvironmentCommand destroyEnvironmentCommand;
+
+
+    @Before
+    public void init()
+    {
+        destroyEnvironmentCommand = mock( DestroyEnvironmentCommand.class );
+    }
+
+
+    @Test
+    public void test()
+    {
+
+    }
 }
