af9fb66cee13232166839624b90d6fa2874fc670
dilshat <daliev@critical-factor.com>
2014-09-27 19:01:56 +0600

    Core Unit Test

--- management/server/core/git-manager/git-manager-api/src/main/java/org/safehaus/subutai/core/git/api/GitChangedFile.java
+++ management/server/core/git-manager/git-manager-api/src/main/java/org/safehaus/subutai/core/git/api/GitChangedFile.java
@@ -38,4 +38,40 @@ public class GitChangedFile
                 ", gitFilePath='" + gitFilePath + '\'' +
                 '}';
     }
+
+
+    @Override
+    public boolean equals( final Object o )
+    {
+        if ( this == o )
+        {
+            return true;
+        }
+        if ( !( o instanceof GitChangedFile ) )
+        {
+            return false;
+        }
+
+        final GitChangedFile that = ( GitChangedFile ) o;
+
+        if ( gitFilePath != null ? !gitFilePath.equals( that.gitFilePath ) : that.gitFilePath != null )
+        {
+            return false;
+        }
+        if ( gitFileStatus != that.gitFileStatus )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        int result = gitFileStatus != null ? gitFileStatus.hashCode() : 0;
+        result = 31 * result + ( gitFilePath != null ? gitFilePath.hashCode() : 0 );
+        return result;
+    }
 }
--- management/server/core/git-manager/git-manager-impl/src/test/java/org/safehaus/subutai/core/git/impl/GitManagerImplTest.java
+++ management/server/core/git-manager/git-manager-impl/src/test/java/org/safehaus/subutai/core/git/impl/GitManagerImplTest.java
@@ -1,30 +1,49 @@
 package org.safehaus.subutai.core.git.impl;
 
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
+import java.util.UUID;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import org.junit.Test;
+import org.safehaus.subutai.common.protocol.Agent;
+import org.safehaus.subutai.core.command.api.CommandRunner;
+import org.safehaus.subutai.core.command.api.command.Command;
+import org.safehaus.subutai.core.git.api.GitChangedFile;
+import org.safehaus.subutai.core.git.api.GitException;
+import org.safehaus.subutai.core.git.api.GitFileStatus;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 /**
- * Created by bahadyr on 9/25/14.
+ * Test for GitManagerImpl
  */
 public class GitManagerImplTest
 {
+    private static final String MASTER_BRANCH = "master";
+    private static final String REPOSITORY_ROOT = "root";
+    private static final String FILEP_PATH =
+            "management/server/core/communication-manager/communication-manager-api/src/main/java/org/safehaus"
+                    + "/subutai/core/communication/api/CommandJson.java";
 
-    GitManagerImpl gitManager;
+    private static final String DIFF_BRANCH_OUTPUT = String.format( "M %s", FILEP_PATH );
 
-    @Before
-    public void setup()
-    {
-        gitManager = mock( GitManagerImpl.class );
-    }
 
     @Test
-    public void test() {
+    public void shouldReturnDiffBranches() throws GitException
+    {
+        Agent agent = MockUtils.getAgent( UUID.randomUUID() );
+        Command command = MockUtils.getCommand( true, true, agent.getUuid(), DIFF_BRANCH_OUTPUT, null, null );
+        CommandRunner commandRunner = MockUtils.getCommandRunner( command );
 
+        GitManagerImpl gitManager = new GitManagerImpl( commandRunner );
+
+        List<GitChangedFile> changedFiles = gitManager.diffBranches( agent, REPOSITORY_ROOT, MASTER_BRANCH );
+        GitChangedFile changedFile = changedFiles.get( 0 );
+
+        assertTrue( changedFiles.contains( new GitChangedFile( GitFileStatus.MODIFIED, FILEP_PATH ) ) );
+        assertEquals( GitFileStatus.MODIFIED, changedFile.getGitFileStatus() );
+        assertEquals( FILEP_PATH, changedFile.getGitFilePath() );
     }
-
 }
