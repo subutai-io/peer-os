3cb70114035e996f08bf33605a53899d73bc3109
dilshat <daliev@critical-factor.com>
2014-10-01 11:00:56 +0600

    Core unit test

--- management/server/core/git-manager/git-manager-cli/src/test/java/org/safehaus/subutai/core/git/cli/AddAllTest.java
+++ management/server/core/git-manager/git-manager-cli/src/test/java/org/safehaus/subutai/core/git/cli/AddAllTest.java
@@ -1,8 +1,114 @@
 package org.safehaus.subutai.core.git.cli;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.safehaus.subutai.common.protocol.Agent;
+import org.safehaus.subutai.core.agent.api.AgentManager;
+import org.safehaus.subutai.core.git.api.GitException;
+import org.safehaus.subutai.core.git.api.GitManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
 /**
  * Test for AddAll
  */
 public class AddAllTest
-{}
+{
+
+    private ByteArrayOutputStream myOut;
+    private static final String AGENT_NOT_CONNECTED_MSG = "Agent not connected";
+    private static final String REPOSITORY_ROOT = "repo root";
+    private static final String HOSTNAME = "hostname";
+    private static final String ERR_MSG = "OOPS";
+
+
+    @Before
+    public void setUp()
+    {
+        myOut = new ByteArrayOutputStream();
+        System.setOut( new PrintStream( myOut ) );
+    }
+
+
+    @After
+    public void tearDown()
+    {
+        System.setOut( System.out );
+    }
+
+
+    private String getSysOut()
+    {
+        return myOut.toString().trim();
+    }
+
+
+    @Test( expected = NullPointerException.class )
+    public void constructorShouldFailOnNullGitManager()
+    {
+        new AddAll( null, mock( AgentManager.class ) );
+    }
+
+
+    @Test( expected = NullPointerException.class )
+    public void constructorShouldFailOnNullAgentManager()
+    {
+        new AddAll( mock( GitManager.class ), null );
+    }
+
+
+    @Test
+    public void shouldFailOnMissingAgent()
+    {
+        AddAll addAll = new AddAll( mock( GitManager.class ), mock( AgentManager.class ) );
+
+        addAll.doExecute();
+
+        assertEquals( AGENT_NOT_CONNECTED_MSG, getSysOut() );
+    }
+
+
+    @Test
+    public void shouldExecuteCommand() throws GitException
+    {
+        Agent agent = mock( Agent.class );
+        AgentManager agentManager = mock( AgentManager.class );
+        when( agentManager.getAgentByHostname( HOSTNAME ) ).thenReturn( agent );
+        GitManager gitManager = mock( GitManager.class );
+        AddAll addAll = new AddAll( gitManager, agentManager );
+        addAll.setHostname( HOSTNAME );
+        addAll.setRepoPath( REPOSITORY_ROOT );
+
+        addAll.doExecute();
+
+        verify( gitManager ).addAll( agent, REPOSITORY_ROOT );
+    }
+
+
+    @Test
+    public void shouldThrowException() throws GitException
+    {
+        Agent agent = mock( Agent.class );
+        AgentManager agentManager = mock( AgentManager.class );
+        when( agentManager.getAgentByHostname( HOSTNAME ) ).thenReturn( agent );
+        GitManager gitManager = mock( GitManager.class );
+        Mockito.doThrow( new GitException( ERR_MSG ) ).when( gitManager ).addAll( agent, REPOSITORY_ROOT );
+        AddAll addAll = new AddAll( gitManager, agentManager );
+        addAll.setHostname( HOSTNAME );
+        addAll.setRepoPath( REPOSITORY_ROOT );
+
+        addAll.doExecute();
+
+        assertEquals( ERR_MSG, getSysOut() );
+    }
+}
