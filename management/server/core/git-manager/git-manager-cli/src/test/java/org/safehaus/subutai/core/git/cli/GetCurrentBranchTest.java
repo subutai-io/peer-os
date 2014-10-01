e25be3b984bef4dab8b9af8a78d3c465c4ce702a
dilshat <daliev@critical-factor.com>
2014-10-01 13:49:21 +0600

    Core unit test

--- management/server/core/git-manager/git-manager-cli/src/test/java/org/safehaus/subutai/core/git/cli/GetCurrentBranchTest.java
+++ management/server/core/git-manager/git-manager-cli/src/test/java/org/safehaus/subutai/core/git/cli/GetCurrentBranchTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.safehaus.subutai.common.protocol.Agent;
 import org.safehaus.subutai.core.agent.api.AgentManager;
+import org.safehaus.subutai.core.git.api.GitBranch;
 import org.safehaus.subutai.core.git.api.GitException;
 import org.safehaus.subutai.core.git.api.GitManager;
 
@@ -22,17 +23,17 @@ import static org.mockito.Mockito.when;
 
 
 /**
- * Test for DiffFile
+ * Test for GetCurrentBranch
  */
-public class DiffFileTest
+public class GetCurrentBranchTest
 {
 
     private ByteArrayOutputStream myOut;
     private static final String AGENT_NOT_CONNECTED_MSG = "Agent not connected";
+    private static final String REPOSITORY_ROOT = "repo root";
     private static final String HOSTNAME = "hostname";
-    private static final String BRANCH_NAME = "branch name";
+    private static final GitBranch GIT_BRANCH = new GitBranch( "branch", true );
     private static final String ERR_MSG = "OOPS";
-    private static final String FILE_DIFF = "file diff";
     private Agent agent = mock( Agent.class );
     private AgentManager agentManager = mock( AgentManager.class );
     private GitManager gitManager = mock( GitManager.class );
@@ -63,25 +64,24 @@ public class DiffFileTest
     @Test( expected = NullPointerException.class )
     public void constructorShouldFailOnNullGitManager()
     {
-        new DiffFile( null, mock( AgentManager.class ) );
+        new GetCurrentBranch( null, mock( AgentManager.class ) );
     }
 
 
     @Test( expected = NullPointerException.class )
     public void constructorShouldFailOnNullAgentManager()
     {
-        new DiffFile( mock( GitManager.class ), null );
+        new GetCurrentBranch( mock( GitManager.class ), null );
     }
 
 
     @Test
     public void shouldFailOnMissingAgent()
     {
-        DiffFile diffFile = new DiffFile( mock( GitManager.class ), mock( AgentManager.class ) );
+        GetCurrentBranch getCurrentBranch =
+                new GetCurrentBranch( mock( GitManager.class ), mock( AgentManager.class ) );
 
-
-        diffFile.doExecute();
-
+        getCurrentBranch.doExecute();
 
         assertEquals( AGENT_NOT_CONNECTED_MSG, getSysOut() );
     }
@@ -90,63 +90,25 @@ public class DiffFileTest
     @Test
     public void shouldExecuteCommand() throws GitException
     {
+        GetCurrentBranch getCurrentBranch = new GetCurrentBranch( gitManager, agentManager );
+        getCurrentBranch.setHostname( HOSTNAME );
+        when( gitManager.currentBranch( eq( agent ), anyString() ) ).thenReturn( GIT_BRANCH );
 
-        DiffFile diffFile = new DiffFile( gitManager, agentManager );
-        diffFile.setHostname( HOSTNAME );
-        when( gitManager.diffFile( eq( agent ), anyString(), anyString(), anyString() ) ).thenReturn( FILE_DIFF );
+        getCurrentBranch.doExecute();
 
-
-        diffFile.doExecute();
-
-
-        verify( gitManager ).diffFile( eq( agent ), anyString(), anyString(), anyString() );
-    }
-
-
-    @Test
-    public void shouldExecuteCommand2() throws GitException
-    {
-
-        DiffFile diffFile = new DiffFile( gitManager, agentManager );
-        diffFile.setHostname( HOSTNAME );
-        diffFile.setBranchName2( BRANCH_NAME );
-
-
-        diffFile.doExecute();
-
-
-        verify( gitManager ).diffFile( eq( agent ), anyString(), anyString(), anyString(), anyString() );
+        verify( gitManager ).currentBranch( eq( agent ), anyString() );
+        assertEquals( GIT_BRANCH.toString(), getSysOut() );
     }
 
 
     @Test
     public void shouldThrowException() throws GitException
     {
-        Mockito.doThrow( new GitException( ERR_MSG ) ).when( gitManager )
-               .diffFile( eq( agent ), anyString(), anyString(), anyString() );
-        DiffFile diffFile = new DiffFile( gitManager, agentManager );
-        diffFile.setHostname( HOSTNAME );
+        Mockito.doThrow( new GitException( ERR_MSG ) ).when( gitManager ).currentBranch( eq( agent ), anyString() );
+        GetCurrentBranch getCurrentBranch = new GetCurrentBranch( gitManager, agentManager );
+        getCurrentBranch.setHostname( HOSTNAME );
 
-
-        diffFile.doExecute();
-
-
-        assertEquals( ERR_MSG, getSysOut() );
-    }
-
-
-    @Test
-    public void shouldThrowException2() throws GitException
-    {
-        Mockito.doThrow( new GitException( ERR_MSG ) ).when( gitManager )
-               .diffFile( eq( agent ), anyString(), anyString(), anyString(), anyString() );
-        DiffFile diffFile = new DiffFile( gitManager, agentManager );
-        diffFile.setHostname( HOSTNAME );
-        diffFile.setBranchName2( BRANCH_NAME );
-
-
-        diffFile.doExecute();
-
+        getCurrentBranch.doExecute();
 
         assertEquals( ERR_MSG, getSysOut() );
     }
