56caa0362aaaf3c383ca8bdbf46b931499d6f812
dilshat <daliev@critical-factor.com>
2014-10-02 15:40:25 +0600

    Removing CommandsSingleton from Hbase plugin

--- management/server/plugins/hbase/hbase-impl/src/main/java/org/safehaus/subutai/plugin/hbase/impl/handler/CheckClusterHandler.java
+++ management/server/plugins/hbase/hbase-impl/src/main/java/org/safehaus/subutai/plugin/hbase/impl/handler/CheckClusterHandler.java
@@ -4,12 +4,10 @@ package org.safehaus.subutai.plugin.hbase.impl.handler;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.safehaus.subutai.core.command.api.command.Command;
 import org.safehaus.subutai.common.protocol.AbstractOperationHandler;
 import org.safehaus.subutai.common.protocol.Agent;
-import org.safehaus.subutai.common.tracker.ProductOperation;
+import org.safehaus.subutai.core.command.api.command.Command;
 import org.safehaus.subutai.plugin.hbase.api.HBaseClusterConfig;
-import org.safehaus.subutai.plugin.hbase.impl.Commands;
 import org.safehaus.subutai.plugin.hbase.impl.HBaseImpl;
 
 
@@ -55,7 +53,7 @@ public class CheckClusterHandler extends AbstractOperationHandler<HBaseImpl>
             return;
         }
 
-        Command checkCommand = Commands.getStatusCommand( allNodes );
+        Command checkCommand = manager.getCommands().getStatusCommand( allNodes );
         manager.getCommandRunner().runCommand( checkCommand );
 
         if ( checkCommand.hasSucceeded() )
@@ -72,7 +70,6 @@ public class CheckClusterHandler extends AbstractOperationHandler<HBaseImpl>
         {
             productOperation.addLogFailed( String.format( "Check failed, %s", checkCommand.getAllErrors() ) );
         }
-
     }
 
 
