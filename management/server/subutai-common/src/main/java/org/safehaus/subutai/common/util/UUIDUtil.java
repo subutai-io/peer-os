bed7b2f1611c9f47fcea8ba9e3ff98b7a14c37b2
daralbaev <daralbaev@gmail.com>
2014-10-05 03:21:42 +0600

    -m SUBUTAI-1347 UUIDUtil changed

--- management/server/subutai-common/src/main/java/org/safehaus/subutai/common/util/UUIDUtil.java
+++ management/server/subutai-common/src/main/java/org/safehaus/subutai/common/util/UUIDUtil.java
@@ -13,32 +13,33 @@ import com.fasterxml.uuid.Generators;
 public class UUIDUtil
 {
 
+    //    public static UUID generateTimeBasedUUID()
+    //    {
+    //        return Generators.timeBasedGenerator( EthernetAddress.fromInterface() ).generate();
+    //    }
+    //
+    //
+    //    public static UUID generateCassandraUUID()
+    //    {
+    //        return Generators.randomBasedGenerator().generate();
+    //    }
+    //
+    //
+    //    public static UUID generateStringUUID( String id )
+    //    {
+    //        return Generators.nameBasedGenerator().generate( id );
+    //    }
+    //
+    //
+    //    public static UUID generateMACBasedUUID()
+    //    {
+    //        return UUID.nameUUIDFromBytes( SysUtil.getMacAddress().getBytes() );
+    //    }
+
+
     public static UUID generateTimeBasedUUID()
     {
-        // http://www.cowtowncoder.com/blog/archives/2010/10/entry_429.html
-        /*
-        // need to pass Ethernet address; can either use real one (shown here)
-        EthernetAddress nic = EthernetAddress.fromInterface();
-        // or bogus which would be gotten with: EthernetAddress.constructMulticastAddress()
-        TimeBasedGenerator uuidGenerator = Generators.timeBasedGenerator( EthernetAddress.fromInterface() );
-        // also: we don't specify synchronizer, getting an intra-JVM syncer; there is
-        // also external file-locking-based synchronizer if multiple JVMs run JUG
-        UUID uuid = uuidGenerator.generate();*/
-
-
         return Generators.timeBasedGenerator( EthernetAddress.fromInterface() ).generate();
-    }
-
-
-    public static UUID generateCassandraUUID()
-    {
-        return Generators.randomBasedGenerator().generate();
-    }
-
-
-    public static UUID generateStringUUID( String id )
-    {
-        return Generators.nameBasedGenerator().generate( id );
     }
 
 
