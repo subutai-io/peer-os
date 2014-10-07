f97b7a8160fc7d65fc6e5e80de6d48b45cc40fa0
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2014-10-07 11:55:04 +0600

    -m SUBUTAI-1645-lxc-quota-unit-test corrected lxc-quota pom files

--- management/server/core/lxc-quota-manager/lxc-quota-manager-impl/src/test/java/org/safehaus/subutai/core/lxc/quota/impl/QuotaManagerImplTest.java
+++ management/server/core/lxc-quota-manager/lxc-quota-manager-impl/src/test/java/org/safehaus/subutai/core/lxc/quota/impl/QuotaManagerImplTest.java
@@ -1,10 +1,45 @@
 package org.safehaus.subutai.core.lxc.quota.impl;
 
 
-/**
- * Created by talas on 10/7/14.
- */
+import org.junit.Before;
+import org.junit.Test;
+import org.safehaus.subutai.common.protocol.Container;
+import org.safehaus.subutai.core.lxc.quota.api.QuotaEnum;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
+
+
 public class QuotaManagerImplTest
 {
+    QuotaEnum parameter = QuotaEnum.MEMORY_LIMIT_IN_BYTES;
+    QuotaManagerImpl quotaManager;
 
+
+    @Before
+    public void setupClasses()
+    {
+        quotaManager = new QuotaManagerImpl();
+    }
+
+
+    @Test
+    public void testSetQuota() throws Exception
+    {
+        Container container = mock( Container.class );
+        quotaManager.setQuota( container, parameter, "200M" );
+        String value = quotaManager.getQuota( container, parameter );
+        assertEquals( "200M", value );
+    }
+
+
+    @Test
+    public void testGetQuota() throws Exception
+    {
+        Container container = mock( Container.class );
+        String value = quotaManager.getQuota( container, parameter );
+        quotaManager.setQuota( container, parameter, "200M" );
+        assertNotEquals( value, "200M" );
+    }
 }
