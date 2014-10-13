ff2f4244a3262edb0046add58e1f4cb9e770b0e7
dilshat <daliev@critical-factor.com>
2014-10-13 14:33:27 +0600

    cover tracker with unit tests

--- management/server/core/tracker/tracker-impl/src/test/java/org/safehaus/subutai/core/tracker/impl/TrackerImplTest.java
+++ management/server/core/tracker/tracker-impl/src/test/java/org/safehaus/subutai/core/tracker/impl/TrackerImplTest.java
@@ -8,107 +8,216 @@ package org.safehaus.subutai.core.tracker.impl;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.sql.Clob;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 import javax.sql.DataSource;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.safehaus.subutai.common.tracker.ProductOperation;
-import org.safehaus.subutai.core.db.api.DbManager;
-import org.safehaus.subutai.core.db.impl.DbManagerImpl;
-import org.safehaus.subutai.core.tracker.api.Tracker;
+import org.safehaus.subutai.common.tracker.ProductOperationView;
+import org.safehaus.subutai.common.util.DbUtil;
+import org.safehaus.subutai.common.util.JsonUtil;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyVararg;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 /**
  * Test for TrackerImpl class
  */
-@Ignore
+
+@RunWith( MockitoJUnitRunner.class )
 public class TrackerImplTest
 {
 
+    @Mock
+    DataSource dataSource;
+    @Mock
+    DbUtil dbUtil;
+    @Mock
+    ResultSet resultSet;
+    @Mock
+    Clob clobInfo;
+    TrackerImpl tracker;
+    private static final String SOURCE = "source";
+    private static final String DESCRIPTION = "description";
+    private static final String DONE = "done";
+    private static final UUID operationId = UUID.randomUUID();
 
-    private final DbManager dbManager = new DbManagerImpl();
-    private final String source = "source";
-    private final String description = "description";
-    private final String testLog = "test";
-    private Tracker tracker;
-    //    @Rule
-    //    public CassandraCQLUnit cassandraCQLUnit =
-    //            new CassandraCQLUnit( new ClassPathCQLDataSet( "po.sql", true, true, "test" ) );
+    private ProductOperationImpl productOperation;
+
+
+    private ByteArrayOutputStream myOut;
 
 
     @Before
     public void setUp() throws SQLException
     {
-        //        ( ( DbManagerImpl ) dbManager ).setSession( cassandraCQLUnit.session );
-        //        ( ( TrackerImpl ) tracker ).setDbManager( dbManager );
-        DataSource dataSource = mock( DataSource.class );
-        tracker = new TrackerImpl( dataSource );
+        productOperation = new ProductOperationImpl( SOURCE, DESCRIPTION, mock( TrackerImpl.class ) );
+        tracker = new TrackerImplExt( dataSource, dbUtil );
+        when( dbUtil.select( anyString(), anyVararg() ) ).thenReturn( resultSet );
+        when( resultSet.next() ).thenReturn( true ).thenReturn( false );
+        when( resultSet.getClob( "info" ) ).thenReturn( clobInfo );
+        when( resultSet.getString( SOURCE ) ).thenReturn( SOURCE );
+        when( clobInfo.length() ).thenReturn( 1L );
+
+        when( clobInfo.getSubString( anyLong(), anyInt() ) ).thenReturn( JsonUtil.toJson( productOperation ) );
+
+        myOut = new ByteArrayOutputStream();
+        System.setOut( new PrintStream( myOut ) );
     }
 
 
     @After
-    public void tearDown()
+    public void tearDown() throws Exception
     {
-        dbManager.executeUpdate( "truncate product_operation;" );
+        System.setOut( System.out );
+    }
+
+
+    private String getSysOut()
+    {
+        return myOut.toString().trim();
+    }
+
+
+    @Test( expected = NullPointerException.class )
+    public void constructorShouldFailOnNullDataSource() throws Exception
+    {
+        new TrackerImpl( null );
     }
 
 
     @Test
-    public void testCreateNGetProductOperation()
+    public void testGetProductOperation() throws Exception
     {
+        ProductOperationView pv = tracker.getProductOperation( SOURCE, operationId );
 
-        ProductOperation po = tracker.createProductOperation( source, description );
-
-        assertNotNull( tracker.getProductOperation( source, po.getId() ) );
+        assertNotNull( pv );
     }
 
 
     @Test
-    public void testGetProductOperations()
+    public void testGetProductOperationException() throws Exception
     {
+        when( dbUtil.select( anyString(), anyVararg() ) ).thenThrow( new SQLException() );
 
-        tracker.createProductOperation( source, description );
-        tracker.createProductOperation( source, description );
+        ProductOperationView pv = tracker.getProductOperation( SOURCE, operationId );
 
-        Date endDate = new Date();
-        Date startDate = new Date( endDate.getTime() - 5 * 1000 );
-        assertEquals( tracker.getProductOperations( source, startDate, endDate, 100 ).size(), 2 );
+        assertNull( pv );
     }
 
 
     @Test
-    public void testGetProductOperationSources()
+    public void testSaveProductOperation() throws Exception
     {
-
-        tracker.createProductOperation( "source1", description );
-
-        tracker.createProductOperation( "source2", description );
-        tracker.createProductOperation( "source3", description );
-
-        assertEquals( 3, tracker.getProductOperationSources().size() );
+        assertTrue( tracker.saveProductOperation( SOURCE, productOperation ) );
     }
 
 
     @Test
-    public void testPrintOperationLog()
+    public void testSaveProductOperationException() throws Exception
     {
-        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
-        System.setOut( new PrintStream( myOut ) );
-        ProductOperation po = tracker.createProductOperation( source, description );
+        when( dbUtil.update( anyString(), anyVararg() ) ).thenThrow( new SQLException() );
 
-        po.addLogDone( testLog );
+        assertFalse( tracker.saveProductOperation( SOURCE, productOperation ) );
+    }
 
-        tracker.printOperationLog( source, po.getId(), 5 * 1000 );
 
-        assertEquals( testLog, myOut.toString().trim() );
+    @Test
+    public void testCreateProductOperation() throws Exception
+    {
+        ProductOperation po = tracker.createProductOperation( SOURCE, DESCRIPTION );
+
+        assertNotNull( po );
+    }
+
+
+    @Test
+    public void testCreateProductOperationException() throws Exception
+    {
+
+        when( dbUtil.update( anyString(), anyVararg() ) ).thenThrow( new SQLException() );
+
+        ProductOperation po = tracker.createProductOperation( SOURCE, DESCRIPTION );
+
+        assertNull( po );
+    }
+
+
+    @Test
+    public void testGetProductOperations() throws Exception
+    {
+
+        List<ProductOperationView> pos = tracker.getProductOperations( SOURCE, new Date(), new Date(), 1 );
+
+        assertNotNull( pos );
+        assertFalse( pos.isEmpty() );
+    }
+
+
+    @Test
+    public void testGetProductOperationsException() throws Exception
+    {
+        when( dbUtil.select( anyString(), anyVararg() ) ).thenThrow( new SQLException() );
+
+        List<ProductOperationView> pos = tracker.getProductOperations( SOURCE, new Date(), new Date(), 1 );
+
+        assertNotNull( pos );
+        assertTrue( pos.isEmpty() );
+    }
+
+
+    @Test
+    public void testGetProductOperationSources() throws Exception
+    {
+        List<String> sources = tracker.getProductOperationSources();
+
+        assertNotNull( sources );
+        assertFalse( sources.isEmpty() );
+    }
+
+
+    @Test
+    public void testGetProductOperationSourcesException() throws Exception
+    {
+        when( dbUtil.select( anyString(), anyVararg() ) ).thenThrow( new SQLException() );
+
+        List<String> sources = tracker.getProductOperationSources();
+
+        assertNotNull( sources );
+        assertTrue( sources.isEmpty() );
+    }
+
+
+    @Test
+    public void testPrintOperationLog() throws Exception
+    {
+        productOperation.addLogDone( DONE );
+        when( clobInfo.getSubString( anyLong(), anyInt() ) ).thenReturn( JsonUtil.toJson( productOperation ) );
+
+        tracker.printOperationLog( SOURCE, operationId, 1000 );
+
+        assertThat( getSysOut(), containsString( DONE ) );
     }
 }
