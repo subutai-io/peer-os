e5924f788ea99a2694b5972e4714f67e9818e026
Emin Inal <emininal@gmail.com>
2014-10-18 01:34:08 +0300

    fix for subutai-management package

--- management/debian/pack.sh
+++ management/debian/pack.sh
@@ -26,15 +26,9 @@ echo "fileName:"$fileName
 #copying template files from repository
 rm -rf $BASE/$fileName/DEBIAN
 rm -rf $BASE/$fileName/opt
-rm -rf $BASE/$fileName/etc
-rm -rf $BASE/$fileName/usr
-rm -rf $BASE/$fileName/var
 
 cp -a $BASE_SOURCE/debian/management/DEBIAN $BASE/$fileName/DEBIAN
 cp -a $BASE_SOURCE/debian/management/opt $BASE/$fileName/opt
-cp -a $BASE_SOURCE/debian/management/etc $BASE/$fileName/etc
-cp -a $BASE_SOURCE/debian/management/usr $BASE/$fileName/usr
-cp -a $BASE_SOURCE/debian/management/var $BASE/$fileName/var
 
 #copying subutai.tar.gz file from maven output target folder
 echo "copying subutai-management snapshot files.."
@@ -55,8 +49,6 @@ tar xzvf $distroName
 cp -a $BASE/$distroFolderName/* $BASE/$fileName/opt/subutai-management/
 
 #removing subutai and subutai.tar.gz fodler and files
-rm -rf $BASE/$fileName/opt/subutai-management/data
-rm -rf $BASE/$fileName/opt/subutai-management/bin
 rm $BASE/$distroName
 rm -rf $BASE/$distroFolderName
 
