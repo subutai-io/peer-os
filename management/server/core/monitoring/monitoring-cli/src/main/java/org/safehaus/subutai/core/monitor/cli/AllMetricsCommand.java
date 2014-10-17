4ca12c301037dc74fa080e841fc1c316618d08b5
dilshat <daliev@critical-factor.com>
2014-10-17 16:49:02 +0600

    Monitoring module in progress

--- management/server/core/monitoring/monitoring-cli/src/main/java/org/safehaus/subutai/core/monitor/cli/AllMetricsCommand.java
+++ management/server/core/monitoring/monitoring-cli/src/main/java/org/safehaus/subutai/core/monitor/cli/AllMetricsCommand.java
@@ -2,8 +2,14 @@ package org.safehaus.subutai.core.monitor.cli;
 
 
 import java.util.Date;
+import java.util.List;
 
+import org.safehaus.subutai.core.monitor.api.Metric;
+import org.safehaus.subutai.core.monitor.api.MetricType;
+import org.safehaus.subutai.core.monitor.api.MonitorException;
 import org.safehaus.subutai.core.monitor.api.Monitoring;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.karaf.shell.commands.Argument;
@@ -11,21 +17,30 @@ import org.apache.karaf.shell.commands.Command;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
 
 
-@Command( scope = "monitor", name = "all-metrics" )
+@Command(scope = "monitor", name = "all-metrics",
+        description = "List all metrics for given host within given number of last days up to given limit")
 public class AllMetricsCommand extends OsgiCommandSupport
 {
+    private static final Logger LOG = LoggerFactory.getLogger( AllMetricsCommand.class.getName() );
+
 
     @Argument( index = 0, name = "hostname", required = true, multiValued = false )
-    protected String hostname = null;
+    protected String hostname;
+    @Argument( index = 1, name = "days", required = false, multiValued = false )
+    protected int days = 1;
+    @Argument(index = 2, name = "limit", required = false, multiValued = false)
+    protected int limit = 100;
 
-    private Monitoring monitoring;
+    private final Monitoring monitoring;
 
 
-    public void setMonitoring( Monitoring monitoring )
+    public AllMetricsCommand( final Monitoring monitoring )
     {
-        Preconditions.checkNotNull( monitoring, "Monitoring is null." );
+        Preconditions.checkNotNull( monitoring, "Monitoring is null" );
+
         this.monitoring = monitoring;
     }
 
@@ -34,11 +49,24 @@ public class AllMetricsCommand extends OsgiCommandSupport
     {
 
         Date endDate = new Date();
-        Date startDate = DateUtils.addDays( endDate, -1 );
+        Date startDate = DateUtils.addDays( endDate, -days );
 
-        //        Map<Metric, Map<Date, Double>> data = monitoring.getDataForAllMetrics( hostname, startDate, endDate );
-        //
-        //        System.out.println( "Data: " + data );
+        try
+        {
+            List<Metric> metrics = monitoring
+                    .getMetrics( Sets.newHashSet( hostname ), Sets.newHashSet( MetricType.values() ), startDate,
+                            endDate, limit );
+
+            for ( Metric metric : metrics )
+            {
+                System.out.println( metric );
+            }
+        }
+        catch ( MonitorException e )
+        {
+            LOG.error( "Error in doExecute", e );
+            System.out.println( e.getMessage() );
+        }
 
         return null;
     }
