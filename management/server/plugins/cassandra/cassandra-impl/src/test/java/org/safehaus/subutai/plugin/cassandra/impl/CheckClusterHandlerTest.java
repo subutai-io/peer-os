afb8a57fb416812172decaa1d1b72155e2601cbb
bahadyr <bahadyr@safehaus.org>
2014-10-25 16:21:25 +0600

    Clean Cassandra dependencies.

--- management/server/plugins/cassandra/cassandra-impl/src/test/java/org/safehaus/subutai/plugin/cassandra/impl/CheckClusterHandlerTest.java
+++ management/server/plugins/cassandra/cassandra-impl/src/test/java/org/safehaus/subutai/plugin/cassandra/impl/CheckClusterHandlerTest.java
@@ -8,7 +8,6 @@ import org.safehaus.subutai.common.tracker.OperationState;
 import org.safehaus.subutai.core.agent.api.AgentManager;
 import org.safehaus.subutai.core.command.api.CommandRunner;
 import org.safehaus.subutai.core.container.api.container.ContainerManager;
-import org.safehaus.subutai.core.db.api.DBException;
 import org.safehaus.subutai.plugin.cassandra.api.CassandraClusterConfig;
 import org.safehaus.subutai.plugin.cassandra.impl.handler.CheckClusterHandler;
 import org.safehaus.subutai.plugin.common.PluginDao;
@@ -28,7 +27,7 @@ public class CheckClusterHandlerTest
 
 
     @Before
-    public void setup() throws DBException
+    public void setup()
     {
         cassandraMock = mock( CassandraImpl.class );
         when( cassandraMock.getAgentManager() ).thenReturn( mock( AgentManager.class ) );
