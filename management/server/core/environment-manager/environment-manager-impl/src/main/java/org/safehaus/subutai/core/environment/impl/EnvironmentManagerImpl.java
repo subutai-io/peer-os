3a7a5735387a2c28db69f4f4b2f5daa1f457cc7f
bahadyr <bahadyr@safehaus.org>
2014-10-29 19:47:25 +0600

    Refactor network manager.

--- management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/core/environment/impl/EnvironmentManagerImpl.java
+++ management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/core/environment/impl/EnvironmentManagerImpl.java
@@ -393,7 +393,7 @@ public class EnvironmentManagerImpl implements EnvironmentManager
                 }
                 if ( blueprint.isLinkHosts() )
                 {
-                                        networkManager.configHostsOnAgents( environment.getContainers(), blueprint.getDomainName() );
+                    networkManager.configHostsOnAgents( environment.getContainers(), blueprint.getDomainName() );
                 }
                 if ( environment.getContainers().size() != containerCount )
                 {
