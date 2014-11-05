694c0c1d91e347f4fae4692a59210f8bb91650bc
dilshat <daliev@critical-factor.com>
2014-11-05 17:13:35 +0600

    New repository manager in progress

---
+++ management/server/core/repository-manager/repository-manager-impl/src/main/java/org/safehaus/subutai/core/repository/impl/RepositoryManagerImpl.java
@@ -0,0 +1,123 @@
+package org.safehaus.subutai.core.repository.impl;
+
+
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.safehaus.subutai.common.exception.CommandException;
+import org.safehaus.subutai.common.protocol.CommandResult;
+import org.safehaus.subutai.common.protocol.RequestBuilder;
+import org.safehaus.subutai.core.peer.api.ManagementHost;
+import org.safehaus.subutai.core.peer.api.PeerException;
+import org.safehaus.subutai.core.peer.api.PeerManager;
+import org.safehaus.subutai.core.repository.api.PackageInfo;
+import org.safehaus.subutai.core.repository.api.RepositoryException;
+import org.safehaus.subutai.core.repository.api.RepositoryManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
+
+
+/**
+ * Implementation of RepositoryManager
+ */
+public class RepositoryManagerImpl implements RepositoryManager
+{
+    private static final Logger LOG = LoggerFactory.getLogger( RepositoryManagerImpl.class.getName() );
+    private static final String LINE_SEPARATOR = "\n";
+
+    private final ManagementHost managementHost;
+    protected Commands commands = new Commands();
+
+
+    public RepositoryManagerImpl( final PeerManager peerManager ) throws RepositoryException
+    {
+        Preconditions.checkNotNull( peerManager, "Peer manager is null" );
+
+        try
+        {
+            managementHost = peerManager.getLocalPeer().getManagementHost();
+        }
+        catch ( PeerException e )
+        {
+            LOG.error( "Error in constructor", e );
+            throw new RepositoryException( e );
+        }
+    }
+
+
+    private CommandResult executeCommand( RequestBuilder requestBuilder ) throws RepositoryException
+    {
+        try
+        {
+            CommandResult result = managementHost.execute( requestBuilder );
+            if ( !result.hasSucceeded() )
+            {
+                if ( result.hasCompleted() )
+                {
+                    throw new RepositoryException( result.getStdErr() );
+                }
+                else
+                {
+                    throw new RepositoryException( "Command timed out" );
+                }
+            }
+
+            return result;
+        }
+        catch ( CommandException e )
+        {
+            throw new RepositoryException( e );
+        }
+    }
+
+
+    @Override
+    public void addPackageByPath( final String pathToPackage ) throws RepositoryException
+    {
+        executeCommand( commands.getAddPackageCommand( pathToPackage ) );
+    }
+
+
+    @Override
+    public void removePackageByName( final String packageName ) throws RepositoryException
+    {
+        executeCommand( commands.getRemovePackageCommand( packageName ) );
+    }
+
+
+    @Override
+    public void extractPackageByName( final String packageName ) throws RepositoryException
+    {
+        executeCommand( commands.getExtractPackageCommand( packageName ) );
+    }
+
+
+    @Override
+    public Set<PackageInfo> listPackages( final String term ) throws RepositoryException
+    {
+        CommandResult result = executeCommand( commands.getListPackagesCommand( term ) );
+
+        Set<PackageInfo> packages = Sets.newHashSet();
+
+        StringTokenizer lines = new StringTokenizer( result.getStdOut(), LINE_SEPARATOR );
+
+        while ( lines.hasMoreTokens() )
+        {
+            String line = lines.nextToken();
+
+            if ( line != null )
+            {
+                String[] packageFields = line.split( "\\s{2,}" );
+                if ( packageFields.length == 3 )
+                {
+                    packages.add( new PackageInfo( packageFields[0], packageFields[1], packageFields[2] ) );
+                }
+            }
+        }
+
+        return packages;
+    }
+}
