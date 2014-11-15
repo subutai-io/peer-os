4e808da252bbe5e0c461c49f74efc9c7dc45ee5d
Emin Inal <emininal@gmail.com>
2014-11-16 00:09:30 +0200

    tested agent communication

--- management/server/core/wol_manager/wol-manager-api/src/main/java/org/safehaus/subutai/wol/api/WolManager.java
+++ management/server/core/wol_manager/wol-manager-api/src/main/java/org/safehaus/subutai/wol/api/WolManager.java
@@ -1,6 +1,8 @@
 package org.safehaus.subutai.wol.api;
 
 
+import java.util.ArrayList;
+
 /**
  * Created by emin on 14/11/14.
  */
@@ -9,5 +11,9 @@ public interface WolManager {
     public String getWolName();
 
     public String helloWol(String name);
+
+    public String sendMagicPackagebyMacId(String macId);
+
+    public Boolean sendMagicPackagebyList(ArrayList<String> macList);
 
 }
--- management/server/core/wol_manager/wol-manager-impl/src/main/java/org/safehaus/subutai/wol/impl/WolImpl.java
+++ management/server/core/wol_manager/wol-manager-impl/src/main/java/org/safehaus/subutai/wol/impl/WolImpl.java
@@ -1,11 +1,49 @@
 package org.safehaus.subutai.wol.impl;
 
+import org.safehaus.subutai.common.command.CommandException;
+import org.safehaus.subutai.common.command.RequestBuilder;
+import org.safehaus.subutai.core.peer.api.*;
 import org.safehaus.subutai.wol.api.WolManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Created by emin on 14/11/14.
  */
-public class WolImpl implements WolManager {
+public abstract class WolImpl implements WolManager, PeerManager {
+    private LocalPeer localPeer;
+    private PeerInfo peerInfo;
+    private static final Logger LOG = LoggerFactory.getLogger(WolImpl.class.getName());
+
+    @Override
+    public PeerInfo getLocalPeerInfo(){
+        return peerInfo;
+    }
+
+
+    @Override
+    public LocalPeer getLocalPeer(){
+        return localPeer;
+    }
+
+
+    @Override
+    public String sendMagicPackagebyMacId(String macId) {
+        try {
+            return getLocalPeer().getManagementHost().execute(new RequestBuilder("ls /root")).getStdOut();
+        } catch (PeerException e) {
+            LOG.error(e.getMessage(), e);
+        } catch (CommandException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return "empty";
+    }
+
+    @Override
+    public Boolean sendMagicPackagebyList(ArrayList<String> macList){ return true; }
 
     @Override
     public String getWolName() {
