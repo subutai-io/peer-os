98dccc5caae45e4c6dece9588ddb20464a7ded1b
Emin Inal <emininal@gmail.com>
2014-11-16 22:26:47 +0200

    Testing Peer Manager Inerface

--- management/server/core/wol_manager/wol-manager-impl/src/main/java/org/safehaus/subutai/wol/impl/WolImpl.java
+++ management/server/core/wol_manager/wol-manager-impl/src/main/java/org/safehaus/subutai/wol/impl/WolImpl.java
@@ -2,6 +2,7 @@ package org.safehaus.subutai.wol.impl;
 
 import org.safehaus.subutai.common.command.CommandException;
 import org.safehaus.subutai.common.command.RequestBuilder;
+import org.safehaus.subutai.common.protocol.Agent;
 import org.safehaus.subutai.core.peer.api.*;
 import org.safehaus.subutai.wol.api.WolManager;
 import org.slf4j.Logger;
@@ -13,34 +14,36 @@ import java.util.UUID;
 /**
  * Created by emin on 14/11/14.
  */
-public abstract class WolImpl implements WolManager, PeerManager {
-    private LocalPeer localPeer;
-    private PeerInfo peerInfo;
+public class WolImpl implements WolManager {
     private static final Logger LOG = LoggerFactory.getLogger(WolImpl.class.getName());
+    private PeerManager peerManager;
+    private ManagementHost managementHost;
 
-    @Override
-    public PeerInfo getLocalPeerInfo(){
-        return peerInfo;
-    }
-
-
-    @Override
-    public LocalPeer getLocalPeer(){
-        return localPeer;
-    }
-
-
-    @Override
-    public String sendMagicPackagebyMacId(String macId) {
+    public WolImpl()
+    {
         try {
+            managementHost = peerManager.getLocalPeer().getManagementHost();
+        }
+        catch(PeerException e){
+            LOG.error(e.getMessage(), e);
+        }
+
+    }
+
+    public String sendMagicPackagebyMacId(String macId) {
+
+       return managementHost.getHostname().toString();
+       /* try {
             return getLocalPeer().getManagementHost().execute(new RequestBuilder("ls /root")).getStdOut();
         } catch (PeerException e) {
             LOG.error(e.getMessage(), e);
         } catch (CommandException e) {
             LOG.error(e.getMessage(), e);
         }
-        return "empty";
+        */
     }
+
+
 
     @Override
     public Boolean sendMagicPackagebyList(ArrayList<String> macList){ return true; }
