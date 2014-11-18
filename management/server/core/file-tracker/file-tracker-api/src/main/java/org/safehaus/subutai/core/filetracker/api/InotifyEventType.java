5b1c5db909aadfac2dcc4f333be8551153b30e9b
dilshat <daliev@critical-factor.com>
2014-11-18 16:21:11 +0600

    file tracker update

---
+++ management/server/core/file-tracker/file-tracker-api/src/main/java/org/safehaus/subutai/core/filetracker/api/InotifyEventType.java
@@ -0,0 +1,13 @@
+package org.safehaus.subutai.core.filetracker.api;
+
+
+/**
+ * I_Notify event types
+ */
+public enum InotifyEventType
+{
+    CREATE_FOLDER,
+    DELETE_FOLDER,
+    MODIFY_FILE,
+    MODIFY_PERMISSION_FOLDER
+}
