39940a3712886e6619526c3c3a375599dc68b3b0
Bahadyr Baimuratov <bbaimuratov@critical-factor.com>
2014-11-18 11:35:37 +0600

    HBase refactor.

--- management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/core/environment/impl/EnvironmentManagerImpl.java
+++ management/server/core/environment-manager/environment-manager-impl/src/main/java/org/safehaus/subutai/core/environment/impl/EnvironmentManagerImpl.java
@@ -8,6 +8,7 @@ package org.safehaus.subutai.core.environment.impl;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import javax.sql.DataSource;
 import org.safehaus.subutai.common.protocol.EnvironmentBlueprint;
 import org.safehaus.subutai.common.protocol.EnvironmentBuildTask;
 import org.safehaus.subutai.common.protocol.NodeGroup;
+import org.safehaus.subutai.common.protocol.PlacementStrategy;
 import org.safehaus.subutai.common.protocol.Template;
 import org.safehaus.subutai.core.environment.api.EnvironmentManager;
 import org.safehaus.subutai.core.environment.api.exception.EnvironmentBuildException;
@@ -511,6 +513,56 @@ public class EnvironmentManagerImpl implements EnvironmentManager
         catch ( EnvironmentManagerException e )
         {
             throw new EnvironmentBuildException( e.getMessage() );
+        }
+    }
+
+
+    @Override
+    public UUID addContainers( final UUID environmentId, final String template, PlacementStrategy strategy,
+                               String nodeGroupName, final Peer peer ) throws EnvironmentManagerException
+    {
+        EnvironmentBuildProcessFactory builder = new Node2PeerBuilder( this );
+        try
+        {
+
+            List<Template> templates = builder.fetchRequiredTemplates( peer.getId(), template );
+            Set<ContainerHost> hosts = peerManager.getPeer( peer.getId() )
+                                                  .createContainers( peerManager.getLocalPeer().getId(), environmentId,
+                                                          templates, 1, strategy.getStrategyId(),
+                                                          strategy.getCriteriaAsList(), nodeGroupName );
+            if ( hosts.isEmpty() )
+            {
+                throw new EnvironmentManagerException( "Containers not created" );
+            }
+            else
+            {
+                ContainerHost newHost = ( ContainerHost ) Arrays.asList( hosts ).get( 0 );
+                Environment environment = getEnvironmentByUUID( environmentId );
+                environment.addContainer( newHost );
+                saveEnvironment( environment );
+                return newHost.getId();
+            }
+        }
+        catch ( ProcessBuilderException | PeerException e )
+        {
+            LOG.error( e.getMessage(), e );
+            throw new EnvironmentManagerException( e.getMessage() );
+        }
+    }
+
+
+    @Override
+    public void removeContainer( final UUID environmentId, final UUID hostId ) throws EnvironmentManagerException
+    {
+        Environment environment = getEnvironmentByUUID( environmentId );
+        ContainerHost host = environment.getContainerHostByUUID( hostId );
+        try
+        {
+            host.dispose();
+        }
+        catch ( PeerException e )
+        {
+            throw new EnvironmentManagerException( e.getMessage() );
         }
     }
 
