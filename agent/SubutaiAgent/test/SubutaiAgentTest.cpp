ffa97e8dc5ebbc4a19347247977898212642ed1e
Ceren Sahin <ocsahin@safehaus.orgvim>
2014-11-19 16:12:47 +0200

    SubutaiAgentTest

--- agent/SubutaiAgent/test/SubutaiAgentTest.cpp
+++ agent/SubutaiAgent/test/SubutaiAgentTest.cpp
@@ -99,13 +99,12 @@ void AgentTest::tearDown(void)
 //SubutaiCommand-Deserialize
 void AgentTest::testCommandDeserialize(void)
 {
-
 	//Test string for deserialization
 	string input = "{\"request\":{\"type\":\"EXECUTE_REQUEST\","
 			"\"id\":\"5373b7c4-a039-44a9-9270-9e0e45d549cf\","
 			"\"commandId\":\"a7349720-9e2f-11e3-b9d6-080027b00009\","
 			"\"workingDirectory\":\"/home\","
-			"\"program\":\"ls\","
+			"\"command\":\"ls\","
 			"\"stdOut\":\"RETURN\","
 			"\"stdErr\":\"RETURN\","
 			"\"runAs\":\"root\","
@@ -113,6 +112,7 @@ void AgentTest::testCommandDeserialize(void)
 			"\"isDeamon\":0}}";
 
 	cmd->deserialize(input);
+
 
 	CPPUNIT_ASSERT("EXECUTE_REQUEST"== cmd->getType());
 	CPPUNIT_ASSERT("5373b7c4-a039-44a9-9270-9e0e45d549cf"== cmd->getUuid());
