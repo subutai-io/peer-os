fdf41681ba1dd53e44d35deadb4a9f5fc1178904
dilshat <daliev@critical-factor.com>
2014-11-20 16:39:57 +0600

    Broker unit tests

--- management/server/core/network-manager/network-manager-impl/src/test/java/org/safehaus/subutai/core/network/impl/NetworkManagerExceptionTest.java
+++ management/server/core/network-manager/network-manager-impl/src/test/java/org/safehaus/subutai/core/network/impl/NetworkManagerExceptionTest.java
@@ -13,17 +13,16 @@ public class NetworkManagerExceptionTest
 
 
     @Test
-    public void testCause() throws Exception
+    public void testException() throws Exception
     {
-        Exception cause = new Exception(  );
+        Exception cause = new Exception();
 
         NetworkManagerException networkManagerException = new NetworkManagerException( cause );
 
-        assertEquals(cause, networkManagerException.getCause());
+        assertEquals( cause, networkManagerException.getCause() );
 
         NetworkManagerException networkManagerException2 = new NetworkManagerException( MSG );
 
         assertEquals( MSG, networkManagerException2.getMessage() );
-
     }
 }
