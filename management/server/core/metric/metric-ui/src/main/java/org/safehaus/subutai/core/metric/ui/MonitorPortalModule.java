1b24b6480a9968d055d333cd9cac6d03e6e7637c
dilshat <daliev@critical-factor.com>
2014-11-20 19:29:21 +0600

    Monitor UI unit test

--- management/server/core/metric/metric-ui/src/main/java/org/safehaus/subutai/core/metric/ui/MonitorPortalModule.java
+++ management/server/core/metric/metric-ui/src/main/java/org/safehaus/subutai/core/metric/ui/MonitorPortalModule.java
@@ -2,16 +2,14 @@ package org.safehaus.subutai.core.metric.ui;
 
 
 import java.io.File;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.naming.NamingException;
 
 import org.safehaus.subutai.common.util.FileUtil;
 import org.safehaus.subutai.common.util.ServiceLocator;
 import org.safehaus.subutai.server.ui.api.PortalModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.vaadin.ui.Component;
 
@@ -21,26 +19,13 @@ public class MonitorPortalModule implements PortalModule
 
     public static final String MODULE_IMAGE = "monitor.png";
     public static final String MODULE_NAME = "Monitor";
-    protected static final Logger LOG = Logger.getLogger( MonitorPortalModule.class.getName() );
-    private final ServiceLocator serviceLocator;
-    private ExecutorService executor;
+    private static final Logger LOG = LoggerFactory.getLogger( MonitorPortalModule.class.getName() );
+    protected ServiceLocator serviceLocator;
 
 
     public MonitorPortalModule()
     {
         this.serviceLocator = new ServiceLocator();
-    }
-
-
-    public void init()
-    {
-        executor = Executors.newFixedThreadPool( 5 );
-    }
-
-
-    public void destroy()
-    {
-        executor.shutdown();
     }
 
 
@@ -70,11 +55,11 @@ public class MonitorPortalModule implements PortalModule
     {
         try
         {
-            return new MonitorForm( executor, serviceLocator );
+            return new MonitorForm( serviceLocator );
         }
         catch ( NamingException e )
         {
-            LOG.log( Level.SEVERE, e.getMessage() );
+            LOG.error( "Error in createComponent", e );
         }
 
         return null;
