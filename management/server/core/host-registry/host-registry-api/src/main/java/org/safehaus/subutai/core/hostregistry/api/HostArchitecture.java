cb7b7d38ecf871790a18970318f1fcf4ac8ab3cc
dilshat <daliev@critical-factor.com>
2014-11-24 16:53:19 +0600

    Added arch to hostinfo

--- management/server/core/host-registry/host-registry-api/src/main/java/org/safehaus/subutai/core/hostregistry/api/HostArchitecture.java
+++ management/server/core/host-registry/host-registry-api/src/main/java/org/safehaus/subutai/core/hostregistry/api/HostArchitecture.java
@@ -7,5 +7,5 @@ package org.safehaus.subutai.core.hostregistry.api;
 public enum HostArchitecture
 {
     I386, I686, X86_64, IA64, ALPHA, AMD64, ARM, ARMEB, ARMEL, HPPA, M32R, M68K, MIPS, MIPSEL, POWERPC, PPC64, S390,
-    S390X, SH3, SH3EB, SH4, SH4EB, SPARC
+    S390X, SH3, SH3EB, SH4, SH4EB, SPARC, UNKNOWN
 }
