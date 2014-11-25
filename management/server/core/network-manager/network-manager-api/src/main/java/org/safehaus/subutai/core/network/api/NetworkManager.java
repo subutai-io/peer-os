2904fdef6b374a3fe5b47b671cca5c296419d1fa
dilshat <daliev@critical-factor.com>
2014-11-25 16:14:30 +0600

    Core documentation in progress

--- management/server/core/network-manager/network-manager-api/src/main/java/org/safehaus/subutai/core/network/api/NetworkManager.java
+++ management/server/core/network-manager/network-manager-api/src/main/java/org/safehaus/subutai/core/network/api/NetworkManager.java
@@ -6,33 +6,72 @@ import java.util.Set;
 
 public interface NetworkManager
 {
+    /**
+     * Sets up an N2N connection to super node
+     */
     public void setupN2NConnection( String superNodeIp, int superNodePort, String interfaceName, String communityName,
                                     String localIp ) throws NetworkManagerException;
 
+    /**
+     * Removes N2N connection to super node
+     */
     public void removeN2NConnection( String interfaceName, String communityName ) throws NetworkManagerException;
 
+    /**
+     * Sets up tunnel to another peer
+     */
     public void setupTunnel( String tunnelName, String tunnelIp, String tunnelType ) throws NetworkManagerException;
 
+    /**
+     * Removes tunnel to another peer
+     */
     public void removeTunnel( String tunnelName ) throws NetworkManagerException;
 
+    /**
+     * Sets container environment IP and VLAN ID
+     */
     public void setContainerIp( String containerName, String ip, int netMask, int vLanId )
             throws NetworkManagerException;
 
+    /**
+     * Removes container environment IP and VLAN ID
+     */
     public void removeContainerIp( String containerName ) throws NetworkManagerException;
 
+    /**
+     * Returns container environment IP
+     */
     public ContainerInfo getContainerIp( String containerName ) throws NetworkManagerException;
 
+    /**
+     * Sets up gateway IP for specified VLAN on management host
+     */
     public void setupGateway( String gatewayIp, int vLanId ) throws NetworkManagerException;
 
+    /**
+     * Sets up gateway IP on a container
+     */
     public void setupGatewayOnContainer( String containerName, String gatewayIp, String interfaceName )
             throws NetworkManagerException;
 
+    /**
+     * Removes gateway IP for specified VLAN on management host
+     */
     public void removeGateway( int vLanId ) throws NetworkManagerException;
 
+    /**
+     * Removes gateway IP on a container
+     */
     public void removeGatewayOnContainer( String containerName ) throws NetworkManagerException;
 
+    /**
+     * Lists existing tunnels on management host
+     */
     public Set<Tunnel> listTunnels() throws NetworkManagerException;
 
+    /**
+     * Lists existing N2N connections on management host
+     */
     public Set<N2NConnection> listN2NConnections() throws NetworkManagerException;
 }
 
