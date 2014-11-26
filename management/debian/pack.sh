3e2b78e8643a2a2d566a02e931308541d1948330
Ceren Sahin <ocsahin@safehaus.orgvim>
2014-11-26 09:55:48 +0200

    update management script

--- management/debian/pack.sh
+++ management/debian/pack.sh
@@ -31,6 +31,9 @@ checkPackageVersion() {
   fi
   echo "version in control:$version_new current version:$version_prev "
 
+  echo "new in order" $versionFirst_new $versionSecond_new $versionThird_new $patch_new
+  echo "prev in order" $versionFirst_prev $versionSecond_prev $versionThird_prev $patch_prev 
+
   if [ $versionFirst_new -lt $versionFirst_prev ]; then
     echo "major: current version is greater than the version in control file. Version in control file:$version_new, current version:$version_prev"
     exit 0;
@@ -63,7 +66,7 @@ sh -c 'cd $BASE'
 cd ..
 MANAGEMENT_BASE=$(pwd)
 SOURCE=$MANAGEMENT_BASE"/workspace/management/server/server-karaf/target"
-BASE_SOURCE=$MANAGEMENT_BASE"/workspace/management"
+BASE_SOURCE="/var/lib/jenkins/jobs/master.get_branch_repo/workspace/management/debian/management/"
 echo "BASE:"$BASE
 echo "MANAGEMENT_BASE:"$MANAGEMENT_BASE
 echo "SOURCE:"$SOURCE
@@ -72,8 +75,9 @@ echo "BASE_SOURCE:"$BASE_SOURCE
 #removing debian file
 cd $BASE
 sh -c 'cd $BASE'
-if ls *.deb ; then
-        rm  *.deb
+
+if [ "`ls | grep deb`" != "" ]; then
+   rm  *.deb
 fi
 
 #getting fileName
@@ -85,78 +89,49 @@ rm -rf $BASE/$fileName/DEBIAN
 rm -rf $BASE/$fileName/opt
 rm -rf $BASE/$fileName/etc
 
-cp -a $BASE_SOURCE/debian/management/DEBIAN $BASE/$fileName/DEBIAN
-cp -a $BASE_SOURCE/debian/management/opt $BASE/$fileName/opt
-cp -a $BASE_SOURCE/debian/management/etc $BASE/$fileName/etc
+cp -a $BASE_SOURCE/DEBIAN $BASE/$fileName/DEBIAN
+cp -a $BASE_SOURCE/opt $BASE/$fileName/opt
+cp -a $BASE_SOURCE/etc $BASE/$fileName/etc
 
 #copying subutai.tar.gz file from maven output target folder
 echo "copying subutai-management snapshot files.."
 
-cd $SOURCE
+pushd $SOURCE
 #getting the name of subutai distro for instance: subutai-2.0.0.tar.gz
 distroName=`ls | grep tar.gz`
 #subutai distro's folder name for instance: subutai-2.0.0 
 distroFolderName=`ls | grep tar.gz | awk '{print substr($0, 0, length($0)-7)}'`
-cd $MANAGEMENT_BASE
+popd
 
+pushd $MANAGEMENT_BASE
 echo "distroName:"$distroName
 echo "distroFolderName:"$distroFolderName
-
 cp $SOURCE/$distroName $BASE/
-cd $BASE
+popd
+
+pushd $BASE
 tar xzvf $distroName
 cp -a $BASE/$distroFolderName/* $BASE/$fileName/opt/subutai-management/
-
 #removing subutai and subutai.tar.gz fodler and files
 rm $BASE/$distroName
 rm -rf $BASE/$distroFolderName
+popd
 
 #packaging subutai-management
-cd $BASE
+pushd $BASE
 lineNumberVersion=$(sed -n '/Version:/=' $fileName/DEBIAN/control)
-lineNumberPackage=$(sed -n '/Package:/=' $fileName/DEBIAN/control)
-echo $lineNumberVersion
-echo $lineNumberPackage
-
 lineVersion=$(sed $lineNumberVersion!d $fileName/DEBIAN/control)
-linePackage=$(sed $lineNumberPackage!d $fileName/DEBIAN/control)
-echo $lineVersion
-echo $linePackage
-
 version=$(echo $lineVersion | awk -F":" '{split($2,a," ");print a[1]}')
-package=$(echo $linePackage | awk -F":" '{split($2,a," ");print a[1]}')
-echo $version
-echo $package
-
-versionFirst=$(echo $version | awk -F"." '{print $1}')
-versionSecond=$(echo $version | awk -F"." '{print $2}')
-echo $versionFirst
-echo $versionSecond
-
-updatedVersion=$(echo `expr $versionSecond`)
-echo $updatedVersion
-updatedRelease=$versionFirst.$updatedVersion
-echo $updatedRelease
-
-replaceVersion="Version: $updatedRelease"
-echo $replaceVersion
-sed -i $fileName/DEBIAN/control -e $lineNumberVersion's!.*!'"$replaceVersion"'!'
-
-packageName=$package-$updatedRelease"_amd64"
-echo $packageName
-
-if [ $fileName = $packageName ]
-then
-   dpkg-deb -z8 -Zgzip --build $fileName/
-else
-   mv $fileName $packageName
-   dpkg-deb -z8 -Zgzip --build $packageName/
-fi
+packageName="subutai-management_"$version"_amd64"
+echo "packageName:$packageName"
+mv $fileName $packageName
+dpkg-deb -z8 -Zgzip --build $packageName/
 
 #copying subutai-management.deb file to our pool
-cp $packageName".deb" /var/lib/jenkins/Automation/Bigdata/management/
 cp $packageName".deb" /var/lib/jenkins/Automation/Bigdata/management/
 
 #editing package properties 
 chown jenkins:jenkins *.deb
 chmod -R 644 *.deb
+popd
+
