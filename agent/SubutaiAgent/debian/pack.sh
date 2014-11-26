c093b979359b056f0ee7f0ed78917deb7bc240ba
Ceren Sahin <ocsahin@safehaus.orgvim>
2014-11-26 10:29:42 +0200

    update on agent scripts

--- agent/SubutaiAgent/debian/pack.sh
+++ agent/SubutaiAgent/debian/pack.sh
@@ -3,16 +3,16 @@ set -e
 . /var/lib/jenkins/jobs/master.agent/Agent/deb/pack-funcs
 
 # 1) Check if the version is changed or not. If not changed, dont create a new debian.
-checkPackageVersion $1
+checkPackageVersion 
 # 2) Remove sources under agent related directories
-removePreviousVersionSource $1
+removePreviousVersionSource 
 # 3) Get new sources under agent related directories
-getNewVersionSource $1
+getNewVersionSource 
 # 4) compile new agent sources
-runMake $1
+runMake 
 # 5) copy needed files under subutai-agent folder like agent.xml
-getSourcesToRelevantDirectories $1
+getSourcesToRelevantDirectories 
 # 6) create debian package for agent
-createDebian $1
+createDebian 
 # 7) Generate repo package for debian.
 generateRepoPackage
