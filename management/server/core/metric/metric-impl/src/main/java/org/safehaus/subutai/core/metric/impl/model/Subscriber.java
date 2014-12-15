4f4457f60da26a1420b2b4138072f27cf212852c
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2014-12-15 13:39:21 +0600

    -m SUBUTAI-1863 integration test for MetricDao

---
+++ management/server/core/metric/metric-impl/src/main/java/org/safehaus/subutai/core/metric/impl/model/Subscriber.java
@@ -0,0 +1,48 @@
+package org.safehaus.subutai.core.metric.impl.model;
+
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+
+/**
+ * Created by talas on 12/12/14.
+ */
+@Entity
+@Table( name = "monitor_subscriber" )
+@Access( AccessType.FIELD )
+@IdClass( SubscriberPK.class )
+public class Subscriber
+{
+    @Id
+    @Column( name = "environment_id" )
+    private String environmentId;
+
+    @Id
+    @Column( name = "subscriber_id" )
+    private String subscriberId;
+
+
+    public Subscriber( final String environmentId, final String subscriberId )
+    {
+        this.environmentId = environmentId;
+        this.subscriberId = subscriberId;
+    }
+
+
+    public String getEnvironmentId()
+    {
+        return environmentId;
+    }
+
+
+    public String getSubscriberId()
+    {
+        return subscriberId;
+    }
+}
