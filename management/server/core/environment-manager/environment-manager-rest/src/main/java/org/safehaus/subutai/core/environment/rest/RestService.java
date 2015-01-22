c63878523151580a07e42315cc839635bc9172e1
dilshat <daliev@critical-factor.com>
2015-01-22 17:15:13 +0600

    SUBUTAI-2197

--- management/server/core/environment-manager/environment-manager-rest/src/main/java/org/safehaus/subutai/core/environment/rest/RestService.java
+++ management/server/core/environment-manager/environment-manager-rest/src/main/java/org/safehaus/subutai/core/environment/rest/RestService.java
@@ -1,20 +1,48 @@
 package org.safehaus.subutai.core.environment.rest;
 
 
-import javax.jws.WebService;
-import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 
-@WebService(serviceName = "peerServices")
 public interface RestService
 {
+
     @POST
-    @Path("/environment")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.TEXT_PLAIN)
-    public String buildNodeGroup( String peer );
+    @Path( "build" )
+    public Response buildLocalEnvironment( @QueryParam( "blueprint" ) String blueprint );
+
+    @GET
+    @Path( "getEnvironmentId" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getContainerEnvironmentId( @QueryParam( "containerId" ) String containerId );
+
+    @GET
+    @Path( "getEnvironment" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getEnvironment( @QueryParam( "environmentId" ) String environmentId );
+
+    @DELETE
+    @Path( "destroy" )
+    public Response destroyEnvironment( @QueryParam( "environmentId" ) String environmentId );
+
+    @DELETE
+    @Path( "container/destroy" )
+    public Response destroyContainer( @QueryParam( "containerId" ) String containerId );
+
+    @POST
+    @Path( "add" )
+    public Response addNodeGroup( @QueryParam( "environmentId" ) String environmentId,
+                                  @QueryParam( "nodeGroup" ) String nodeGroup );
+
+    @GET
+    @Path( "getContainerState" )
+    @Produces( { MediaType.APPLICATION_JSON } )
+    public Response getContainerState( @QueryParam( "containerId" ) String containerId );
 }
--- management/server/core/environment-manager/environment-manager-rest/src/main/java/org/safehaus/subutai/core/environment/rest/RestServiceImpl.java
+++ management/server/core/environment-manager/environment-manager-rest/src/main/java/org/safehaus/subutai/core/environment/rest/RestServiceImpl.java
@@ -1,35 +1,245 @@
 package org.safehaus.subutai.core.environment.rest;
 
 
+import java.util.UUID;
+
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import org.safehaus.subutai.common.peer.ContainerHost;
+import org.safehaus.subutai.common.peer.PeerException;
+import org.safehaus.subutai.common.protocol.EnvironmentBlueprint;
+import org.safehaus.subutai.common.protocol.NodeGroup;
+import org.safehaus.subutai.common.util.JsonUtil;
+import org.safehaus.subutai.common.util.UUIDUtil;
 import org.safehaus.subutai.core.environment.api.EnvironmentManager;
+import org.safehaus.subutai.core.environment.api.exception.EnvironmentBuildException;
+import org.safehaus.subutai.core.environment.api.exception.EnvironmentManagerException;
+import org.safehaus.subutai.core.environment.api.helper.Environment;
+import org.safehaus.subutai.core.peer.api.PeerManager;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonSyntaxException;
 
 
 public class RestServiceImpl implements RestService
 {
 
-    private EnvironmentManager environmentManager;
+    private static final String ERROR_KEY = "ERROR";
+    private final EnvironmentManager environmentManager;
+    private final PeerManager peerManager;
 
 
-    public RestServiceImpl()
+    public RestServiceImpl( final EnvironmentManager environmentManager, final PeerManager peerManager )
     {
-    }
+        Preconditions.checkNotNull( environmentManager );
+        Preconditions.checkNotNull( peerManager );
 
-
-    public EnvironmentManager getEnvironmentManager()
-    {
-        return environmentManager;
-    }
-
-
-    public void setEnvironmentManager( final EnvironmentManager environmentManager )
-    {
         this.environmentManager = environmentManager;
+        this.peerManager = peerManager;
     }
 
 
     @Override
-    public String buildNodeGroup( final String peer )
+    public Response buildLocalEnvironment( final String blueprint )
     {
+        EnvironmentBlueprint environmentBlueprint;
+        try
+        {
+            environmentBlueprint = JsonUtil.fromJson( blueprint, EnvironmentBlueprint.class );
+        }
+        catch ( JsonSyntaxException | NullPointerException e )
+        {
+            return Response.status( Response.Status.BAD_REQUEST ).entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+        try
+        {
+            environmentManager.buildEnvironment( environmentBlueprint );
+            return Response.ok().build();
+        }
+        catch ( EnvironmentBuildException e )
+        {
+            return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+    }
+
+
+    @Override
+    public Response getContainerEnvironmentId( final String containerId )
+    {
+        if ( !UUIDUtil.isStringAUuid( containerId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid container id" ) ).build();
+        }
+        UUID hostId = UUID.fromString( containerId );
+
+        Environment environment = findEnvironmentByContainerId( hostId );
+
+        if ( environment != null )
+        {
+            return Response.ok( JsonUtil.toJson( "ENV_ID", environment.getId() ) ).build();
+        }
+
+        return Response.status( Response.Status.NOT_FOUND ).build();
+    }
+
+
+    @Override
+    public Response getEnvironment( final String environmentId )
+    {
+        if ( !UUIDUtil.isStringAUuid( environmentId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid environment id" ) ).build();
+        }
+
+        UUID envId = UUID.fromString( environmentId );
+
+        try
+        {
+            Environment environment = environmentManager.findEnvironmentByID( envId );
+
+            return Response.ok( JsonUtil.toJson( environment ) ).build();
+        }
+        catch ( EnvironmentManagerException e )
+        {
+            return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+    }
+
+
+    @Override
+    public Response destroyEnvironment( final String environmentId )
+    {
+        if ( !UUIDUtil.isStringAUuid( environmentId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid environment id" ) ).build();
+        }
+
+        UUID envId = UUID.fromString( environmentId );
+
+        try
+        {
+            environmentManager.destroyEnvironment( envId );
+
+            return Response.ok().build();
+        }
+        catch ( EnvironmentManagerException e )
+        {
+            return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+    }
+
+
+    @Override
+    public Response destroyContainer( final String containerId )
+    {
+        if ( !UUIDUtil.isStringAUuid( containerId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid container id" ) ).build();
+        }
+        UUID hostId = UUID.fromString( containerId );
+
+        Environment environment = findEnvironmentByContainerId( hostId );
+
+        if ( environment != null )
+        {
+            try
+            {
+                environmentManager.destroyContainer( hostId );
+            }
+            catch ( EnvironmentManagerException e )
+            {
+                return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+            }
+        }
+
+        return Response.status( Response.Status.NOT_FOUND ).build();
+    }
+
+
+    private Environment findEnvironmentByContainerId( UUID containerId )
+    {
+        for ( Environment environment : environmentManager.getEnvironments() )
+        {
+            for ( ContainerHost containerHost : environment.getContainerHosts() )
+            {
+                if ( containerHost.getId().equals( containerId ) )
+                {
+                    return environment;
+                }
+            }
+        }
+
         return null;
+    }
+
+
+    @Override
+    public Response addNodeGroup( final String environmentId, final String nodeGroup )
+    {
+
+        if ( !UUIDUtil.isStringAUuid( environmentId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid environment id" ) ).build();
+        }
+
+        NodeGroup ng;
+        try
+        {
+            ng = JsonUtil.fromJson( nodeGroup, NodeGroup.class );
+        }
+        catch ( NullPointerException | JsonSyntaxException e )
+        {
+            return Response.status( Response.Status.BAD_REQUEST ).entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+
+        try
+        {
+            environmentManager
+                    .createAdditionalContainers( UUID.fromString( environmentId ), ng, peerManager.getLocalPeer() );
+            return Response.ok().build();
+        }
+        catch ( EnvironmentBuildException e )
+        {
+            return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+        }
+    }
+
+
+    @Override
+    public Response getContainerState( @QueryParam( "containerId" ) final String containerId )
+    {
+        if ( !UUIDUtil.isStringAUuid( containerId ) )
+        {
+            return Response.status( Response.Status.BAD_REQUEST )
+                           .entity( JsonUtil.toJson( ERROR_KEY, "Invalid container id" ) ).build();
+        }
+        UUID hostId = UUID.fromString( containerId );
+
+        Environment environment = findEnvironmentByContainerId( hostId );
+
+        if ( environment != null )
+        {
+            ContainerHost containerHost = environment.getContainerHostById( hostId );
+
+            if ( containerHost != null )
+            {
+                try
+                {
+                    return Response.ok().entity( JsonUtil.toJson( "STATE", containerHost.getState() ) ).build();
+                }
+                catch ( PeerException e )
+                {
+                    return Response.serverError().entity( JsonUtil.toJson( ERROR_KEY, e ) ).build();
+                }
+            }
+        }
+
+        return Response.status( Response.Status.NOT_FOUND ).build();
     }
 }
