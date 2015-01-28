de9bbf5a4ba87a8af5176000100d5dbc88f3a6ee
dilshat <daliev@critical-factor.com>
2015-01-28 19:41:49 +0600

    SUBUTAI-2225

--- management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/Commands.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/org/safehaus/subutai/core/network/impl/Commands.java
@@ -162,9 +162,7 @@ public class Commands
     {
         return new RequestBuilder( String.format( "mkdir -p /root/.ssh && " +
                 "chmod 700 /root/.ssh && " +
-                "(chmod 700 /root/.ssh/authorized_keys && "
-                + "grep -i -v \"%s\" /root/.ssh/authorized_keys > auth-keys-cleaned "
-                + " && mv auth-keys-cleaned /root/.ssh/authorized_keys ) ; " +
+                "sed -i \"\\,%s,d\" /root/.ssh/authorized_keys ; " +
                 "echo '%s' >> /root/.ssh/authorized_keys && " +
                 "chmod 644 /root/.ssh/authorized_keys", oldKey, newKey ) );
     }
@@ -212,9 +210,8 @@ public class Commands
 
     public RequestBuilder getRemoveSshKeyCommand( final String key )
     {
-        return new RequestBuilder( String.format( "chmod 700 /root/.ssh/authorized_keys && " +
-                "grep -i -v \"%s\" /root/.ssh/authorized_keys > auth-keys-cleaned "
-                + " && mv auth-keys-cleaned /root/.ssh/authorized_keys && " +
+        return new RequestBuilder( String.format( "chmod 700 /root/.ssh && " +
+                "sed -i \"\\,%s,d\" /root/.ssh/authorized_keys && " +
                 "chmod 644 /root/.ssh/authorized_keys", key ) );
     }
 }
