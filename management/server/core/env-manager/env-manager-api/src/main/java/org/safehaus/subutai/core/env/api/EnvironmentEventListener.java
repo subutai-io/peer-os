6a40c30b5bc33091cfa19c1a89ae162be2da9e3c
dilshat <daliev@critical-factor.com>
2015-01-29 13:31:26 +0600

    Added environment events

---
+++ management/server/core/env-manager/env-manager-api/src/main/java/org/safehaus/subutai/core/env/api/EnvironmentEventListener.java
@@ -0,0 +1,19 @@
+package org.safehaus.subutai.core.env.api;
+
+
+import java.util.Set;
+import java.util.UUID;
+
+import org.safehaus.subutai.common.peer.ContainerHost;
+
+
+public interface EnvironmentEventListener
+{
+    public void onEnvironmentCreated( Environment environment );
+
+    public void onEnvironmentGrown( Environment environment, Set<ContainerHost> newContainers );
+
+    public void onContainerDestroyed( Environment environment, UUID containerId );
+
+    public void onEnvironmentDestroyed( UUID environmentId );
+}
