fa9935556f519f0e6c365c21f59caea8cf86825f
dilshat <daliev@critical-factor.com>
2015-03-03 12:57:55 +0600

    Fix container tags

--- management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/util/SubutaiVaadinUtils.java
+++ management/server/vaadin/vaadin-ui/src/main/java/org/safehaus/subutai/server/ui/util/SubutaiVaadinUtils.java
@@ -1,9 +1,13 @@
 package org.safehaus.subutai.server.ui.util;
 
 
+import javax.servlet.http.Cookie;
+
 import org.safehaus.subutai.common.security.NullSubutaiLoginContext;
 import org.safehaus.subutai.common.security.SubutaiLoginContext;
+import org.safehaus.subutai.common.util.JsonUtil;
 
+import com.google.common.base.Strings;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinService;
 
@@ -19,12 +23,28 @@ public abstract class SubutaiVaadinUtils
         VaadinRequest request = VaadinService.getCurrentRequest();
         SubutaiLoginContext loginContext = NullSubutaiLoginContext.getInstance();
 
-        if ( request != null
-                && request.getWrappedSession().getAttribute( SubutaiLoginContext.SUBUTAI_LOGIN_CONTEXT_NAME ) != null )
+        if ( request != null )
         {
-            loginContext = ( SubutaiLoginContext ) request.getWrappedSession().getAttribute(
-                    SubutaiLoginContext.SUBUTAI_LOGIN_CONTEXT_NAME );
+            if ( request.getWrappedSession().getAttribute( SubutaiLoginContext.SUBUTAI_LOGIN_CONTEXT_NAME ) != null )
+            {
+                loginContext = ( SubutaiLoginContext ) request.getWrappedSession().getAttribute(
+                        SubutaiLoginContext.SUBUTAI_LOGIN_CONTEXT_NAME );
+            }
+            else
+            {
+                for ( Cookie cookie : request.getCookies() )
+                {
+                    if ( cookie.getName().equals( SubutaiLoginContext.SUBUTAI_LOGIN_CONTEXT_NAME ) && !Strings
+                            .isNullOrEmpty( cookie.getValue() ) )
+                    {
+                        loginContext = JsonUtil.fromJson( cookie.getValue(), SubutaiLoginContext.class );
+                        break;
+                    }
+                }
+            }
         }
+
+
         return loginContext;
     }
 }
