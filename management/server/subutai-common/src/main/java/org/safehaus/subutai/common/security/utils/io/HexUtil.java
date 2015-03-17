6d2f832656ac25c1d0ac8fb431ac195504deca49
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-03-17 14:59:58 +0600

    resolved sonar issue for HexUtil issue groups resolved in this commit
    *************
    
    Dodgy - int division result cast to double or float
    
    This code casts the result of an integer division operation to double or float. Doing division on integers truncates the result to the integer value closest to zero. The fact that the result was cast to double suggests that this precision should have been retained. What was probably meant was to cast one or both of the operands to double before performing the division.
    
    *************
    
    Performance - Method invokes inefficient Number constructor; use static valueOf instead
    
    Using new Integer(int) is guaranteed to always result in a new object whereas Integer.valueOf(int) allows caching of values to be done by the compiler, class library, or JVM. Using of cached values avoids object allocation and the code will be faster.
    
    Values between -128 and 127 are guaranteed to have corresponding cached instances and using valueOf is approximately 3.5 times faster than using constructor. For values outside the constant range the performance of both styles is the same.
    
    Unless the class must be compatible with JVMs predating Java 1.5, use either autoboxing or the valueOf() method when creating instances of Long, Integer, Short, Character, and Byte.
    
    *************
    
    Dodgy - Dead store to local variable
    
    This instruction assigns a value to a local variable, but the value is not read or used in any subsequent instruction. Often, this indicates an error, because the value computed is never used.
    
    Note that Sun's javac compiler often generates dead stores for final local variables. Because FindBugs is a bytecode-based tool, there is no easy way to eliminate these false positives.

--- management/server/subutai-common/src/main/java/org/safehaus/subutai/common/security/utils/io/HexUtil.java
+++ management/server/subutai-common/src/main/java/org/safehaus/subutai/common/security/utils/io/HexUtil.java
@@ -8,228 +8,226 @@ import java.math.BigInteger;
 
 /**
  * Class of utility methods to output data in hex.
- * 
  */
 public class HexUtil
 {
-	private static final String NEWLINE = "\n";
+    private static final String NEWLINE = "\n";
 
 
-	private HexUtil()
-	{
-	}
-	
-	
-	/**************************************************************************
-	 */
-	public static byte[] hexStringToByteArray(String s) 
-	{
-	    byte[] b = new byte[s.length() / 2];
-	    
-	    for (int i = 0; i < b.length; i++) 
-	    {
-	      int index = i * 2;
-	      int v = Integer.parseInt(s.substring(index, index + 2), 16);
-	      b[i] = (byte) v;
-	    }
-	    return b;
-	  }
-	
-	/**************************************************************************
-	 */
-	public static String byteArrayToHexString(byte[] b) 
-	{
-		StringBuffer sb = new StringBuffer(b.length * 2);
-		
-	    for (int i = 0; i < b.length; i++)
-	    {
-	    	int v = b[i] & 0xff;
-	    	if (v < 16) 
-	    	{
-	    		sb.append('0');
-	    	}
-	    	
-	        sb.append(Integer.toHexString(v));
-	     }
-	     return sb.toString().toUpperCase();
-	 }
-	
-
-	/****************************************************************************
-	 * Get hex string for the supplied big integer: "0x<hex string>" where hex
-	 * string is outputted in groups of exactly four characters sub-divided by
-	 * spaces.
-	 * 
-	 * @param bigInt
-	 *            Big integer
-	 * @return Hex string
-	 */
-	public static String getHexString( BigInteger bigInt )
-	{
-		// Convert number to hex string
-		String hex = bigInt.toString( 16 ).toUpperCase();
-
-		// Get number padding bytes
-		int padding = ( 4 - ( hex.length() % 4 ) );
-
-		// Insert any required padding to get groups of exactly 4 characters
-		if ( ( padding > 0 ) && ( padding < 4 ) )
-		{
-			StringBuffer sb = new StringBuffer( hex );
-
-			for ( int i = 0; i < padding; i++ )
-			{
-				sb.insert( 0, '0' );
-			}
-
-			hex = sb.toString();
-		}
-
-		// Output with leading "0x" and spaces to form groups
-		StringBuffer strBuff = new StringBuffer();
-
-		strBuff.append( "0x" );
-
-		for ( int i = 0; i < hex.length(); i++ )
-		{
-			strBuff.append( hex.charAt( i ) );
-
-			if ( ( ( ( i + 1 ) % 4 ) == 0 ) && ( ( i + 1 ) != hex.length() ) )
-			{
-				strBuff.append( ' ' );
-			}
-		}
-
-		return strBuff.toString();
-	}
+    private HexUtil()
+    {
+    }
 
 
-	/**
-	 * Get hex string for the supplied byte array: "0x<hex string>" where hex
-	 * string is outputted in groups of exactly four characters sub-divided by
-	 * spaces.
-	 * 
-	 * @param bytes
-	 *            Byte array
-	 * @return Hex string
-	 */
-	public static String getHexString( byte[] bytes )
-	{
-		return getHexString( new BigInteger( 1, bytes ) );
-	}
+    /**
+     * ***********************************************************************
+     */
+    public static byte[] hexStringToByteArray( String s )
+    {
+        byte[] b = new byte[s.length() / 2];
+
+        for ( int i = 0; i < b.length; i++ )
+        {
+            int index = i * 2;
+            int v = Integer.parseInt( s.substring( index, index + 2 ), 16 );
+            b[i] = ( byte ) v;
+        }
+        return b;
+    }
 
 
-	/**
-	 * Get hex and clear text dump of byte array.
-	 * 
-	 * @param bytes
-	 *            Array of bytes
-	 * @return Hex/clear dump
-	 * @throws IOException
-	 *             If an I/O problem occurs
-	 */
-	public static String getHexClearDump( byte[] bytes ) throws IOException
-	{
-		ByteArrayInputStream bais = null;
+    /**
+     * ***********************************************************************
+     */
+    public static String byteArrayToHexString( byte[] b )
+    {
+        StringBuilder sb = new StringBuilder( b.length * 2 );
 
-		try
-		{
-			// Divide dump into 8 byte lines
-			StringBuffer strBuff = new StringBuffer();
+        for ( final byte aB : b )
+        {
+            int v = aB & 0xff;
+            if ( v < 16 )
+            {
+                sb.append( '0' );
+            }
 
-			bais = new ByteArrayInputStream( bytes );
-			byte[] line = new byte[8];
-			int read = -1;
-			boolean firstLine = true;
-
-			while ( ( read = bais.read( line ) ) != -1 )
-			{
-				if ( firstLine )
-				{
-					firstLine = false;
-				}
-				else
-				{
-					strBuff.append( NEWLINE );
-				}
-
-				strBuff.append( getHexClearLineDump( line, read ) );
-			}
-
-			return strBuff.toString();
-		}
-		finally
-		{
-			SafeCloseUtil.close( bais );
-		}
-	}
+            sb.append( Integer.toHexString( v ) );
+        }
+        return sb.toString().toUpperCase();
+    }
 
 
-	private static String getHexClearLineDump( byte[] bytes, int len )
-	{
-		StringBuffer sbHex = new StringBuffer();
-		StringBuffer sbClr = new StringBuffer();
+    /**
+     * ************************************************************************* Get hex string for the supplied big
+     * integer: "0x<hex string>" where hex string is outputted in groups of exactly four characters sub-divided by
+     * spaces.
+     *
+     * @param bigInt Big integer
+     *
+     * @return Hex string
+     */
+    public static String getHexString( BigInteger bigInt )
+    {
+        // Convert number to hex string
+        String hex = bigInt.toString( 16 ).toUpperCase();
 
-		for ( int cnt = 0; cnt < len; cnt++ )
-		{
-			// Convert byte to int
-			byte b = bytes[cnt];
-			int i = b & 0xFF;
+        // Get number padding bytes
+        int padding = ( 4 - ( hex.length() % 4 ) );
 
-			// First part of byte will be one hex char
-			int i1 = (int) Math.floor( i / 16 );
+        // Insert any required padding to get groups of exactly 4 characters
+        if ( ( padding > 0 ) && ( padding < 4 ) )
+        {
+            StringBuilder sb = new StringBuilder( hex );
 
-			// Second part of byte will be one hex char
-			int i2 = i % 16;
+            for ( int i = 0; i < padding; i++ )
+            {
+                sb.insert( 0, '0' );
+            }
 
-			// Get hex characters
-			sbHex.append( Character.toUpperCase( Character.forDigit( i1, 16 ) ) );
-			sbHex.append( Character.toUpperCase( Character.forDigit( i2, 16 ) ) );
+            hex = sb.toString();
+        }
 
-			if ( ( cnt + 1 ) < len )
-			{
-				// Divider between hex characters
-				sbHex.append( ' ' );
-			}
+        // Output with leading "0x" and spaces to form groups
+        StringBuilder strBuff = new StringBuilder();
 
-			// Get clear character
+        strBuff.append( "0x" );
 
-			// Character to display if character not defined in Unicode or is a
-			// control charcter
-			char c = '.';
+        for ( int i = 0; i < hex.length(); i++ )
+        {
+            strBuff.append( hex.charAt( i ) );
 
-			// Not a control character and defined in Unicode
-			if ( ( !Character.isISOControl( (char) i ) )
-			        && ( Character.isDefined( (char) i ) ) )
-			{
-				Character clr = new Character( (char) i );
-				c = clr.charValue();
-			}
+            if ( ( ( ( i + 1 ) % 4 ) == 0 ) && ( ( i + 1 ) != hex.length() ) )
+            {
+                strBuff.append( ' ' );
+            }
+        }
 
-			sbClr.append( c );
-		}
+        return strBuff.toString();
+    }
+
+
+    /**
+     * Get hex string for the supplied byte array: "0x<hex string>" where hex string is outputted in groups of exactly
+     * four characters sub-divided by spaces.
+     *
+     * @param bytes Byte array
+     *
+     * @return Hex string
+     */
+    public static String getHexString( byte[] bytes )
+    {
+        return getHexString( new BigInteger( 1, bytes ) );
+    }
+
+
+    /**
+     * Get hex and clear text dump of byte array.
+     *
+     * @param bytes Array of bytes
+     *
+     * @return Hex/clear dump
+     *
+     * @throws IOException If an I/O problem occurs
+     */
+    public static String getHexClearDump( byte[] bytes ) throws IOException
+    {
+        ByteArrayInputStream bais = null;
+
+        try
+        {
+            // Divide dump into 8 byte lines
+            StringBuilder strBuff = new StringBuilder();
+
+            bais = new ByteArrayInputStream( bytes );
+            byte[] line = new byte[8];
+            int read = -1;
+            boolean firstLine = true;
+
+            while ( ( read = bais.read( line ) ) != -1 )
+            {
+                if ( firstLine )
+                {
+                    firstLine = false;
+                }
+                else
+                {
+                    strBuff.append( NEWLINE );
+                }
+
+                strBuff.append( getHexClearLineDump( line, read ) );
+            }
+
+            return strBuff.toString();
+        }
+        finally
+        {
+            SafeCloseUtil.close( bais );
+        }
+    }
+
+
+    private static String getHexClearLineDump( byte[] bytes, int len )
+    {
+        StringBuilder sbHex = new StringBuilder();
+        StringBuilder sbClr = new StringBuilder();
+
+        for ( int cnt = 0; cnt < len; cnt++ )
+        {
+            // Convert byte to int
+            byte b = bytes[cnt];
+            int i = b & 0xFF;
+
+            // First part of byte will be one hex char
+            int i1 = ( int ) Math.floor( i / ( double ) 16 );
+
+            // Second part of byte will be one hex char
+            int i2 = i % 16;
+
+            // Get hex characters
+            sbHex.append( Character.toUpperCase( Character.forDigit( i1, 16 ) ) );
+            sbHex.append( Character.toUpperCase( Character.forDigit( i2, 16 ) ) );
+
+            if ( ( cnt + 1 ) < len )
+            {
+                // Divider between hex characters
+                sbHex.append( ' ' );
+            }
+
+            // Get clear character
+
+            // Character to display if character not defined in Unicode or is a
+            // control charcter
+            char c = '.';
+
+            // Not a control character and defined in Unicode
+            if ( ( !Character.isISOControl( ( char ) i ) ) && ( Character.isDefined( ( char ) i ) ) )
+            {
+                Character clr = Character.valueOf( ( char ) i );
+                c = clr.charValue();
+            }
+
+            sbClr.append( c );
+        }
 
 		/*
-		 * Put both dumps together in one string (hex, clear) with appropriate
+         * Put both dumps together in one string (hex, clear) with appropriate
 		 * padding between them (pad to array length)
 		 */
-		StringBuffer strBuff = new StringBuffer();
+        StringBuilder strBuff = new StringBuilder();
 
-		strBuff.append( sbHex.toString() );
-		sbHex = new StringBuffer();
+        strBuff.append( sbHex.toString() );
 
-		int i = bytes.length - len;
-		for ( int cnt = 0; cnt < i; cnt++ )
-		{
-			strBuff.append( "   " ); // Each missing byte takes up three spaces
-		}
+        int i = bytes.length - len;
+        for ( int cnt = 0; cnt < i; cnt++ )
+        {
+            strBuff.append( "   " ); // Each missing byte takes up three spaces
+        }
 
-		strBuff.append( "   " ); // The gap between hex and clear output is
-								 // three
-		                         // spaces
-		strBuff.append( sbClr.toString() );
-		sbClr = new StringBuffer();
+        strBuff.append( "   " ); // The gap between hex and clear output is
+        // three
+        // spaces
+        strBuff.append( sbClr.toString() );
 
-		return strBuff.toString();
-	}
+        return strBuff.toString();
+    }
 }
