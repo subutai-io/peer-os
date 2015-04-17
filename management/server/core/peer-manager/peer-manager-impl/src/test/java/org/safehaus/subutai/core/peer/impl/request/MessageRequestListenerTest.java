fc7e39b7bdae2e102b942ea8abf265415a90debe
dilshat <daliev@critical-factor.com>
2015-04-17 18:53:48 +0600

    covered request package with unit tests

---
+++ management/server/core/peer-manager/peer-manager-impl/src/test/java/org/safehaus/subutai/core/peer/impl/request/MessageRequestListenerTest.java
@@ -0,0 +1,71 @@
+package org.safehaus.subutai.core.peer.impl.request;
+
+
+import java.util.concurrent.ExecutorService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.safehaus.subutai.core.messenger.api.Message;
+import org.safehaus.subutai.core.messenger.api.Messenger;
+import org.safehaus.subutai.core.peer.api.PeerManager;
+import org.safehaus.subutai.core.peer.api.RequestListener;
+
+import com.google.common.collect.Sets;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@RunWith( MockitoJUnitRunner.class )
+public class MessageRequestListenerTest
+{
+    private static final String RECIPIENT = "recipient";
+    @Mock
+    PeerManager peerManager;
+    @Mock
+    Messenger messenger;
+    @Mock
+    RequestListener requestListener;
+    @Mock
+    ExecutorService notifier;
+    @Mock
+    Message message;
+    @Mock
+    MessageRequest messageRequest;
+
+    MessageRequestListener listener;
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        listener = new MessageRequestListener( peerManager, messenger, Sets.newHashSet( requestListener ) );
+        listener.notifier = notifier;
+        when( requestListener.getRecipient() ).thenReturn( RECIPIENT );
+        when( messageRequest.getRecipient() ).thenReturn( RECIPIENT );
+        when( message.getPayload( MessageRequest.class ) ).thenReturn( messageRequest );
+    }
+
+
+    @Test
+    public void testDispose() throws Exception
+    {
+        listener.dispose();
+
+        verify( notifier ).shutdown();
+    }
+
+
+    @Test
+    public void testOnMessage() throws Exception
+    {
+
+        listener.onMessage( message );
+
+        verify( notifier ).execute( isA( RequestNotifier.class ) );
+    }
+}
