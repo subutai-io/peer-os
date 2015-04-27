d7a6f2e21fe318a05156a628fdd703fb59651154
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-04-27 16:24:15 +0600

    sonar fix Correctness - instanceof will always return false
    This instanceof test will always return false. Although this is safe, make sure it isn't an indication of some misunderstanding or some other logic error.

--- management/server/core/identity-manager/identity-manager-impl/src/main/java/org/safehaus/subutai/core/identity/impl/ShiroLoginModule.java
+++ management/server/core/identity-manager/identity-manager-impl/src/main/java/org/safehaus/subutai/core/identity/impl/ShiroLoginModule.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 
 import org.apache.karaf.jaas.boot.principal.RolePrincipal;
 import org.apache.karaf.jaas.boot.principal.UserPrincipal;
-import org.apache.karaf.jaas.config.JaasRealm;
 import org.apache.karaf.jaas.modules.AbstractKarafLoginModule;
 
 
@@ -39,17 +38,10 @@ public class ShiroLoginModule extends AbstractKarafLoginModule
     public void initialize( Subject subject, CallbackHandler callbackHandler, Map<String, ?> sharedState,
                             Map<String, ?> options )
     {
+        // just for importing JaasRealm
         super.initialize( subject, callbackHandler, options );
 
         LOGGER.info( "Initializing shiro login module." );
-
-
-        // just for importing JaasRealm
-        Object o = subject;
-        if ( o instanceof JaasRealm )
-        {
-            // empty
-        }
     }
 
 
