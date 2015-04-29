b7069f1ef15caf94de98fe1fe270717750d4a75d
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-04-29 13:57:25 +0600

    sonar issue fix for ChannelTokenManagerImpl

--- management/server/core/channel-manager/channel-manager-impl/src/main/java/org/safehaus/subutai/core/channel/impl/token/ChannelTokenManagerImpl.java
+++ management/server/core/channel-manager/channel-manager-impl/src/main/java/org/safehaus/subutai/core/channel/impl/token/ChannelTokenManagerImpl.java
@@ -3,14 +3,13 @@ package org.safehaus.subutai.core.channel.impl.token;
 
 import java.util.List;
 
-import org.safehaus.subutai.core.channel.api.entity.IUserChannelToken;
-import org.safehaus.subutai.core.channel.api.token.ChannelTokenManager;
-import org.safehaus.subutai.core.channel.impl.entity.UserChannelToken;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
+import org.safehaus.subutai.core.channel.api.entity.IUserChannelToken;
+import org.safehaus.subutai.core.channel.api.token.ChannelTokenManager;
+import org.safehaus.subutai.core.channel.impl.entity.UserChannelToken;
 
 
 /**
@@ -19,103 +18,112 @@ import javax.persistence.Query;
 
 public class ChannelTokenManagerImpl implements ChannelTokenManager
 {
-    private EntityManagerFactory EntityManagerFactory = null;
-
+    private EntityManagerFactory entityManagerFactory = null;
 
 
     public EntityManagerFactory getEntityManagerFactory()
     {
-        return EntityManagerFactory;
+        return entityManagerFactory;
     }
+
 
     public void setEntityManagerFactory( EntityManagerFactory entityManagerFactory )
     {
-        EntityManagerFactory = entityManagerFactory;
+        this.entityManagerFactory = entityManagerFactory;
     }
 
 
-    /***********************************************************************************************************
-     *
-     * */
-    public long getUserChannelTokenId(String token)
+    /**
+     * ********************************************************************************************************
+     */
+    public long getUserChannelTokenId( String token )
     {
-        long user_id = 0;
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        long userId = 0;
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
             Query query;
-            query = entityManager.createQuery( "select ucht FROM UserChannelToken AS ucht WHERE ucht.token=:tokenParam and ucht.validPeriod>0" );
+            query = entityManager.createQuery(
+                    "select ucht FROM UserChannelToken AS ucht WHERE ucht.token=:tokenParam and ucht.validPeriod>0" );
             query.setParameter( "tokenParam", token );
-            UserChannelToken userChannelToken = (UserChannelToken)query.getSingleResult();
+            UserChannelToken userChannelToken = ( UserChannelToken ) query.getSingleResult();
 
-            if(userChannelToken!=null)
+            if ( userChannelToken != null )
             {
-                user_id = userChannelToken.getUserId();
+                userId = userChannelToken.getUserId();
             }
             else
             {
-                user_id = 0;
+                userId = 0;
             }
         }
-        catch ( Exception e )
+        catch ( Exception ignore )
         {
+            //ignore
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
 
-        return  user_id;
-
+        return userId;
     }
-    /***********************************************************************************************************
-     *
-     * */
-    public IUserChannelToken getUserChannelToken(String token)
+
+
+    /**
+     * ********************************************************************************************************
+     */
+    public IUserChannelToken getUserChannelToken( String token )
     {
         IUserChannelToken userChannelToken = null;
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
             Query query;
-            query = entityManager.createQuery( "select ucht FROM UserChannelToken AS ucht WHERE ucht.token=:tokenParam and ucht.validPeriod>0" );
+            query = entityManager.createQuery(
+                    "select ucht FROM UserChannelToken AS ucht WHERE ucht.token=:tokenParam and ucht.validPeriod>0" );
             query.setParameter( "tokenParam", token );
-            userChannelToken = (UserChannelToken)query.getSingleResult();
+            userChannelToken = ( UserChannelToken ) query.getSingleResult();
         }
-        catch ( Exception e )
+        catch ( Exception ignore )
         {
+            //ignore
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
 
-        return  userChannelToken;
-
+        return userChannelToken;
     }
 
-    /***********************************************************************************************************
-     *
-     * */
+
+    /**
+     * ********************************************************************************************************
+     */
     public void setTokenValidity()
     {
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
             entityManager.getTransaction().begin();
 
-            //-------- Update Validity Period ------------------------------------------------------------------------------
+            //-------- Update Validity Period
+            // ------------------------------------------------------------------------------
             Query query;
 
-            query = entityManager.createNativeQuery(" update user_channel_token set valid_period  = "
-                    + " case when (valid_period-1)<0 then  0"
-                    + " else valid_period-1"
-                    + " end" );
+            query = entityManager.createNativeQuery(
+                    " UPDATE user_channel_token SET valid_period  = " + " CASE WHEN (valid_period-1)<0 THEN  0"
+                            + " ELSE valid_period-1" + " END" );
 
             query.executeUpdate();
             //--------------------------------------------------------------------------------------
@@ -124,45 +132,57 @@ public class ChannelTokenManagerImpl implements ChannelTokenManager
         }
         catch ( Exception e )
         {
-            if(entityManager.getTransaction().isActive())
+            if ( entityManager.getTransaction().isActive() )
+            {
                 entityManager.getTransaction().rollback();
+            }
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
     }
-    /***********************************************************************************************************
-     *
-     * */
-    public void saveUserChannelToken(IUserChannelToken userChannelToken)
+
+
+    /**
+     * ********************************************************************************************************
+     */
+    public void saveUserChannelToken( IUserChannelToken userChannelToken )
     {
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
             entityManager.getTransaction().begin();
-            entityManager.merge(userChannelToken);
+            entityManager.merge( userChannelToken );
             entityManager.getTransaction().commit();
         }
         catch ( Exception e )
         {
-            if(entityManager.getTransaction().isActive())
+            if ( entityManager.getTransaction().isActive() )
+            {
                 entityManager.getTransaction().rollback();
+            }
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
     }
-    /***********************************************************************************************************
-     *
-     * */
-    public void removeUserChannelToken(String token)
+
+
+    /**
+     * ********************************************************************************************************
+     */
+    public void removeUserChannelToken( String token )
     {
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
@@ -177,50 +197,60 @@ public class ChannelTokenManagerImpl implements ChannelTokenManager
         }
         catch ( Exception e )
         {
-            if(entityManager.getTransaction().isActive())
+            if ( entityManager.getTransaction().isActive() )
+            {
                 entityManager.getTransaction().rollback();
+            }
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
     }
-    /***********************************************************************************************************
-     *
-     * */
-    public List<IUserChannelToken> getUserChannelTokenData(long userId)
+
+
+    /**
+     * ********************************************************************************************************
+     */
+    public List<IUserChannelToken> getUserChannelTokenData( long userId )
     {
         List userChannelTokenList = null;
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
             Query query;
-            query = entityManager.createQuery( "select ucht FROM UserChannelToken AS ucht WHERE ucht.userId = :user_id" );
+            query = entityManager
+                    .createQuery( "select ucht FROM UserChannelToken AS ucht WHERE ucht.userId = :user_id" );
             query.setParameter( "user_id", userId );
             userChannelTokenList = query.getResultList();
         }
         catch ( Exception e )
         {
-            System.out.println(e.toString());
+            System.out.println( e.toString() );
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
 
-        return  userChannelTokenList;
-
+        return userChannelTokenList;
     }
-    /***********************************************************************************************************
-     *
-     * */
+
+
+    /**
+     * ********************************************************************************************************
+     */
     public List<IUserChannelToken> getAllUserChannelTokenData()
     {
         List userChannelTokenList = null;
-        EntityManager entityManager = EntityManagerFactory.createEntityManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         try
         {
@@ -230,24 +260,25 @@ public class ChannelTokenManagerImpl implements ChannelTokenManager
         }
         catch ( Exception e )
         {
-            System.out.println(e.toString());
+            System.out.println( e.toString() );
         }
         finally
         {
-            if(entityManager.isOpen())
+            if ( entityManager.isOpen() )
+            {
                 entityManager.close();
+            }
         }
 
-        return  userChannelTokenList;
-
+        return userChannelTokenList;
     }
 
-    /***********************************************************************************************************
-     *
-     * */
+
+    /**
+     * ********************************************************************************************************
+     */
     public IUserChannelToken createUserChannelToken()
     {
         return new UserChannelToken();
     }
-
 }
