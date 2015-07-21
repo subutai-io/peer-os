19cb2a09a53b403aae857e40ece0ffc4003e44d2
dilshat <daliev@critical-factor.com>
2015-07-21 11:15:00 +0600

    externalized app data path to environment variable

--- management/server/subutai-common/src/main/java/io/subutai/common/settings/SecuritySettings.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/settings/SecuritySettings.java
@@ -8,21 +8,21 @@ public class SecuritySettings
 {
     //************* Certificates / KeyStore Settings *********************************************************
     public static final String KARAF_HOME = System.getProperty( "karaf.base" );
-    //    public static final String CERT_EXPORT_DIR = KARAF_HOME + "/etc/export/";
-    //    public static final String CERT_IMPORT_DIR = KARAF_HOME + "/etc/import/";
-    //    public static final String TRUSTSTORE_PX2_FILE = KARAF_HOME + "/etc/keystores/truststore_server_px2.jks";
-    //    public static final String TRUSTSTORE_PX1_FILE = KARAF_HOME + "/etc/keystores/truststore_server_px1.jks";
-    //    public static final String KEYSTORE_PX2_FILE = KARAF_HOME + "/etc/keystores/keystore_server_px2.jks";
-    //    public static final String KEYSTORE_PX1_FILE = KARAF_HOME + "/etc/keystores/keystore_server_px1.jks";
 
-    public static final String CERT_EXPORT_DIR = "/apps/subutai-mng/current/keystores/export/";
-    public static final String CERT_IMPORT_DIR = "/apps/subutai-mng/current/keystores/import/";
-    public static final String TRUSTSTORE_PX2_FILE = "/apps/subutai-mng/current/keystores/truststore_server_px2.jks";
-    public static final String TRUSTSTORE_PX1_FILE = "/apps/subutai-mng/current/keystores/truststore_server_px1.jks";
-    public static final String TRUSTSTORE_SPECIAL_PX1_FILE = "/apps/subutai-mng/current/keystores/truststore_special_px1.jks";
-    public static final String KEYSTORE_PX2_FILE = "/apps/subutai-mng/current/keystores/keystore_server_px2.jks";
-    public static final String KEYSTORE_PX1_FILE = "/apps/subutai-mng/current/keystores/keystore_server_px1.jks";
-    public static final String KEYSTORE_SPECIAL_PX1_FILE = "/apps/subutai-mng/current/keystores/keystore_special_px1.jks";
+    public static final String CERT_EXPORT_DIR = String.format( "%s/keystores/export/", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String CERT_IMPORT_DIR = String.format( "%s/keystores/import/", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String TRUSTSTORE_PX2_FILE =
+            String.format( "%s/keystores/truststore_server_px2.jks", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String TRUSTSTORE_PX1_FILE =
+            String.format( "%s/keystores/truststore_server_px1.jks", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String TRUSTSTORE_SPECIAL_PX1_FILE =
+            String.format( "%s/keystores/truststore_special_px1.jks", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String KEYSTORE_PX2_FILE =
+            String.format( "%s/keystores/keystore_server_px2.jks", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String KEYSTORE_PX1_FILE =
+            String.format( "%s/keystores/keystore_server_px1.jks", Common.SUBUTAI_APP_DATA_PATH );
+    public static final String KEYSTORE_SPECIAL_PX1_FILE =
+            String.format( "%s/keystores/keystore_special_px1.jks", Common.SUBUTAI_APP_DATA_PATH );
 
     public static final String KEYSTORE_PX1_PSW = "subutai";
     public static final String KEYSTORE_PX2_PSW = "subutai";
