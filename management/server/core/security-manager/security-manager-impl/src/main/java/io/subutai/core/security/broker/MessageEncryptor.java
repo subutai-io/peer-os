c6a87b3f68de04b982d3b535f011ade2886f9df6
dilshat <daliev@critical-factor.com>
2015-09-21 19:43:01 +0600

    fix pgp key obtaining SS-3083

--- management/server/core/security-manager/security-manager-impl/src/main/java/io/subutai/core/security/broker/MessageEncryptor.java
+++ management/server/core/security-manager/security-manager-impl/src/main/java/io/subutai/core/security/broker/MessageEncryptor.java
@@ -2,6 +2,7 @@ package io.subutai.core.security.broker;
 
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.naming.NamingException;
 
@@ -18,6 +19,7 @@ import io.subutai.common.util.JsonUtil;
 import io.subutai.common.util.ServiceLocator;
 import io.subutai.common.util.UUIDUtil;
 import io.subutai.core.broker.api.TextMessagePostProcessor;
+import io.subutai.core.peer.api.PeerManager;
 import io.subutai.core.registration.api.RegistrationManager;
 import io.subutai.core.security.api.SecurityManager;
 import io.subutai.core.security.api.crypto.EncryptionTool;
@@ -53,6 +55,13 @@ public class MessageEncryptor implements TextMessagePostProcessor
     }
 
 
+    public static PeerManager getPeerManager() throws NamingException
+    {
+
+        return ServiceLocator.getServiceNoCache( PeerManager.class );
+    }
+
+
     @Override
     public String process( final String topic, final String message )
     {
@@ -70,9 +79,15 @@ public class MessageEncryptor implements TextMessagePostProcessor
 
                 Request originalRequest = requestWrapper.getRequest();
 
+                UUID mgmtId = getPeerManager().getLocalPeer().getManagementHost().getId();
+
+                //todo refactor here when agent supplies proper fingerprint id and peer too
+                UUID keyId = mgmtId.equals( originalRequest.getId() ) ? getPeerManager().getLocalPeer().getId() :
+                             originalRequest.getId();
+
                 //obtain target host pub key for encrypting
-                PGPPublicKey hostKeyForEncrypting = MessageEncryptor.getSecurityManager().getKeyManager()
-                                                                    .getPublicKey( originalRequest.getId().toString() );
+                PGPPublicKey hostKeyForEncrypting =
+                        MessageEncryptor.getSecurityManager().getKeyManager().getPublicKey( keyId.toString() );
 
                 if ( originalRequest.getCommand().toLowerCase().matches( CLONE_CMD_REGEX ) )
                 {
