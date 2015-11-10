e14e8c5553bfb918aba87ba4a0cd6053650093f9
dilshat <daliev@critical-factor.com>
2015-11-10 14:31:09 +0600

    minor bugfix SS-3083

--- management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
+++ management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
@@ -508,11 +508,6 @@ public class LocalPeerImpl implements LocalPeer, HostListener, Disposable
 
         executorService.shutdown();
 
-        // updating resource host entities
-        for ( ResourceHost resourceHost : containerDistribution.keySet() )
-        {
-            resourceHostDataService.saveOrUpdate( resourceHost );
-        }
 
         return result;
     }
