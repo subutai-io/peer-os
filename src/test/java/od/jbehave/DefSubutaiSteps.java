6ded98e451dfe5b224bc7abac51bd25f3d9763b8
Oleg Kutc <okutc@critical-factor.com>
2015-11-23 15:39:00 +0600

    master update DefSteps

--- src/test/java/od/jbehave/DefSubutaiSteps.java
+++ src/test/java/od/jbehave/DefSubutaiSteps.java
@@ -1,6 +1,8 @@
 package od.jbehave;
 
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
+import od.pages.ReaderFromFile;
 import od.steps.SubutaiSteps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
@@ -14,4 +16,13 @@ import static org.hamcrest.Matchers.is;
 
 public class DefSubutaiSteps {
 
+    @Step
+    public String getMngHost1() throws FileNotFoundException {
+        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h1");
+    }
+
+    @Step
+    public String getMngHost2() throws FileNotFoundException {
+        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h2");
+    }
 }
