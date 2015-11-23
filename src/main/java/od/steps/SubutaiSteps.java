a463546e6191162bb9cb867af0d0f4fef26a7fd3
Oleg Kutc <okutc@critical-factor.com>
2015-11-23 15:44:14 +0600

    master update DefSteps

--- src/main/java/od/steps/SubutaiSteps.java
+++ src/main/java/od/steps/SubutaiSteps.java
@@ -13,4 +13,13 @@ import static org.hamcrest.Matchers.*;
 
 public class SubutaiSteps extends ScenarioSteps {
 
+    @Step
+    public String getMngHost1() throws FileNotFoundException {
+        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h1");
+    }
+
+    @Step
+    public String getMngHost2() throws FileNotFoundException {
+        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h2");
+    }
 }
--- src/test/java/od/jbehave/DefSubutaiSteps.java
+++ src/test/java/od/jbehave/DefSubutaiSteps.java
@@ -16,13 +16,4 @@ import static org.hamcrest.Matchers.is;
 
 public class DefSubutaiSteps {
 
-    @Step
-    public String getMngHost1() throws FileNotFoundException {
-        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h1");
-    }
-
-    @Step
-    public String getMngHost2() throws FileNotFoundException {
-        return ReaderFromFile.readDataFromFile("src/test/resources/parameters/mng_h2");
-    }
 }
