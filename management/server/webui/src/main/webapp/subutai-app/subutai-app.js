6c94c4eed1ee34277b4d4b46b49ac4e1775baa6b
istus <istus@mail.critical-factor.com>
2015-11-24 21:33:12 +0600

    service registration files added for cassandra plugin  SS-3858 fix

--- management/server/webui/src/main/webapp/subutai-app/subutai-app.js
+++ management/server/webui/src/main/webapp/subutai-app/subutai-app.js
@@ -394,8 +394,7 @@ app.directive('checkbox-list-dropdown', function() {
 });
 
 //Global variables
-//var SERVER_URL = '/';
-var SERVER_URL = 'http://172.16.131.205:8181/';
+var SERVER_URL = '/';
 
 var STATUS_UNDER_MODIFICATION = 'UNDER_MODIFICATION';
 var VARS_TOOLTIP_TIMEOUT = 900;
