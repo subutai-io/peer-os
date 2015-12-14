ee0aea53c18f8c1ae8e55598c01bbf18a3f82513
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-12-14 23:21:20 +0200

    SS-4039 phase-10 properly encode gpg message on client side

--- management/server/webui/src/main/webapp/subutai-app/environment/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/environment/controller.js
@@ -199,7 +199,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, SweetAlert,
     }
 
 	function buildEnvironment() {
-		environmentService.startEnvironmentBuild (vm.currentEnvironment.id, vm.signedMessage).success(function (data) {
+		environmentService.startEnvironmentBuild (vm.currentEnvironment.id, encodeURIComponent(vm.signedMessage)).success(function (data) {
 			SweetAlert.swal("Success!", "Your environment has started building.", "success");
 			vm.dtInstance.reloadData(null, false);
 			ngDialog.closeAll();
