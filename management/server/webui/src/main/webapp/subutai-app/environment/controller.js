785cbd4da437805879d3c4640e9f8997b53d0f6d
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2015-12-17 13:22:40 +0200

    SS-4039 minor fix, updating relation status

--- management/server/webui/src/main/webapp/subutai-app/environment/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/environment/controller.js
@@ -199,7 +199,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, SweetAlert,
     }
 
 	function buildEnvironment() {
-		environmentService.startEnvironmentBuild (vm.currentEnvironment.id, encodeURIComponent(vm.signedMessage)).success(function (data) {
+		environmentService.startEnvironmentBuild (vm.currentEnvironment.id, encodeURIComponent(vm.currentEnvironment.relationDeclaration)).success(function (data) {
 			SweetAlert.swal("Success!", "Your environment has started building.", "success");
 			vm.dtInstance.reloadData(null, false);
 			ngDialog.closeAll();
