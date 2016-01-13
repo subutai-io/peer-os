8a212bc7477c33c17d61edf60e4e19096e49a7ce
Kudaiar Pirimbaev <kpirimbaev@critical-factor.com>
2016-01-13 11:16:10 +0600

    Merge branch 'SS-4039' of https://stash.subutai.io/scm/ss/main into SS-4039
    Conflicts:
    	management/server/webui-impl/src/main/resources/subutai-app.js
    	management/server/webui/src/main/webapp/subutai-app/environment/controller.js
    	management/server/webui/src/main/webapp/subutai-app/subutai-app.js

--- management/server/webui/src/main/webapp/subutai-app/subutai-app.js
+++ management/server/webui/src/main/webapp/subutai-app/subutai-app.js
@@ -197,7 +197,7 @@ function routesConf($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 			}
 		})
 		.state('environments', {
-			url: '/environments',
+			url: '/environments/{activeTab}',
 			templateUrl: 'subutai-app/environment/partials/view.html',
 			data: {
 				bodyClass: '',
