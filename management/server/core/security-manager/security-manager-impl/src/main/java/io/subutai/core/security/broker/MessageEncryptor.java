096033d0d81038b8d4a744df1733a997f983ef49
dilshat <daliev@critical-factor.com>
2016-01-16 22:48:44 +0600

    minor refactor SS-3083

--- management/server/core/security-manager/security-manager-impl/src/main/java/io/subutai/core/security/broker/MessageEncryptor.java
+++ management/server/core/security-manager/security-manager-impl/src/main/java/io/subutai/core/security/broker/MessageEncryptor.java
@@ -1,27 +1,18 @@
 package io.subutai.core.security.broker;
 
 
-import java.util.List;
-
 import javax.naming.NamingException;
 
 import org.bouncycastle.openpgp.PGPPublicKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Lists;
-
 import io.subutai.common.command.Request;
-import io.subutai.common.command.RequestBuilder;
-import io.subutai.common.settings.Common;
 import io.subutai.common.util.JsonUtil;
 import io.subutai.common.util.ServiceLocator;
 import io.subutai.core.broker.api.TextMessagePostProcessor;
-import io.subutai.core.registration.api.RegistrationManager;
 import io.subutai.core.security.api.SecurityManager;
 import io.subutai.core.security.api.crypto.EncryptionTool;
-
-//import io.subutai.core.registration.api.RegistrationManager;
 
 
 /**
@@ -30,7 +21,6 @@ import io.subutai.core.security.api.crypto.EncryptionTool;
 public class MessageEncryptor implements TextMessagePostProcessor
 {
     private static final Logger LOG = LoggerFactory.getLogger( MessageEncryptor.class.getName() );
-    private static final String CLONE_CMD_REGEX = "\\s*subutai\\s*clone[\\s\\S]*?";
 
     private final boolean encryptionEnabled;
 
@@ -47,17 +37,9 @@ public class MessageEncryptor implements TextMessagePostProcessor
     }
 
 
-    public static RegistrationManager getRegistrationManager() throws NamingException
-    {
-
-        return ServiceLocator.getServiceNoCache( RegistrationManager.class );
-    }
-
-
     @Override
     public String process( final String topic, final String message )
     {
-        //        LOG.debug( String.format( "OUTGOING %s", message ) );
 
         if ( encryptionEnabled )
         {
@@ -72,28 +54,6 @@ public class MessageEncryptor implements TextMessagePostProcessor
                 //obtain target host pub key for encrypting
                 PGPPublicKey hostKeyForEncrypting =
                         MessageEncryptor.getSecurityManager().getKeyManager().getPublicKey( originalRequest.getId() );
-
-
-                //TODO move container token out to LocalPeer
-                if ( originalRequest.getCommand().toLowerCase().matches( CLONE_CMD_REGEX ) )
-                {
-
-                    //add token for container creation
-                    List<String> args = Lists.newArrayList( originalRequest.getArgs() );
-                    args.add( "-t" );
-                    args.add( getRegistrationManager().generateContainerTTLToken(
-                            ( originalRequest.getTimeout() + Common.WAIT_CONTAINER_CONNECTION_SEC + 10 ) * 1000L )
-                                                      .getToken() );
-
-                    originalRequest =
-                            new RequestBuilder.RequestImpl( originalRequest.getType(), originalRequest.getId(),
-                                    originalRequest.getCommandId(), originalRequest.getWorkingDirectory(),
-                                    originalRequest.getCommand(), args, originalRequest.getEnvironment(),
-                                    originalRequest.getStdOut(), originalRequest.getStdErr(),
-                                    originalRequest.getRunAs(), originalRequest.getTimeout(),
-                                    originalRequest.isDaemon(), originalRequest.getConfigPoints(),
-                                    originalRequest.getPid() );
-                }
 
                 String encryptedRequestString = new String( encryptionTool
                         .signAndEncrypt( JsonUtil.toJson( originalRequest ).getBytes(), hostKeyForEncrypting, true ) );
