feb99b7c1c6549533365284255b3d3090dd2c079
Kairat Saparbaev <s.kayrat@gmail.com>
2016-01-28 18:16:07 +0600

    KURJUN-145: Added api and rest for deleting apt packages

--- management/server/core/kurjun/kurjun-impl/src/main/java/io/subutai/core/kurjun/impl/vapt/AptManagerImpl.java
+++ management/server/core/kurjun/kurjun-impl/src/main/java/io/subutai/core/kurjun/impl/vapt/AptManagerImpl.java
@@ -258,6 +258,23 @@ public class AptManagerImpl implements AptManager
         List<AptPackage> deflist = list.stream().map( t -> convertToAptPackage( ( DefaultPackageMetadata ) t ) ).collect( Collectors.toList() );
         return deflist;
     }
+    
+    
+    @Override
+    public boolean delete( byte[] md5 ) throws IOException
+    {
+        LocalRepository repo = getLocalRepository();
+        try
+        {
+            repo.delete( md5 );
+            return true;
+        }
+        catch ( IOException ex )
+        {
+            LOGGER.error( "Failed to delete apt package", ex );
+            return false;
+        }
+    }
 
 
     private AptPackage convertToAptPackage( DefaultPackageMetadata meta )
