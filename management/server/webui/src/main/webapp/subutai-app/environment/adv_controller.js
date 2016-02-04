0c052c094e36efb825c63734af38b91ae2ee0432
Manas Atabaev <matabaev@critical-factor.com>
2016-02-04 20:11:46 +0600

    advanced environment creation

--- management/server/webui/src/main/webapp/subutai-app/environment/adv_controller.js
+++ management/server/webui/src/main/webapp/subutai-app/environment/adv_controller.js
@@ -752,17 +752,23 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 					'<title/>',
 					'<image/>',
 					'<rect class="b-magnet"/>',
+					'<g class="b-container-plus-icon">',
+						'<line fill="none" stroke="#FFFFFF" stroke-miterlimit="10" x1="0" y1="4.5" x2="9" y2="4.5"/>',
+						'<line fill="none" stroke="#FFFFFF" stroke-miterlimit="10" x1="4.5" y1="0" x2="4.5" y2="9"/>',
+					'</g>',
 				'</g>'
 			].join(''),
 
 			defaults: joint.util.deepSupplement({
 				type: 'tm.devElement',
-				size: { width: 70, height: 70 },
+				size: { width: 40, height: 40 },
 				attrs: {
 					title: {text: 'Static Tooltip'},
-					'rect.b-border': {fill: '#fff', stroke: '#dcdcdc', 'stroke-width': 1, width: 70, height: 70, rx: 50, ry: 50},
-					'rect.b-magnet': {fill: '#04346E', width: 10, height: 10, rx: 2, ry: 2, magnet: true, transform: 'translate(30,53)'},
-					image: {'ref-x': 9, 'ref-y': 9, ref: 'rect', width: 50, height: 50},
+					'rect.b-border': {fill: '#fff', stroke: '#dcdcdc', 'stroke-width': 1, width: 40, height: 40, rx: 50, ry: 50},
+					//'rect.b-magnet': {fill: '#04346E', width: 10, height: 10, rx: 50, ry: 50, magnet: true, transform: 'translate(16,28)'},
+					'rect.b-magnet': {fill: '#04346E', width: 10, height: 10, rx: 50, ry: 50, transform: 'translate(16,28)'},
+					'g.b-container-plus-icon': {'ref-x': 17.5, 'ref-y': 30, ref: 'rect', transform: 'scale(0.8)'},
+					image: {'ref-x': 9, 'ref-y': 9, ref: 'rect', width: 25, height: 25},
 				}
 			}, joint.shapes.tm.toolElement.prototype.defaults)
 		});
@@ -826,92 +832,64 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 		joint.shapes.tm.devElementView = joint.shapes.tm.ToolElementView;
 
 		//recource host object
-		joint.shapes.tm.recourceHostTools = joint.shapes.basic.Generic.extend({
-
-			toolMarkup: [
-				'<g class="element-tools">',
-					'<g class="element-tool-remove">',
-						'<circle fill="#F8FBFD" r="8" stroke="#dcdcdc"/>',
-						'<polygon transform="scale(1.2) translate(-5, -5)" fill="#292F6C" points="8.4,2.4 7.6,1.6 5,4.3 2.4,1.6 1.6,2.4 4.3,5 1.6,7.6 2.4,8.4 5,5.7 7.6,8.4 8.4,7.6 5.7,5 "/>',
-						'<title>Remove</title>',
-					'</g>',
-				'</g>'
-			].join(''),
-
+		joint.shapes.recourceHostHtml = {};
+		joint.shapes.recourceHostHtml.Element = joint.shapes.basic.Rect.extend({
 			defaults: joint.util.deepSupplement({
+				type: 'recourceHostHtml.Element',
 				attrs: {
-					text: { 'font-weight': 400, 'font-size': 'small', fill: 'black', 'text-anchor': 'middle', 'ref-x': .5, 'ref-y': .5, 'y-alignment': 'middle' },
-				},
-			}, joint.shapes.basic.Generic.prototype.defaults)
-
-		});
-
-		joint.shapes.tm.recourceHost = joint.shapes.tm.recourceHostTools.extend({
-
-			markup: [
-				'<g class="rotatable">',
-					'<g class="scalable">',
-						'<rect class="b-border-level1"/>',
-						'<rect class="b-border-level2"/>',
-					'</g>',
-					'<title/>',
-				'</g>'
-			].join(''),
-
-			defaults: joint.util.deepSupplement({
-				type: 'tm.recourceHost',
-				size: { width: 155, height: 185 },
-				attrs: {
-					title: {text: 'Static Tooltip'},
-					'rect.b-border-level1': {fill: '#F5F6FB', stroke: '#dcdcdc', 'stroke-width': 1, width: 155, height: 185, rx: 4, ry: 4},
-					'rect.b-border-level2': {'ref-x': 5, 'ref-y': 5, fill: '#fff', stroke: '#dcdcdc', 'stroke-width': 1, width: 145, height: 175, rx: 4, ry: 4},
-					'rect.b-border-level3': {'ref-x': 5, 'ref-y': 35, fill: '#fff', stroke: '#dcdcdc', 'stroke-width': 1, width: 135, height: 135, rx: 4, ry: 4},
-					//image: {'ref-x': 9, 'ref-y': 9, ref: 'rect', width: 50, height: 50},
+					rect: { stroke: 'none', 'fill-opacity': 0 }
 				}
-			}, joint.shapes.tm.toolElement.prototype.defaults)
+			}, joint.shapes.basic.Rect.prototype.defaults)
 		});
 
-		//custom view
-		joint.shapes.tm.RecourceHostToolsView = joint.dia.ElementView.extend({
+		// Create a custom view for that element that displays an HTML div above it.
+		// -------------------------------------------------------------------------
+
+		joint.shapes.recourceHostHtml.ElementView = joint.dia.ElementView.extend({
+
+			template: [
+				'<div class="b-recource-host">',
+					'<div class="b-peer-title"></div>',
+					'<div class="b-recource-host__inner">',
+						'<div class="b-recouce-host-title"></div>',
+						'<div class="b-recource-host__containers">',
+							'<div class="b-recource-host-containers">',
+							'</div>',
+						'</div>',
+					'</div>',
+				'</div>'
+			].join(''),
+
 			initialize: function() {
+				_.bindAll(this, 'updateBox');
 				joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+
+				this.$box = $(_.template(this.template)());
+				this.$box.find('.delete').on('click', _.bind(this.model.remove, this.model));
+				this.model.on('change', this.updateBox, this);
+				// Remove the box when the model gets removed from the graph.
+				this.model.on('remove', this.removeBox, this);
+
+				this.updateBox();
 			},
-			render: function () {
+			render: function() {
 				joint.dia.ElementView.prototype.render.apply(this, arguments);
-				this.renderTools();
-				this.update();
+				this.paper.$el.prepend(this.$box);
+				this.updateBox();
 				return this;
 			},
-			renderTools: function () {
-				var toolMarkup = this.model.toolMarkup || this.model.get('toolMarkup');
-				if (toolMarkup) {
-					var nodes = V(toolMarkup);
-					V(this.el).append(nodes);
-				}
-				return this;
+			updateBox: function() {
+				// Set the position and dimension of the box so that it covers the JointJS element.
+				var bbox = this.model.getBBox();
+
+				this.$box.find('.b-peer-title').text(this.model.get('peerName'));
+				this.$box.find('.b-recouce-host-title').text(this.model.get('recourceHostName'));
+				this.$box.css({ width: bbox.width, height: bbox.height, left: bbox.x, top: bbox.y, transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)' });
 			},
-			mouseover: function(evt, x, y) {
-			},
-			pointerclick: function (evt, x, y) {
-				this._dx = x;
-				this._dy = y;
-				this._action = '';
-				var className = evt.target.parentNode.getAttribute('class');
-				switch (className) {
-					case 'element-tool-remove':
-						this.model.remove();
-						return;
-						break;
-					case 'rotatable':
-						console.log(this.model);
-						return;
-						break;
-					default:
-				}
-				joint.dia.CellView.prototype.pointerclick.apply(this, arguments);
+			removeBox: function(evt) {
+				this.$box.remove();
 			}
 		});
-		joint.shapes.tm.recourceHostView = joint.shapes.tm.RecourceHostToolsView;		
 
 		var paper = new joint.dia.Paper({
 			el: $('#js-environment-creation'),
@@ -948,7 +926,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 			p0 = cellView.model.get('position');
 		});
 
-		paper.on('cell:pointerup',
+		/*paper.on('cell:pointerup',
 			function(cellView, evt, x, y) {
 
 				var pos = cellView.model.get('position');
@@ -977,42 +955,96 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 				else
 					cellView.model.set('position', p0);
 			}
-		);
+		);*/
 
 		$('.js-scrollbar').perfectScrollbar();
 		var containerCounter = 1;
 		$('.b-tools-menu').on('click', '.js-add-dev-element', function(){
-			var pos = findEmptyCubePostion();
+			//var pos = findEmptyCubePostion();
+			var parentPos = mainRecourceHost.get('position');
+			var pos = findEmptyPostionInRecource();
 			var img = $(this).find('img');
+
 			var devElement = new joint.shapes.tm.devElement({
-				position: { x: (GRID_CELL_SIZE * pos.x) + 20, y: (GRID_CELL_SIZE * pos.y) + 20 },
+				//position: { x: (GRID_CELL_SIZE * pos.x) + 20, y: (GRID_CELL_SIZE * pos.y) + 20 },
+				position: { x: parentPos.x + pos.x, y: parentPos.y + pos.y },
 				templateName: $(this).data('template'),
 				quotaSize: 'SMALL',
 				containerName: vm.environment2BuildName + ' ' + (containerCounter++).toString(),
 				attrs: {
 					image: { 'xlink:href': img.attr('src') },
+					'rect.b-magnet': {fill: vm.colors['SMALL']},
 					title: {text: $(this).data('template')}
 				}
 			});
+
 			graph.addCell(devElement);
+			mainRecourceHost.embed(devElement);
+			vm.containersInRecource.push(devElement);
 			return false;
 		});
+
+		var mainRecourceHost = new joint.shapes.recourceHostHtml.Element({
+			position: { x: 80, y: 80 },
+			size: { width: 155, height: 185 },
+			'recourceHostName': 'RH1',
+			'peerName': 'Peer 1'
+		});
+		graph.addCell(mainRecourceHost);
 
 
 		//add recource host
 		$('.b-tools-menu').on('click', '.js-add-recource-host', function(){
 			var pos = findEmptyCubePostion();
 			var img = $(this).find('img');
-			var recourceHost = new joint.shapes.tm.recourceHost({
-				position: { x: (GRID_CELL_SIZE * pos.x) + 20, y: (GRID_CELL_SIZE * pos.y) + 20 },
-				attrs: {
-					image: { 'xlink:href': img.attr('src') },
-					title: {text: $(this).data('template')}
-				}
+			var recourceHost = new joint.shapes.recourceHostHtml.Element({
+				position: { x: 80, y: 80 },
+				size: { width: 155, height: 185 },
+				'recourceHostName': 'RH1',
+				'peerName': 'Peer 1'
 			});
 			graph.addCell(recourceHost);
 			return false;
 		});
+
+		vm.containersInRecource = [];
+		vm.containersInRecourceMax = 4;
+		vm.containersInRow = 2;
+		function findEmptyPostionInRecource() {
+			console.log(mainRecourceHost.get('position'));
+			if(vm.containersInRecource.length == 0) {
+				return {x: 28, y: 54};
+			} else {
+				var row = 0;
+				for(var i = 0; i < vm.containersInRecource.length; i++) {
+					if((i + 1) % vm.containersInRow == 0) {
+						row++;
+					}
+				}
+
+				var x = 88;
+				if(vm.containersInRecource.length % vm.containersInRow == 0) {
+					x = 28;
+				}
+
+				if(vm.containersInRecource.length == vm.containersInRecourceMax) {
+					if(vm.containersInRecourceMax == 4) {
+						vm.containersInRecourceMax = vm.containersInRecourceMax + 5;
+					} else {
+						vm.containersInRecourceMax = vm.containersInRecourceMax + (vm.containersInRecourceMax + 2);
+					}
+
+					//vm.containersInRecourceMax = vm.containersInRecourceMax + 2;
+
+					var currentSize = mainRecourceHost.get('size');
+					vm.containersInRow++;
+					mainRecourceHost.resize(currentSize.width + 60, currentSize.height + 60);
+				}
+				return {x: x, y: (row * 60) + 54};
+			}
+		}
+
+		findEmptyPostionInRecource();
 
 		function findEmptyCubePostion() {
 			for( var j = 0; j < vm.cubeGrowth; j++ ) {
@@ -1123,6 +1155,7 @@ function EnvironmentViewCtrl($scope, $rootScope, environmentService, peerRegistr
 
 	function addSettingsToTemplate(settings) {
 		vm.currentTemplate.set('quotaSize', settings.quotaSize);
+		vm.currentTemplate.attr('rect.b-magnet/fill', vm.colors[settings.quotaSize]);
 		vm.currentTemplate.set('containerName', settings.containerName);
 		ngDialog.closeAll();
 	}
