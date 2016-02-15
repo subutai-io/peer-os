b09eb6fc9a001d231ebc8efa9c52d32d42da369d
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-02-15 23:36:02 +0600

    added improvements to system-manager, particularly to peer settings and kurjun settings

--- management/server/webui/src/main/webapp/subutai-app/settingsPeer/service.js
+++ management/server/webui/src/main/webapp/subutai-app/settingsPeer/service.js
@@ -7,15 +7,21 @@ SettingsPeerSrv.$inject = ["$http"];
 
 function SettingsPeerSrv ($http) {
 	var SettingsPeerSrv = {
-		getConfig: getConfig,
-		updateConfig: updateConfig
+		getSettingsConfig: getSettingsConfig,
+		getPolicyConfig: getPolicyConfig,
+		updateSettingsConfig: updateSettingsConfig,
+		updatePolicyConfig: updatePolicyConfig
 	};
 
-	function getConfig() {
+	function getSettingsConfig() {
 		return $http.get (SERVER_URL + "rest/v1/system/peer_settings", {withCredentials: true, headers: {'Content-Type': 'application/json'}});
 	}
 
-	function updateConfig (config) {
+	function getPolicyConfig() {
+    		return $http.get (SERVER_URL + "rest/v1/system/peer_policy", {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }
+
+	function updateSettingsConfig (config) {
         return $http.post(
             SERVER_URL + "rest/v1/system/update_peer_settings",
             {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
@@ -23,6 +29,14 @@ function SettingsPeerSrv ($http) {
 
     }
 
+	function updatePolicyConfig (config) {
+		var postData = "peerId=" + config.peerId + "&diskUsageLimit=" + config.diskUsageLimit + "&cpuUsageLimit=" + config.cpuUsageLimit + "&memoryUsageLimit=" + config.memoryUsageLimit /*+ "&networkUsageLimit=" + config.networkUsageLimit*/ + "&environmentLimit=" + config.environmentLimit + "&containerLimit=" + config.containerLimit;
+		return $http.post(
+			SERVER_URL + "rest/v1/system/update_peer_policy",
+			postData,
+			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+		);
+	}
 
 	return SettingsPeerSrv;
 }
