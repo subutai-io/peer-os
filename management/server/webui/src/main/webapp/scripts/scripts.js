ead482c8187499302b57c25e7cefd84f3981df4a
Manas Atabaev <manas.atabaev@gmail.com>
2016-02-15 22:31:31 +0600

    previous commit fix

--- management/server/webui/src/main/webapp/scripts/scripts.js
+++ management/server/webui/src/main/webapp/scripts/scripts.js
@@ -29,14 +29,31 @@ $(".b-form-input_dropdown").click(function () {
 $(".b-form-input-dropdown-list").click(function(e) {
 	e.stopPropagation();
 });
-$(".b-hub-status__btn").click(function () {
-	$(".b-hub-status__dropdown").toggleClass("show");
-});
 
 $('.js-scrollbar').perfectScrollbar();
+$('.js-scrollbar-cloud').perfectScrollbar();
+
+$('body').on('click', '.js-hide-resources', function(){
+	$('.b-cloud-add-tools').animate({'left': 0}, 300);
+	return false;
+});
 
 var UPDATE_NIGHTLY_BUILD_STATUS;
 
 //document.getElementById("uploadBtn").onchange = function () {
 //	document.getElementById("uploadFile").value = this.value;
 //};
+/*$('a.js-cbox-modal').colorbox({
+	title: " ",
+	transition: "none",
+	previous: false,
+	next: false,
+	arrowKey: false,
+	rel: false,
+	overlayClose: true,
+	opacity: 0.8,
+	closeButton: false,
+	onComplete: function() {
+		$.colorbox.resize();
+	}
+});*/
