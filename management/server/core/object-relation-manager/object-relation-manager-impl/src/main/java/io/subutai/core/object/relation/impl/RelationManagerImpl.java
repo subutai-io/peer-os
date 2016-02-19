54355b5450f50a061794f843623a77f78cc23b3b
Talas Zholdoshbekov <tjoldoshbekov@critical-factor.com>
2016-02-19 18:34:06 +0200

    relation reflection fix

--- management/server/core/object-relation-manager/object-relation-manager-impl/src/main/java/io/subutai/core/object/relation/impl/RelationManagerImpl.java
+++ management/server/core/object-relation-manager/object-relation-manager-impl/src/main/java/io/subutai/core/object/relation/impl/RelationManagerImpl.java
@@ -170,9 +170,9 @@ public class RelationManagerImpl implements RelationManager
     public void saveRelation( final Relation relation )
     {
         //TODO check if relation valid otherwise break relation build
-        relationDataService.save( relation.getSource() );
-        relationDataService.save( relation.getTarget() );
-        relationDataService.save( relation.getTrustedObject() );
+        relationDataService.save( new RelationLinkImpl( relation.getSource() ) );
+        relationDataService.save( new RelationLinkImpl( relation.getTarget() ) );
+        relationDataService.save( new RelationLinkImpl( relation.getTrustedObject() ) );
         relationDataService.update( relation );
     }
 
