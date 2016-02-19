4fbc8a0aedd4720a7d2bca0e431c09cebe4451d0
tzununbekov <tzununbekov@critical-factor.com>
2016-02-19 16:23:46 +0600

    Agent version string fix

--- agent/main.go
+++ agent/main.go
@@ -23,7 +23,7 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Subutai"
-	app.Version = "4.0.0 RC5"
+	app.Version = "v4.0.0-RC6"
 	app.Usage = "daemon and command line interface binary"
 
 	app.Flags = []cli.Flag{cli.BoolFlag{
