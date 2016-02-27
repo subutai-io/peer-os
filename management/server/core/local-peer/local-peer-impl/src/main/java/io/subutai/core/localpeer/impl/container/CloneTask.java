ed122a94b76c9fb3261b2e5583f49d9d84966345
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-02-27 23:05:17 +0600

    added CloneRequest and CloneResponse classes

--- management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/container/CloneTask.java
+++ management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/container/CloneTask.java
@@ -8,77 +8,50 @@ import javax.naming.NamingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import io.subutai.common.command.CommandResult;
 import io.subutai.common.command.CommandResultParseException;
 import io.subutai.common.command.CommandResultParser;
-import io.subutai.common.command.RequestBuilder;
+import io.subutai.common.environment.CloneRequest;
+import io.subutai.common.environment.CloneResponse;
 import io.subutai.common.host.ContainerHostInfo;
-import io.subutai.common.host.HostInfo;
-import io.subutai.common.peer.ContainerSize;
-import io.subutai.common.peer.Host;
-import io.subutai.common.peer.PeerException;
-import io.subutai.common.peer.ResourceHost;
-import io.subutai.common.protocol.TemplateKurjun;
+import io.subutai.common.host.ContainerHostInfoModel;
+import io.subutai.common.host.HostInterface;
+import io.subutai.common.host.NullHostInterface;
 import io.subutai.common.quota.ContainerQuota;
 import io.subutai.common.quota.ContainerResource;
 import io.subutai.common.settings.Common;
-import io.subutai.common.util.NumUtil;
-import io.subutai.common.util.ServiceLocator;
-import io.subutai.core.hostregistry.api.HostDisconnectedException;
-import io.subutai.core.hostregistry.api.HostRegistry;
 import io.subutai.common.task.Command;
 import io.subutai.common.task.CommandBatch;
+import io.subutai.common.util.ServiceLocator;
+import io.subutai.common.util.StringUtil;
+import io.subutai.core.hostregistry.api.HostDisconnectedException;
+import io.subutai.core.hostregistry.api.HostRegistry;
 import io.subutai.core.localpeer.impl.LocalPeerImpl;
-import io.subutai.core.localpeer.impl.entity.ContainerHostEntity;
 import io.subutai.core.registration.api.RegistrationManager;
 
 
-public class CloneTask extends AbstractTask<HostInfo> implements CommandResultParser<HostInfo>
+public class CloneTask extends AbstractTask<CloneRequest, CloneResponse> implements CommandResultParser<CloneResponse>
 {
     protected static final Logger LOG = LoggerFactory.getLogger( CloneTask.class );
 
     private static final int CLONE_TIMEOUT = 60 * 10; // 10 min
-    private final ResourceHost resourceHost;
-    private final String hostname;
-    private final TemplateKurjun template;
-    private final String ip;
-    private final int vlan;
-    private final ContainerQuota quota;
-    private final ContainerSize size;
-    private final String environmentId;
+
     private final LocalPeerImpl localPeer;
+    private final int vlan;
     private HostRegistry hostRegistry;
-    private String ownerId;
-    private String initiatorPeerId;
+    private ContainerQuota quota;
 
 
-    public CloneTask( LocalPeerImpl localPeer, HostRegistry hostRegistry, final ResourceHost resourceHost,
-                      final TemplateKurjun template, final String hostname, final String environmentId,
-                      final String ownerId, final String initiatorPeerId, final ContainerSize size,
-                      final ContainerQuota quota, final String ip, final int vlan )
+    public CloneTask( LocalPeerImpl localPeer, HostRegistry hostRegistry, ContainerQuota quota, int vlan,
+                      CloneRequest request )
     {
-        Preconditions.checkNotNull( resourceHost );
-        Preconditions.checkArgument( !Strings.isNullOrEmpty( hostname ) );
-        Preconditions.checkNotNull( template );
-        Preconditions.checkArgument( !Strings.isNullOrEmpty( ip ) && ip.matches( Common.CIDR_REGEX ) );
-        Preconditions.checkArgument( NumUtil.isIntBetween( vlan, Common.MIN_VLAN_ID, Common.MAX_VLAN_ID ) );
-
+        super( request );
         this.localPeer = localPeer;
         this.hostRegistry = hostRegistry;
-        this.resourceHost = resourceHost;
-        this.template = template;
-        this.hostname = hostname;
-        this.environmentId = environmentId;
-        this.ownerId = ownerId;
-        this.initiatorPeerId = initiatorPeerId;
-        this.ip = ip;
-        this.vlan = vlan;
         this.quota = quota;
-        this.size = size;
+        this.vlan = vlan;
     }
 
 
@@ -94,10 +67,10 @@ public class CloneTask extends AbstractTask<HostInfo> implements CommandResultPa
         CommandBatch result = new CommandBatch();
 
         Command cloneAction = new Command( "clone",
-                Lists.newArrayList( template.getName(), hostname, "-i", String.format( "\"%s %s\"", ip, vlan ), "-e",
-                        environmentId, "-t",
-                        getRegistrationManager().generateContainerTTLToken( ( CLONE_TIMEOUT + 10 ) * 1000L )
-                                                .getToken() ) );
+                Lists.newArrayList( request.getTemplateName(), request.getHostname(), "-i",
+                        String.format( "\"%s %d\"", request.getIp(), this.vlan ), "-e", request.getEnvironmentId(),
+                        "-t", getRegistrationManager().generateContainerTTLToken( ( CLONE_TIMEOUT + 10 ) * 1000L )
+                                                      .getToken() ) );
 
         result.addCommand( cloneAction );
 
@@ -105,7 +78,7 @@ public class CloneTask extends AbstractTask<HostInfo> implements CommandResultPa
         {
 
             Command quotaCommand = new Command( "quota" );
-            quotaCommand.addArgument( hostname );
+            quotaCommand.addArgument( request.getHostname() );
             quotaCommand.addArgument( r.getContainerResourceType().getKey() );
             quotaCommand.addArgument( "-s" );
             quotaCommand.addArgument( r.getWriteValue() );
@@ -117,29 +90,28 @@ public class CloneTask extends AbstractTask<HostInfo> implements CommandResultPa
 
 
     @Override
-    public Host getHost()
-    {
-        return resourceHost;
-    }
-
-
-    @Override
-    public CommandResultParser<HostInfo> getCommandResultParser()
+    public CommandResultParser<CloneResponse> getCommandResultParser()
     {
         return this;
     }
 
 
     @Override
-    public HostInfo parse( final CommandResult commandResult ) throws CommandResultParseException
+    public CloneResponse parse( final CommandResult commandResult ) throws CommandResultParseException
     {
-        HostInfo result = null;
+        ContainerHostInfoModel r = null;
         int counter = 0;
-        while ( result == null && counter < Common.WAIT_CONTAINER_CONNECTION_SEC )
+        String ip = null;
+        while ( ip == null && counter < Common.WAIT_CONTAINER_CONNECTION_SEC )
         {
             try
             {
-                result = findHostInfo();
+                r = findHostInfo();
+                HostInterface i = r.getHostInterfaces().findByName( Common.DEFAULT_CONTAINER_INTERFACE );
+                if ( !( i instanceof NullHostInterface ) )
+                {
+                    ip = i.getIp();
+                }
                 TimeUnit.SECONDS.sleep( 1 );
             }
             catch ( HostDisconnectedException | InterruptedException e )
@@ -149,17 +121,32 @@ public class CloneTask extends AbstractTask<HostInfo> implements CommandResultPa
             counter++;
         }
 
-        if ( result == null )
+        if ( r == null )
         {
-            throw new CommandResultParseException( "Heartbeat not received from: " + hostname );
+            throw new CommandResultParseException( "Heartbeat not received from: " + request.getContainerName() );
         }
+
+        if ( ip == null )
+        {
+            throw new CommandResultParseException( "IP not assigned: " + request.getContainerName() );
+        }
+
+        final CloneResponse result =
+                new CloneResponse( request.getResourceHostId(), request.getHostname(), request.getContainerName(),
+                        r.getId(), request.getIp(), request.getTemplateName(), request.getTemplateArch() );
+        result.addSucceededMessage(
+                String.format( "Container %s on %s successfully created.", request.getContainerName(),
+                        request.getResourceHostId() ) );
+
         return result;
     }
 
 
-    private ContainerHostInfo findHostInfo() throws HostDisconnectedException
+    private ContainerHostInfoModel findHostInfo() throws HostDisconnectedException
     {
-        return hostRegistry.getContainerHostInfoByHostname( hostname );
+        final ContainerHostInfo info = hostRegistry.getContainerHostInfoByHostname( request.getHostname() );
+
+        return new ContainerHostInfoModel( info );
     }
 
 
@@ -174,25 +161,5 @@ public class CloneTask extends AbstractTask<HostInfo> implements CommandResultPa
     public boolean isSequential()
     {
         return false;
-    }
-
-
-    public void onSuccess()
-    {
-        ContainerHostEntity containerHostEntity =
-                new ContainerHostEntity( result.getId(), result, template.getName(), template.getArchitecture() );
-        containerHostEntity.setEnvironmentId( environmentId );
-        containerHostEntity.setOwnerId( ownerId );
-        containerHostEntity.setInitiatorPeerId( initiatorPeerId );
-        containerHostEntity.setContainerSize( size );
-
-        try
-        {
-            localPeer.registerContainer( resourceHost, containerHostEntity );
-        }
-        catch ( PeerException e )
-        {
-            LOG.error( "Error on registering container.", e );
-        }
     }
 }
