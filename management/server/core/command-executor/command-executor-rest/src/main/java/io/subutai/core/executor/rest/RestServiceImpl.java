3697e39a9c3149151b4e42dab97dddd8830230ec
dilshat <daliev@critical-factor.com>
2016-02-29 15:46:39 +0600

    added rest module to command executor

---
+++ management/server/core/command-executor/command-executor-rest/src/main/java/io/subutai/core/executor/rest/RestServiceImpl.java
@@ -0,0 +1,14 @@
+package io.subutai.core.executor.rest;
+
+
+import javax.ws.rs.core.Response;
+
+
+public class RestServiceImpl implements RestService
+{
+    @Override
+    public Response getHosts()
+    {
+        return null;
+    }
+}
