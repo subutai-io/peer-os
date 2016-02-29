7478a115c8d465b5d0f7180515d0ade52f9ff675
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-03-01 00:02:38 +0600

    added publicUrl to System Settings

--- management/server/core/system-manager/system-manager-impl/src/main/java/io/subutai/core/systemmanager/impl/SystemManagerImpl.java
+++ management/server/core/system-manager/system-manager-impl/src/main/java/io/subutai/core/systemmanager/impl/SystemManagerImpl.java
@@ -10,6 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.configuration.ConfigurationException;
 
+import com.google.common.base.Preconditions;
+
 import io.subutai.common.command.CommandException;
 import io.subutai.common.command.CommandResult;
 import io.subutai.common.command.RequestBuilder;
@@ -47,24 +49,27 @@ public class SystemManagerImpl implements SystemManager
     private IdentityManager identityManager;
     private PeerManager peerManager;
 
-    public SystemManagerImpl( /*final String globalKurjunUrls, final int securePortX1, final int securePortX2,
-                              final int securePortX3 */ ) throws ConfigurationException
+
+    public SystemManagerImpl( final String globalKurjunUrls, final int securePortX1, final int securePortX2,
+                              final int securePortX3, final String publicUrl ) throws ConfigurationException
 
     {
-        //        Preconditions.checkNotNull( globalKurjunUrls, "Invalid Global Kurjun URLs could not be null." );
-        //
-        //        String[] urls = new String[] { globalKurjunUrls };
-        //
-        //        if ( urls.length < 1 )
-        //        {
-        //            urls = new String[] { DEFAULT_KURJUN_REPO };
-        //        }
-        //        validateGlobalKurjunUrls( urls );
-        //
-        //        SystemSettings.setGlobalKurjunUrls( urls );
-        //        SystemSettings.setSecurePortX1( securePortX1 );
-        //        SystemSettings.setSecurePortX2( securePortX2 );
-        //        SystemSettings.setSecurePortX3( securePortX3 );
+        Preconditions.checkNotNull( globalKurjunUrls, "Invalid Global Kurjun URLs could not be null." );
+
+        String[] urls = new String[] { globalKurjunUrls };
+
+        if ( urls.length < 1 )
+        {
+            urls = new String[] { DEFAULT_KURJUN_REPO };
+        }
+        validateGlobalKurjunUrls( urls );
+        validatePublicUrl( publicUrl );
+
+        SystemSettings.setGlobalKurjunUrls( urls );
+        SystemSettings.setSecurePortX1( securePortX1 );
+        SystemSettings.setSecurePortX2( securePortX2 );
+        SystemSettings.setSecurePortX3( securePortX3 );
+        SystemSettings.setPublicUrl( publicUrl );
     }
 
 
@@ -151,12 +156,13 @@ public class SystemManagerImpl implements SystemManager
 
 
     @Override
-    public void setNetworkSettings( final String securePortX1, final String securePortX2, final String securePortX3 )
-            throws ConfigurationException
+    public void setNetworkSettings( final String securePortX1, final String securePortX2, final String securePortX3,
+                                    final String publicUrl ) throws ConfigurationException
     {
         SystemSettings.setSecurePortX1( Integer.parseInt( securePortX1 ) );
         SystemSettings.setSecurePortX2( Integer.parseInt( securePortX2 ) );
         SystemSettings.setSecurePortX3( Integer.parseInt( securePortX3 ) );
+        SystemSettings.setPublicUrl( publicUrl );
     }
 
 
@@ -205,13 +211,41 @@ public class SystemManagerImpl implements SystemManager
 
 
     @Override
+    public void setKurjunSettingsUrls( final String[] globalKurjunUrls ) throws ConfigurationException
+    {
+        SystemSettings.setGlobalKurjunUrls( globalKurjunUrls );
+    }
+
+
+    @Override
+    public boolean setKurjunSettingsQuotas( final long publicDiskQuota, final long publicThreshold,
+                                            final long publicTimeFrame, final long trustDiskQuota,
+                                            final long trustThreshold, final long trustTimeFrame )
+    {
+        templateManager.setDiskQuota( publicDiskQuota, "public" );
+        templateManager.setDiskQuota( trustDiskQuota, "trust" );
+
+        KurjunTransferQuota publicTransferQuota =
+                new KurjunTransferQuota( publicThreshold, publicTimeFrame, TimeUnit.HOURS );
+        KurjunTransferQuota trustTransferQuota =
+                new KurjunTransferQuota( trustThreshold, trustTimeFrame, TimeUnit.HOURS );
+
+        boolean isPublicQuotaSaved = templateManager.setTransferQuota( publicTransferQuota, "public" );
+        boolean isTrustQuotaSaved = templateManager.setTransferQuota( trustTransferQuota, "trust" );
+
+        return isPublicQuotaSaved && isTrustQuotaSaved;
+    }
+
+
+    @Override
     public NetworkSettings getNetworkSettings() throws ConfigurationException
     {
         NetworkSettings pojo = new NetworkSettingsPojo();
 
-        pojo.setSecurePortX1( io.subutai.common.settings.SystemSettings.getSecurePortX1() );
-        pojo.setSecurePortX2( io.subutai.common.settings.SystemSettings.getSecurePortX2() );
-        pojo.setSecurePortX3( io.subutai.common.settings.SystemSettings.getSecurePortX3() );
+        pojo.setSecurePortX1( SystemSettings.getSecurePortX1() );
+        pojo.setSecurePortX2( SystemSettings.getSecurePortX2() );
+        pojo.setSecurePortX3( SystemSettings.getSecurePortX3() );
+        pojo.setPublicUrl( SystemSettings.getPublicUrl() );
 
         return pojo;
     }
@@ -229,6 +263,19 @@ public class SystemManagerImpl implements SystemManager
             {
                 throw new ConfigurationException( "Invalid URL: " + url );
             }
+        }
+    }
+
+
+    protected static void validatePublicUrl( String publicUrl ) throws ConfigurationException
+    {
+        try
+        {
+            new URL( publicUrl );
+        }
+        catch ( MalformedURLException e )
+        {
+            throw new ConfigurationException( "Invalid URL: " + publicUrl );
         }
     }
 
