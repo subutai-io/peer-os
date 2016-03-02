77a1d53d585afd849588f34d340609138f03cc0b
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-02 10:33:42 +0600

    added command types to CommandBatch class

--- management/server/subutai-common/src/main/java/io/subutai/common/task/CommandBatch.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/task/CommandBatch.java
@@ -12,22 +12,62 @@ import io.subutai.common.util.JsonUtil;
  */
 public class CommandBatch
 {
+    enum Type
+    {
+        STANDARD, CHAIN, JSON;
+    }
+
+
+    private Type type;
+
+
     List<Command> commands = new ArrayList<>();
+
+
+    public CommandBatch( final Type type )
+    {
+        this.type = type;
+    }
+
+
+    public CommandBatch( final Command command )
+    {
+        this.type = Type.STANDARD;
+        commands.add( command );
+    }
 
 
     public void addCommand( Command comm )
     {
+        if ( this.type == Type.STANDARD && commands.size() > 0 )
+        {
+            throw new IllegalArgumentException( "Standrad command already contain command." );
+        }
+
         commands.add( comm );
     }
 
 
-    public String asJson()
+    @Override
+    public String toString()
     {
-        return JsonUtil.toJson( commands );
+        if ( commands.size() < 1 )
+        {
+            throw new IllegalStateException( "There is no command in command batch." );
+        }
+
+        if ( type == Type.JSON )
+        {
+            return JsonUtil.toJson( commands );
+        }
+        else
+        {
+            return asChain();
+        }
     }
 
 
-    public String asChain()
+    private String asChain()
     {
         StringBuilder sb = new StringBuilder();
         for ( Command command : commands )
