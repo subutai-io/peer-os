0a1e75cbf6b5ef9b1fab20b3cef4b1a1610099ba
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-03-07 20:09:59 +0600

    added sending info about RH to hub after registration

--- management/server/webui-impl/src/main/resources/subutai-app.js
+++ management/server/webui-impl/src/main/resources/subutai-app.js
@@ -102,7 +102,20 @@ function CurrentUserCtrl($location, $rootScope, $http, SweetAlert) {
                     //hubPopupLoadScreen();
                 });
 
-			}).error (function (error) {
+                $http.post( SERVER_URL + 'rest/v1/hub/send-rh-configurations?hubIp=hub.subut.ai', {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+                    .success(function () {
+                        //localStorage.setItem('hubRegistered', true);
+                        //vm.hubStatus = true;
+                        //hubPopupLoadScreen();
+                        //SweetAlert.swal ("Success!", "Your peer was registered to Hub.", "success");
+                    }).error (function (error) {
+                    console.log('hub/register error: ', error);
+                    vm.hubRegisterError = error;
+                    //hubPopupLoadScreen();
+                });
+
+
+            }).error (function (error) {
 				console.log('hub/register error: ', error);
 				vm.hubRegisterError = error;
 				hubPopupLoadScreen();
