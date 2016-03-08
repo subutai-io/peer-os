340f835d4615926575fb0285a0faf8a8d324671e
dilshat <daliev@critical-factor.com>
2016-03-09 00:54:56 +0600

    added custom log event wrapper

--- management/server/core/test/sol-appender/src/main/java/io/subutai/core/test/appender/SolAppender.java
+++ management/server/core/test/sol-appender/src/main/java/io/subutai/core/test/appender/SolAppender.java
@@ -1,6 +1,8 @@
 package io.subutai.core.test.appender;
 
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Deque;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,13 +19,21 @@ public class SolAppender extends AppenderSkeleton
     final static Deque<LoggingEvent> loggingEvents = new LinkedBlockingDeque<>();
 
 
-    public static Set<String> getLoggingEvents()
+    public static Set<SubutaiLogEvent> getLoggingEvents()
     {
-
-        Set<String> events = new HashSet<>();
+        Set<SubutaiLogEvent> events = new HashSet<>();
         for ( LoggingEvent loggingEvent : loggingEvents )
         {
-            events.add( loggingEvent.getRenderedMessage() );
+            String stacktrace = null;
+            if ( loggingEvent.getThrowableInformation() != null
+                    && loggingEvent.getThrowableInformation().getThrowable() != null )
+            {
+                StringWriter errors = new StringWriter();
+                loggingEvent.getThrowableInformation().getThrowable().printStackTrace( new PrintWriter( errors ) );
+                stacktrace = errors.toString();
+            }
+            events.add( new SubutaiLogEvent( loggingEvent.getTimeStamp(), loggingEvent.getMessage(),
+                    loggingEvent.getLoggerName(), loggingEvent.getRenderedMessage(), stacktrace ) );
         }
         loggingEvents.clear();
         return events;
@@ -53,5 +63,40 @@ public class SolAppender extends AppenderSkeleton
     public boolean requiresLayout()
     {
         return false;
+    }
+
+
+    public static class SubutaiLogEvent
+    {
+        final long timeStamp;
+        final Object message;
+        final String loggerName;
+        final String renderedMessage;
+        final String fullInfo;
+
+
+        public SubutaiLogEvent( final long timeStamp, final Object message, final String loggerName,
+                                final String renderedMessage, final String fullInfo )
+        {
+            this.timeStamp = timeStamp;
+            this.message = message;
+            this.loggerName = loggerName;
+            this.renderedMessage = renderedMessage;
+            this.fullInfo = fullInfo;
+        }
+
+
+        @Override
+        public String toString()
+        {
+            final StringBuilder sb = new StringBuilder( "SubutaiLogEvent{" );
+            sb.append( "timeStamp=" ).append( timeStamp );
+            sb.append( ", message=" ).append( message );
+            sb.append( ", loggerName='" ).append( loggerName ).append( '\'' );
+            sb.append( ", renderedMessage='" ).append( renderedMessage ).append( '\'' );
+            sb.append( ", fullInfo='" ).append( fullInfo ).append( '\'' );
+            sb.append( '}' );
+            return sb.toString();
+        }
     }
 }
