7d5de02cb4ac62aa5980847a06a603bd7c1e0f83
dilshat <daliev@critical-factor.com>
2016-03-10 13:42:19 +0600

    Merge branch 'remove-subutai-mng' of https://github.com/subutai-io/base into remove-subutai-mng
    Conflicts:
    	management/server/server-karaf/src/main/assembly/etc/subutaisystem.cfg

--- management/shell-snippets/clear_up_snappy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#sudo journalctl -u subutai-mng_subutai-mng-service_IHPIYGeYeGLB.service
-
-sudo snappy remove subutai-mng
-sudo rm -rf /apps/subutai-mng/
-sudo rm -rf /var/lib/apps/subutai-mng/
