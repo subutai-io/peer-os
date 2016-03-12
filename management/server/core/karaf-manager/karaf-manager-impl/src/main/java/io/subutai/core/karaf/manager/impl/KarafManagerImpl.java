b5574470a2d05a4a1053b9a37ee98024739ea01e
Nurkaly Isakov <nisakov@critical-factor.com>
2016-03-12 23:13:55 +0600

    Created new karaf-Manager module to manage/execute Karaf commands (shell commands). Removed feature WEBCONSOLE from Karaf. Aries-Blueprint-Authz module moved under Identity-Manager_Parent module.

---
+++ management/server/core/karaf-manager/karaf-manager-impl/src/main/java/io/subutai/core/karaf/manager/impl/KarafManagerImpl.java
@@ -0,0 +1,126 @@
+package io.subutai.core.karaf.manager.impl;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import org.apache.felix.service.command.CommandProcessor;
+import org.apache.felix.service.command.CommandSession;
+import io.subutai.common.mdc.SubutaiExecutors;
+import io.subutai.core.karaf.manager.api.KarafManager;
+
+
+/**
+ * Manages/Executes Karaf Commands
+ */
+public class KarafManagerImpl implements KarafManager
+{
+
+    private CommandProcessor commandProcessor = null;
+    protected ExecutorService executor = SubutaiExecutors.newCachedThreadPool();
+
+
+    @Override
+    public String executeShellCommand( final String commandStr )
+    {
+        String response = "";
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream( byteArrayOutputStream );
+        final CommandSession commandSession = commandProcessor.createSession( System.in, printStream, System.err );
+
+
+        //************************************************
+        FutureTask<String> commandFuture = new FutureTask<String>( new Callable<String>()
+        {
+            public String call()
+            {
+                try
+                {
+                    System.err.println( commandStr );
+                    commandSession.execute( commandStr );
+                }
+                catch ( Exception e )
+                {
+                }
+
+                printStream.flush();
+                return byteArrayOutputStream.toString();
+            }
+        } );
+        //************************************************
+
+        try
+        {
+            executor.submit( commandFuture );
+
+            do
+            {
+                response += commandFuture.get(2,TimeUnit.SECONDS);
+            }
+            while(!commandFuture.isDone());
+        }
+        catch ( Exception e )
+        {
+            response += "Command Timout: ";
+
+            if(byteArrayOutputStream != null)
+            {
+                response += byteArrayOutputStream.toString();
+            }
+        }
+
+        return response;
+    }
+
+
+
+    /* *************
+    */
+    public String executeJMXCommand( final String commandStr)
+    {
+        String result= "No Result";
+
+        try
+        {
+            HashMap   environment = new HashMap();
+            String[]  credentials = new String[] {"admin", "secret"};
+            environment.put (JMXConnector.CREDENTIALS, credentials);
+
+            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:1099/karaf-root");
+            JMXConnector connector = null;
+            connector = JMXConnectorFactory.connect( url, environment );
+            MBeanServerConnection mbeanServer = connector.getMBeanServerConnection();
+            ObjectName systemMBean = new ObjectName("org.apache.karaf:type=bundle,name=root");
+            mbeanServer.invoke(systemMBean,commandStr, null, null);
+            connector.close();
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+
+    public CommandProcessor getCommandProcessor()
+    {
+        return commandProcessor;
+    }
+
+
+    public void setCommandProcessor( final CommandProcessor commandProcessor )
+    {
+        this.commandProcessor = commandProcessor;
+    }
+}
