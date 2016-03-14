b86fb27072338202cf5d757082296ae08caf36c0
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-14 19:50:57 +0600

    improved put alert CLI

--- management/server/core/local-peer/local-peer-cli/src/main/java/io/subutai/core/localpeer/cli/PutAlertCommand.java
+++ management/server/core/local-peer/local-peer-cli/src/main/java/io/subutai/core/localpeer/cli/PutAlertCommand.java
@@ -4,6 +4,7 @@ package io.subutai.core.localpeer.cli;
 import java.util.Date;
 
 import org.apache.commons.lang.time.DateUtils;
+import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 
 import io.subutai.common.host.HostId;
@@ -11,6 +12,8 @@ import io.subutai.common.metric.ExceededQuota;
 import io.subutai.common.metric.QuotaAlert;
 import io.subutai.common.metric.QuotaAlertValue;
 import io.subutai.common.peer.AlertEvent;
+import io.subutai.common.peer.ContainerHost;
+import io.subutai.common.peer.ContainerId;
 import io.subutai.common.peer.LocalPeer;
 import io.subutai.common.resource.ByteUnit;
 import io.subutai.common.resource.ByteValueResource;
@@ -24,6 +27,9 @@ public class PutAlertCommand extends SubutaiShellCommandSupport
 
     private LocalPeer localPeer;
 
+    @Argument( index = 0, name = "container ID", multiValued = false, required = true, description = "Container ID" )
+    private String containerId;
+
 
     public PutAlertCommand( final LocalPeer localPeer )
     {
@@ -34,13 +40,17 @@ public class PutAlertCommand extends SubutaiShellCommandSupport
     @Override
     protected Object doExecute() throws Exception
     {
+
+        final ContainerHost host = localPeer.findContainerById( new ContainerId( containerId ) );
+
+
         QuotaAlertValue alertValue = new QuotaAlertValue(
-                new ExceededQuota( new HostId( "hostId" ), ContainerResourceType.RAM,
+                new ExceededQuota( new HostId( host.getId() ), ContainerResourceType.RAM,
                         new ByteValueResource( ByteValueResource.toBytes( "1.1", ByteUnit.MB ) ),
                         new ByteValueResource( ByteValueResource.toBytes( "2.2", ByteUnit.MB ) ) ) );
         QuotaAlert value = new QuotaAlert( alertValue, System.currentTimeMillis() );
-        AlertEvent alertEvent = new AlertEvent( localPeer.getId(), "environmentId", "containerId", "master", value,
-                DateUtils.addMinutes( new Date(), 1 ).getTime() );
+        AlertEvent alertEvent = new AlertEvent( localPeer.getId(), host.getEnvironmentId().getId(), host.getId(),
+                host.getTemplateName(), value, DateUtils.addMinutes( new Date(), 1 ).getTime() );
         localPeer.alert( alertEvent );
         return null;
     }
