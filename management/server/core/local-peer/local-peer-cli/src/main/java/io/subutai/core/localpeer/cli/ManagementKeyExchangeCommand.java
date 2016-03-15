869a4f9f4b6eb209ec7678faffacb60f2066cb44
dilshat <daliev@critical-factor.com>
2016-03-15 19:18:04 +0600

    added method to perform key exchange to LocalPeer

--- management/server/core/local-peer/local-peer-cli/src/main/java/io/subutai/core/localpeer/cli/ManagementKeyExchangeCommand.java
+++ management/server/core/local-peer/local-peer-cli/src/main/java/io/subutai/core/localpeer/cli/ManagementKeyExchangeCommand.java
@@ -3,11 +3,8 @@ package io.subutai.core.localpeer.cli;
 
 import org.apache.karaf.shell.commands.Command;
 
-import io.subutai.common.command.CommandResult;
-import io.subutai.common.command.RequestBuilder;
 import io.subutai.common.peer.LocalPeer;
 import io.subutai.core.identity.rbac.cli.SubutaiShellCommandSupport;
-import io.subutai.core.registration.api.RegistrationManager;
 
 
 @Command( scope = "localpeer", name = "management-key-exchange" )
@@ -15,29 +12,20 @@ public class ManagementKeyExchangeCommand extends SubutaiShellCommandSupport
 {
 
     private LocalPeer localPeer;
-    private RegistrationManager registrationManager;
 
 
-    public ManagementKeyExchangeCommand( final LocalPeer localPeer, final RegistrationManager registrationManager )
+    public ManagementKeyExchangeCommand( final LocalPeer localPeer )
     {
         this.localPeer = localPeer;
-        this.registrationManager = registrationManager;
     }
 
 
     @Override
     protected Object doExecute() throws Exception
     {
+        localPeer.exchangeMhKeysWithRH();
 
-
-        String token = registrationManager.generateContainerTTLToken( 30 * 1000L ).getToken();
-
-        final RequestBuilder requestBuilder =
-                new RequestBuilder( String.format( "subutai import management -t %s", token ) );
-
-        CommandResult commandResult = localPeer.getManagementHost().execute( requestBuilder );
-
-        System.out.println( commandResult.toString() );
+        System.out.println( "Keys are exchanged successfully" );
 
         return null;
     }
