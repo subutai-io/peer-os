dd638465aec9371698335f7c98ef7c0e4f42a96f
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-17 20:03:16 +0600

    improved remote container state getter

--- management/server/core/environment-manager/environment-manager-cli/src/main/java/io/subutai/core/environment/cli/GetContainerQuotaCommand.java
+++ management/server/core/environment-manager/environment-manager-cli/src/main/java/io/subutai/core/environment/cli/GetContainerQuotaCommand.java
@@ -10,22 +10,19 @@ import io.subutai.common.quota.ContainerQuota;
 import io.subutai.common.quota.ContainerResource;
 import io.subutai.core.environment.api.EnvironmentManager;
 import io.subutai.core.identity.rbac.cli.SubutaiShellCommandSupport;
-import io.subutai.core.peer.api.PeerManager;
 
 
 @Command( scope = "environment", name = "get-quota", description = "gets quota information from peer for container" )
 public class GetContainerQuotaCommand extends SubutaiShellCommandSupport
 {
-    @Argument( index = 0, name = "peer id", multiValued = false, required = true, description = "Id of target peer" )
-    private String peerId;
 
-    @Argument( index = 1, name = "environment id", multiValued = false, required = true, description = "Id of "
+    @Argument( index = 0, name = "environment id", multiValued = false, required = true, description = "Id of "
             + "environment" )
     protected String environmentId;
 
-    @Argument( index = 2, name = "container name", multiValued = false, required = true, description = "container "
-            + "name" )
-    protected String containerName;
+    @Argument( index = 1, name = "container id", multiValued = false, required = true, description = "container "
+            + "id" )
+    protected String containerId;
 
     private EnvironmentManager environmentManager;
 
@@ -41,10 +38,10 @@ public class GetContainerQuotaCommand extends SubutaiShellCommandSupport
     {
         Environment environment = environmentManager.loadEnvironment( environmentId );
 
-        ContainerHost targetContainer = environment.getContainerHostByHostname( containerName );
+        ContainerHost targetContainer = environment.getContainerHostById( containerId );
         if ( targetContainer == null )
         {
-            System.out.println( "Couldn't get container host by name: " + containerName );
+            System.out.println( "Couldn't get container host by name: " + containerId );
         }
         else
         {
