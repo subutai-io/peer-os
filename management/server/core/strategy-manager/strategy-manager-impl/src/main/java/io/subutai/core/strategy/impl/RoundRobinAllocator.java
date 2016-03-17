10042e2d67ed01b94920adfe5ba00ff8d8419e3e
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-03-17 15:34:09 +0600

    added round robin container placement strategy

---
+++ management/server/core/strategy-manager/strategy-manager-impl/src/main/java/io/subutai/core/strategy/impl/RoundRobinAllocator.java
@@ -0,0 +1,62 @@
+package io.subutai.core.strategy.impl;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import com.google.common.collect.Iterators;
+
+import io.subutai.common.peer.ContainerSize;
+import io.subutai.common.quota.ContainerQuota;
+import io.subutai.common.resource.HostResources;
+import io.subutai.common.resource.PeerResources;
+
+
+/**
+ * Round robin allocator class
+ */
+public class RoundRobinAllocator extends PeerResources
+{
+    private List<AllocatedContainer> containers = new ArrayList<>();
+
+    private Iterator<HostResources> iterator;
+
+
+    public RoundRobinAllocator( final PeerResources peerResources )
+    {
+        super( peerResources.getPeerId(), peerResources.getEnvironmentLimit(), peerResources.getContainerLimit(),
+                peerResources.getNetworkLimit(), peerResources.getHostResources() );
+
+        iterator = Iterators.cycle( getHostResources() );
+    }
+
+
+    public boolean allocate( final String containerName, final String templateName, final ContainerSize size,
+                             ContainerQuota containerQuota )
+    {
+        int counter = 0;
+
+        while ( counter < getHostResources().size() )
+        {
+            HostResources hostResources = iterator.next();
+            if ( hostResources.allocate( containerQuota ) )
+            {
+                AllocatedContainer container = new AllocatedContainer( containerName, templateName, size, getPeerId(),
+                        hostResources.getHostId() );
+                containers.add( container );
+                return true;
+            }
+            counter++;
+        }
+
+        return false;
+    }
+
+    public List<AllocatedContainer> getContainers()
+    {
+        return containers;
+    }
+}
