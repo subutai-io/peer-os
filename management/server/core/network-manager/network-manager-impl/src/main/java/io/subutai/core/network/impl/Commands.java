47d2e4cae5d528ace2cac65db950ef8ad1e44b70
dilshat <daliev@critical-factor.com>
2016-03-21 13:07:39 +0600

    changed timeout for setup p2p command to 90 sec

--- management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/Commands.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/Commands.java
@@ -67,7 +67,7 @@ public class Commands
         return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING ).withCmdArgs(
                 Lists.newArrayList( "p2p", "-c", interfaceName, communityName, secretKey,
                         String.valueOf( secretKeyTtlSec ), Strings.isNullOrEmpty( localIp ) ? "" : localIp ) )
-                                                                    .withTimeout( 180 );
+                                                                    .withTimeout( 90 );
     }
 
 
