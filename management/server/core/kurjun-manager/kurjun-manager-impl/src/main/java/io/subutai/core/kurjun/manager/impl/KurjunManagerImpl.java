3816421baac1bd7ad62d8e69f1cfe42080466bdf
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-03-24 13:37:40 +0600

    Fixed loading properties file and and injecting in context.xml

--- management/server/core/kurjun-manager/kurjun-manager-impl/src/main/java/io/subutai/core/kurjun/manager/impl/KurjunManagerImpl.java
+++ management/server/core/kurjun-manager/kurjun-manager-impl/src/main/java/io/subutai/core/kurjun/manager/impl/KurjunManagerImpl.java
@@ -1,8 +1,17 @@
 package io.subutai.core.kurjun.manager.impl;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.ws.rs.core.Response;
+
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.cxf.jaxrs.client.WebClient;
 
 import com.google.common.base.Strings;
@@ -16,7 +25,6 @@ import io.subutai.core.kurjun.manager.api.model.Kurjun;
 import io.subutai.core.kurjun.manager.impl.dao.KurjunDataService;
 import io.subutai.core.kurjun.manager.impl.model.KurjunEntity;
 import io.subutai.core.kurjun.manager.impl.model.KurjunType;
-import io.subutai.core.kurjun.manager.impl.utils.PropertyUtils;
 import io.subutai.core.security.api.SecurityManager;
 
 
@@ -25,12 +33,12 @@ import io.subutai.core.security.api.SecurityManager;
  */
 public class KurjunManagerImpl implements KurjunManager
 {
-
     //**********************************
-    private IdentityManager identityManager = null;
-    private SecurityManager securityManager = null;
+    private IdentityManager identityManager;
+    private SecurityManager securityManager;
     private DaoManager daoManager;
     private KurjunDataService dataService;
+    private static Properties properties;
     //**********************************
 
 
@@ -41,47 +49,51 @@ public class KurjunManagerImpl implements KurjunManager
 
 
     //****************************************
-    public void KurjunManagerImpl( IdentityManager identityManager, SecurityManager securityManager,
-                                   DaoManager daoManager )
-    {
-        this.identityManager = identityManager;
-        this.securityManager = securityManager;
-        this.daoManager = daoManager;
-
-        dataService = new KurjunDataService(daoManager);
-    }
+    //    public void KurjunManagerImpl( /*IdentityManager identityManager, SecurityManager securityManager,*/
+    //                                   DaoManager daoManager )
+    //    {
+    //        this.identityManager = identityManager;
+    //        this.securityManager = securityManager;
+    //        this.daoManager = daoManager;
+    //
+    //        dataService = new KurjunDataService( daoManager );
+    //    }
 
 
     //****************************************
     public void init()
     {
-        String fingerprint = identityManager.getActiveUser().getFingerprint();
+        String fingerprint = securityManager.getKeyManager().getFingerprint( null );
+        properties = loadProperties();
 
-        if(Strings.isNullOrEmpty( getUser( KurjunType.Local.getId(), fingerprint ) ))
+        dataService = new KurjunDataService( daoManager );
+
+
+        if ( Strings.isNullOrEmpty( getUser( KurjunType.Local.getId(), fingerprint ) ) )
         {
-            registerUser( KurjunType.Local.getId() ,fingerprint );
+            registerUser( KurjunType.Local.getId(), fingerprint );
         }
         else
         {
-            authorizeUser( KurjunType.Local.getId(),fingerprint );
+            authorizeUser( KurjunType.Local.getId(), fingerprint );
         }
     }
 
 
     //****************************************
-    private String getKurjunUrl(int kurjunType, String uri )
+    private String getKurjunUrl( int kurjunType, String uri )
     {
-        if(kurjunType == KurjunType.Local.getId())
+        if ( kurjunType == KurjunType.Local.getId() )
         {
-            return localKurjunURL+uri;
+            return localKurjunURL + uri;
         }
-        else if(kurjunType == KurjunType.Global.getId())
+        else if ( kurjunType == KurjunType.Global.getId() )
         {
-            return globalKurjunURL+uri;
+            return globalKurjunURL + uri;
         }
         else
         {
-            return globalKurjunURL+uri;
+            return globalKurjunURL + uri;
         }
     }
 
@@ -93,16 +105,18 @@ public class KurjunManagerImpl implements KurjunManager
         //*****************************************
         String authId = "";
 
-        if(Strings.isNullOrEmpty( getUser( kurjunType, fingerprint )))
+        if ( Strings.isNullOrEmpty( getUser( kurjunType, fingerprint ) ) )
         {
-            String url = getKurjunUrl(kurjunType, PropertyUtils.getValue( "url.identity.user.add" ));
+            String url = getKurjunUrl( kurjunType, properties.getProperty( "url.identity.user.add" ) );
             WebClient client = RestUtil.createTrustedWebClient( url );
+            Response response = client.get();
+
             //TODO get authID from client
             //authId = client Output;
         }
 
 
-        if(dataService.getKurjunData( fingerprint ) == null)
+        if ( dataService.getKurjunData( fingerprint ) == null )
         {
 
             //************* Sign *********************
@@ -125,7 +139,7 @@ public class KurjunManagerImpl implements KurjunManager
     @Override
     public String authorizeUser( int kurjunType, String fingerprint )
     {
-        String url = getKurjunUrl(kurjunType, PropertyUtils.getValue( "url.identity.user.get" ));
+        String url = getKurjunUrl( kurjunType, properties.getProperty( "url.identity.user.get" ) );
         WebClient client = RestUtil.createTrustedWebClient( url );
 
         return null;
@@ -145,7 +159,7 @@ public class KurjunManagerImpl implements KurjunManager
     @Override
     public String getSystemOwner( int kurjunType )
     {
-        String url = getKurjunUrl(kurjunType, PropertyUtils.getValue( "url.identity.user.auth" ));
+        String url = getKurjunUrl( kurjunType, properties.getProperty( "url.identity.user.auth" ) );
         WebClient client = RestUtil.createTrustedWebClient( url );
 
         return null;
@@ -156,11 +170,46 @@ public class KurjunManagerImpl implements KurjunManager
     @Override
     public String getUser( int kurjunType, String fingerprint )
     {
-        String url = getKurjunUrl(kurjunType, PropertyUtils.getValue( "url.identity.user.get" ));
+        String url = getKurjunUrl( kurjunType, properties.getProperty( "url.identity.user.get" ) );
         WebClient client = RestUtil.createTrustedWebClient( url );
 
 
         return null;
     }
 
+
+    public void setIdentityManager( final IdentityManager identityManager )
+    {
+        this.identityManager = identityManager;
+    }
+
+
+    public void setSecurityManager( final SecurityManager securityManager )
+    {
+        this.securityManager = securityManager;
+    }
+
+
+    private Properties loadProperties()
+    {
+        Properties configProp = new Properties();
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream( "rest.properties" );
+        System.out.println( "Read all properties from file" );
+        try
+        {
+            configProp.load( in );
+            return configProp;
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public void setDaoManager( final DaoManager daoManager )
+    {
+        this.daoManager = daoManager;
+    }
 }
