07be700ae96bf47484486413d9611ed6c524cd9c
Askat Asanaliev <aasanaliev@optimal-dynamics.com>
2016-03-26 04:27:24 +0600

    Hub env post build: setting EnvironmentManager into proxies.

--- management/server/core/environment-manager/environment-manager-impl/src/main/java/io/subutai/core/environment/impl/adapter/ProxyEnvironment.java
+++ management/server/core/environment-manager/environment-manager-impl/src/main/java/io/subutai/core/environment/impl/adapter/ProxyEnvironment.java
@@ -1,11 +1,15 @@
 package io.subutai.core.environment.impl.adapter;
 
 
+import io.subutai.core.environment.impl.EnvironmentManagerImpl;
 import io.subutai.core.environment.impl.entity.EnvironmentImpl;
 
 
 class ProxyEnvironment extends EnvironmentImpl
 {
+    // NOTE: Using environmentManager from EnvironmentImpl gives side effects. For example, empty container list.
+    private EnvironmentManagerImpl environmentManager;
+
     ProxyEnvironment( String name, String subnetCidr, String sshKey, Long userId, String peerId )
     {
         super( name, subnetCidr, sshKey, userId, peerId );
@@ -17,4 +21,10 @@ class ProxyEnvironment extends EnvironmentImpl
         environmentId = id;
     }
 
+
+    @Override
+    public void setEnvironmentManager( final EnvironmentManagerImpl environmentManager )
+    {
+        this.environmentManager = environmentManager;
+    }
 }
