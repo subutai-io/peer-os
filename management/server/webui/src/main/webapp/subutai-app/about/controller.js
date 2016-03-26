f97d01764a362f06be51e26389e3535d76385743
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-03-26 20:41:23 +0600

    plugin version migrate to about

--- management/server/webui/src/main/webapp/subutai-app/about/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/about/controller.js
@@ -3,14 +3,20 @@
 angular.module('subutai.about.controller', [])
 	.controller('AboutCtrl', AboutCtrl)
 
-AboutCtrl.$inject = ['$http', 'ngDialog', 'SweetAlert', '$scope'];
+AboutCtrl.$inject = ['$http', 'ngDialog', 'SweetAlert', '$scope', '$timeout'];
 
 
-function AboutCtrl ($http, ngDialog, SweetAlert, $scope) {
+function AboutCtrl ($http, ngDialog, SweetAlert, $scope, $timeout) {
 	var vm = this;
 	vm.info = {};
 	$http.get (SERVER_URL + "rest/v1/system/about").success (function (data) {
 		vm.info = data;
 		console.log (data);
 	});
+
+	vm.hasPGPplugin = false;
+	$timeout(function() {
+		vm.hasPGPplugin = hasPGPplugin();
+		console.log(vm.hasPGPplugin);
+	}, 2000);
 }
