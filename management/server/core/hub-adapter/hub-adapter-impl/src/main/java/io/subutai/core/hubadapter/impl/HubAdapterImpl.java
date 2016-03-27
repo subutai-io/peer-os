43e6459b38e442d4bfe51c5a4fc89a95cd54e6a8
Askat Asanaliev <aasanaliev@optimal-dynamics.com>
2016-03-27 12:59:23 +0600

    Hub env post build. Added hub-adapter module.

---
+++ management/server/core/hub-adapter/hub-adapter-impl/src/main/java/io/subutai/core/hubadapter/impl/HubAdapterImpl.java
@@ -0,0 +1,65 @@
+package io.subutai.core.hubadapter.impl;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.subutai.core.hubadapter.api.HubAdapter;
+import io.subutai.core.peer.api.PeerManager;
+import io.subutai.core.security.api.SecurityManager;
+
+
+public class HubAdapterImpl implements HubAdapter
+{
+    private final Logger log = LoggerFactory.getLogger( getClass() );
+
+    private SecurityManager securityManager;
+
+    private PeerManager peerManager;
+
+    private ConfigManager configManager;
+
+    public HubAdapterImpl( SecurityManager securityManager, PeerManager peerManager )
+    {
+        this.securityManager = securityManager;
+        this.peerManager = peerManager;
+
+        try
+        {
+            configManager = new ConfigManager( securityManager, peerManager );
+        }
+        catch ( Exception e )
+        {
+            log.error( "Error to init: ", e );
+        }
+    }
+
+
+    @Override
+    public String sayHello()
+    {
+
+
+        return "DONE";
+    }
+
+
+//    @Override
+//    public String sayHello()
+//    {
+//        try
+//        {
+//            WebClient client = configManager.getTrustedWebClientWithAuth( "/rest/v1.1/marketplace/products", "hub.subut.ai" );
+//
+//            Response r = client.get();
+//
+//            log.debug( "status: {}", r.getStatus() );
+//        }
+//        catch ( Exception e )
+//        {
+//            log.error( "Error to test: ", e );
+//        }
+//
+//        return "done";
+//    }
+}
