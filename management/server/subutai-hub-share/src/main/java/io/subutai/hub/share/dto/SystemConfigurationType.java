d3380356d71697fa9731faedf82c2162c6b0c029
Askat Asanaliev <aasanaliev@optimal-dynamics.com>
2016-03-27 19:53:40 +0600

    Added subutai-hub-share as a module (instead of embedded jar).

---
+++ management/server/subutai-hub-share/src/main/java/io/subutai/hub/share/dto/SystemConfigurationType.java
@@ -0,0 +1,10 @@
+package io.subutai.hub.share.dto;
+
+
+/**
+ * Created by ${Zubaidullo} on 2/18/16.
+ */
+public enum SystemConfigurationType
+{
+    SUBUTAI_SOCIAL, SUBUTAI_SOCIAL_PLUGIN, SUBUTAI_SOCIAL_HUB
+}
---
+++ management/server/subutai-hub-share/src/main/java/io/subutai/hub/share/dto/product/ProductDto.java
@@ -0,0 +1,146 @@
+package io.subutai.hub.share.dto.product;
+
+
+import java.util.Date;
+import java.util.Set;
+
+//Version 1.1
+public class ProductDto
+{
+    public enum Type
+    {
+        PLUGIN
+    }
+    private String id;
+
+    private String name;
+
+    private Type type;
+
+    private Date createDate;
+
+    private String description;
+
+    private String ownerId;
+
+    private String version;
+
+    private Set<String> metadata;
+
+    private Set<String> dependencies;
+
+    public ProductDto()
+    {
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+
+    public void setId( final String id )
+    {
+        this.id = id;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+    public void setName( final String name )
+    {
+        this.name = name;
+    }
+
+
+    public Type getType()
+    {
+        return type;
+    }
+
+
+    public void setType( final Type type )
+    {
+        this.type = type;
+    }
+
+
+    public Date getCreateDate()
+    {
+        return createDate;
+    }
+
+
+    public void setCreateDate( final Date createDate )
+    {
+        this.createDate = createDate;
+    }
+
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+
+    public void setDescription( final String description )
+    {
+        this.description = description;
+    }
+
+
+    public String getOwnerId()
+    {
+        return ownerId;
+    }
+
+
+    public void setOwnerId( final String ownerId )
+    {
+        this.ownerId = ownerId;
+    }
+
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+
+    public void setVersion( final String version )
+    {
+        this.version = version;
+    }
+
+
+    public Set<String> getMetadata()
+    {
+        return metadata;
+    }
+
+
+    public void setMetadata( final Set<String> metadata )
+    {
+        this.metadata = metadata;
+    }
+
+    public void addDependency( final String dependencyId )
+    {
+        this.dependencies.add( dependencyId );
+    }
+
+    public Set<String> getDependencies()
+    {
+        return dependencies;
+    }
+
+
+    public void setDependencies( final Set<String> dependencies )
+    {
+        this.dependencies = dependencies;
+    }
+}
