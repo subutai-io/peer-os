f3c71191a95944331f846a9b2d11bc8df19f0af7
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-03-31 23:21:07 +0600

    SS kurjun fix

--- management/server/webui/src/main/webapp/subutai-app/kurjun/service.js
+++ management/server/webui/src/main/webapp/subutai-app/kurjun/service.js
@@ -8,7 +8,8 @@ kurjunService.$inject = ['$http', 'Upload', 'SettingsKurjunSrv'];
 
 function kurjunService($http, Upload, SettingsKurjunSrv) {
 
-	var GLOBAL_KURJUN_URL = "https://peer.noip.me:8339/kurjun";
+	//var GLOBAL_KURJUN_URL = "https://peer.noip.me:8339/kurjun";
+	console.log(GLOBAL_KURJUN_URL);
 	var BASE_URL = GLOBAL_KURJUN_URL + "/rest/";
 	var TEMPLATE_URL = BASE_URL + "template/";
 	var REPOSITORY_URL = BASE_URL + "repository/";
@@ -16,17 +17,17 @@ function kurjunService($http, Upload, SettingsKurjunSrv) {
 	var RAW_URL = BASE_URL + "file/";
 
 
-	var promise = SettingsKurjunSrv.getConfig().success (function (data) {
+	/*var promise = SettingsKurjunSrv.getConfig().success (function (data) {
 		GLOBAL_KURJUN_URL = data.globalKurjunUrls[0];
 		BASE_URL = GLOBAL_KURJUN_URL + "/";
 		TEMPLATE_URL = BASE_URL + "template/";
 		REPOSITORY_URL = BASE_URL + "repository/";
 		DEB_URL = BASE_URL + "deb/";
 		RAW_URL = BASE_URL + "file/";
-	});
+	});*/
 
 	var kurjunService = {
-		promise:promise,
+		//promise:promise,
 		getRepositories: getRepositories,
 		getTemplates: getTemplates,
 		getAPTList: getAPTList,
