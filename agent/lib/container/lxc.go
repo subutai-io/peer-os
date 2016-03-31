ae205131a68b23503e2b8aac50fbf1d44c20005e
tzununbekov <tzununbekov@critical-factor.com>
2016-03-31 19:32:52 +0600

    Apt port changed in containers

--- agent/lib/container/lxc.go
+++ agent/lib/container/lxc.go
@@ -99,7 +99,7 @@ func SetApt(name string) {
 
 	// kurjun := []byte("deb [arch=amd64,all] http://" + config.Management.Host + ":8330/rest/kurjun/vapt trusty main contrib\n" +
 	// 	"deb [arch=amd64,all] http://" + config.Management.Cdn + ":8330/kurjun/rest/deb trusty main contrib\n")
-	kurjun := []byte("deb [arch=amd64,all] http://" + config.Management.Cdn + ":8330/kurjun/rest/deb trusty main contrib\n")
+	kurjun := []byte("deb [arch=amd64,all] http://" + config.Management.Cdn + ":8080/kurjun/rest/deb trusty main contrib\n")
 	log.Check(log.DebugLevel, "Writing apt source kurjun list",
 		ioutil.WriteFile(config.Agent.LxcPrefix+name+"/rootfs/etc/apt/sources.list.d/subutai-repo.list", kurjun, 0644))
 }
