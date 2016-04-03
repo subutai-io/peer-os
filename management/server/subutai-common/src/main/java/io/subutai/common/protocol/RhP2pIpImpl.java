e9ecae68fe189c44fccd213048144fb6a4da5876
dilshat <daliev@critical-factor.com>
2016-04-03 12:43:31 +0600

    bug fix

--- management/server/subutai-common/src/main/java/io/subutai/common/protocol/RhP2pIpImpl.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/protocol/RhP2pIpImpl.java
@@ -33,4 +33,40 @@ public class RhP2pIpImpl implements RhP2pIp
     {
         return p2pIp;
     }
+
+
+    @Override
+    public boolean equals( final Object o )
+    {
+        if ( this == o )
+        {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() )
+        {
+            return false;
+        }
+
+        final RhP2pIpImpl rhP2pIp = ( RhP2pIpImpl ) o;
+
+        if ( p2pIp != null ? !p2pIp.equals( rhP2pIp.p2pIp ) : rhP2pIp.p2pIp != null )
+        {
+            return false;
+        }
+        if ( rhId != null ? !rhId.equals( rhP2pIp.rhId ) : rhP2pIp.rhId != null )
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        int result = rhId != null ? rhId.hashCode() : 0;
+        result = 31 * result + ( p2pIp != null ? p2pIp.hashCode() : 0 );
+        return result;
+    }
 }
