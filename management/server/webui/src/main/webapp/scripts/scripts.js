82867a336d8acb0af573631c5317618908cb1f8c
Ernist Bekboev <ebekboev@optimal-dynamics.com>
2016-04-05 20:02:13 +0600

    #417 - add ssh button for containers

--- management/server/webui/src/main/webapp/scripts/scripts.js
+++ management/server/webui/src/main/webapp/scripts/scripts.js
@@ -16,5 +16,34 @@ $('body').on('click', '.js-hide-resources', function(){
 	return false;
 });
 
+
+$('body').click(function (event) {
+	if (
+		!$(event.target).hasClass('ssh-info-button') &&
+		!$(event.target).parent().hasClass("ssh-plugin-info-tooltip") &&
+		!$(event.target).hasClass("ssh-plugin-info-tooltip")
+	) {
+		$('.ssh-plugin-info-tooltip').hide();
+	}
+});
+
+$(document).keyup(function(e) {
+	if (e.keyCode == 27) {
+		$('.ssh-plugin-info-tooltip').hide();
+	}
+});
+
+$(document).on('click', '.ssh-info-button', function (event) {
+	if($(event.target).hasClass("ssh-info-button")) {
+		var status = $(event.target).find(".ssh-plugin-info-tooltip").css('display') == "none";
+		if(status) {
+			$('.ssh-plugin-info-tooltip').hide();
+			$(event.target).find('.ssh-plugin-info-tooltip').toggle();
+		} else {
+			$(event.target).find('.ssh-plugin-info-tooltip').toggle();
+		}
+	}
+});
+
 var UPDATE_NIGHTLY_BUILD_STATUS;
 
