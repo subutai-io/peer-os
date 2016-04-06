4edb64af7c69d3140e46d05bf0a6e84a27d78e91
dilshat <daliev@critical-factor.com>
2016-04-06 18:19:21 +0600

    refactored env mgr

--- management/server/core/environment-manager/environment-manager-impl/src/main/java/io/subutai/core/environment/impl/workflow/modification/steps/DestroyContainersStep.java
+++ management/server/core/environment-manager/environment-manager-impl/src/main/java/io/subutai/core/environment/impl/workflow/modification/steps/DestroyContainersStep.java
@@ -3,15 +3,11 @@ package io.subutai.core.environment.impl.workflow.modification.steps;
 
 import java.util.List;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
-
 import io.subutai.common.peer.ContainerHost;
-import io.subutai.common.peer.HostNotFoundException;
-import io.subutai.common.peer.PeerException;
 import io.subutai.common.tracker.TrackerOperation;
 import io.subutai.core.environment.impl.EnvironmentManagerImpl;
-import io.subutai.core.environment.impl.entity.EnvironmentContainerImpl;
 import io.subutai.core.environment.impl.entity.EnvironmentImpl;
+import io.subutai.core.environment.impl.workflow.destruction.steps.helpers.ContainerDestroyTask;
 import io.subutai.core.environment.impl.workflow.util.TaskUtil;
 
 
@@ -67,45 +63,6 @@ public class DestroyContainersStep
                         String.format( "Failed to destroy container %s. Reason: %s", containerHost.getHostname(),
                                 destroyResult.getFailureReason() ) );
             }
-        }
-    }
-
-
-    private static class ContainerDestroyTask extends TaskUtil.Task<Object>
-    {
-
-        private final ContainerHost containerHost;
-
-
-        public ContainerDestroyTask( final ContainerHost containerHost )
-        {
-            this.containerHost = containerHost;
-        }
-
-
-        public ContainerHost getContainerHost()
-        {
-            return containerHost;
-        }
-
-
-        @Override
-        public Object call() throws Exception
-        {
-            try
-            {
-                ( ( EnvironmentContainerImpl ) containerHost ).destroy();
-            }
-            catch ( PeerException e )
-            {
-                if ( !( e instanceof HostNotFoundException || ( ExceptionUtils.getRootCauseMessage( e )
-                                                                              .contains( "HostNotFoundException" ) ) ) )
-                {
-                    throw e;
-                }
-            }
-
-            return null;
         }
     }
 }
