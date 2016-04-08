3be834c66b20b1ba1a7342ff02923634e1c02597
dilshat <daliev@critical-factor.com>
2016-04-08 18:26:00 +0600

    HostUtil in progress

--- management/server/subutai-common/src/main/java/io/subutai/common/util/HostUtil.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/util/HostUtil.java
@@ -1,6 +1,7 @@
 package io.subutai.common.util;
 
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -17,71 +18,146 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
+import io.subutai.common.peer.Host;
 
-public class HostUtil<T>
+
+public class HostUtil
 {
     private static final Logger LOG = LoggerFactory.getLogger( HostUtil.class );
 
-    private final Set<HostTask<T>> tasks = Sets.newHashSet();
+    private final Set<HostTask> tasks = Sets.newConcurrentHashSet();
 
 
-    public void addTask( HostTask<T> task )
+    public Set getTasks()
     {
-        Preconditions.checkNotNull( task, "Invalid task" );
-
-        this.tasks.add( task );
+        return Collections.unmodifiableSet( tasks );
     }
 
 
-    public HostResults<T> execute()
+    public HostResults execute( Set<HostTask> tasks )
     {
         Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( tasks ), "No task found for execution" );
 
-        Set<HostResult<T>> results = executeParallel( tasks );
+        this.tasks.addAll( tasks );
 
-        tasks.clear();
+        Set<HostResult> results = executeParallel( tasks );
 
-        return new HostResults<>( results );
+        return new HostResults( results );
     }
 
 
-    public abstract static class HostTask<T> implements Callable<T>
+    public abstract static class HostTask<R> implements Callable<R>
     {
+
+        public static enum TaskState
+        {
+            NEW, RUNNING, SUCCEEDED, FAILED
+        }
+
+
+        private long submitTimestamp;
+
+        private final Host host;
+
+        private TaskState taskState = TaskState.NEW;
+
+
+        public HostTask( final Host host )
+        {
+            this.host = host;
+        }
+
+
+        private void setSubmitTimestamp( long submitTimestamp )
+        {
+            this.submitTimestamp = submitTimestamp;
+        }
+
+
+        private void setTaskState( final TaskState taskState )
+        {
+            this.taskState = taskState;
+        }
+
+
+        /**
+         * Returns duration in millis after task was submitted to execution up to current time
+         */
+        public final long getDuration()
+        {
+            return System.currentTimeMillis() - submitTimestamp;
+        }
+
+
         /**
          * Maximum instances of this task that can be run in parallel on a given host.
          *
          * A value less than or equal to 0 indicates unlimited number of parallel tasks
          */
         abstract int maxParallelTasks();
+
+
+        /**
+         * Name of task. May contain short description of task
+         */
+        abstract String name();
+
+
+        /**
+         * Returns target host
+         */
+        public final Host getHost()
+        {
+            return host;
+        }
+
+
+        /**
+         * Returns state of task
+         */
+        public final TaskState getTaskState()
+        {
+            return taskState;
+        }
     }
 
 
-    public static class HostResult<T>
+    public static class HostResult
     {
-        private final HostTask<T> task;
-        private T result;
+        private final HostTask task;
+        private Object result;
         private Exception exception;
-        private boolean hasSucceeded = true;
 
 
-        protected HostResult( final HostTask<T> task, final T result )
+        protected HostResult( final HostTask task, final Object result )
         {
             this.task = task;
             this.result = result;
         }
 
 
-        protected HostResult( final HostTask<T> task, final Exception exception )
+        protected HostResult( final HostTask task, final Exception exception )
         {
             this.task = task;
             this.exception = exception;
-            this.hasSucceeded = false;
         }
 
 
-        public T getResult()
+        public HostTask getTask()
+        {
+            return task;
+        }
+
+
+        public Object getResult()
         {
             return result;
+        }
+
+
+        public boolean hasSucceeded()
+        {
+            return task.getTaskState() == HostTask.TaskState.SUCCEEDED;
         }
 
 
@@ -95,28 +171,16 @@ public class HostUtil<T>
         {
             return exception == null ? "Unknown" : exception.getMessage();
         }
-
-
-        public boolean hasSucceeded()
-        {
-            return hasSucceeded;
-        }
-
-
-        public HostTask<T> getTask()
-        {
-            return task;
-        }
     }
 
 
-    public static class HostResults<T>
+    public static class HostResults
     {
-        private final Set<HostResult<T>> hostResults;
+        private final Set<HostResult> hostResults;
         private boolean hasFailures = false;
 
 
-        protected HostResults( final Set<HostResult<T>> hostResults )
+        protected HostResults( final Set<HostResult> hostResults )
         {
             Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( hostResults ) );
 
@@ -124,7 +188,7 @@ public class HostUtil<T>
 
             for ( HostResult taskResult : hostResults )
             {
-                if ( !taskResult.hasSucceeded() )
+                if ( taskResult.getTask().getTaskState() == HostTask.TaskState.FAILED )
                 {
                     hasFailures = true;
 
@@ -140,43 +204,51 @@ public class HostUtil<T>
         }
 
 
-        public Set<HostResult<T>> getHostResults()
+        public Set<HostResult> getHostResults()
         {
             return hostResults;
         }
     }
 
 
-    protected Set<HostResult<T>> executeParallel( Set<HostTask<T>> tasks )
+    protected Set<HostResult> executeParallel( Set<HostTask> tasks )
     {
         Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( tasks ) );
 
-        Set<HostResult<T>> taskResults = Sets.newHashSet();
+        Set<HostResult> taskResults = Sets.newHashSet();
 
+        //todo use maxParallelTasks to obtain executor from static cache
         ExecutorService taskExecutor = Executors.newFixedThreadPool( tasks.size() );
-        CompletionService<T> taskCompletionService = new ExecutorCompletionService<>( taskExecutor );
+        CompletionService taskCompletionService = new ExecutorCompletionService<>( taskExecutor );
 
-        Map<HostTask<T>, Future<T>> taskFutures = Maps.newHashMap();
+        Map<HostTask, Future> taskFutures = Maps.newHashMap();
 
-        for ( HostTask<T> task : tasks )
+        for ( HostTask task : tasks )
         {
+            task.setSubmitTimestamp( System.currentTimeMillis() );
+            task.setTaskState( HostTask.TaskState.RUNNING );
             taskFutures.put( task, taskCompletionService.submit( task ) );
         }
 
         taskExecutor.shutdown();
 
-        for ( Map.Entry<HostTask<T>, Future<T>> futureEntry : taskFutures.entrySet() )
+        for ( Map.Entry<HostTask, Future> taskFuture : taskFutures.entrySet() )
         {
             try
             {
-                taskResults.add( new HostResult<>( futureEntry.getKey(), futureEntry.getValue().get() ) );
+                taskFuture.getKey().setTaskState( HostTask.TaskState.SUCCEEDED );
+                taskResults.add( new HostResult( taskFuture.getKey(), taskFuture.getValue().get() ) );
             }
             catch ( Exception e )
             {
                 LOG.error( "Error executing task ", e );
 
-                taskResults.add( new HostResult<>( futureEntry.getKey(), e ) );
+                taskFuture.getKey().setTaskState( HostTask.TaskState.FAILED );
+                taskResults.add( new HostResult( taskFuture.getKey(), e ) );
             }
+
+            //remove completed task
+            this.tasks.remove( taskFuture.getKey() );
         }
 
         return taskResults;
