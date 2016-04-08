535ca95ffa14359257cc942e905ead6a7380e768
dilshat <daliev@critical-factor.com>
2016-04-08 20:10:08 +0600

    HostUtil completed

--- management/server/subutai-common/src/main/java/io/subutai/common/util/HostUtil.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/util/HostUtil.java
@@ -23,9 +23,13 @@ import io.subutai.common.peer.Host;
 
 public class HostUtil
 {
+    public static final int MAX_EXECUTOR_SIZE = 20;
+
     private static final Logger LOG = LoggerFactory.getLogger( HostUtil.class );
 
     private final Set<HostTask> tasks = Sets.newConcurrentHashSet();
+
+    private Map<String, ExecutorService> taskExecutors = Maps.newConcurrentMap();
 
 
     public Set getTasks()
@@ -34,15 +38,15 @@ public class HostUtil
     }
 
 
-    public HostResults execute( Set<HostTask> tasks )
+    public TaskResults execute( Set<HostTask> tasks )
     {
         Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( tasks ), "No task found for execution" );
 
         this.tasks.addAll( tasks );
 
-        Set<HostResult> results = executeParallel( tasks );
+        executeParallel( tasks );
 
-        return new HostResults( results );
+        return new TaskResults( tasks );
     }
 
 
@@ -58,6 +62,10 @@ public class HostUtil
         private long submitTimestamp;
 
         private final Host host;
+
+        private R result;
+
+        private Exception exception;
 
         private TaskState taskState = TaskState.NEW;
 
@@ -80,12 +88,15 @@ public class HostUtil
         }
 
 
-        /**
-         * Returns duration in millis after task was submitted to execution up to current time
-         */
-        public final long getDuration()
+        private void setResult( R result )
         {
-            return System.currentTimeMillis() - submitTimestamp;
+            this.result = result;
+        }
+
+
+        private void setException( Exception exception )
+        {
+            this.exception = exception;
         }
 
 
@@ -94,13 +105,46 @@ public class HostUtil
          *
          * A value less than or equal to 0 indicates unlimited number of parallel tasks
          */
-        abstract int maxParallelTasks();
+        public abstract int maxParallelTasks();
 
 
         /**
          * Name of task. May contain short description of task
          */
-        abstract String name();
+        public abstract String name();
+
+
+        public final R getResult()
+        {
+            return result;
+        }
+
+
+        /**
+         * Returns exception thrown during execution of the task or null if task succeeded
+         */
+        public final Exception getException()
+        {
+            return exception;
+        }
+
+
+        /**
+         * Returns string representation of failure
+         */
+        public final String getFailureReason()
+        {
+            return exception == null ? "Unknown" : exception.getMessage();
+        }
+
+
+        /**
+         * Returns duration in millis after task was submitted to execution up to current time
+         */
+        public final long getDuration()
+        {
+            return System.currentTimeMillis() - submitTimestamp;
+        }
 
 
         /**
@@ -122,73 +166,21 @@ public class HostUtil
     }
 
 
-    public static class HostResult
+    public static class TaskResults
     {
-        private final HostTask task;
-        private Object result;
-        private Exception exception;
-
-
-        protected HostResult( final HostTask task, final Object result )
-        {
-            this.task = task;
-            this.result = result;
-        }
-
-
-        protected HostResult( final HostTask task, final Exception exception )
-        {
-            this.task = task;
-            this.exception = exception;
-        }
-
-
-        public HostTask getTask()
-        {
-            return task;
-        }
-
-
-        public Object getResult()
-        {
-            return result;
-        }
-
-
-        public boolean hasSucceeded()
-        {
-            return task.getTaskState() == HostTask.TaskState.SUCCEEDED;
-        }
-
-
-        public Exception getException()
-        {
-            return exception;
-        }
-
-
-        public String getFailureReason()
-        {
-            return exception == null ? "Unknown" : exception.getMessage();
-        }
-    }
-
-
-    public static class HostResults
-    {
-        private final Set<HostResult> hostResults;
+        private final Set<HostTask> tasks;
         private boolean hasFailures = false;
 
 
-        protected HostResults( final Set<HostResult> hostResults )
+        protected TaskResults( final Set<HostTask> tasks )
         {
-            Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( hostResults ) );
+            Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( tasks ) );
 
-            this.hostResults = hostResults;
+            this.tasks = tasks;
 
-            for ( HostResult taskResult : hostResults )
+            for ( HostTask task : tasks )
             {
-                if ( taskResult.getTask().getTaskState() == HostTask.TaskState.FAILED )
+                if ( task.getTaskState() == HostTask.TaskState.FAILED )
                 {
                     hasFailures = true;
 
@@ -204,18 +196,35 @@ public class HostUtil
         }
 
 
-        public Set<HostResult> getHostResults()
+        public Set<HostTask> getTasks()
         {
-            return hostResults;
+            return tasks;
         }
     }
 
 
-    protected Set<HostResult> executeParallel( Set<HostTask> tasks )
+    private ExecutorService getTaskExecutor( HostTask task )
+    {
+        String executorId = task.name() + "-" + task.getClass().getName();
+
+        ExecutorService executorService = taskExecutors.get( executorId );
+
+        if ( executorService == null )
+        {
+            executorService = task.maxParallelTasks() > 0 ?
+                              Executors.newFixedThreadPool( Math.min( MAX_EXECUTOR_SIZE, task.maxParallelTasks() ) ) :
+                              Executors.newCachedThreadPool();
+
+            taskExecutors.put( executorId, executorService );
+        }
+
+        return executorService;
+    }
+
+
+    protected void executeParallel( Set<HostTask> tasks )
     {
         Preconditions.checkArgument( !CollectionUtil.isCollectionEmpty( tasks ) );
-
-        Set<HostResult> taskResults = Sets.newHashSet();
 
         //todo use maxParallelTasks to obtain executor from static cache
         ExecutorService taskExecutor = Executors.newFixedThreadPool( tasks.size() );
@@ -237,20 +246,18 @@ public class HostUtil
             try
             {
                 taskFuture.getKey().setTaskState( HostTask.TaskState.SUCCEEDED );
-                taskResults.add( new HostResult( taskFuture.getKey(), taskFuture.getValue().get() ) );
+                taskFuture.getKey().setResult( taskFuture.getValue().get() );
             }
             catch ( Exception e )
             {
                 LOG.error( "Error executing task ", e );
 
                 taskFuture.getKey().setTaskState( HostTask.TaskState.FAILED );
-                taskResults.add( new HostResult( taskFuture.getKey(), e ) );
+                taskFuture.getKey().setException( e );
             }
 
             //remove completed task
             this.tasks.remove( taskFuture.getKey() );
         }
-
-        return taskResults;
     }
 }
