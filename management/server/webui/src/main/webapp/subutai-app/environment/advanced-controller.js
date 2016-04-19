3a23649d007c2a138d02d98739a71774ec49382e
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-04-20 04:12:39 +0600

    environment creation, add conteiners by installed plugins

--- management/server/webui/src/main/webapp/subutai-app/environment/advanced-controller.js
+++ management/server/webui/src/main/webapp/subutai-app/environment/advanced-controller.js
@@ -958,15 +958,6 @@ function checkResourceHost(model) {
 	
 }
 
-function imageExists(image_url){
-	var http = new XMLHttpRequest();
-
-	http.open('HEAD', image_url, false);
-	http.send();
-
-	return http.status != 404;
-}
-
 function startDrag( event ) {
 
 	var containerImage = $(event.target).parent().find('img');
