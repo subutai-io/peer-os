ad9dad94fb3abc443748544bf8079f934ea9f899
Kylych Tynybekov <ktynybekov@gmail.com>
2016-04-20 17:21:15 +0600

    jar updated, alias continued

--- management/server/core/kurjun/kurjun-api/src/main/java/io/subutai/core/kurjun/api/AliasManager.java
+++ management/server/core/kurjun/kurjun-api/src/main/java/io/subutai/core/kurjun/api/AliasManager.java
@@ -1,13 +1,8 @@
 package io.subutai.core.kurjun.api;
 
 
-import ai.subut.kurjun.model.metadata.SerializableMetadata;
-
-
 public interface AliasManager
 {
-
-    SerializableMetadata getMetadataByAliasName( String alias );
 
     void addAliasName( String alias, String md5 );
 
