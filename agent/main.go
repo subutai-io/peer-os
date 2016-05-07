63406cc4c81e6029a90803ddf4a3933944b40b86
tzununbekov <t.zununbekov@gmail.com>
2016-05-07 22:08:39 +0600

    RC11 release

--- agent/main.go
+++ agent/main.go
@@ -28,7 +28,7 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "Subutai"
-	app.Version = "4.0.0-RC11-SNAPSHOT"
+	app.Version = "4.0.0-RC11"
 	app.Usage = "daemon and command line interface binary"
 
 	app.Flags = []cli.Flag{cli.BoolFlag{
