eb35b25b7d3d11a086ee1443e122535a275f1a51
Askat Asanaliev <aasanaliev@optimal-dynamics.com>
2016-05-11 02:43:10 +0600

    Refactoring. Limited command response out up to 1000 chars.

--- management/server/core/command-executor/command-executor-impl/src/main/java/io/subutai/core/executor/impl/CommandProcessor.java
+++ management/server/core/command-executor/command-executor-impl/src/main/java/io/subutai/core/executor/impl/CommandProcessor.java
@@ -335,10 +335,8 @@ public class CommandProcessor implements RestProcessor
 
             if ( commandProcess != null )
             {
-
                 LOG.debug( String.format( "Response:%n%s", JsonUtil.toJson( response ) ) );
 
-                //process response
                 commandProcess.processResponse( response );
             }
             else
--- management/server/subutai-common/src/main/java/io/subutai/common/command/ResponseImpl.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/command/ResponseImpl.java
@@ -3,6 +3,8 @@ package io.subutai.common.command;
 
 import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 
@@ -96,8 +98,10 @@ public class ResponseImpl implements Response
     @Override
     public String toString()
     {
+        String out = StringUtils.abbreviate( stdOut, 1000 );
+
         return MoreObjects.toStringHelper( this ).add( "type", type ).add( "id", id ).add( "commandId", commandId )
-                          .add( "pid", pid ).add( "responseNumber", responseNumber ).add( "stdOut", stdOut )
+                          .add( "pid", pid ).add( "responseNumber", responseNumber ).add( "stdOut", out )
                           .add( "stdErr", stdErr ).add( "exitCode", exitCode ).toString();
     }
 }
