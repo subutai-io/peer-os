80e340cc581da7700d6f5cb64e2629ad8a39cd25
Nurkaly Isakov <nisakov@critical-factor.com>
2016-05-23 16:23:32 +0600

    Added security annotations in module karafManagement (For KarafManagementImpl service)

--- management/server/core/karaf-manager/karaf-manager-api/src/main/java/io/subutai/core/karaf/manager/api/KarafManager.java
+++ management/server/core/karaf-manager/karaf-manager-api/src/main/java/io/subutai/core/karaf/manager/api/KarafManager.java
@@ -1,10 +1,19 @@
 package io.subutai.core.karaf.manager.api;
 
 
+import javax.annotation.security.RolesAllowed;
+
+
 /**
  * Karaf Manager
  */
 public interface KarafManager
 {
     String executeShellCommand( String commandStr );
+
+    /* *************
+        */
+    @RolesAllowed( {"Karaf-Server-Administration|Write","Karaf-Server-Administration|Read",
+                    "System-Management|Write", "System-Management|Update" } )
+    String executeJMXCommand( String commandStr );
 }
