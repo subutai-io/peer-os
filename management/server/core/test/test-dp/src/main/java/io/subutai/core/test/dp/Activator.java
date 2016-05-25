8fd866d59b4156fdc35fcb1bbbb0843c846b96dc
TalasZh <tjoldoshbekov@optimal-dynamics.com>
2016-05-26 01:07:28 +0600

    implemented streaming file

--- management/server/core/test/test-dp/src/main/java/io/subutai/core/test/dp/Activator.java
+++ management/server/core/test/test-dp/src/main/java/io/subutai/core/test/dp/Activator.java
@@ -5,65 +5,10 @@
 package io.subutai.core.test.dp;
 
 
-import java.io.FileOutputStream;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-
-import org.osgi.framework.BundleContext;
-
-
-/**
- * @author slim ouertani
- */
 public class Activator
 {
     public void start()
     {
 
-        URL website = null;
-        try
-        {
-            website = new URL("http://bndtools.org/installation.html");
-
-            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-
-            ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-            FileOutputStream fos = new FileOutputStream("information.html");
-            FileChannel fChannel = fos.getChannel();
-
-            int bytesRead = rbc.read(byteBuffer);
-
-            while(bytesRead > 0){
-
-                //limit is set to current position and position is set to zero
-                byteBuffer.flip();
-
-                while(byteBuffer.hasRemaining()){
-                    fChannel.write( byteBuffer );
-//                    char ch = (char) byteBuffer.get();
-//                    System.out.print(ch);
-                }
-
-//                fos.write( bytesRead );
-                byteBuffer.clear();
-                bytesRead = rbc.read(byteBuffer);
-            }
-
-            fChannel.close();
-//            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        //        WSClient customWSClient = new AhcWSClient(ahcBuilder.build(), materializer);
-    }
-
-
-    public void stop( BundleContext context ) throws Exception
-    {
     }
 }
--- management/server/core/test/test-dp/src/main/java/io/subutai/core/test/dp/SampleRest.java
+++ management/server/core/test/test-dp/src/main/java/io/subutai/core/test/dp/SampleRest.java
@@ -1,26 +1,16 @@
 package io.subutai.core.test.dp;
 
 
-import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
+import java.nio.channels.WritableByteChannel;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -39,78 +29,30 @@ public class SampleRest
     private static final Logger logger = LoggerFactory.getLogger( SampleRest.class );
 
 
-    private interface Logging
-    {
-        void logCounter( Integer integer );
-    }
-
-    public static class OpenTrustManager implements X509TrustManager
-    {
-
-        @Override
-        public void checkClientTrusted(X509Certificate[] arg0, String arg1)
-                throws CertificateException {
-        }
-
-        @Override
-        public void checkServerTrusted(X509Certificate[] arg0, String arg1)
-                throws CertificateException
-        {
-        }
-
-        @Override
-        public X509Certificate[] getAcceptedIssuers() {
-            return null;
-        }
-
-        public static void apply(HttpsURLConnection conn)
-                throws NoSuchAlgorithmException, KeyManagementException
-        {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, new TrustManager[] { new OpenTrustManager() },
-                    new java.security.SecureRandom());
-            conn.setSSLSocketFactory(sc.getSocketFactory());
-        }
-    }
-
-
     @GET
     @Path( "/books/pdf" )
     @Produces( "application/text" )
     public Response getPdf()
     {
-        final Logging logging = new Logging()
-        {
-            @Override
-            public void logCounter( final Integer integer )
-            {
-                logger.info( String.valueOf( integer ) );
-            }
-        };
+        final String filename = "information.html";
         StreamingOutput sOut = new StreamingOutput()
         {
             public void write( final OutputStream os )
             {
                 Integer counter = 0;
-                OutputStreamWriter outWriter = new OutputStreamWriter( os );
+                WritableByteChannel outChannel = Channels.newChannel( os );
 
-                Writer writer = new BufferedWriter( outWriter );
                 URL website = null;
                 try
                 {
-                    // https://images.unsplash.com/photo-1462733441571-9312d0b53818?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=deb060f2ba359f2691166b4278ecdc5b
-//                    website = new URL( "https://images.unsplash.com/photo-1462733441571-9312d0b53818?format=auto&auto=compress&dpr=1&crop=entropy&fit=crop&w=1910&h=1273&q=80" );
-                    website = new URL( "https://images.unsplash.com/photo-1462733441571-9312d0b53818?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=deb060f2ba359f2691166b4278ecdc5b" );
+                    website = new URL( "http://bndtools.org/installation.html" );
                     URLConnection conn = website.openConnection();
-                    if(conn instanceof HttpsURLConnection) {
-                        OpenTrustManager.apply((HttpsURLConnection)conn);
-                    }
                     conn.connect();
 
                     ReadableByteChannel rbc = Channels.newChannel( conn.getInputStream() );
 
                     ByteBuffer byteBuffer = ByteBuffer.allocate( 8192 );
-                    FileOutputStream fos = new FileOutputStream( "information.jpg" );
+                    FileOutputStream fos = new FileOutputStream( filename );
                     FileChannel fChannel = fos.getChannel();
 
                     int bytesRead = rbc.read( byteBuffer );
@@ -135,22 +77,21 @@ public class SampleRest
 
                         byteBuffer.clear();
                         bytesRead = rbc.read( byteBuffer );
-//                        Thread.sleep( 2000 );
+                        Thread.sleep( 2000 );
                     }
 
                     fChannel.close();
-                    logging.logCounter( counter );
                 }
                 catch ( Exception ex )
                 {
-                    logger.error("Error downloading file", ex);
+                    logger.error( "Error downloading file", ex );
                 }
             }
         };
         Response.ResponseBuilder responseBuilder = Response.ok( sOut );
-        responseBuilder.header( "Content-Disposition", "attachment; filename=information.jpg" );
-        responseBuilder.header( "Content-Length", 5668272 );
-//        responseBuilder.header( "Content-Length", 241777);
-                return responseBuilder.build();
+        responseBuilder.header( "Content-Disposition", "attachment; filename=" + filename );
+//        responseBuilder.header( "Content-Length", 5668272 );
+        responseBuilder.header( "Content-Length", 241777);
+        return responseBuilder.build();
     }
 }
