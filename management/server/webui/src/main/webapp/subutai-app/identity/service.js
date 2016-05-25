193ef7f0c9f2928d47a2e375369aa1c3554ec711
Ermek Abyshev <eabyshev@optimal-dynamics.com>
2016-05-25 17:02:08 +0600

    Added notification about update to bar. #812

--- management/server/webui/src/main/webapp/subutai-app/identity/service.js
+++ management/server/webui/src/main/webapp/subutai-app/identity/service.js
@@ -1,185 +1,211 @@
 'use strict';
 
 angular.module('subutai.identity.service', [])
-	.factory('identitySrv', identitySrv);
+    .factory('identitySrv', identitySrv);
 
 
 identitySrv.$inject = ['$http'];
 
 function identitySrv($http) {
-	var BASE_URL = SERVER_URL + 'rest/ui/identity/';
-	var USERS_URL = BASE_URL;
-	var ROLES_URL = BASE_URL + 'roles/';
-	var TOKENS_URL = BASE_URL + 'users/tokens/';
+    var BASE_URL = SERVER_URL + 'rest/ui/identity/';
+    var USERS_URL = BASE_URL;
+    var ROLES_URL = BASE_URL + 'roles/';
+    var TOKENS_URL = BASE_URL + 'users/tokens/';
 
-	var identitySrv = {
-		getTokens: getTokens,
-		addToken: addToken,
-		editToken: editToken,
-		deleteToken: deleteToken,
-		getUsers: getUsers,
-		addUser : addUser,
-		deleteUser: deleteUser,
-		getRoles: getRoles,
-		addRole: addRole,
-		deleteRole: deleteRole,
-		getTokenTypes: getTokenTypes,
-		getPermissionsScops: getPermissionsScops,
-		signUp: signUp,
-		approve: approve,
-		getKey: getKey,
-		getCurrentUser: getCurrentUser,
+    var identitySrv = {
+        getTokens: getTokens,
+        addToken: addToken,
+        editToken: editToken,
+        deleteToken: deleteToken,
+        getUsers: getUsers,
+        addUser: addUser,
+        deleteUser: deleteUser,
+        getRoles: getRoles,
+        addRole: addRole,
+        deleteRole: deleteRole,
+        getTokenTypes: getTokenTypes,
+        getPermissionsScops: getPermissionsScops,
+        signUp: signUp,
+        approve: approve,
+        getKey: getKey,
+        getCurrentUser: getCurrentUser,
 
-		updatePublicKey: updatePublicKey,
-		createIdentityDelegateDocument: createIdentityDelegateDocument,
-		getIdentityDelegateDocument: getIdentityDelegateDocument,
-		approveIdentityDelegate: approveIdentityDelegate,
+        getConfig: getConfig,
 
-		getPublicKeyData: getPublicKeyData,
-		checkUserKey: checkUserKey,
+        updatePublicKey: updatePublicKey,
+        createIdentityDelegateDocument: createIdentityDelegateDocument,
+        getIdentityDelegateDocument: getIdentityDelegateDocument,
+        approveIdentityDelegate: approveIdentityDelegate,
 
-		getUsersUrl : function(){ return USERS_URL },
-		getRolesUrl : function(){ return ROLES_URL },
-		getTokensUrl : function(){ return TOKENS_URL }
-	};
+        getPublicKeyData: getPublicKeyData,
+        checkUserKey: checkUserKey,
 
-	return identitySrv;
+        getUsersUrl: function () {
+            return USERS_URL
+        },
+        getRolesUrl: function () {
+            return ROLES_URL
+        },
+        getTokensUrl: function () {
+            return TOKENS_URL
+        }
+    };
 
-	//// Implementation
+    return identitySrv;
 
-	
-	function getTokens() {
-		return $http.get(TOKENS_URL, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    //// Implementation
 
-	function addToken(token) {
-		var postData = 'token=' + token.token + '&period=' + token.period + '&userId=' + token.userId;
-		return $http.post(
-			TOKENS_URL,
-			postData, 
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
 
-	function editToken(token) {
-		var postData = 'token=' + token.token 
-			+ '&period=' + token.period 
-			+ '&userId=' + token.userId 
-			+ '&newToken=' + token.newToken;
-		return $http.put(
-			TOKENS_URL,
-			postData, 
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}	
+    function getTokens() {
+        return $http.get(TOKENS_URL, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }
 
-	function deleteToken(token) {
-		return $http.delete(TOKENS_URL + token);
-	}
+    function addToken(token) {
+        var postData = 'token=' + token.token + '&period=' + token.period + '&userId=' + token.userId;
+        return $http.post(
+            TOKENS_URL,
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function getUsers() {
-		return $http.get(USERS_URL + "all", {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function editToken(token) {
+        var postData = 'token=' + token.token
+            + '&period=' + token.period
+            + '&userId=' + token.userId
+            + '&newToken=' + token.newToken;
+        return $http.put(
+            TOKENS_URL,
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function addUser(postData) {
-		return $http.post(
-			USERS_URL,
-			postData, 
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function deleteToken(token) {
+        return $http.delete(TOKENS_URL + token);
+    }
 
-	function deleteUser(userId) {
-		return $http.delete(USERS_URL + userId);
-	}
+    function getUsers() {
+        return $http.get(USERS_URL + "all", {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }
 
-	function getRoles() {
-		return $http.get(ROLES_URL, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function addUser(postData) {
+        return $http.post(
+            USERS_URL,
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function addRole(postData) {
-		return $http.post(
-			ROLES_URL,
-			postData, 
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function deleteUser(userId) {
+        return $http.delete(USERS_URL + userId);
+    }
 
-	function deleteRole(roleId) {
-		return $http.delete(ROLES_URL + roleId);
-	}
+    function getRoles() {
+        return $http.get(ROLES_URL, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
+    }
 
-	function getTokenTypes() {
-		return $http.get(USERS_URL + 'tokens/types', {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function addRole(postData) {
+        return $http.post(
+            ROLES_URL,
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function getPermissionsScops() {
-		return $http.get(USERS_URL + 'permissions/scopes', {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function deleteRole(roleId) {
+        return $http.delete(ROLES_URL + roleId);
+    }
 
-	function getCurrentUser() {
-		return $http.get (SERVER_URL + 'rest/ui/identity/user');
-	}
+    function getTokenTypes() {
+        return $http.get(USERS_URL + 'tokens/types', {
+            withCredentials: true,
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
 
-	function signUp (username, fullName, password, email, publicKey) {
-		var postData = "username=" + username + "&full_name=" + fullName + "&password=" + password + "&email=" + email + "&public_key=" + publicKey;
-		return $http.post(
-			USERS_URL + "signup",
-			postData,
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function getPermissionsScops() {
+        return $http.get(USERS_URL + 'permissions/scopes', {
+            withCredentials: true,
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
 
-	function updatePublicKey (publicKey) {
-		var postData = "publicKey=" + publicKey;
-		return $http.post(
-			USERS_URL + "set-public-key",
-			postData,
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function getCurrentUser() {
+        return $http.get(SERVER_URL + 'rest/ui/identity/user');
+    }
 
-	function createIdentityDelegateDocument() {
-		return $http.post(
-			USERS_URL + "delegate-identity",
-			"",
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function signUp(username, fullName, password, email, publicKey) {
+        var postData = "username=" + username + "&full_name=" + fullName + "&password=" + password + "&email=" + email + "&public_key=" + publicKey;
+        return $http.post(
+            USERS_URL + "signup",
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function getIdentityDelegateDocument() {
-		return $http.get( USERS_URL + "delegate-identity" );
-	}
+    function updatePublicKey(publicKey) {
+        var postData = "publicKey=" + publicKey;
+        return $http.post(
+            USERS_URL + "set-public-key",
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function approveIdentityDelegate(signedDocument) {
-		var postData = "signedDocument="+signedDocument;
-		return $http.post(
-			USERS_URL + "approve-delegate",
-			postData,
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function createIdentityDelegateDocument() {
+        return $http.post(
+            USERS_URL + "delegate-identity",
+            "",
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function approve (username, roles) {
-		var postData = "username=" + username + "&roles=" + roles;
-		console.log (postData);
-		return $http.post(
-			USERS_URL + "approve",
-			postData,
-			{withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-		);
-	}
+    function getIdentityDelegateDocument() {
+        return $http.get(USERS_URL + "delegate-identity");
+    }
 
-	function getKey (id) {
-		return $http.get (SERVER_URL + "rest/v1/security/keyman/getpublickey", {params: {hostid: id}});
-	}
+    function approveIdentityDelegate(signedDocument) {
+        var postData = "signedDocument=" + signedDocument;
+        return $http.post(
+            USERS_URL + "approve-delegate",
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function getPublicKeyData(userId) {
-		return $http.get(USERS_URL + 'key-data/' + userId, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function approve(username, roles) {
+        var postData = "username=" + username + "&roles=" + roles;
+        console.log(postData);
+        return $http.post(
+            USERS_URL + "approve",
+            postData,
+            {withCredentials: true, headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
+        );
+    }
 
-	function checkUserKey(userId) {
-		return $http.get(USERS_URL + 'check-user-key/' + userId, {withCredentials: true, headers: {'Content-Type': 'application/json'}});
-	}
+    function getKey(id) {
+        return $http.get(SERVER_URL + "rest/v1/security/keyman/getpublickey", {params: {hostid: id}});
+    }
 
+    function getPublicKeyData(userId) {
+        return $http.get(USERS_URL + 'key-data/' + userId, {
+            withCredentials: true,
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+
+    function checkUserKey(userId) {
+        return $http.get(USERS_URL + 'check-user-key/' + userId, {
+            withCredentials: true,
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+
+    function getConfig() {
+        return $http.get(SERVER_URL + "rest/v1/system/management_updates", {
+            withCredentials: true,
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
 }
--- management/server/webui/src/main/webapp/subutai-app/settingsUpdates/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/settingsUpdates/controller.js
@@ -7,9 +7,10 @@ angular.module("subutai.settings-updates.controller", [])
 SettingsUpdatesCtrl.$inject = ["$scope", "SettingsUpdatesSrv", "SweetAlert"];
 function SettingsUpdatesCtrl($scope, SettingsUpdatesSrv, SweetAlert) {
     var vm = this;
-    vm.config = {};
+    vm.config = {isUpdatesAvailable: "waiting"};
 
     function getConfig() {
+        LOADING_SCREEN();
         SettingsUpdatesSrv.getConfig().success(function (data) {
             LOADING_SCREEN('none');
             vm.config = data;
@@ -21,9 +22,12 @@ function SettingsUpdatesCtrl($scope, SettingsUpdatesSrv, SweetAlert) {
 
     vm.update = update;
     function update() {
+        LOADING_SCREEN();
         SettingsUpdatesSrv.update(vm.config).success(function (data) {
+            LOADING_SCREEN('none');
             SweetAlert.swal("Success!", "Your settings were saved.", "success");
         }).error(function (error) {
+            LOADING_SCREEN('none');
             SweetAlert.swal("ERROR!", "Save config error: " + error.replace(/\\n/g, " "), "error");
         });
     }
