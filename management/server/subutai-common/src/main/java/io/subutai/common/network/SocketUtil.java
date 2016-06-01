a20da5dfa6fca7e996312dca17dedfcdfb8e6014
Timur Jamakeev <tjamakeev@critical-factor.com>
2016-06-01 15:21:31 +0600

    show error message if public secure port is not open

---
+++ management/server/subutai-common/src/main/java/io/subutai/common/network/SocketUtil.java
@@ -0,0 +1,65 @@
+package io.subutai.common.network;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import io.subutai.common.exception.NetworkException;
+
+
+public class SocketUtil
+{
+    public static void check( String node, int timeout, int port ) throws NetworkException
+    {
+        Socket s = null;
+        String reason = null;
+        boolean success = false;
+        try
+        {
+            s = new Socket();
+            s.setReuseAddress( true );
+            SocketAddress sa = new InetSocketAddress( node, port );
+            s.connect( sa, timeout * 1000 );
+            success = s.isConnected();
+        }
+        catch ( IOException e )
+        {
+            if ( e.getMessage().equals( "Connection refused" ) )
+            {
+                reason = "port " + port + " on " + node + " is closed.";
+            }
+            ;
+            if ( e instanceof UnknownHostException )
+            {
+                reason = "node " + node + " is unresolved.";
+            }
+            if ( e instanceof SocketTimeoutException )
+            {
+                reason = "timeout while attempting to reach node " + node + " on port " + port;
+            }
+        }
+        finally
+        {
+            if ( s != null )
+            {
+                try
+                {
+                    s.close();
+                }
+                catch ( Exception e )
+                {
+                    // ignore
+                }
+            }
+        }
+
+        if ( !success )
+        {
+            throw new NetworkException( "Port " + port + " on " + node + " is not reachable; reason: " + reason );
+        }
+    }
+}
