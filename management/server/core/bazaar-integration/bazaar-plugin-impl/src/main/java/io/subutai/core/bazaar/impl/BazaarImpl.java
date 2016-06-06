c51f7dd301f8aba69303b163719f8c162c789f13
Abdysamat Mamutov <amamutov@optimal-dynamics.com>
2016-06-06 19:45:59 +0600

    Bazaar bug fixes  - When plugin was installed from SS, it wasn't reflected to Hub, fixed that.

--- management/server/core/bazaar-integration/bazaar-plugin-impl/src/main/java/io/subutai/core/bazaar/impl/BazaarImpl.java
+++ management/server/core/bazaar-integration/bazaar-plugin-impl/src/main/java/io/subutai/core/bazaar/impl/BazaarImpl.java
@@ -3,8 +3,6 @@ package io.subutai.core.bazaar.impl;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
