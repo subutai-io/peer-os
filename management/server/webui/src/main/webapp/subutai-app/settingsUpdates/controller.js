c9c8b4d0be00de4a81668cd6bdff165b98d1b453
Manas Atabaev <matabaev@optimal-dynamics.com>
2016-06-13 19:41:34 +0600

    fix #998

--- management/server/webui/src/main/webapp/subutai-app/settingsUpdates/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/settingsUpdates/controller.js
@@ -1,40 +1,50 @@
 "use strict";
 
 angular.module("subutai.settings-updates.controller", [])
-    .controller("SettingsUpdatesCtrl", SettingsUpdatesCtrl);
+.controller("SettingsUpdatesCtrl", SettingsUpdatesCtrl);
 
 
 SettingsUpdatesCtrl.$inject = ["$scope", "SettingsUpdatesSrv", "SweetAlert"];
 function SettingsUpdatesCtrl($scope, SettingsUpdatesSrv, SweetAlert) {
-    var vm = this;
-    vm.config = {isUpdatesAvailable: "waiting"};
+	var vm = this;
+	vm.config = {isUpdatesAvailable: "waiting"};
+	vm.updateText = 'Your system is already up-to-date';
 
-    function getConfig() {
-        LOADING_SCREEN();
-        SettingsUpdatesSrv.getConfig().success(function (data) {
-            LOADING_SCREEN('none');
-            vm.config = data;
-        }).error(function(error) {
-            LOADING_SCREEN('none');
-            SweetAlert.swal("ERROR!", error, "error");
+	function getConfig() {
+		LOADING_SCREEN();
+		SettingsUpdatesSrv.getConfig().success(function (data) {
+			LOADING_SCREEN('none');
+			vm.config = data;
+			if(vm.config.isUpdatesAvailable == true) {
+				vm.updateText = 'Update is available';
+			} else {
+				vm.updateText = 'Your system is already up-to-date';
+			}
+		}).error(function(error) {
+			LOADING_SCREEN('none');
+			SweetAlert.swal("ERROR!", error, "error");
 		});
-    }
+	}
 
-    getConfig();
+	getConfig();
 
 
-    vm.update = update;
-    function update() {
-        LOADING_SCREEN();
-        SettingsUpdatesSrv.update(vm.config).success(function (data) {
-            LOADING_SCREEN('none');
-            localStorage.removeItem('notifications');
-            SweetAlert.swal("Success!", "Subutai Successfully updated.", "success");
+	vm.update = update;
+	function update() {
+		LOADING_SCREEN();
+		vm.updateText = 'Please wait, update is in progress. System will restart automatically';
+		SettingsUpdatesSrv.update(vm.config).success(function (data) {
+			LOADING_SCREEN('none');
+			localStorage.removeItem('notifications');
+			SweetAlert.swal("Success!", "Subutai Successfully updated.", "success");
 			getConfig();
-        }).error(function (error) {
-            LOADING_SCREEN('none');
-            //SweetAlert.swal("ERROR!", "Save config error: " + error, "error");
+		}).error(function (error) {
+			//LOADING_SCREEN('none');
+			//SweetAlert.swal("ERROR!", "Save config error: " + error, "error");
 			//getConfig();
-        });
-    }
+			setInterval(function() {
+				getConfig();
+			}, 120000);
+		});
+	}
 }
