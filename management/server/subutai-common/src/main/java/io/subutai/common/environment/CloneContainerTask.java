9dd96af6f351f3efa4a3dc1113f7391ccdfc118d
dilshat <dilshat.aliev@gmail.com>
2016-07-29 18:07:56 +0600

    container name will container first full hostname and will not change we dont allow change of container names only hostnames

--- management/server/subutai-common/src/main/java/io/subutai/common/environment/CloneContainerTask.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/environment/CloneContainerTask.java
@@ -82,8 +82,7 @@ public class CloneContainerTask extends HostUtil.Task<String>
         request.setHostname( String.format( "%s-%d-%s", request.getHostname(), networkResource.getVlan(),
                 StringUtils.substringAfterLast( request.getIp().split( "/" )[0], "." ) ) );
 
-        //todo uncomment this line
-        //request.setContainerName( request.getHostname() );
+        request.setContainerName( request.getHostname() );
 
         String containerId = resourceHost
                 .cloneContainer( request.getTemplateName(), request.getHostname(), request.getIp(),
