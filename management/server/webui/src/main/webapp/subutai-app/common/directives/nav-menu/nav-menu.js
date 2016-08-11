21608f681dca89e98a4f7a5e9fb6299502c0ae47
larenso <istuts@optimal-dynamics.com>
2016-08-11 18:45:59 +0600

    reverting binding changes, not removing the controller of nav directive

--- management/server/webui/src/main/webapp/subutai-app/common/directives/nav-menu/nav-menu.js
+++ management/server/webui/src/main/webapp/subutai-app/common/directives/nav-menu/nav-menu.js
@@ -3,24 +3,15 @@
 angular.module('subutai.nav-menu', [])
         .directive('navMenu', NavMenu);
 
-NavMenu.$inject = ['$compile'];
-
-function NavMenu($compile) {
+function NavMenu() {
     return {
         restrict: 'E',
-        scope: false,
-        link: function (scope, element, attr) {
+        scope: {adminMenus : '='},
+        bindToController: true,
+        templateUrl: 'subutai-app/common/partials/nav-menu.html',
+        controller: function( ) {
 
-            scope.$watch(function () { return localStorage.currentUserPermissions; },function(newVal,oldVal){
-                // var templateUrl = getTemplateUrl(scope.data[0].typeID);
-                // var data = $templateCache.get(templateUrl);
-                // element.html(data);
-                // $compile(element.contents())(scope);
-
-                if( newVal !== oldVal && newVal !== undefined )
-                    scope.$apply();
-            });
         },
-        templateUrl: 'subutai-app/common/partials/nav-menu.html'
+        controllerAs: 'dir'
     }
 }
