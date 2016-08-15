d44629946059d6003e953b49d70505767c79e25a
dilshat <dilshat.aliev@gmail.com>
2016-08-15 18:25:02 +0600

    added templateId to DTO for simple env build mode

--- management/server/core/environment-manager/environment-manager-rest-ui/src/main/java/io/subutai/core/environment/rest/ui/entity/ContainerDto.java
+++ management/server/core/environment-manager/environment-manager-rest-ui/src/main/java/io/subutai/core/environment/rest/ui/entity/ContainerDto.java
@@ -17,6 +17,7 @@ public class ContainerDto
     private String hostname;
     private String ip;
     private String templateName;
+    private String templateId;
     private ContainerSize type;
     private String arch;
     private Set<String> tags;
@@ -28,11 +29,12 @@ public class ContainerDto
     // Where environment of container created: subutai, hub
     private String dataSource;
 
+
     //TODO:TEMPLATE add templateId
     public ContainerDto( final String id, final String name, final String environmentId, final String hostname,
                          final String ip, final String templateName, final ContainerSize type, final String arch,
                          final Set<String> tags, final String peerId, final String hostId, boolean local,
-                         String className )
+                         String className, final String templateId )
     {
         this.id = id;
         this.name = name;
@@ -46,6 +48,8 @@ public class ContainerDto
         this.peerId = peerId;
         this.hostId = hostId;
         this.local = local;
+
+        this.templateId = templateId;
 
         dataSource = className.contains( "ProxyEnvironment" ) ? "hub" : "subutai";
     }
