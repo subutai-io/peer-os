fe56121cc98c483712a1e6a6f9078e4dc6da6142
dilshat <dilshat.aliev@gmail.com>
2016-08-18 14:07:37 +0600

    added test cli command for checking template download progress

--- management/server/core/test/test-cli/src/main/java/io/subutai/core/test/cli/TestCommand.java
+++ management/server/core/test/test-cli/src/main/java/io/subutai/core/test/cli/TestCommand.java
@@ -1,6 +1,8 @@
 package io.subutai.core.test.cli;
 
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +10,8 @@ import org.apache.karaf.shell.commands.Command;
 
 import io.subutai.common.environment.Environment;
 import io.subutai.common.environment.PeerTemplatesDownloadProgress;
-import io.subutai.common.peer.LocalPeer;
+import io.subutai.common.environment.RhTemplatesDownloadProgress;
+import io.subutai.common.host.HostId;
 import io.subutai.common.peer.Peer;
 import io.subutai.common.util.ServiceLocator;
 import io.subutai.core.environment.api.EnvironmentManager;
@@ -31,12 +34,28 @@ public class TestCommand extends SubutaiShellCommandSupport
 
             for ( Environment environment : environmentManager.getEnvironments() )
             {
+                System.out.format( "Environment %s:%n", environment.getName() );
+
                 for ( Peer peer : environment.getPeers() )
                 {
                     PeerTemplatesDownloadProgress downloadProgress =
                             peer.getTemplateDownloadProgress( environment.getEnvironmentId() );
 
-                    System.out.println( downloadProgress );
+                    System.out.format( "\tPeer %s%n", peer.getName() );
+
+                    for ( Map.Entry<HostId, RhTemplatesDownloadProgress> rhProgress : downloadProgress
+                            .getPeerTemplatesDownloadProgressMap().entrySet() )
+                    {
+                        System.out.format( "\t\tRH %s%n", rhProgress.getKey().getId() );
+
+                        for ( Map.Entry<String, Integer> templateProgress : rhProgress.getValue()
+                                                                                      .getTemplatesDownloadProgressMap()
+                                                                                      .entrySet() )
+                        {
+                            System.out.format( "\t\t\tTemplate %s -> %d%n", templateProgress.getKey(),
+                                    templateProgress.getValue() );
+                        }
+                    }
                 }
             }
         }
