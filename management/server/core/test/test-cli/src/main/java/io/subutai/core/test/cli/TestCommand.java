30da11c3cb05d6d9d6974c29a06042e3f3799ac0
dilshat <dilshat.aliev@gmail.com>
2016-08-18 14:01:59 +0600

    added test cli command for checking template download progress

--- management/server/core/test/test-cli/src/main/java/io/subutai/core/test/cli/TestCommand.java
+++ management/server/core/test/test-cli/src/main/java/io/subutai/core/test/cli/TestCommand.java
@@ -1,16 +1,17 @@
 package io.subutai.core.test.cli;
 
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.karaf.bundle.core.BundleStateService;
 import org.apache.karaf.shell.commands.Command;
 
+import io.subutai.common.environment.Environment;
+import io.subutai.common.environment.PeerTemplatesDownloadProgress;
+import io.subutai.common.peer.LocalPeer;
+import io.subutai.common.peer.Peer;
 import io.subutai.common.util.ServiceLocator;
+import io.subutai.core.environment.api.EnvironmentManager;
 import io.subutai.core.identity.rbac.cli.SubutaiShellCommandSupport;
 
 
@@ -24,19 +25,25 @@ public class TestCommand extends SubutaiShellCommandSupport
     protected Object doExecute()
     {
 
-        BundleContext ctx = FrameworkUtil.getBundle( TestCommand.class ).getBundleContext();
-
-        Bundle[] bundles = ctx.getBundles();
-
-        BundleStateService bundleStateService = ServiceLocator.getServiceNoCache( BundleStateService.class );
-
-
-        for ( Bundle bundle : bundles )
+        try
         {
-            System.out.println(bundle.getSymbolicName() + " : "+ bundleStateService.getState( bundle )  + " : " + bundle.getState());
+            EnvironmentManager environmentManager = ServiceLocator.getServiceNoCache( EnvironmentManager.class );
+
+            for ( Environment environment : environmentManager.getEnvironments() )
+            {
+                for ( Peer peer : environment.getPeers() )
+                {
+                    PeerTemplatesDownloadProgress downloadProgress =
+                            peer.getTemplateDownloadProgress( environment.getEnvironmentId() );
+
+                    System.out.println( downloadProgress );
+                }
+            }
         }
-
-
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
         return null;
     }
 }
