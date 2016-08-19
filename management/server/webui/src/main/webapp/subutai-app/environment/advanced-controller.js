85017d0cd4614c0406ad62ce0c9781a705fe0230
larenso <istuts@optimal-dynamics.com>
2016-08-19 16:27:55 +0600

    nextline span

--- management/server/webui/src/main/webapp/subutai-app/environment/advanced-controller.js
+++ management/server/webui/src/main/webapp/subutai-app/environment/advanced-controller.js
@@ -169,7 +169,7 @@ function AdvancedEnvironmentCtrl($scope, $rootScope, environmentService, tracker
                                 output += 'Peer ' + key + ':<br/>';
                                 for( var tpl in p[key]['templatesDownloadProgressMap'] )
                                 {
-                                    output += '<span class="g-text-blue">' + tpl + '&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;' + p[key]['templatesDownloadProgressMap'][tpl] + ' %</span>';
+                                    output += '<span class="g-text-blue">' + tpl + '&nbsp;&nbsp;&nbsp;...&nbsp;&nbsp;&nbsp;' + p[key]['templatesDownloadProgressMap'][tpl] + ' %</span><br/>';
                                     if( p[key]['templatesDownloadProgressMap'][tpl] != 100 )
                                     {
                                         checker = true;
