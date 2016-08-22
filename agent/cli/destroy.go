901e56616add610470c2872230a99383df0adf27
tzununbekov <t.zununbekov@gmail.com>
2016-08-22 16:40:12 +0600

    Destroy argument check added

--- agent/cli/destroy.go
+++ agent/cli/destroy.go
@@ -5,9 +5,13 @@ import (
 	"github.com/subutai-io/base/agent/lib/container"
 	"github.com/subutai-io/base/agent/lib/net"
 	"github.com/subutai-io/base/agent/lib/template"
+	"github.com/subutai-io/base/agent/log"
 )
 
 func LxcDestroy(name string) {
+	if len(name) == 0 {
+		log.Error("Please specify container or template name")
+	}
 	net.DelIface(container.GetConfigItem(config.Agent.LxcPrefix+name+"/config", "lxc.network.veth.pair"))
 	container.Destroy(name)
 
