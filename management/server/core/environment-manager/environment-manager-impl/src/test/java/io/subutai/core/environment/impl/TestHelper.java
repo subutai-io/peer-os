3ee2999a8cfe4660b4311bdda01ce1ab30e1f663
dilshat <dilshat.aliev@gmail.com>
2016-09-28 10:20:05 +0600

    increased code coverage of env creation helpers

--- management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/TestHelper.java
+++ management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/TestHelper.java
@@ -5,16 +5,23 @@ import com.google.common.collect.Sets;
 
 import io.subutai.common.environment.Environment;
 import io.subutai.common.environment.Node;
+import io.subutai.common.host.HostId;
+import io.subutai.common.host.HostInterfaceModel;
+import io.subutai.common.host.HostInterfaces;
+import io.subutai.common.peer.ContainerId;
 import io.subutai.common.peer.ContainerSize;
 import io.subutai.common.peer.EnvironmentId;
 import io.subutai.common.peer.LocalPeer;
 import io.subutai.common.peer.Peer;
 import io.subutai.common.peer.PeerException;
+import io.subutai.common.settings.Common;
 import io.subutai.common.tracker.TrackerOperation;
 import io.subutai.common.util.PeerUtil;
+import io.subutai.core.environment.impl.entity.EnvironmentContainerImpl;
 import io.subutai.core.environment.impl.entity.EnvironmentImpl;
 import io.subutai.core.security.api.crypto.KeyManager;
 
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
@@ -23,17 +30,20 @@ public class TestHelper
 {
     public static final String SUBNET_CIDR = "192.168.0.1/24";
     public static final String CONTAINER_ID = "123";
-    public static final String PEER_NAME = "123";
-    public static final String SSH_KEY = "123";
+    public static final String PEER_NAME = "peer123";
+    public static final String SSH_KEY = "key";
     public static final String RH_ID = "123";
     public static final String PEER_ID = "123";
     public static final String ENV_ID = "123";
     public static final String OWNER_ID = "123";
     public static final String TEMPLATE_ID = "123";
+    public static final String TEMPLATE_NAME = "template";
     public static final String HOSTNAME = "hostname";
     public static final String CONTAINER_NAME = "container";
     public static final String MESSAGE = "msg";
     public static final EnvironmentId ENVIRONMENT_ID = new EnvironmentId( ENV_ID );
+    public static final HostId RES_HOST_ID = new HostId( RH_ID );
+    public static final ContainerId CONT_HOST_ID = new ContainerId( CONTAINER_ID );
 
 
     public static EnvironmentImpl ENVIRONMENT()
@@ -92,6 +102,29 @@ public class TestHelper
 
 
         return LOCAL_PEER;
+    }
+
+
+    public static EnvironmentContainerImpl ENV_CONTAINER()
+    {
+        EnvironmentContainerImpl environmentContainer = mock( EnvironmentContainerImpl.class );
+        doReturn( ENVIRONMENT_ID ).when( environmentContainer ).getEnvironmentId();
+        doReturn( CONTAINER_ID ).when( environmentContainer ).getId();
+        doReturn( CONT_HOST_ID ).when( environmentContainer ).getContainerId();
+        doReturn( PEER_ID ).when( environmentContainer ).getPeerId();
+        doReturn( CONTAINER_NAME ).when( environmentContainer ).getContainerName();
+        doReturn( HOSTNAME ).when( environmentContainer ).getHostname();
+        doReturn( RES_HOST_ID ).when( environmentContainer ).getResourceHostId();
+        doReturn( OWNER_ID ).when( environmentContainer ).getOwnerId();
+        doReturn( TEMPLATE_ID ).when( environmentContainer ).getTemplateId();
+        doReturn( TEMPLATE_NAME ).when( environmentContainer ).getTemplateName();
+        HostInterfaceModel hostInterface =
+                new HostInterfaceModel( Common.DEFAULT_CONTAINER_INTERFACE, Common.LOCAL_HOST_IP );
+        doReturn( new HostInterfaces( CONTAINER_ID, Sets.newHashSet( hostInterface ) ) ).when( environmentContainer )
+                                                                                        .getHostInterfaces();
+        doCallRealMethod().when( environmentContainer ).getInterfaceByName( Common.DEFAULT_CONTAINER_INTERFACE );
+
+        return environmentContainer;
     }
 
 
