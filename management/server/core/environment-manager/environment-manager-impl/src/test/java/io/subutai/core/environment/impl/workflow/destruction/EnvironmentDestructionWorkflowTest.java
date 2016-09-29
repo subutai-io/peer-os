4bd8385b5dd68b250798d8549c4d3e7dd0ae9a87
dilshat <dilshat.aliev@gmail.com>
2016-09-29 10:27:11 +0600

    increased code coverage of env destruction helpers

---
+++ management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/workflow/destruction/EnvironmentDestructionWorkflowTest.java
@@ -0,0 +1,91 @@
+package io.subutai.core.environment.impl.workflow.destruction;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Sets;
+
+import io.subutai.common.security.relation.RelationManager;
+import io.subutai.common.tracker.TrackerOperation;
+import io.subutai.core.environment.impl.EnvironmentManagerImpl;
+import io.subutai.core.environment.impl.TestHelper;
+import io.subutai.core.environment.impl.entity.EnvironmentImpl;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+
+@RunWith( MockitoJUnitRunner.class )
+public class EnvironmentDestructionWorkflowTest
+{
+    EnvironmentDestructionWorkflow workflow;
+
+    @Mock
+    EnvironmentManagerImpl environmentManager;
+    EnvironmentImpl environment = TestHelper.ENVIRONMENT();
+    TrackerOperation trackerOperation = TestHelper.TRACKER_OPERATION();
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        workflow = new EnvironmentDestructionWorkflow( environmentManager, environment, trackerOperation );
+        doReturn( environment ).when( environmentManager ).update( environment );
+    }
+
+
+    @Test
+    public void testINIT() throws Exception
+    {
+        workflow.INIT();
+
+        verify( environmentManager, atLeastOnce() ).update( environment );
+    }
+
+
+    @Test
+    public void testCLEANUP_ENVIRONMENT() throws Exception
+    {
+        workflow.CLEANUP_ENVIRONMENT();
+
+        verify( environmentManager, atLeastOnce() ).update( environment );
+    }
+
+
+    @Test
+    public void testFINALIZE() throws Exception
+    {
+        RelationManager relationManager = mock( RelationManager.class );
+        doReturn( relationManager ).when( environmentManager ).getRelationManager();
+        doReturn( Sets.newHashSet( TestHelper.ENV_CONTAINER() ) ).when( environment ).getContainerHosts();
+
+        workflow.FINALIZE();
+
+        verify( environmentManager, atLeastOnce() ).remove( environment );
+    }
+
+
+    @Test
+    public void testFail() throws Exception
+    {
+        workflow.fail( "", null );
+
+        verify( trackerOperation, atLeastOnce() ).addLogFailed( anyString() );
+    }
+
+
+    @Test
+    public void testOnCancellation() throws Exception
+    {
+        workflow.onCancellation();
+
+        verify( trackerOperation, atLeastOnce() ).addLogFailed( anyString() );
+    }
+}
