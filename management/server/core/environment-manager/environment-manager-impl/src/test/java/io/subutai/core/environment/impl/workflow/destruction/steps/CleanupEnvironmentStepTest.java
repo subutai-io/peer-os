353b3403b91fce36e003afe5f1b0bb1991524fab
dilshat <dilshat.aliev@gmail.com>
2016-09-29 09:16:19 +0600

    increased code coverage of env destruction helpers

--- management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/workflow/destruction/steps/CleanupEnvironmentStepTest.java
+++ management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/workflow/destruction/steps/CleanupEnvironmentStepTest.java
@@ -1,4 +1,4 @@
-package io.subutai.core.environment.impl.workflow.creation.steps;
+package io.subutai.core.environment.impl.workflow.destruction.steps;
 
 
 import org.junit.Before;
@@ -9,29 +9,28 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.common.collect.Sets;
 
-import io.subutai.common.environment.Topology;
 import io.subutai.common.peer.Peer;
+import io.subutai.common.tracker.TrackerOperation;
 import io.subutai.common.util.PeerUtil;
-import io.subutai.core.environment.api.exception.EnvironmentCreationException;
 import io.subutai.core.environment.impl.TestHelper;
-import io.subutai.core.environment.impl.entity.EnvironmentContainerImpl;
 import io.subutai.core.environment.impl.entity.EnvironmentImpl;
-import io.subutai.core.peer.api.PeerManager;
 
-import static org.mockito.Matchers.anySet;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
 @RunWith( MockitoJUnitRunner.class )
-public class ReservationStepTest
+public class CleanupEnvironmentStepTest
 {
+    CleanupEnvironmentStep step;
 
-    ReservationStep step;
-    @Mock
-    Topology topology;
     EnvironmentImpl environment = TestHelper.ENVIRONMENT();
+
     @Mock
     PeerUtil<Object> peerUtil;
     @Mock
@@ -39,33 +38,39 @@ public class ReservationStepTest
     @Mock
     PeerUtil.PeerTaskResult peerTaskResult;
     Peer peer = TestHelper.PEER();
-    @Mock
-    PeerManager peerManager;
+    TrackerOperation trackerOperation = TestHelper.TRACKER_OPERATION();
 
 
     @Before
     public void setUp() throws Exception
     {
-        step = new ReservationStep( topology, environment, peerManager, TestHelper.TRACKER_OPERATION() );
+        step = new CleanupEnvironmentStep( environment, trackerOperation);
+        step.cleanupUtil = peerUtil;
+
         TestHelper.bind( environment, peer, peerUtil, peerTaskResults, peerTaskResult );
-        step.peerUtil = peerUtil;
-        doReturn( Sets.newHashSet( peer ) ).when( peerManager ).resolve( anySet() );
     }
 
 
-    @Test( expected = EnvironmentCreationException.class )
+    @Test
     public void testExecute() throws Exception
     {
-        doReturn( 123 ).when( peerTaskResult ).getResult();
         doReturn( true ).when( peerTaskResult ).hasSucceeded();
 
         step.execute();
 
-        verify( environment ).setP2PSubnet( anyString() );
+        verify( trackerOperation ).addLog( anyString() );
 
         doReturn( false ).when( peerTaskResult ).hasSucceeded();
-        doReturn( true ).when( peerTaskResults ).hasFailures();
 
         step.execute();
+
+        verify( trackerOperation, times( 2 ) ).addLog( anyString() );
+
+        doReturn( Sets.newHashSet() ).when( environment ).getPeers();
+        reset( trackerOperation );
+
+        step.execute();
+
+        verify( trackerOperation, never() ).addLog( anyString() );
     }
 }
