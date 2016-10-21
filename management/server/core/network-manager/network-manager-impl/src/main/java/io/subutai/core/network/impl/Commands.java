a4ba45275dfd38de74567bfc24aa149edb4994b2
dilshat <dilshat.aliev@gmail.com>
2016-10-21 11:02:30 +0600

    updated logic to use "subutai p2p" and "subutai vxlan" instead of "subutai management_network" binding

--- management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/Commands.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/Commands.java
@@ -18,37 +18,38 @@ import io.subutai.common.network.ProxyLoadBalanceStrategy;
 
 public class Commands
 {
-    private static final String MANAGEMENT_HOST_NETWORK_BINDING = "subutai management_network";
-    private static final String MANAGEMENT_PROXY_BINDING = "subutai proxy";
+    private static final String TUNNEL_BINDING = "subutai tunnel";
+    private static final String VXLAN_BINDING = "subutai vxlan";
+    private static final String P2P_BINDING = "subutai p2p";
+    private static final String PROXY_BINDING = "subutai proxy";
     private final SimpleDateFormat p2pDateFormat = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
 
 
     public RequestBuilder getGetP2pVersionCommand()
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING )
-                .withCmdArgs( Lists.<String>newArrayList( "p2p", "-v" ) );
+        return new RequestBuilder( P2P_BINDING ).withCmdArgs( Lists.<String>newArrayList( "-v" ) );
     }
 
 
     public RequestBuilder getP2PConnectionsCommand()
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING ).withCmdArgs( Lists.newArrayList( "p2p", "-p" ) );
+        return new RequestBuilder( P2P_BINDING ).withCmdArgs( Lists.newArrayList( "-p" ) );
     }
 
 
     public RequestBuilder getJoinP2PSwarmCommand( String interfaceName, String localIp, String p2pHash,
                                                   String secretKey, long secretKeyTtlSec, String portRange )
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING ).withCmdArgs(
-                Lists.newArrayList( "p2p", "-c", interfaceName, p2pHash, secretKey, String.valueOf( secretKeyTtlSec ),
-                        localIp, portRange ) ).withTimeout( 90 );
+        return new RequestBuilder( P2P_BINDING ).withCmdArgs(
+                Lists.newArrayList( "-c", interfaceName, p2pHash, secretKey, String.valueOf( secretKeyTtlSec ), localIp,
+                        portRange ) ).withTimeout( 90 );
     }
 
 
     public RequestBuilder getResetP2PSecretKey( String p2pHash, String newSecretKey, long ttlSeconds )
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING )
-                .withCmdArgs( Lists.newArrayList( "p2p", "-u", p2pHash, newSecretKey, String.valueOf( ttlSeconds ) ) );
+        return new RequestBuilder( P2P_BINDING )
+                .withCmdArgs( Lists.newArrayList( "-u", p2pHash, newSecretKey, String.valueOf( ttlSeconds ) ) );
     }
 
 
@@ -62,29 +63,28 @@ public class Commands
 
     public RequestBuilder getCreateTunnelCommand( String tunnelName, String tunnelIp, int vlan, long vni )
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING ).withCmdArgs(
-                Lists.newArrayList( "tunnel", "-create", tunnelName, "-remoteip", tunnelIp, "-vlan",
-                        String.valueOf( vlan ), "-vni", String.valueOf( vni ) ) );
+        return new RequestBuilder( VXLAN_BINDING ).withCmdArgs(
+                Lists.newArrayList( "-create", tunnelName, "-remoteip", tunnelIp, "-vlan", String.valueOf( vlan ),
+                        "-vni", String.valueOf( vni ) ) );
     }
 
 
     public RequestBuilder getGetTunnelsCommand()
     {
-        return new RequestBuilder( MANAGEMENT_HOST_NETWORK_BINDING )
-                .withCmdArgs( Lists.newArrayList( "tunnel", "-list" ) );
+        return new RequestBuilder( VXLAN_BINDING ).withCmdArgs( Lists.newArrayList( "-list" ) );
     }
 
 
     public RequestBuilder getGetVlanDomainCommand( int vLanId )
     {
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING )
+        return new RequestBuilder( PROXY_BINDING )
                 .withCmdArgs( Lists.newArrayList( "check", String.valueOf( vLanId ), "-d" ) );
     }
 
 
     public RequestBuilder getRemoveVlanDomainCommand( final String vLanId )
     {
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING ).withCmdArgs( Lists.newArrayList( "del", vLanId, "-d" ) );
+        return new RequestBuilder( PROXY_BINDING ).withCmdArgs( Lists.newArrayList( "del", vLanId, "-d" ) );
     }
 
 
@@ -106,33 +106,33 @@ public class Commands
             args.add( sslCertPath );
         }
 
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING ).withCmdArgs( args );
+        return new RequestBuilder( PROXY_BINDING ).withCmdArgs( args );
     }
 
 
     public RequestBuilder getCheckIpInVlanDomainCommand( final String hostIp, final int vLanId )
     {
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING )
+        return new RequestBuilder( PROXY_BINDING )
                 .withCmdArgs( Lists.newArrayList( "check", String.valueOf( vLanId ), "-h", hostIp ) );
     }
 
 
     public RequestBuilder getAddIpToVlanDomainCommand( final String hostIp, final String vLanId )
     {
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING )
-                .withCmdArgs( Lists.newArrayList( "add", vLanId, "-h", hostIp ) );
+        return new RequestBuilder( PROXY_BINDING ).withCmdArgs( Lists.newArrayList( "add", vLanId, "-h", hostIp ) );
     }
 
 
     public RequestBuilder getRemoveIpFromVlanDomainCommand( final String hostIp, final int vLanId )
     {
-        return new RequestBuilder( MANAGEMENT_PROXY_BINDING )
+        return new RequestBuilder( PROXY_BINDING )
                 .withCmdArgs( Lists.newArrayList( "del", String.valueOf( vLanId ), "-h", hostIp ) );
     }
 
 
     public RequestBuilder getSetupContainerSshTunnelCommand( final String containerIp, final int sshIdleTimeout )
     {
-        return new RequestBuilder( String.format( "subutai tunnel add %s %d", containerIp, sshIdleTimeout ) );
+        return new RequestBuilder( TUNNEL_BINDING )
+                .withCmdArgs( Lists.newArrayList( "add", containerIp, String.valueOf( sshIdleTimeout ) ) );
     }
 }
