e2bbc6c7ba5cbb8cde265729f5672796293167b0
dilshat <dilshat.aliev@gmail.com>
2016-10-26 15:18:30 +0600

    removed dead code

--- management/server/subutai-common/src/main/java/io/subutai/common/metric/Alert.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/metric/Alert.java
@@ -9,6 +9,12 @@ import io.subutai.common.host.HostId;
 
 /**
  * Alert value interface
+ *
+ * Example of usage:
+ * QuotaAlertValue quotaAlertValue = alert.getAlertValue( QuotaAlertValue.class );
+ * StringAlertValue stringAlertValue = alert.getAlertValue( StringAlertValue.class );
+ * ExceededQuota v1 = quotaAlertValue.getValue();
+ * String v2 = stringAlertValue.getValue();
  */
 @JsonTypeInfo( use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type" )
 @JsonSubTypes( {
