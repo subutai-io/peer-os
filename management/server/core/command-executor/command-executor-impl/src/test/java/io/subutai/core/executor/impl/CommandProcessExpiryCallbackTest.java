88e81847264c474f948beb7d1ab9bfd098a6c160
dilshat <dilshat.aliev@gmail.com>
2016-11-22 18:41:10 +0600

    decreased timeout for response awaiting

--- management/server/core/command-executor/command-executor-impl/src/test/java/io/subutai/core/executor/impl/CommandProcessExpiryCallbackTest.java
+++ management/server/core/command-executor/command-executor-impl/src/test/java/io/subutai/core/executor/impl/CommandProcessExpiryCallbackTest.java
@@ -1,19 +1,35 @@
 package io.subutai.core.executor.impl;
 
 
-import org.junit.Test;
+import java.util.Map;
 
-import io.subutai.core.executor.impl.CommandProcess;
-import io.subutai.core.executor.impl.CommandProcessExpiryCallback;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import io.subutai.common.cache.ExpiringCache;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 
+@RunWith( MockitoJUnitRunner.class )
 public class CommandProcessExpiryCallbackTest
 {
 
-    CommandProcessExpiryCallback callback = new CommandProcessExpiryCallback();
+    @Mock
+    ExpiringCache<String, Map<String, String>> requests;
+
+    CommandProcessExpiryCallback callback;
+
+
+    @Before
+    public void setUp() throws Exception
+    {
+        callback = new CommandProcessExpiryCallback( requests, "RH_ID", "CMD_ID" );
+    }
 
 
     @Test
