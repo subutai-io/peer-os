5faa50819fe89fe8509b244ad71ea3601cfab3f0
dilshat <dilshat.aliev@gmail.com>
2016-12-03 12:27:00 +0600

    Added local-peer-rest-ui module

--- management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
+++ management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
@@ -157,9 +157,7 @@ import io.subutai.hub.share.resource.RamResource;
 /**
  * Local peer implementation
  *
- * TODO externalize security specific operations to LocalPeerSecureProxy
  * TODO add proper security annotations
- * TODO remove duplicate functionality: addReverseProxy, addCustomProxy and setVniDomain+add/remove/isIpVNiDomain
  */
 @PermitAll
 public class LocalPeerImpl implements LocalPeer, HostListener, Disposable
---
+++ management/server/core/local-peer/local-peer-rest-ui/src/main/java/io/subutai/localpeer/rest/ui/RestServiceImpl.java
@@ -0,0 +1,72 @@
+package io.subutai.localpeer.rest.ui;
+
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+
+import io.subutai.common.host.ContainerHostInfo;
+import io.subutai.common.peer.ContainerId;
+import io.subutai.common.peer.LocalPeer;
+
+
+public class RestServiceImpl implements RestService
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger( RestServiceImpl.class );
+    private final LocalPeer localPeer;
+
+
+    public RestServiceImpl( final LocalPeer localPeer )
+    {
+        Preconditions.checkNotNull( localPeer );
+
+        this.localPeer = localPeer;
+    }
+
+
+    @Override
+    public Response getNotRegisteredContainers()
+    {
+        return Response.ok( localPeer.getNotRegisteredContainers() ).build();
+    }
+
+
+    @Override
+    public Response destroyNotRegisteredContainer( final String containerId )
+    {
+        try
+        {
+            boolean notRegistered = false;
+
+            for ( ContainerHostInfo containerHostInfo : localPeer.getNotRegisteredContainers() )
+            {
+                if ( containerHostInfo.getId().equals( containerId ) )
+                {
+                    notRegistered = true;
+
+                    break;
+                }
+            }
+
+            if ( notRegistered )
+            {
+                localPeer.destroyContainer( new ContainerId( containerId ) );
+
+                return Response.ok().build();
+            }
+            else
+            {
+                return Response.status( Response.Status.NOT_FOUND ).build();
+            }
+        }
+        catch ( Exception e )
+        {
+            LOGGER.error( e.getMessage(), e );
+            throw new WebApplicationException( Response.serverError().entity( e.getMessage() ).build() );
+        }
+    }
+}
