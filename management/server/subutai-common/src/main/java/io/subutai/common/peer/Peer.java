f0895ffa680432b9fad93183c876d76199bbd970
dilshat <dilshat.aliev@gmail.com>
2016-12-15 10:10:10 +0600

    removed @Deprecated annotation

--- management/server/subutai-common/src/main/java/io/subutai/common/peer/Peer.java
+++ management/server/subutai-common/src/main/java/io/subutai/common/peer/Peer.java
@@ -278,10 +278,8 @@ public interface Peer extends RelationLink
 
     void addReverseProxy( ReverseProxyConfig reverseProxyConfig ) throws PeerException;
 
-    @Deprecated
     void addCustomProxy( CustomProxyConfig proxyConfig ) throws PeerException;
 
-    @Deprecated
     void removeCustomProxy( CustomProxyConfig proxyConfig ) throws PeerException;
 
     SshKeys getSshKeys( EnvironmentId environmentId, SshEncryptionType sshEncryptionType ) throws PeerException;
