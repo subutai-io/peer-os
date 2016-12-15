01c044f2386eaa968d1028bcab4cabe2dfee6f87
dilshat <dilshat.aliev@gmail.com>
2016-12-15 16:01:17 +0600

    fixed sorting by date of tracker logs and updates history

--- management/server/webui/src/main/webapp/subutai-app/tracker/controller.js
+++ management/server/webui/src/main/webapp/subutai-app/tracker/controller.js
@@ -76,7 +76,7 @@ function TrackerCtrl(trackerSrv, $scope, $rootScope, DTOptionsBuilder, DTColumnB
 	}
 
 	function dateHTML(data, type, full, meta) {
-		return '<div>' + moment( data ).format('MMM Do YYYY HH:mm:ss') + '</div>';
+		return '<div>' + moment( data ).format('YYYY-MM-DD HH:mm:ss') + '</div>';
 	}
 
 	function viewLogsButton(data, type, full, meta) {
