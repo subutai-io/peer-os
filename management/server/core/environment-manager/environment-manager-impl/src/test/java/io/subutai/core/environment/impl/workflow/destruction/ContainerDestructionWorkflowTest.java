0713080d236b97bd8a780268e23e35d194753249
Dilshat <dilshat.aliev@gmail.com>
2017-07-14 14:36:06 +0600

    #1895  unit test fix

--- management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/workflow/destruction/ContainerDestructionWorkflowTest.java
+++ management/server/core/environment-manager/environment-manager-impl/src/test/java/io/subutai/core/environment/impl/workflow/destruction/ContainerDestructionWorkflowTest.java
@@ -18,7 +18,6 @@ import io.subutai.core.environment.impl.entity.EnvironmentContainerImpl;
 import io.subutai.core.environment.impl.entity.LocalEnvironment;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
@@ -78,7 +77,7 @@ public class ContainerDestructionWorkflowTest
     @Test
     public void testVALIDATE() throws Exception
     {
-        assertNull( workflow.VALIDATE() );
+        assertEquals( ContainerDestructionWorkflow.ContainerDestructionPhase.FINALIZE, workflow.VALIDATE() );
 
         doReturn( Sets.newHashSet( container, TestHelper.ENV_CONTAINER() ) ).when( environment ).getContainerHosts();
 
