83a6a7f3aa57ba47408eb6c702d13ae89839f14d
Dilshat <dilshat.aliev@gmail.com>
2017-09-09 15:31:10 +0600

    added a logo and a message, that the console is loading, to custom 404 error handler

--- management/server/core/security-manager/http-context-factory/src/main/java/io/subutai/core/http/context/jetty/CustomErrorHandler.java
+++ management/server/core/security-manager/http-context-factory/src/main/java/io/subutai/core/http/context/jetty/CustomErrorHandler.java
@@ -18,12 +18,13 @@ public class CustomErrorHandler extends ErrorHandler
     protected void writeErrorPageBody( final HttpServletRequest request, final Writer writer, final int code,
                                        final String message, final boolean showStacks ) throws IOException
     {
-        super.writeErrorPageBody( request, writer, code, message, showStacks );
-
-        writer.write( "Oops, you did it again" );
-        writer.write( "<div>" );
+        writer.write(
+                "<div style='position: absolute; top: 50%; margin-left: -162px; left: 50%; margin-top: -206px; width:"
+                        + " 325px;'>" );
         writer.write( IOUtils.toString( CustomErrorHandler.class.getClassLoader().getResourceAsStream( "logo.svg" ),
                 "UTF-8" ) );
+        writer.write( "<br><br><br>" );
+        writer.write( "<center>Subutai Console is loading, please wait...</center>" );
         writer.write( "</div>" );
     }
 }
