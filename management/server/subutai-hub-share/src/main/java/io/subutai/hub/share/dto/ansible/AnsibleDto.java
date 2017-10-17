82edaf8d35727ecff3ada6a0b1a38a217d2b9e1d
Sydyk Akhmataliev <sahmataliev@gmail.com>
2017-10-17 17:09:43 +0600

    Added passing ansible scripts

---
+++ management/server/subutai-hub-share/src/main/java/io/subutai/hub/share/dto/ansible/AnsibleDto.java
@@ -0,0 +1,7 @@
+package io.subutai.hub.share.dto;
+
+
+public class AnsibleDto
+{
+    
+}
---
+++ management/server/subutai-hub-share/src/main/java/io/subutai/hub/share/dto/ansible/Host.java
@@ -0,0 +1,9 @@
+package io.subutai.hub.share.dto.ansible;
+
+
+/**
+ * Created by root on 10/17/17.
+ */
+public class Host
+{
+}
