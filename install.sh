6421300e49cb26ff69d10ba1f862235dffa25742
Jacob McQueen <jacobcmcqueen@gmail.com>
2017-11-22 00:22:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing... Please wait! =^_^= This may take some time..."
-sudo apt-get install python-software-properties debconf-utils git build-essential tar
+sudo apt-get install -y python-software-properties debconf-utils git build-essential tar
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
