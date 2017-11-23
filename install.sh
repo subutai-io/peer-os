eeab57affbbfe1dec6d8790cd7bf137cad8b7f16
Jacob McQueen <jacobcmcqueen@gmail.com>
2017-11-22 20:50:03 -0600

    Almost done for debian

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+sudo apt-get update
 echo "PLEASE MAKE SURE IF YOU GIT CLONED ALREADY TO PLACE THIS SCRIPT ABOVE THE BASE DIRECTORY THAT WAS CLONED!"
 echo "THIS SCRIPT WILL GIT CLONE FOR YOU!!!! Waiting 15s....."
 sleep 15s
 echo "Installing... Please wait! =^_^= This may take some time... Get some coffee."
-sudo apt-get install -y dirmngr
-sudo apt-get install -y software-properties-common debconf-utils git build-essential tar
+sudo apt-get install -y dirmngr --fix-missing
+sudo apt-get install -y software-properties-common debconf-utils git build-essential tar --fix-missing
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
-sudo apt-get install -y oracle-java8-unlimited-jce-policy
+sudo apt-get install -y oracle-java8-installer --fix-missing
+sudo apt-get install -y oracle-java8-unlimited-jce-policy --fix-missing
 echo "The below is the JAVA_HOME:"
 echo $JAVA_HOME
 echo "If its empty we need to configure it..."
@@ -28,10 +29,9 @@ java -version
 echo "Purging old mavens:"
 sudo apt-get purge maven maven2 maven3
 echo "Installing latest maven:"
-sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y universe
 sudo apt-get update
-sudo apt install -y maven
+sudo apt install -y maven --fix-missing
 echo "Below is your maven version, please verify it is ABOVE version 3.2.2:"
 mvn -version
 echo "If the version IS NOT ABOVE 3.2.2 something went wrong...."
@@ -39,7 +39,7 @@ echo "Git cloning base p2p cluster software..."
 git clone https://github.com/Gr1dd/base.git
 cd base/
 echo "Installing immortal! Cool software!"
-sudo apt install ./immortal_0.17.0_amd64.deb
+sudo apt install ./immortal_0.17.0_amd64.deb --fix-missing
 cd management/
 echo "ATTEMPTING BUILD OF BASE PACKAGE - THIS TAKES VERY VERY LONG"
 mvn clean install -P deb
