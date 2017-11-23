90941dd52bd465d03e310b1682772c030cccb0f3
Jacob McQueen <jacobcmcqueen@gmail.com>
2017-11-22 19:34:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-# First, install gpg-agent
 sudo apt-get install -y gnupg-agent
-
-# Then, add the following to your .bashrc/.zshrc
-# Invoke GnuPG-Agent the first time we login.
-# Does `~/.gpg-agent-info' exist and points to gpg-agent process accepting signals?
-if test -f $HOME/.gpg-agent-info && \
-    kill -0 `cut -d: -f 2 $HOME/.gpg-agent-info` 2>/dev/null; then
-    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info | cut -c 16-`
-else
-    # No, gpg-agent not available; start gpg-agent
-    echo "Starting gpg-agent"
-    eval `gpg-agent --daemon --no-grab --use-agent $HOME/.gpg-agent-info`
-fi
+echo "Starting gpg-agent"
+eval `gpg-agent --daemon --no-grab`
+eval `gpg-agent use-agent`
 export GPG_TTY=`tty`
 export GPG_AGENT_INFO
 sudo service apparmor stop
