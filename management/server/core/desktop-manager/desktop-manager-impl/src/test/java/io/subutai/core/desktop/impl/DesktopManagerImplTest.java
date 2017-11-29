70b6700828252b7cdecf159e4143878da2fca72b
Zubaidullo <zubaidullo.n.uulu@gmail.com>
2017-11-29 12:21:04 +0600

    Add OSGi configuration file to Desktop Manager Impl module

--- management/server/core/desktop-manager/desktop-manager-impl/src/test/java/io/subutai/core/desktop/impl/DesktopManagerImplTest.java
+++ management/server/core/desktop-manager/desktop-manager-impl/src/test/java/io/subutai/core/desktop/impl/DesktopManagerImplTest.java
@@ -3,16 +3,27 @@ package io.subutai.core.desktop.impl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import io.subutai.common.peer.ContainerHost;
+import io.subutai.core.peer.api.PeerManager;
 
 
 @RunWith( MockitoJUnitRunner.class )
 public class DesktopManagerImplTest
 {
+
+    private DesktopManagerImpl desktopManager;
+
+    @Mock
+    PeerManager peerManager;
+
+
     @Test
     public void isDesktop() throws Exception
     {
-
+        boolean isDekstop = desktopManager.isDesktop( null );
     }
 
 
