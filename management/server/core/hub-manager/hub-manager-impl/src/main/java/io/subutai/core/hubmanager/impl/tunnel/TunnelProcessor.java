a06e9da479c82ddf70692882f2204cda30dae1c7
Dilshat Aliev <daliev@optimal-dynamics.com>
2018-08-23 11:35:46 +0600

    fixing `subutai tunnel` command

--- management/server/core/hub-manager/hub-manager-impl/src/main/java/io/subutai/core/hubmanager/impl/tunnel/TunnelProcessor.java
+++ management/server/core/hub-manager/hub-manager-impl/src/main/java/io/subutai/core/hubmanager/impl/tunnel/TunnelProcessor.java
@@ -28,7 +28,7 @@ import static io.subutai.hub.share.dto.TunnelInfoDto.TunnelStatus.READY;
 
 public class TunnelProcessor implements StateLinkProcessor
 {
-    public static final String CREATE_TUNNEL_COMMAND = "subutai tunnel add %s:%s %s -g";
+    public static final String CREATE_TUNNEL_COMMAND = "subutai tunnel add %s:%s %s";
 
     private static final String DELETE_TUNNEL_COMMAND = "subutai tunnel del %s:%s";
 
