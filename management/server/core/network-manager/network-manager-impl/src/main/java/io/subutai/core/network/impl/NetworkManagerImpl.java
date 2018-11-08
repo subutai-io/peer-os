57d22787ed5a4990acd1b1547228c6d0fbe34dca
tjamakeev@optimal-dynamics.com <tjamakeev@optimal-dynamics.com>
2018-11-08 12:17:18 +0600

    #2707 - refactored logic to support Let's Encrypt provided certificates

--- management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/NetworkManagerImpl.java
+++ management/server/core/network-manager/network-manager-impl/src/main/java/io/subutai/core/network/impl/NetworkManagerImpl.java
@@ -492,7 +492,8 @@ public class NetworkManagerImpl implements NetworkManager
     public void mapContainerPortToDomain( final Host host, final Protocol protocol, final String containerIp,
                                           final int containerPort, final int rhPort, final String domain,
                                           final String sslCertPath, final LoadBalancing loadBalancing,
-                                          final boolean sslBackend ) throws NetworkManagerException
+                                          final boolean sslBackend, final boolean redirect )
+            throws NetworkManagerException
     {
         Preconditions.checkNotNull( host );
         Preconditions.checkNotNull( protocol );
@@ -508,7 +509,7 @@ public class NetworkManagerImpl implements NetworkManager
 
         execute( host,
                 commands.getMapContainerPortToDomainCommand( protocol, containerIp, containerPort, rhPort, domain,
-                        sslCertPath, loadBalancing, sslBackend ) );
+                        sslCertPath, loadBalancing, sslBackend, redirect ) );
     }
 
 
