308f68f7bc7981f083810dac8d796e8e8aa85a22
absidish <sahmataliev@gmail.com>
2020-03-25 09:08:06 +0600

    refactored getting resources for new nodes

--- management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
+++ management/server/core/local-peer/local-peer-impl/src/main/java/io/subutai/core/localpeer/impl/LocalPeerImpl.java
@@ -1135,9 +1135,13 @@ public class LocalPeerImpl extends HostListener implements LocalPeer, Disposable
         {
             for ( Node node : nodes.getNewNodes() )
             {
-                double requestedRam = node.getQuota().getContainerSize().getRamQuota();
-                double requestedDisk = node.getQuota().getContainerSize().getDiskQuota();
-                double requestedCpu = node.getQuota().getContainerSize().getCpuQuota();
+                double requestedRam =
+                        node.getQuota().get( ContainerResourceType.RAM ).getAsRamResource().getResource().doubleValue();
+                double requestedDisk =
+                        node.getQuota().get( ContainerResourceType.DISK ).getAsDiskResource().getResource()
+                            .doubleValue();
+                double requestedCpu =
+                        node.getQuota().get( ContainerResourceType.CPU ).getAsCpuResource().getResource().doubleValue();
 
                 ResourceHost resourceHost = getResourceHostById( node.getHostId() );
 
